8ac2da1d40421e118330d8074a17ad8e3a2efdef
Mark Wilden <mark@mwilden.com>
2010-07-22 11:01:11 -0700

    Focus first input element. Make header link to main page. Use lightgreen when link clicked.

--- app/views/base.rb
+++ app/views/base.rb
@@ -3,7 +3,8 @@ class Views::Base < Erector::Widgets::Page
   def head_content
     super
     css '/stylesheets/application.css'
-    javascript '/javascripts/ext/jquery-1.4.2.js'
+    javascript_include_tag '/javascripts/ext/jquery-1.4.2.js'
+    jquery '$(":input:visible:enabled:first").focus();'
   end
 
   def page_title
@@ -12,11 +13,9 @@ class Views::Base < Erector::Widgets::Page
 
   def body_content
     div :id => 'container' do
-      h3 'ANTBIB'
+      a (:href => references_path) {h3 'ANTBIB'}
       hr
-
       container_content
-
     end
   end
 end
