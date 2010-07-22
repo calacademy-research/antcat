cdbf39b516d05402f6a938fd497929a49e3bdf70
Mark Wilden <mark@mwilden.com>
2010-07-22 14:52:52 -0700

    Format edit form. Move search to its own widget.

--- app/views/references/form.rb
+++ app/views/references/form.rb
@@ -5,53 +5,44 @@ class Views::References::Form < Erector::Widget
   def content
     @f.error_messages
 
-    p do
-      @f.label :authors
-      br
-      @f.text_area :authors
-    end
-    p do
-      @f.label :year
-     br
-      @f.text_field :year
-    end
-    p do
-      @f.label :title
-      br
-      @f.text_field :title
-    end
-    p do
-      @f.label :citation
-      br
-      @f.text_area :citation
-    end
-    p do
-      @f.label :notes
-      br
-      @f.text_area :notes
-    end
-    p do
-      @f.label :possess
-      br
-      @f.text_field :possess
-    end
-    p do
-      @f.label :date
-      br
-      @f.text_field :date
-    end
-    p do
-      @f.label :excel_file_name
-      br
-      @f.text_field :excel_file_name
-    end
-    p do
-      @f.label :cite_code
-      br
-      @f.text_field :cite_code
-    end
-    p do
-      @f.submit @submit_text
+    table(:class => 'reference-fields') do
+      tr do
+        td(:style => "width: 70px") {@f.label :authors}
+        td {@f.text_field :authors, :class => "wide"}
+      end
+      tr do
+        td {@f.label :year}
+        td {@f.text_field :year, :class => "small"}
+      end
+      tr do
+        td {@f.label :title}
+        td {@f.text_field :title, :class => "wide"}
+      end
+      tr do
+        td {@f.label :citation}
+        td {@f.text_area :citation, :rows => 3, :class => "wide"}
+      end
+      tr do
+        td {@f.label :notes}
+        td {@f.text_area :notes, :rows => 2, :class => 'wide'}
+      end
+      tr do
+        td {@f.label :cite_code}
+        td {@f.text_field :cite_code, :class => 'small'}
+      end
+      tr do
+        td {@f.label :possess}
+        td {@f.text_field :possess, :class => 'small'}
+      end
+      tr do
+        td {@f.label :date}
+        td {@f.text_field :date, :class => 'small'}
+      end
+      tr do
+        td
+        td {@f.submit @submit_text}
+      end
+      
     end
   end
 
--- app/views/references/index.rb
+++ app/views/references/index.rb
@@ -3,18 +3,7 @@ class Views::References::Index < Views::Base
   include ActionController::UrlWriter
 
   def container_content
-    div :id => 'search' do
-      widget Form, :action => '/references', :method => 'get' do
-        label 'Author', :for => 'author'
-        input :name => 'author', :id => 'author', :type => 'text', :value => params[:author]
-        label 'Year', :for => 'start_year'
-        input :name => 'start_year', :id => 'start_year', :class => 'year', :type => 'text', :value => params[:start_year]
-        label 'to', :for => 'end_year'
-        input :name => 'end_year', :id => 'end_year', :class => 'year', :type => 'text', :value => params[:end_year]
-        button 'Search', :value => 'search', :name => 'commit', :type => 'submit'
-        button 'Clear', :value => 'clear', :name => 'commit', :type => 'submit'
-      end
-    end
+    widget Views::Search.new
 
     hr
 
