e1d000500e5bd9decfd7c03b74e91859e8658cc6
Mark Wilden <mark@mwilden.com>
2010-10-09 16:23:29 -0700

    Scraper

---
+++ spec/lib/scraper_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'curl'
+
+describe Scraper do
+  it "should call Curl appropriately" do
+    result = mock
+    Curl::Easy.should_receive(:perform).with('foo').and_return result
+    result.should_receive(:body_str).and_return 'bar'
+    Nokogiri.should_receive(:HTML).with('bar').and_return 'foobar'
+
+    result = Scraper.new.get('foo')
+
+    result.should == 'foobar'
+  end
+end
