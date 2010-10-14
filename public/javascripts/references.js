8d3e8e83175e6862a0568d8a5c8caf8fbed2db99
Mark Wilden <mark@mwilden.com>
2010-10-14 15:45:58 -0700

    Remove watermarks, since they don't clear now that the form isn't actually being submitted

--- public/javascripts/references.js
+++ public/javascripts/references.js
@@ -176,7 +176,6 @@ function showReferenceForm($reference, options) {
   $('.icon').hide();
 
   var $form = $('.reference_form', $reference);
-  setWatermarks($form);
   setTabs($reference);
   $form.show();
 
@@ -189,28 +188,6 @@ function showReferenceForm($reference, options) {
   setupAuthorAutocomplete($reference);
   setupJournalAutocomplete($reference);
   setupPublisherAutocomplete($reference);
-}
-
-function setWatermarks($form) {
-  $('#reference_authors_string', $form).watermark('Authors');
-  $('#reference_citation_year', $form).watermark('Year');
-  $('#reference_title', $form).watermark('Title');
-
-  $('#journal_title', $form).watermark('Journal');
-  $('#reference_series_volume_issue', $form).watermark('Series, volume, issue');
-  $('#article_pagination', $form).watermark('Page');
-
-  $('#publisher_string', $form).watermark('Place: Publisher');
-  $('#book_pagination', $form).watermark('Pages');
-
-  $('#reference_citation', $form).watermark('Citation');
-
-  $('#reference_public_notes', $form).watermark('Published notes');
-  $('#reference_editor_notes', $form).watermark("Editor's notes");
-  $('#reference_taxonomic_notes', $form).watermark('Taxonomic notes');
-  $('#reference_cite_code', $form).watermark('Cite code');
-  $('#reference_possess', $form).watermark('Possess');
-  $('#reference_date', $form).watermark('Date');
 }
 
 function setTabs($reference) {
