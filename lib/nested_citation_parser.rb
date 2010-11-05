6ceed3aec069b4e191b04b56770247f10a7b0fa7
Mark Wilden <mark@mwilden.com>
2010-11-05 09:37:20 -0700

    Use PagesInGrammar in NestedCitationParser

--- lib/nested_citation_parser.rb
+++ lib/nested_citation_parser.rb
@@ -1,15 +1,11 @@
 module NestedCitationParser
 
   def self.parse string
-    return unless string.present?
+    pages_in = PagesInGrammar.parse string rescue nil
+    return unless pages_in
 
-    matches = string.match(/^.*\d.*?\s*\bIn\b\s*:?\s*/i) || string.match(/In: /)
-    return unless matches.present?
-
-    rest = string[matches.end(0)..-1].strip
-
+    rest = string[pages_in.length..-1]
     inner = ReferenceParser.parse rest
-    pages_in = matches[0].strip
 
     {:nested => inner.merge(:pages_in => pages_in.strip)}
   end
