0fad946efc75006358449304b4e09ca860bca09c
Mark Wilden <mark@mwilden.com>
2010-11-10 18:44:05 -0800

    Who knew there was a 3-character place name? (Ufa)

--- lib/book_citation_parser.rb
+++ lib/book_citation_parser.rb
@@ -8,7 +8,7 @@ module BookCitationParser
     return unless pagination
 
     match = PublisherGrammar.parse string
-    return unless match.value[:publisher][:place].length > 3
+    return unless match.value[:publisher][:place].length > 2
     string.gsub! /#{Regexp.escape match}/, ''
     book = match.value
     return unless place?(book[:publisher][:place], possibly_embedded)
