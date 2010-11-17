7a18090c2b1e81c9e3a7e3c2f704224b6b7958d5
Mark Wilden <mark@mwilden.com>
2010-11-16 18:36:54 -0800

    Utility script

---
+++ db/find_reference_for_ward_citation.sql
@@ -0,0 +1,6 @@
+select wr.citation, j.name, r.series_volume_issue, r.pagination
+  from ward_references wr
+  join `references` r
+  on wr.reference_id = r.id
+  left join journals j on j.id = r.journal_id
+  where wr.citation like '%6(3(c))%'\G
