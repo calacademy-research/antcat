984f571df4921f9c19740f8694a6d6dccc0fbf30
Mark Wilden <mark@mwilden.com>
2010-11-24 19:57:09 -0800

    Author.sorted_by_name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,9 @@
 class Author < ActiveRecord::Base
   has_many :names, :class_name => 'AuthorName'
+
+  named_scope :sorted_by_name,
+    :select => 'authors.id',
+    :joins => 'JOIN author_names ON author_id = authors.id',
+    :group => 'authors.id',
+    :order => 'name ASC'
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -6,4 +6,14 @@ describe Author do
     author.names << Factory(:author_name)
     author.should have(1).name
   end
+
+  describe "sorting by first author name" do
+    it "should work" do
+      ward = Factory :author_name, :name => 'Ward'
+      fisher_b_l = Factory :author_name, :name => 'Fisher, B. L.'
+      fisher = Factory :author_name, :name => 'Fisher', :author => fisher_b_l.author 
+      bolton = Factory :author_name, :name => 'Bolton'
+      Author.sorted_by_name.should == [bolton.author, fisher.author, ward.author]
+    end
+  end
 end
