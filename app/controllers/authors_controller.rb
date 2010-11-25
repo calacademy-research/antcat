eb0a84d97469b884bf4edbc3f014a4fa803404ee
Mark Wilden <mark@mwilden.com>
2010-11-24 19:57:58 -0800

    Authors report

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,8 @@
 class AuthorsController < ApplicationController
   def index
-    render :json => AuthorName.search(params[:term]).to_json
+    respond_to do |format|
+      format.json {render :json => AuthorName.search(params[:term]).to_json}
+      format.html
+    end
   end
 end
