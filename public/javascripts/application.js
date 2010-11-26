765256d1a6262f70da9de4dac7c2f655244c1fa5
Mark Wilden <mark@mwilden.com>
2010-11-26 14:51:01 -0800

    HOL and antbase logos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,8 +8,8 @@ $(window).resize(function() {
 });
 
 function spaceOutImages() {
-  var totalImageWidth = 271;
-  var imageCount = 4;
+  var totalImageWidth = 379 + 154;
+  var imageCount = 6;
   var availableWidth = $('#images').width();
   var marginInBetween = (availableWidth - totalImageWidth) / (imageCount - 1);
   $(".spacer").width(marginInBetween);
