dd2ef8af11411b6dbbc4adb8c678999087a67b41
Mark Wilden <mark@mwilden.com>
2010-11-30 15:51:36 -0800

    Fix another duplicated nestee

---
+++ db/migrate/20101130234103_fix_duplicate_reference2.rb
@@ -0,0 +1,12 @@
+class FixDuplicateReference2 < ActiveRecord::Migration
+  def self.up
+    duplicate = Reference.find_by_year_and_pagination_and_cite_code 2007, '1-53', nil 
+    original = Reference.find_by_year_and_pagination_and_cite_code 2007, '1-53', '96-1629'
+    nester = Reference.find_by_nested_reference_id duplicate.id
+    nester.update_attribute :nested_reference_id, original.id
+    duplicate.destroy
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101130231544) do
+ActiveRecord::Schema.define(:version => 20101130234103) do
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
