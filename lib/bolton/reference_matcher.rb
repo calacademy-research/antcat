315819e66f7437284b0931bec3adeb407b65f759
Mark Wilden <mark@mwilden.com>
2010-12-18 19:55:13 -0800

    Restart Bolton reference matching. #match -> #find_match.

--- lib/bolton/reference_matcher.rb
+++ lib/bolton/reference_matcher.rb
@@ -1,198 +1,43 @@
 class Bolton::ReferenceMatcher
-  SUFFICIENT_SIMILARITY = 0.75
 
   def initialize show_progress = false
-    Progress.init show_progress
-    @all_count = Bolton::Reference.count
-    @unmatched_count = @suspect_count = 0
-    make_ward_index
-    @start = Time.now
+    Progress.init show_progress, Bolton::Reference.count
+    @unmatched_count = 0
   end
 
   def match_all
     Bolton::Reference.all.each_with_index do |bolton, i|
-      bolton.update_attributes :ward_reference => match(bolton, i), :suspect => suspect?
+      match = find_match bolton
+      bolton.update_attribute :ward_reference_id, match && match.id
+      @unmatched_count += 1 unless match
+      show_progress
     end
     show_results
   end
 
-  def suspect?
-    @suspect
-  end
-
-  def match bolton, i = 0
-    @bolton = bolton
-    @title_and_citation = normalize(bolton.title_and_citation)
-    @max_similarity = 0
-    @best_match = nil
-    @found_ward = nil
-    @suspect = false
-
-    @wards = @ward_index[@bolton.authors.downcase[0,2]]
-    if @wards
-      @found_ward = match_similarity || match_suffix || match_author_year_and_digits
-    end
-
-    if @found_ward
-      ward = @found_ward[:reference]
-      @suspect ||= ward.numeric_year != @bolton.year.to_i
-      @suspect ||= normalize(ward.author_names) != normalize(@bolton.authors)
-    else
-      @unmatched_count += 1
-    end
-
-    @suspect_count += 1 if @suspect
-
-    show_progress i
-
-    @found_ward && @found_ward[:reference]
-  end
-
-  private
-  def match_similarity
-    @wards.each do |ward|
-      similarity = string_similarity(@title_and_citation, ward[:pairs])
-      if similarity > @max_similarity
-        @max_similarity = similarity
-        @best_match = ward
-      end
-    end
-    @max_similarity >= SUFFICIENT_SIMILARITY ? @best_match : nil
-  end
-
-  SUFFICIENT_MATCHING_SUFFIX_LENGTH = 10
-
-  def match_suffix
-    @wards.find do |ward|
-      matching_suffix_length = 0
-      bolton_string = @title_and_citation
-      ward_string = ward[:title_and_citation]
-      bolton_index = bolton_string.length - 1
-      ward_index = ward_string.length - 1
-      loop do
-        break if bolton_index < 0
-        break if ward_index < 0
-        break if bolton_string[bolton_index] != ward_string[ward_index]
-        matching_suffix_length += 1
-        if matching_suffix_length >= SUFFICIENT_MATCHING_SUFFIX_LENGTH
-          return ward
-        end
-        bolton_index -= 1
-        ward_index -= 1
-      end
-    end
-    nil
-  end
-
-  SUFFICIENT_MATCHING_DIGITS_LENGTH = 4
-
-  def match_author_year_and_digits
-    @wards.find do |ward|
-      next unless ward[:reference].author_names == @bolton.authors
-      next unless ward[:reference].numeric_year == @bolton.year.to_i
-      bolton_digits = @title_and_citation.gsub(/\D/, '')
-      ward_digits = ward[:title_and_citation].gsub(/\D/, '')
-      next unless bolton_digits == ward_digits
-      next unless bolton_digits.length >= SUFFICIENT_MATCHING_DIGITS_LENGTH
-      @suspect = true
+  def find_match bolton
+    bolton_last_name = last_name(bolton.authors)
+    ::Reference.all(:conditions => ["author_names_string like ?", bolton_last_name + '%']).find do |reference|
+      next unless reference.author_names.first.last_name == bolton_last_name
+      next unless reference.type == bolton.reference_type
+      next unless reference.matches? bolton
       true
     end
   end
 
-  def make_ward_index
-    Progress.print "Setting up..."
-    @ward_index = Reference.all.inject({}) do |index, reference|
-      s = remove_parenthesized_taxon_names(reference.title + reference.citation)
-      s = normalize s
-      entry = {
-        :reference => reference,
-        :title_and_citation => s,
-        :pairs => make_pairs(s)
-      }
-      key = reference.author_names.downcase[0, 2]
-      index[key] ||= []
-      index[key] << entry
-      index
-    end
-    Progress.puts 'done'
-  end
-
-  def remove_parenthesized_taxon_names s
-    match = s.match(/ \(.+?\)/)
-    return s unless match
-    b = match.begin(0)
-    e = match.end(0) - 1
-    possible_taxon_names = match.to_s.strip.gsub(/[(),:]/, '').split(/[ ]/)
-    is_wards_taxon_names = possible_taxon_names.any? do |possible_taxon_name|
-      ['Formicidae', 'Hymenoptera'].include? possible_taxon_name
-    end
-    if is_wards_taxon_names
-      s[b..e] = ''
-    end
-    s
-  end
-
-  def normalize s
-    s = s.gsub(/\(No\. (\d+)\)/, '\1') # "(No. 1)" => "(1)"
-    s = remove_punctuation s
-    s
-  end
-
-  def remove_punctuation s
-    s.gsub(/\W/, '')
-  end
-
-  def make_pairs str1
-    str1.downcase 
-    (0..str1.length-2).collect {|i| str1[i,2]}.reject { |pair| pair.include? " "}
-  end
-
-  def string_similarity str1, pairs2
-    pairs1 = make_pairs str1
-    pairs2 = pairs2.clone
-    union = pairs1.size + pairs2.size 
-    intersection = 0 
-    pairs1.each do |p1| 
-      0.upto(pairs2.size-1) do |i| 
-        if p1 == pairs2[i] 
-          intersection += 1 
-          pairs2.slice!(i) 
-          break 
-        end 
-      end 
-    end 
-    (2.0 * intersection) / union
-  end
-
-  def show_progress i
-    return unless !@found_ward || @suspect
-
-    elapsed = Time.now - @start
-    rate = ((i + 1) / elapsed)
-    rate_s = sprintf("%.2f", rate) + "/sec"
-    time_left = sprintf("%.0f", (@all_count - i + 1) / rate / 60) + " mins left"
-
-    Progress.puts "******************** No match" unless @found_ward
-    Progress.puts "???????????????????? Suspect" if @suspect
-    Progress.puts @bolton
-
-    if @found_ward
-      Progress.puts "WARD: "
-      Progress.puts @found_ward[:reference].to_s
-    else
-      Progress.puts "Best was #{@max_similarity}:\n#{@best_match[:reference]}" if @best_match
-    end
-    Progress.puts "#{i + 1}/#{@all_count} (#{@unmatched_count} unmatched, #{@suspect_count} suspect) #{rate_s} #{time_left}"
-    Progress.puts
+  private
+  def show_progress
+    Progress.tally
+    return unless Progress.processed_count % 100 == 0
+    Progress.puts "#{Progress.progress_message} #{Progress.count @unmatched_count, Progress.processed_count, 'unmatched'}"
   end
 
   def show_results
-    Progress.puts
-    elapsed = Time.now - @start
-    elapsed = sprintf("%.0f mins", elapsed / 60)
-    unmatched_percent = sprintf("%.0f%%", @unmatched_count * 100.0 / @all_count)
-    suspect_percent = sprintf("%.0f%%", @suspect_count * 100.0 / @all_count)
-    Progress.puts "#{elapsed}. #{@all_count} processed, #{@unmatched_count} unmatched (#{unmatched_percent}), #{@suspect_count} suspect (#{suspect_percent})"
+    Progress.puts "#{Progress.results_message} #{Progress.count @unmatched_count, Progress.processed_count, 'unmatched'}"
+  end
+
+  def last_name string
+    string.split(',').first
   end
 
 end
--- spec/lib/bolton/reference_matcher_spec.rb
+++ spec/lib/bolton/reference_matcher_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-=begin
 describe Bolton::ReferenceMatcher do
   describe "matching all references" do
     it "should set the appropriate Ward reference for each" do
@@ -35,22 +34,65 @@ describe Bolton::ReferenceMatcher do
       suspect_bolton.should be_suspect
     end
   end
+
   describe "matching Bolton's references against Ward's" do
     it "should not match an obvious mismatch" do
-      Reference.create!(:authors => 'Fisher, B.L.', :title => "My life among the ants", :citation => "Playboy", :year => '2009')
-      bolton = Bolton::Reference.new(:authors => 'Dlussky, G.M.', :title_and_citation =>
-                                   "Ants of the genus Formica L. of Mongolia and northeast Tibet. Annales Zoologici 23: 15-43", :year => '1965a')
+      ArticleReference.create! :author_names => [Factory :author_name, :name => 'Fisher, B.L.'],
+                               :title => "My life among the ants",
+                               :journal => Factory(:journal, :name => "Playboy"),
+                               :series_volume_issue => '1',
+                               :pagination => '3 pp.',
+                               :citation_year => '2009'
+      bolton = Bolton::Reference.new(:authors => 'Dlussky, G.M.',
+                                     :title => "Ants of the genus Formica L. of Mongolia and northeast Tibet",
+                                     :reference_type => 'ArticleReference',
+                                     :journal => 'Annales Zoologici',
+                                     :series_volume_issue => "23",
+                                     :pagination => "15-43",
+                                     :year => '1965a')
+      Bolton::ReferenceMatcher.new.find_match(bolton).should be_nil
+    end
 
-      Bolton::ReferenceMatcher.new.match(bolton).should be_nil
+    it "should handle an author last name with an apostrophe in it" do
+      ward, bolton = create_ward_and_bolton
+      ward.update_attribute :author_names, [Factory(:author_name, :name => "Arnol'di, G.")]
+      bolton.update_attribute :authors, "Arnol'di, G."
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
+    end
+      
+    private
+    def create_ward_and_bolton
+      ward = ArticleReference.create! :author_names => [Factory :author_name, :name => "Arnol'di, G."],
+                                      :title => "My life among the ants",
+                                      :journal => Factory(:journal, :name => "Psyche"),
+                                      :series_volume_issue => '1',
+                                      :pagination => '15-43',
+                                      :citation_year => '1965'
+      bolton = Bolton::Reference.new :authors => "Arnol'di, G.",
+                                     :title => "My life among the ants",
+                                     :reference_type => 'ArticleReference',
+                                     :series_volume_issue => '1',
+                                     :pagination => '15-43',
+                                     :journal => 'Psyche',
+                                     :year => '1965a'
+      return ward, bolton
     end
 
     it "should find an exact match" do
-      ward = Reference.create!(:authors => 'Dlussky, G.M.',
-                               :title => "Ants of the genus Formica L. of Mongolia and northeast Tibet",
-                               :citation => "Annales Zoologici 23: 15-43", :year => '1965a')
-      bolton = Bolton::Reference.new(:authors => 'Dlussky, G.M.', :title_and_citation =>
-                                    "Ants of the genus Formica L. of Mongolia and northeast Tibet. Annales Zoologici 23: 15-43", :year => '1965a')
-      Bolton::ReferenceMatcher.new.match(bolton).should == ward
+      ward = ArticleReference.create! :author_names => [Factory :author_name, :name => 'Fisher, B.L.'],
+                                      :title => "My life among the ants",
+                                      :journal => Factory(:journal, :name => "Psyche"),
+                                      :series_volume_issue => '1',
+                                      :pagination => '15-43',
+                                      :citation_year => '1965'
+      bolton = Bolton::Reference.new :authors => 'Fisher, B.L.',
+                                     :title => "My life among the ants",
+                                     :reference_type => 'ArticleReference',
+                                     :series_volume_issue => '1',
+                                     :pagination => '15-43',
+                                     :journal => 'Psyche',
+                                     :year => '1965a'
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
     end
 
     it "should find a match when Ward has markup" do
@@ -60,7 +102,7 @@ describe Bolton::ReferenceMatcher do
       bolton = Bolton::Reference.new(:authors => 'Dlussky, G.M.', :title_and_citation =>
                                    "Ants of the genus Formica L. of Mongolia and northeast Tibet. Annales Zoologici 23: 15-43", :year => '1965a')
 
-      Bolton::ReferenceMatcher.new.match(bolton).should == reference
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == reference
     end
 
     it "should find a match when Ward has extra text" do
@@ -70,7 +112,7 @@ describe Bolton::ReferenceMatcher do
       bolton = Bolton::Reference.new(:authors => 'Dlussky, G.M.', :title_and_citation =>
                                    "Ants of the genus Formica L. of Mongolia and northeast Tibet. Annales Zoologici 23: 15-43", :year => '1965a')
 
-      Bolton::ReferenceMatcher.new.match(bolton).should == reference
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == reference
     end
 
     it "should find a match when two entries have similar authors" do
@@ -78,7 +120,7 @@ describe Bolton::ReferenceMatcher do
       reference = Reference.create!(:authors => 'abd', :title => "another title", :citation => "another citation", :year => 'year')
       bolton = Bolton::Reference.new(:authors => 'abd', :title_and_citation => "another title another citation", :year => 'year')
 
-      Bolton::ReferenceMatcher.new.match(bolton).should == reference
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == reference
     end
 
     it "should find a match when there is a long enough common suffix (e.g., from journal title, volume and page)" do
@@ -87,7 +129,7 @@ describe Bolton::ReferenceMatcher do
                                :citation => "Zoologicheskii Zhurnal 70(2):149-152."
       bolton = Bolton::Reference.new :authors => 'Arakelian, G.R. & Dlussky, G.M.', :year => '1991',
                                    :title_and_citation => "Murav'i triby Dacetini SSSR. Zoologicheskii Zhurnal 70 (2): 149-152."
-      Bolton::ReferenceMatcher.new.match(bolton).should == ward
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
     end
 
     it "should find a match when Ward includes taxon names" do
@@ -96,7 +138,7 @@ describe Bolton::ReferenceMatcher do
         :citation => "Izvestiya Akademii Nauk Turkmenskoi SSR. Seriya Biologicheskikh Nauk 1988(4):29-37"
       bolton = Bolton::Reference.new :authors => "Dlussky, G.M. & Soyunov, O.S.", :year => "1988",
         :title_and_citation => "Murav'i roda Temnothorax Mayr SSSR. Izvestiya Akademii Nauk Turkmenskoi SSR. Seriya Biologicheskikh Nauk 1988 (No. 4): 29-37."
-      Bolton::ReferenceMatcher.new.match(bolton).should == ward
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
     end
 
     it "should find a match when Ward includes taxon names, as long as one of them is one we know about" do
@@ -105,21 +147,21 @@ describe Bolton::ReferenceMatcher do
         :citation => "Izvestiya Akademii Nauk Turkmenskoi SSR. Seriya Biologicheskikh N."
       bolton = Bolton::Reference.new :authors => "Dlussky, G.M. & Soyunov, O.S.", :year => "1988",
         :title_and_citation => "Murav'i roda Temnothorax Mayr SSSR. Izvestiya Akademii Nauk Turkmenskoi SSR. Seriya Biologicheskikh Nauk"
-      Bolton::ReferenceMatcher.new.match(bolton).should == ward
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
     end
 
     it "should find a match when the author and year are the same, and a sufficient number of digits in the title + citation are the same" do
       ward = Reference.create! :authors => 'De Geer, C.', :year => '1778', :title => "Mémoires pour servir à l'histoire des insectes. Tome septième (7)",
         :citation => "Stockholm: Pierre Hesselberg, 950 pp"
       bolton = Bolton::Reference.new :authors => 'De Geer, C.', :year => '1778', :title_and_citation => "Memoirs pour Servir à l'Histoire des Insectes 7: 950 pp. Stockholm."
-      Bolton::ReferenceMatcher.new.match(bolton).should == ward
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
     end
 
     it "should mark matches as 'suspect' if the authors and year don't match exactly" do
       ward = Reference.create! :authors => 'De Geer, C.', :year => '1777', :title => "Ants", :citation => "Stockholm: Pierre Hesselberg, 950 pp"
       bolton = Bolton::Reference.new :authors => 'De Geer, C.', :year => '1778', :title_and_citation => "Ants. Stockholm: Pierre Hesselberg, 950 pp"
       matcher = Bolton::ReferenceMatcher.new
-      matcher.match(bolton).should == ward
+      matcher.find_match(bolton).should == ward
       matcher.should be_suspect
     end
 
@@ -127,9 +169,8 @@ describe Bolton::ReferenceMatcher do
       Reference.create! :authors => 'De Geer, C.', :year => '1777', :title => "Ants 2", :citation => "Stockholm: Pierre Hesselberg, 950 pp"
       ward = Reference.create! :authors => 'De Geer, C.', :year => '1777', :title => "Ants 1", :citation => "Stockholm: Pierre Hesselberg, 950 pp"
       bolton = Bolton::Reference.new :authors => 'De Geer, C.', :year => '1777', :title_and_citation => "Ants 1. Stockholm: Pierre Hesselberg, 950 pp"
-      Bolton::ReferenceMatcher.new.match(bolton).should == ward
+      Bolton::ReferenceMatcher.new.find_match(bolton).should == ward
     end
 
   end
 end
-=end
