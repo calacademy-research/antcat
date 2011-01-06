390ad74ef16c25318c1ac5c3d2b6dabfd52a3aa0
Mark Wilden <mark@mwilden.com>
2011-01-06 12:17:01 -0800

    Fix duplicate references

--- app/models/duplicate_reference.rb
+++ app/models/duplicate_reference.rb
@@ -1,4 +1,46 @@
 class DuplicateReference < ActiveRecord::Base
   belongs_to :reference
   belongs_to :duplicate, :class_name => 'Reference'
+
+  def resolve
+    original, copy = pick_original_and_copy
+    DuplicateReference.merge original.id, copy.id
+  end
+
+  def self.merge good_id, bad_id
+    transaction do
+      NestedReference.all(:conditions => ['nested_reference_id = ?', bad_id]).each do |reference|
+        reference.update_attributes :nested_reference_id => good_id
+      end
+      Reference.find(bad_id).destroy
+    end
+  end
+
+  def to_s
+    "#{ReferenceFormatter.format(reference)}\n#{ReferenceFormatter.format(duplicate)}"
+  end
+
+  def show_resolution
+    puts self
+    original, copy = pick_original_and_copy
+    puts "Original: #{ReferenceFormatter.format(original)}"
+  end
+
+  private
+  def pick_original_and_copy
+    result = nil
+    return result if result = pick {|reference| reference.public_notes.present?}
+    return result if result = pick {|reference| reference.editor_notes.present?}
+    return result if result = pick {|reference| reference.taxonomic_notes.present?}
+    return result if result = pick {|reference| reference.cite_code.present?}
+    return result if result = pick {|reference| reference.possess.present?}
+    return result if result = pick {|reference| reference.date.present?}
+    return result if result = pick {|reference| reference.citation_year =~ /\D/}
+    raise StandardError.new "#{id} (#{similarity}): Couldn't decide which of\n#{reference.id} #{ReferenceFormatter.format(reference)}\nand\n#{duplicate.id} #{ReferenceFormatter.format(duplicate)}\nis the good one"
+  end
+
+  def pick
+    return [reference, duplicate] if yield reference
+    return [duplicate, reference] if yield duplicate
+  end
 end
--- spec/models/duplicate_reference_spec.rb
+++ spec/models/duplicate_reference_spec.rb
@@ -10,4 +10,89 @@ describe DuplicateReference do
     DuplicateReference.first.duplicate.should == duplicate
     DuplicateReference.first.similarity.should == 0.5
   end
+
+  describe 'resolving a duplicate reference' do
+
+    describe 'replacing one reference with another' do
+      it 'should replace the second references with the first' do
+        ward = Factory :reference, :public_notes => 'Public notes'
+        copy = Factory :reference
+        nested = Factory :nested_reference, :nested_reference => copy
+        DuplicateReference.merge ward.id, copy.id
+        Reference.count.should == 2
+        nested.reload.nested_reference.should == ward
+        Reference.find(ward)
+      end
+    end
+
+    describe 'fixing nested references' do
+      it 'should replace the nested reference id with the good reference' do
+        ward = Factory :reference, :public_notes => 'Public notes'
+        copy = Factory :reference
+        nested = Factory :nested_reference, :nested_reference => copy
+        DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+        Reference.count.should == 2
+        nested.reload.nested_reference.should == ward
+        Reference.find(ward)
+      end
+      it 'should just leave everything alone if the nested reference already points to the good reference' do
+        ward = Factory :reference, :public_notes => 'Public notes'
+        copy = Factory :reference
+        nested = Factory :nested_reference, :nested_reference => ward
+        DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+        Reference.count.should == 2
+        nested.reload.nested_reference.should == ward
+        Reference.find(ward)
+      end
+    end
+
+    describe 'choosing which one should be kept' do
+      it "should bail out if it can't find the real one" do
+        ward = Factory :reference
+        copy = Factory :reference
+        lambda {DuplicateReference.new(:reference => ward, :duplicate => copy).resolve}.should raise_error
+      end
+      describe 'choosing the one with a date' do
+        it 'should pick the one with a date' do
+          ward = Factory :reference, :date => '[1982.1.2]'
+          copy = Factory :reference
+          DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+          Reference.count.should == 1
+          Reference.first.should == ward
+        end
+      end
+      describe 'choosing the one with public notes' do
+        it 'should pick the one with public notes' do
+          ward = Factory :reference, :public_notes => 'Public notes'
+          copy = Factory :reference
+          DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+          Reference.count.should == 1
+          Reference.first.should == ward
+        end
+        it 'should pick the one with public notes' do
+          ward = Factory :reference
+          copy = Factory :reference, :public_notes => 'Public notes'
+          DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+          Reference.count.should == 1
+          Reference.first.should == copy
+        end
+      end
+      describe 'choosing the one with a letter in its citation year' do
+        it 'should pick the one with a letter in its citation year' do
+          ward = Factory :reference, :citation_year => '2001'
+          copy = Factory :reference, :citation_year => '2001b'
+          DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+          Reference.count.should == 1
+          Reference.first.should == copy
+        end
+        it 'should pick the one with a letter in its citation year' do
+          ward = Factory :reference, :citation_year => '2001b'
+          copy = Factory :reference, :citation_year => '2001'
+          DuplicateReference.new(:reference => ward, :duplicate => copy).resolve
+          Reference.count.should == 1
+          Reference.first.should == ward
+        end
+      end
+    end
+  end
 end
