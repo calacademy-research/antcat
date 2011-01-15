2b923f3e833fed2a5afaa0a6b901294f3bfde71b
Mark Wilden <mark@mwilden.com>
2011-01-14 17:26:55 -0800

    Taxatry tweaking

--- app/controllers/taxatry_controller.rb
+++ app/controllers/taxatry_controller.rb
@@ -1,11 +1,20 @@
 class TaxatryController < ApplicationController
   def index
-    @subfamily = Taxon.find(params[:subfamily].to_i) if params[:subfamily]
-    @tribe = Taxon.find(params[:tribe].to_i) if params[:tribe]
-    @genus = Taxon.find(params[:genus].to_i) if params[:genus]
 
     @subfamilies = Taxon.all(:conditions => "rank = 'subfamily'", :order => :name)
-    @tribes = @subfamily.children(:order => :name) if @subfamily
-    @genera = @tribe.children(:order => :name) if @tribe
+    if params[:subfamily]
+      @selected_subfamily = Taxon.find(params[:subfamily].to_i)
+    else
+      @selected_subfamily = @subfamilies.first
+    end
+
+    @tribes = @selected_subfamily.children
+    if params[:tribe]
+      @selected_tribe = Taxon.find(params[:tribe].to_i)
+    else
+      @selected_tribe = @tribes.first
+    end
+
+    @genera = @selected_tribe.children if @selected_tribe
   end
 end
