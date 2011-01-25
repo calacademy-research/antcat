354a743cd079b62e00a758cbe3423c35bf3d3869
Mark Wilden <mark@mwilden.com>
2011-01-25 12:25:07 -0800

    Use s for space rule

--- lib/bolton/species_catalog.rb
+++ lib/bolton/species_catalog.rb
@@ -23,8 +23,8 @@ class Bolton::SpeciesCatalog
   def import_files filenames
     Species.delete_all
     @genus = nil
-    #filenames.each do |filename|
-    ['data/bolton/NGC-Spmo-myo.htm'].each do |filename|
+    filenames.each do |filename|
+    #['data/bolton/NGC-Spmo-myo.htm'].each do |filename|
       @filename = filename
       Progress.puts "Importing #{@filename}..."
       @inserting_camponotus_genus_header = filename =~ /NGC-Spcam2/
