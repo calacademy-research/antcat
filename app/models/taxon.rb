6ece82adaa74decd842c08e28517f15d22706b13
Mark Wilden <mark@mwilden.com>
2011-02-07 15:20:18 -0800

    Taxon predicates

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,13 +1,11 @@
 class Taxon < ActiveRecord::Base
   set_table_name :taxa
 
-  def unavailable?
-    status == 'unavailable'
-  end
-
-  def invalid?
-    status != 'valid'
-  end
+  def unavailable?;     status == 'unavailable' end
+  def available?;       !unavailable? end
+  def invalid?;         !status.blank? end
+  def unidentifiable?;  status == 'unidentifiable' end
+  def synonym?;         status == 'synonym' end
 
   def children
     raise NotImplementedError
