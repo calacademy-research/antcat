238cad66c9636f30edc07d2c9e809a4349cfbf99
Mark Wilden <mark@mwilden.com>
2011-02-07 09:43:49 -0800

    Add explicit :not_understood result for genus parsing

--- lib/bolton/genus_catalog_parser.rb
+++ lib/bolton/genus_catalog_parser.rb
@@ -3,8 +3,9 @@ module Bolton::GenusCatalogParser
   def self.parse string
     string = string.gsub(/\n/, ' ').strip
     Bolton::GenusCatalogGrammar.parse(string).value
-  rescue Citrus::ParseError
-    :unparseable
+  rescue Citrus::ParseError => e
+    p e
+    nil
   end
 
 end
