5aa644cb13f999e434a5132634018c2654670a6f
Mark Wilden <mark@mwilden.com>
2011-02-07 10:27:13 -0800

    Don't need to report all counts

--- lib/bolton/genus_catalog.rb
+++ lib/bolton/genus_catalog.rb
@@ -11,8 +11,7 @@ class Bolton::GenusCatalog
   def initialize show_progress = false
     Progress.init show_progress
     Progress.open_log 'log/bolton_genus_import.log'
-    @fossil_count = @unidentifiable_count = @subgenus_count = @not_understood_count = 
-      @valid_count = @unavailable_count = 0
+    @not_understood_count = 0
   end
 
   def import_files filenames
@@ -31,15 +30,6 @@ class Bolton::GenusCatalog
         @not_understood_count += 1
         next
       end
-      if record == :unidentifiable
-        @unidentifiable_count += 1
-        next
-      end
-      if record == :subgenus
-        @subgenus_count += 1
-        next
-      end
-      record[:genus][:is_valid] = record[:genus].delete(:valid)
       record[:genus][:taxonomic_history] = p.to_html.strip
 
       subfamily_name = record[:genus].delete(:subfamily)
@@ -50,20 +40,12 @@ class Bolton::GenusCatalog
 
       genus = Genus.find_or_create_by_name record[:genus].merge :parent => tribe || subfamily
 
-      @fossil_count += 1 if genus.fossil?
-      @valid_count += 1 if genus.is_valid?
-      @unavailable_count += 1 unless genus.available?
       Progress.tally
     end
   end
 
   def show_results
     Progress.show_results
-    Progress.puts Progress.count(@valid_count, Progress.processed_count, 'valid')
-    Progress.puts Progress.count(@unavailable_count, Progress.processed_count, 'unavailable')
-    Progress.puts Progress.count(@subgenus_count, Progress.processed_count, 'subgenera')
-    Progress.puts Progress.count(@fossil_count, Progress.processed_count, 'fossils')
-    Progress.puts Progress.count(@unidentifiable_count, Progress.processed_count, 'unidentifiable')
     Progress.puts "(#{@not_understood_count} sections not understood)"
   end
 
