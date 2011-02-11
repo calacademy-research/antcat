df366cf670ee5ab294c7f67362648eb661bc899e
Mark Wilden <mark@mwilden.com>
2011-02-11 13:49:26 -0800

    Show status by color

--- app/helpers/taxatry_helper.rb
+++ app/helpers/taxatry_helper.rb
@@ -1,6 +1,6 @@
 module TaxatryHelper
   def all_of_a_taxon_link rank, selected, super_path = {}
-    content_tag :div, :class => make_css_class('all', selected) do
+    content_tag :div, :class => make_css_class(rank, 'all', selected) do
       link_to 'All', taxatry_path({rank => 'all'}.merge super_path)
     end
   end
@@ -15,21 +15,19 @@ module TaxatryHelper
 
   private
   def link rank, selected, current, super_path, additional_class = ''
-    content_tag :div, :class => make_css_class(current, selected, additional_class) do
+    content_tag :div, :class => make_css_class(rank, current, selected, additional_class) do
       fossil_symbol = current.fossil? ? "&dagger;" : ''
-      suffix = current.kind_of?(Species) ? '' : "(#{current.children.count})" 
+      suffix = [Species, Subfamily].any? {|klass| current.kind_of?(klass)} ? '' : "(#{current.children.count})" 
       link_to "#{fossil_symbol}#{current.name}#{suffix}", taxatry_path({rank => current}.merge super_path)
     end
   end
 
-  def make_css_class current, selected, additional_class = ''
+  def make_css_class rank, current, selected, additional_class = ''
     css_classes = ['taxon']
+    css_classes << rank.to_s
+    css_classes << current.status.to_s unless current == 'all'
     css_classes << additional_class if additional_class.present?
     css_classes << 'selected' if current == selected
-    if current.kind_of? Taxon
-      css_classes << 'taxon_unavailable' if current.unavailable?
-      css_classes << 'taxon_invalid' if current.invalid?
-    end
     css_classes.join ' '
   end
 
