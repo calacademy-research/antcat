14d6fc0d15d4c890d933108ebfb7d5f2c04409a0
Mark Wilden <mark@mwilden.com>
2011-02-14 16:27:06 -0800

    Antweb::Exporter (restart)

---
+++ lib/antweb/exporter.rb
@@ -0,0 +1,5 @@
+class Antweb::Exporter
+  def export_taxon taxon
+    ['Myrmicinae', nil, 'Acalama', nil, nil, nil, nil, 'FALSE', 'FALSE', 'Gauromyrmex', nil, '<i>ACALAMA</i>']
+  end
+end
---
+++ spec/lib/antweb/exporter_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Antweb::Exporter do
+  before do
+    @exporter = Antweb::Exporter.new
+  end
+
+  describe "exporting one taxon" do
+
+    it "should export a genus that's a junior synonym" do
+      gauromyrmex = Genus.create! :name => 'Gauromyrmex'
+      acalama = Genus.create! :name => 'Acalama', :status => 'synonym', :synonym_of => gauromyrmex, :taxonomic_history => '<i>ACALAMA</i>'
+      @exporter.export_taxon(acalama).should == ['Myrmicinae', nil, 'Acalama', nil, nil, nil, nil, 'FALSE', 'FALSE', 'Gauromyrmex', nil, '<i>ACALAMA</i>']
+    end
+
+  end
+
+end
