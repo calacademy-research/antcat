1dd695c931b7320d78e334ddef286e33a7f9e532
Mark Wilden <mark@mwilden.com>
2011-02-21 19:20:34 -0800

    Differentiate between resolved and unresolved junior homonyms and point the former at its replacement

--- spec/lib/bolton/species_catalog_species_spec.rb
+++ spec/lib/bolton/species_catalog_species_spec.rb
@@ -46,25 +46,25 @@ describe Bolton::SpeciesCatalog do
       it "should handle an unresolved junior homonym species" do
         @species_catalog.parse(%{
 <b><i><span style="color:maroon">bidentatum</span></i></b><i>. Monomorium bidentatum</i> Mayr, 1887: 616 (w.q.) CHILE. Snelling, 1975: 5 (m.); Wheeler, G.C. &amp; Wheeler, J. 1980: 533 (l.). Combination in <i>Monomorium (Notomyrmex</i>): Emery, 1922e: 169; in <i>Notomyrmex</i>: Kusnezov, 1960b: 345; in <i>Nothidris</i>: Ettershank, 1966: 107; in <i>Antichthonidris</i>: Snelling, 1975: 6; in <i>Monomorium</i>: Fernández, 2007b: 132. Senior synonym of <i>piceonigrum</i>: Kusnezov, 1960b: 345. See also: Kusnezov, 1949a: 431. [Note. If the dubious combination of <i>bidentata</i> Smith, F. in <i>Monomorium</i> (above) is correct then <i>bidentatum</i> Mayr, 1887 becomes an <b>unresolved junior secondary homonym</b> of <i>bidentata</i> Smith, F. 1858.]
-        }).should == {:type => :species, :name => 'bidentatum', :status => :homonym}
+        }).should == {:type => :species, :name => 'bidentatum', :status => :unresolved_homonym}
       end
 
       it "should handle an unresolved junior secondary homonym" do
         @species_catalog.parse(%{
 <b><i><span style="color:#984806">flavum</span></i></b>. <i>Tetramorium flavum</i> Chang &amp; He, 2001a: 2, figs. 1, 2, 16 (w.) CHINA. [<b>Unresolved junior secondary homonym</b> of <i>flavus</i> Donisthorpe, below.]
-        }).should == {:type => :species, :name => 'flavum', :status => :homonym}
+        }).should == {:type => :species, :name => 'flavum', :status => :unresolved_homonym}
       end
 
       it "should handle a resolved junior secondary homonym where the binomial is inside the italics" do
         @species_catalog.parse(%{
   *<i>gracillimus. *Lampromyrmex gracillimus</i> Mayr, 1868c: 95, pl. 5, figs. 97, 98 (w.) BALTIC AMBER (Eocene). [Junior secondary homonym of <i>gracillima</i> Smith, above.] Replacement name: *<i>mayrianum</i> Wheeler, W.M. 1915h: 45. [Combination in error, with <i>Lophomyrmex gracillimus</i> for *<i>Lampromyrmex gracillimus</i>: Dlussky, 1997: 57.]
-        }).should == {:type => :species, :name => 'gracillimus', :status => :uncertain, :fossil => true}
+        }).should == {:type => :species, :name => 'gracillimus', :status => :homonym, :homonym_resolved_to => 'mayrianum', :fossil => true}
       end
 
-      it "should recognize a junior primary homonym" do
+      it "should recognize a unresolved junior primary homonym" do
         @species_catalog.parse(%{
 *<b><i><span style="color:maroon">major</span></i></b><i>. *Solenopsis major</i> Théobald, 1937b: 201, pl. 4, fig. 16; pl. 14, fig. 4 (m.) FRANCE (Oligocene). [<b>Unresolved junior primary homonym</b> of <i>major</i> Forel, above.]
-        }).should == {:type => :species, :name => 'major', :status => :homonym, :fossil => true}
+        }).should == {:type => :species, :name => 'major', :status => :unresolved_homonym, :fossil => true}
       end
     end
 
