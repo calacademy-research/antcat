c704430a6a36e421bd95ff5a89a13b098f6930e7
Mark Wilden <mark@mwilden.com>
2011-03-07 09:53:59 -0800

    Fix supersubfamily header

--- spec/lib/bolton/subfamily_catalog_parser_spec.rb
+++ spec/lib/bolton/subfamily_catalog_parser_spec.rb
@@ -5,9 +5,21 @@ describe Bolton::SubfamilyCatalog do
     @subfamily_catalog = Bolton::SubfamilyCatalog.new
   end
 
+  it "should recognize the usual supersubfamily header" do
+    @subfamily_catalog.parse(%{
+<b><span lang=EN-GB>THE DOLICHODEROMORPHS: SUBFAMILIES ANEURETINAE AND DOLICHODERINAE<o:p></o:p></span></b>
+    }).should == {:type => :supersubfamily_header}
+  end
+
   it "should recognize the supersubfamily header when there's only one subfamily" do
     @subfamily_catalog.parse(%{
 <b><span lang=EN-GB>THE MYRMICOMORPHS: SUBFAMILY MYRMICINAE<o:p></o:p></span></b></p>
+    }).should == {:type => :supersubfamily_header}
+  end
+
+  it "should recognize the supersubfamily header for extinct subfamilies" do
+    @subfamily_catalog.parse(%{
+<b><span lang=EN-GB>EXTINCT SUBFAMILIES OF FORMICIDAE: *ARMANIINAE, *BROWNIMECIINAE, *FORMICIINAE, *SPHECOMYRMINAE<o:p></o:p></span></b>
     }).should == {:type => :supersubfamily_header}
   end
 
