9eb17a15cfb4e86df1fc953e133771c550d4e773
Mark Wilden <mark@mwilden.com>
2011-03-08 16:51:22 -0800

    Genus incertae sedis list parse

--- spec/lib/bolton/subfamily_catalog_subfamily_parser_spec.rb
+++ spec/lib/bolton/subfamily_catalog_subfamily_parser_spec.rb
@@ -218,6 +218,11 @@ describe Bolton::SubfamilyCatalog do
         }).should == {:type => :genera_list, :incertae_sedis => true, :genera => [['Archimyrmex', true]]}
       end
 
+      it "should be recognized when the incertae sedis part is a bit different" do
+        @subfamily_catalog.parse(%{
+<b><span lang=EN-GB>Genus (extinct) <i>incertae sedis </i>in Ectatommini</span></b><span lang=EN-GB>: *<i>Electroponera</i>.</span>
+        }).should == {:type => :genera_list, :genera => [['Electroponera', true]], :incertae_sedis => true}
+      end
 
     end
 
