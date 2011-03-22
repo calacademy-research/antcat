812c0bed7b062ecae805205435ce34926bc76b9a
Mark Wilden <mark@mwilden.com>
2011-03-22 16:43:36 -0700

    Taxatry columns

--- app/controllers/taxatry_controller.rb
+++ app/controllers/taxatry_controller.rb
@@ -4,21 +4,20 @@ class TaxatryController < ApplicationController
   def index
     @subfamilies = Subfamily.all :order => :name
     set_selected_taxon 'subfamily'
-
     if @selected_subfamily
       @genera = @selected_subfamily == 'all' ? Genus.all(:order => :name) : @selected_subfamily.genera
-      @taxonomic_history = @selected_subfamily.taxonomic_history
+      @taxonomic_history = @selected_subfamily.taxonomic_history unless @selected_subfamily == 'all'
     end
-    set_selected_taxon 'genus'
 
+    set_selected_taxon 'genus'
     if @selected_genus
-      @species = @selected_genus.species
-      @taxonomic_history = @selected_genus.taxonomic_history
+      @species = @selected_genus == 'all' ? Species.all(:order => :name) : @selected_genus.species
+      @taxonomic_history = @selected_genus.taxonomic_history unless @selected_genus == 'all'
     end
 
     set_selected_taxon 'species'
     if @selected_species
-      @taxonomic_history = @selected_species.taxonomic_history
+      @taxonomic_history = @selected_species.taxonomic_history unless @selected_species == 'all'
     end
   end
 
