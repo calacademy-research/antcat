d00823c871ffab54037646d04ede6745cdccc1a6
Mark Wilden <mark@mwilden.com>
2011-04-04 11:58:10 -0700

    Preload images. Set background to black. Fix missing unit. Try to avoid flashing in Chrome/Safari.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,20 @@
 document.documentElement.className = 'js'; // FOUC fix
 
+preloadImages();
+
 $(function() {
   setupLogin();
   spaceOutImages();
   $("input[type=text]:first").focus()
 })
+
+function preloadImages() {
+  var images = new Array('images/header_bg.png', 'images/antcat_logo.png', 'images/site_header_ant_5.png')
+  for (var i = 0; i < images.length; i++) {
+    var image = new Image();
+    image.src = images[i];
+  }
+}
 
 function setupLogin() {
   $('#login .form').hide();
