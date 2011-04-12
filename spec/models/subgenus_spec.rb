a2c8ad5509eac0304484674172b02605b7a847fe
Mark Wilden <mark@mwilden.com>
2011-04-12 16:24:28 -0700

    Delete unused import code

--- spec/models/subgenus_spec.rb
+++ spec/models/subgenus_spec.rb
@@ -10,25 +10,4 @@ describe Subgenus do
     colobopsis.reload.genus.name.should == 'Camponotus'
   end
 
-  describe "Import" do
-
-    it "should import all the fields correctly" do
-      subgenus = Subgenus.import :name => 'Colobopsis',
-        :genus => 'Camponotus',
-        :fossil => true, :status => 'valid',
-        :taxonomic_history => '<p>history</p>'
-      subgenus.reload
-      subgenus.name.should == 'Colobopsis'
-      subgenus.genus.name.should == 'Camponotus'
-      subgenus.fossil?.should be_true
-      subgenus.status.should == 'valid'
-      subgenus.taxonomic_history.should == '<p>history</p>'
-      subgenus.incertae_sedis_in.should be_nil
-    end
-
-    it "should not create the genus if the passed-in information isn't valid" do
-      lambda {Subgenus.import :name => 'Acalama', :status => 'valid', :genus => ''}.should raise_error
-    end
-
-  end
 end
