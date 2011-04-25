dcd218ff2ae58b40b0be278b7a17ba16a440216f
Mark Wilden <mark@mwilden.com>
2011-04-24 19:45:08 -0700

    Add (back??) Taxa indexes

---
+++ db/migrate/20110425024104_add_taxa_indexes.rb
@@ -0,0 +1,13 @@
+class AddTaxaIndexes < ActiveRecord::Migration
+  def self.up
+    add_index "taxa", ["id", "type"], :name => "taxa_id_and_type_idx"
+    add_index "taxa", ["name"], :name => "taxa_name_idx"
+    add_index "taxa", ["type"], :name => "taxa_type_idx"
+  end
+
+  def self.down
+    remove_index "taxa", :name => "taxa_type_idx"
+    remove_index "taxa", :name => "taxa_name_idx"
+    remove_index "taxa", :name => "taxa_id_and_type_idx"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110423180205) do
+ActiveRecord::Schema.define(:version => 20110425024104) do
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
