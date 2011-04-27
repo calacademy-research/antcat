e3ff177c4224d523f78cae4fee7434eb8905773e
Mark Wilden <mark@mwilden.com>
2011-04-27 10:22:49 -0700

    Use actual dimensions to calculate browser height

--- public/javascripts/taxatry/index.js
+++ public/javascripts/taxatry/index.js
@@ -10,8 +10,8 @@ function setupPage() {
 }
 
 function setPageHeight() {
-  height = $('#page').height() - 240
-  $("#page_contents").height(height);
-  $("#browser").height(height - 48 - $("#taxon_header").height());
+  height = $('#page').height() - $('#site_header').height() - $('#page_header').height() - 2 - $('#page_notice').height() - $('#page_alert').height() - $('#search_results').height() - 3 - 2 - 2 - $('#taxon_key').height() - 2 - $('#site_footer').height() - 8;
+  $("#browser").height(height);
+  $("#browser .contents").height(height - $("#browser .header").height());
 }
 
