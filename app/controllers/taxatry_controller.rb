fc399778d2c8965a93e64441d194c0ec8beaa3a5
Mark Wilden <mark@mwilden.com>
2011-04-27 12:23:39 -0700

    Fix IE problem

--- app/controllers/taxatry_controller.rb
+++ app/controllers/taxatry_controller.rb
@@ -16,7 +16,7 @@ class TaxatryController < ApplicationController
     @search_params = {'search_type' => params['search_type'], 'q' => params['q']}
 
     if @search_params['q'].present?
-      params['id'] = nil if params['commit'] != 'Clear'
+      params['id'] = nil
       @search_results = Taxon.find_name @search_params['q'], @search_params['search_type']
       unless @search_results.present?
         @search_results_message = "No results found"
