ad66bb7182fc134062286f37d3ee96f4d4817bff
Mark Wilden <mark@mwilden.com>
2011-05-04 15:40:34 -0700

    Show regular header in browser header and contents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
     [taxon.type.downcase, 'taxon']
   end
 
+  def taxon_header taxon
+    fossil_symbol = taxon.fossil? ? "&dagger;" : ''
+    label = (fossil_symbol + h(taxon.name.upcase)).html_safe
+    css_classes = taxon_rank_css_classes taxon
+    css_classes << taxon.status.gsub(/ /, '_')
+    css_classes = css_classes.sort.join ' '
+    (taxon.rank.capitalize + ' ' + link_to(label, browser_taxatry_path(taxon), :class => css_classes)).html_safe
+  end
+
 end
