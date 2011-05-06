ade86f7a470cf5a8338d127b73b59744068fc412
Mark Wilden <mark@mwilden.com>
2011-05-06 10:44:45 -0700

    Google Analytics: track page load time

--- public/javascripts/google_analytics.js
+++ public/javascripts/google_analytics.js
@@ -1,6 +1,7 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-5512531-2']);
 _gaq.push(['_trackPageview']);
+_gaq.push(['_trackPageLoadTime']);
 
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
