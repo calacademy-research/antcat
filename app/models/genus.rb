de15cc8acfc99eab56bf0a0780eab56a662382aa
Mark Wilden <mark@mwilden.com>
2011-05-12 14:21:47 -0700

    Remove Genus.import

--- app/models/genus.rb
+++ app/models/genus.rb
@@ -5,64 +5,10 @@ class Genus < Taxon
   has_many :subspecies, :class_name => 'Subspecies', :order => :name
   has_many :subgenera, :class_name => 'Subgenus', :order => :name
 
+  scope :without_subfamily, where(:subfamily_id => nil)
+
   def children
     species
-  end
-
-  def self.import record
-    subfamily_name = record[:subfamily]
-    tribe_name = record[:tribe]
-    synonym_of_name = record[:synonym_of]
-    homonym_replaced_by_name = record[:homonym_replaced_by]
-    status = record[:status].present? ? record[:status] : nil
-    incertae_sedis_in = record[:incertae_sedis_in] && record[:incertae_sedis_in]
-    fossil = record[:fossil]
-
-    # the parse returns subfamilies that are outside the family - exclude them
-    subfamily_name = nil if ['Aculeata', 'Symphyta', 'Apocrita', 'Homoptera', 'Ichneumonidae', 'Embolemidae'].include? subfamily_name
-
-    subfamily = subfamily_name && Subfamily.find_or_create_by_name(subfamily_name, :status => 'valid')
-    raise if subfamily && !subfamily.valid?
-
-    tribe = tribe_name && Tribe.find_or_create_by_name(tribe_name, :subfamily => subfamily, :status => 'valid')
-    raise if tribe && !tribe.valid?
-
-    synonym_of = find_referent synonym_of_name
-    homonym_replaced_by = find_referent homonym_replaced_by_name
-
-    # sorry, Barry, but it's just not
-    fossil = nil if record[:name] == 'Pseudoatta'
-
-    attributes = {:name => record[:name], :fossil => fossil, :status => status,
-                  :subfamily => subfamily, :tribe => tribe,
-                  :synonym_of => synonym_of, :homonym_replaced_by => homonym_replaced_by,
-                  :taxonomic_history => record[:taxonomic_history],
-                  :incertae_sedis_in => incertae_sedis_in}
-
-    possible_matches = Genus.all :conditions => ['name = ?', record[:name]]
-    if genus = possible_matches.find {|possible_match| possible_match.status.nil?}
-      genus.update_attributes! attributes
-    elsif possible_matches.none? {|possible_match| possible_match.status == status}
-      genus = create! attributes
-    else
-      already_printed_genus_name = false
-      existing_genus = possible_matches.find {|possible_match| possible_match.status == status}
-      if subfamily && existing_genus.subfamily && existing_genus.subfamily != subfamily
-        raise "#{existing_genus.name}: trying to replace subfamily '#{existing_genus.subfamily && existing_genus.subfamily.name}' with '#{subfamily_name}'"
-      end
-      if tribe && existing_genus.tribe && existing_genus.tribe != tribe
-        raise "#{existing_genus.name}: trying to replace tribe '#{existing_genus.tribe && existing_genus.tribe.name}' with '#{tribe_name}'"
-      end
-      if !!existing_genus.fossil? != !!fossil
-        raise "#{existing_genus.name}: trying to replace fossil flag '#{existing_genus.fossil?}' with '#{fossil}'"
-      end
-    end
-    genus
-  end
-
-  def self.find_referent name
-    return unless name
-    find_by_name(name) || Subgenus.find_by_name(name) || create!(:name => name)
   end
 
   def full_name
