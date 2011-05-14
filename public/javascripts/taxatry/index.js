c7c9fe5f04dda454fa88665d599edfa584bf740c
Mark Wilden <mark@mwilden.com>
2011-05-14 11:10:34 -0700

    Rename index elements/classes

--- public/javascripts/taxatry/index.js
+++ public/javascripts/taxatry/index.js
@@ -11,7 +11,7 @@ function setupPage() {
 
 function setPageHeight() {
   height = $('#page').height() - $('#site_header').height() - $('#page_header').height() - 2 - $('#page_notice').height() - $('#page_alert').height() - $('#search_results').height() - 3 - 2 - 2 - $('#taxon_key').height() - 2 - $('#site_footer').height() - 8;
-  $("#browser").height(height);
-  $("#browser .contents").height(height - $("#browser .header").height());
+  $("#catalog").height(height);
+  $("#catalog .index").height(height - $("#catalog .content").height());
 }
 
