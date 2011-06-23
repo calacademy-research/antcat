7b759682e8b159fbd27387dc3718ede4a1187dba
Mark Wilden <mark@mwilden.com>
2011-06-23 15:43:11 -0700

    Export fossil taxa to AntWeb

--- spec/lib/antweb/exporter_spec.rb
+++ spec/lib/antweb/exporter_spec.rb
@@ -6,33 +6,39 @@ describe Antweb::Exporter do
   end
 
   describe "exporting one taxon" do
-
     it "should export a subfamily" do
       ponerinae = Subfamily.create! :name => 'Ponerinae', :status => 'valid', :taxonomic_history => '<p>Ponerinae</p>'
-      Factory :genus, :subfamily => ponerinae
-      Factory :genus, :subfamily => ponerinae, :fossil => true
-      @exporter.export_taxon(ponerinae).should == ['Ponerinae', nil, nil, nil, nil, nil, 'TRUE', nil, nil, nil, '<p class="taxon_statistics">1 genus</p><p>Ponerinae</p>']
+      Factory :genus, :subfamily => ponerinae, :tribe => nil
+      @exporter.export_taxon(ponerinae).should == ['Ponerinae', nil, nil, nil, nil, nil, 'TRUE', nil, nil, nil, '<p class="taxon_statistics">1 genus</p><p>Ponerinae</p>', nil]
+    end
+
+    it "should export fossil taxa" do
+      ponerinae = Subfamily.create! :name => 'Ponerinae', :status => 'valid', :taxonomic_history => '<p>Ponerinae</p>'
+      Factory :genus, :subfamily => ponerinae, :tribe => nil
+      fossil = Factory :genus, :subfamily => ponerinae, :tribe => nil, :fossil => true, :name => 'Atta', :taxonomic_history => 'Atta'
+      @exporter.export_taxon(ponerinae).should == ['Ponerinae', nil, nil, nil, nil, nil, 'TRUE', nil, nil, nil, '<p class="taxon_statistics">Extant: 1 genus</p><p class="taxon_statistics">Fossil: 1 genus</p><p>Ponerinae</p>', nil]
+      @exporter.export_taxon(fossil).should == ['Ponerinae', nil, 'Atta', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'Atta', 'TRUE']
     end
 
     it "should export a genus" do
       myrmicinae = Subfamily.create! :name => 'Myrmicinae', :status => 'valid'
       dacetini = Tribe.create! :name => 'Dacetini', :subfamily => myrmicinae, :status => 'valid'
       acanthognathus = Genus.create! :name => 'Acanothognathus', :subfamily => myrmicinae, :tribe => dacetini, :status => 'valid', :taxonomic_history => '<i>Acanthognathous</i>'
-      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(acanthognathus, :include_fossil => false, :include_invalid => false).and_return 'history'
-      @exporter.export_taxon(acanthognathus).should == ['Myrmicinae', 'Dacetini', 'Acanothognathus', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history']
+      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(acanthognathus, :include_invalid => false).and_return 'history'
+      @exporter.export_taxon(acanthognathus).should == ['Myrmicinae', 'Dacetini', 'Acanothognathus', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history', nil]
     end
 
     it "should export a genus without a tribe" do
       myrmicinae = Subfamily.create! :name => 'Myrmicinae', :status => 'valid'
       acanthognathus = Genus.create! :name => 'Acanothognathus', :subfamily => myrmicinae, :status => 'valid', :taxonomic_history => '<i>Acanthognathous</i>'
-      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(acanthognathus, :include_fossil => false, :include_invalid => false).and_return 'history'
-      @exporter.export_taxon(acanthognathus).should == ['Myrmicinae', nil, 'Acanothognathus', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history']
+      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(acanthognathus, :include_invalid => false).and_return 'history'
+      @exporter.export_taxon(acanthognathus).should == ['Myrmicinae', nil, 'Acanothognathus', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history', nil]
     end
 
     it "should export a genus without a subfamily as being in 'incertae_sedis'" do
       acanthognathus = Genus.create! :name => 'Acanothognathus', :status => 'valid', :taxonomic_history => '<i>Acanthognathous</i>'
-      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(acanthognathus, :include_fossil => false, :include_invalid => false).and_return 'history'
-      @exporter.export_taxon(acanthognathus).should == ['incertae_sedis', nil, 'Acanothognathus', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history']
+      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(acanthognathus, :include_invalid => false).and_return 'history'
+      @exporter.export_taxon(acanthognathus).should == ['incertae_sedis', nil, 'Acanothognathus', nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history', nil]
     end
 
     it "should not export invalid taxa" do
@@ -40,9 +46,9 @@ describe Antweb::Exporter do
       tribe = Factory :tribe, :subfamily => subfamily
       valid_genus = Factory :genus, :subfamily => subfamily, :tribe => tribe, :status => 'valid'
       invalid_genus = Factory :genus, :subfamily => subfamily, :tribe => tribe, :status => 'syononym'
-      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(valid_genus, :include_fossil => false, :include_invalid => false).and_return 'history'
-      CatalogFormatter.should_not_receive(:format_taxonomic_history_with_statistics).with(invalid_genus, :include_fossil => false, :include_invalid => false)
-      @exporter.export_taxon(valid_genus).should == [subfamily.name, tribe.name, valid_genus.name, nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history']
+      CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(valid_genus, :include_invalid => false).and_return 'history'
+      CatalogFormatter.should_not_receive(:format_taxonomic_history_with_statistics).with(invalid_genus, :include_invalid => false)
+      @exporter.export_taxon(valid_genus).should == [subfamily.name, tribe.name, valid_genus.name, nil, nil, nil, 'TRUE', 'TRUE', nil, nil, 'history', nil]
       @exporter.export_taxon(invalid_genus).should == nil
     end
 
@@ -59,8 +65,8 @@ describe Antweb::Exporter do
         attini = Factory :tribe, :name => 'Attini', :subfamily => myrmicinae
         atta = Factory :genus, :name => 'Atta', :tribe => attini
         species = Factory :species, :name => 'robustus', :genus => atta, :taxonomic_history => 'Taxonomic history'
-        CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(species, :include_fossil => false, :include_invalid => false).and_return 'history'
-        @exporter.export_taxon(species).should == ['Myrmicinae', 'Attini', 'Atta', 'robustus', nil, nil, 'TRUE', 'TRUE', nil, nil, 'history']
+        CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(species, :include_invalid => false).and_return 'history'
+        @exporter.export_taxon(species).should == ['Myrmicinae', 'Attini', 'Atta', 'robustus', nil, nil, 'TRUE', 'TRUE', nil, nil, 'history', nil]
       end
 
     end
@@ -73,8 +79,8 @@ describe Antweb::Exporter do
         atta = Factory :genus, :name => 'Atta', :tribe => attini
         species = Factory :species, :name => 'robustus', :genus => atta
         subspecies = Factory :subspecies, :name => 'emeryii', :species => species, :taxonomic_history => 'Taxonomic history'
-        CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(subspecies, :include_fossil => false, :include_invalid => false).and_return 'history'
-        @exporter.export_taxon(subspecies).should == ['Myrmicinae', 'Attini', 'Atta', 'robustus emeryii', nil, nil, 'TRUE', 'TRUE', nil, nil, 'history']
+        CatalogFormatter.should_receive(:format_taxonomic_history_with_statistics).with(subspecies, :include_invalid => false).and_return 'history'
+        @exporter.export_taxon(subspecies).should == ['Myrmicinae', 'Attini', 'Atta', 'robustus emeryii', nil, nil, 'TRUE', 'TRUE', nil, nil, 'history', nil]
       end
 
     end
