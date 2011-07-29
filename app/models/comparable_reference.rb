5dd9ca5e2af5af41b0639cd8eed0ff84f0537354
Mark Wilden <mark@mwilden.com>
2011-07-28 18:39:03 -0700

    ComparableReference can take 0 args for initialize

--- app/models/comparable_reference.rb
+++ app/models/comparable_reference.rb
@@ -2,7 +2,7 @@ class ComparableReference
   include ReferenceComparable
   attr_accessor :author, :year, :title, :type, :series_volume_issue, :pagination
 
-  def initialize hash
+  def initialize hash = {}
     update hash
   end
 
