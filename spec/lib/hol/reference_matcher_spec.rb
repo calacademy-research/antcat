5caa17911673f89161ab37236bf14f3ac911f80c
Mark Wilden <mark@mwilden.com>
2011-07-29 11:02:02 -0700

    Return just best match. Raise if > 1 best match.

--- spec/lib/hol/reference_matcher_spec.rb
+++ spec/lib/hol/reference_matcher_spec.rb
@@ -15,4 +15,41 @@ describe Hol::ReferenceMatcher do
     end
   end
 
+  describe "Returning just one match" do
+    it "should return the best match" do
+      best_match = mock
+      good_match = mock
+      bibliography = mock 'Bibliography'
+      Hol::Bibliography.stub!(:new).and_return bibliography
+      reference = Factory.build :article_reference
+      bibliography.stub!(:read_references).and_return [good_match, best_match]
+      reference.should_receive(:<=>).with(best_match).and_return 0.9
+      reference.should_receive(:<=>).with(good_match).and_return 0.8
+      @matcher.match(reference).should == best_match
+    end
+  end
+
+  describe "No match found, but matching author exists" do
+    it "should return nil" do
+      reference = Factory.build :article_reference
+      @matcher.stub!(:candidates_for).and_return [reference]
+      reference.stub(:<=>).and_return 0.0
+      @matcher.match(reference).should be_nil
+    end
+  end
+
+  describe "Two matches with equal similarity found" do
+    it "should raise exception" do
+      best_match = mock
+      good_match = mock
+      bibliography = mock 'Bibliography'
+      Hol::Bibliography.stub!(:new).and_return bibliography
+      reference = Factory.build :article_reference
+      bibliography.stub!(:read_references).and_return [good_match, best_match]
+      reference.should_receive(:<=>).with(best_match).and_return 0.9
+      reference.should_receive(:<=>).with(good_match).and_return 0.9
+      lambda {@matcher.match(reference)}.should raise_error
+    end
+  end
+
 end
