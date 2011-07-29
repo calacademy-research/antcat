eb8cdd09d785747a56045c2de7c524d099afd1df
Mark Wilden <mark@mwilden.com>
2011-07-28 19:06:55 -0700

    Handle no matches for author. Remove HOL tests that are in base class specs.

--- lib/hol/reference_matcher.rb
+++ lib/hol/reference_matcher.rb
@@ -2,14 +2,16 @@ class Hol::ReferenceMatcher < ReferenceMatcher
 
   def match target_reference
     matches = super
+    return :no_entries_for_author if @no_entries_for_author
     return unless matches.present?
-    return :no_entries_for_author if matches.first[:status] == :no_entries_for_author
     matches
   end
 
   def read_references target
     @bibliography ||= Hol::Bibliography.new
-    @bibliography.read_references target.principal_author_last_name
+    result = @bibliography.read_references target
+    @no_entries_for_author = result.blank?
+    result
   end
 
   def possible_match? target, candidate
