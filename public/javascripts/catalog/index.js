84eb1da88a2225c22a93144c0bcaafe107202162
Mark Wilden <mark@mwilden.com>
2011-08-21 04:47:04 -0700

    Resize index content to fit window width

--- public/javascripts/catalog/index.js
+++ public/javascripts/catalog/index.js
@@ -3,15 +3,31 @@ $(function() {
 })
 
 function setupPage() {
-  setPageHeight();
+  setDimensions();
   $(window).resize(function() {
-    setPageHeight();
+    setDimensions();
   });
 }
 
-function setPageHeight() {
-  height = $('#page').height() - $('#site_header').height() - $('#page_header').height() - 2 - $('#page_notice').height() - $('#page_alert').height() - $('#search_results').height() - 3 - 2 - 2 - $('#taxon_key').height() - 2 - $('#site_footer').height() - 8;
+function setDimensions() {
+  setHeight();
+  setWidth();
+}
+
+function setHeight() {
+  height = $('#page').height()
+         - $('#site_header').height()
+         - $('#page_header').height() - 2
+         - $('#page_notice').height()
+         - $('#page_alert').height()
+         - $('#search_results').height() - 3 - 2 - 2 -
+         $('#taxon_key').height() - 2 -
+         $('#site_footer').height() - 8;
   $("#catalog").height(height);
   $("#catalog .index").height(height - $("#catalog .content").height());
+}
+
+function setWidth() {
+  $("#catalog .content").width($('#page').width());
 }
 
