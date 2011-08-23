7b7284f49a4b5cb2183b91776d7462645cdcf319
Mark Wilden <mark@mwilden.com>
2011-08-23 13:35:17 -0700

    Backpatch Rack to avoid warning on /.../n

---
+++ config/initializers/rack_util_escape_patch.rb
@@ -0,0 +1,11 @@
+# This is from rack/util.rb as of Aug 15, 2011
+# It fixes warnings about comparing /.../n regexp
+# to UTF-8 strings
+
+module Rack
+  module Utils
+    def escape(s)
+      URI.encode_www_form_component(s)
+    end
+  end
+end
