fe65de2bd074e94495009f04c035be83fca3e38b
Mark Wilden <mark@mwilden.com>
2011-08-30 15:48:06 -0700

    Fix URLs that s3_url code doesn't like

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# coding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110503014112) do
+ActiveRecord::Schema.define(:version => 20110830203207) do
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
@@ -152,7 +152,7 @@ ActiveRecord::Schema.define(:version => 20110503014112) do
     t.string   "principal_author_last_name_cache"
   end
 
-  add_index "references", ["author_names_string_cache", "citation_year"], :name => "references_author_names_string_citation_year_idx", :length => {"citation_year"=>nil, "author_names_string_cache"=>255}
+  add_index "references", ["author_names_string_cache", "citation_year"], :name => "references_author_names_string_citation_year_idx", :length => {"author_names_string_cache"=>255, "citation_year"=>nil}
   add_index "references", ["created_at"], :name => "references_created_at_idx"
   add_index "references", ["journal_id"], :name => "references_journal_id_idx"
   add_index "references", ["nested_reference_id"], :name => "references_nested_reference_id_idx"
