d5d3c90cb13b98351b21c201a69e348104ecb831
Mark Wilden <mark@mwilden.com>
2011-08-30 15:48:06 -0700

    Fix URLs that s3_url code doesn't like

---
+++ db/migrate/20110830203207_fix_accents_in_pdf_names.rb
@@ -0,0 +1,65 @@
+# coding: UTF-8
+class FixAccentsInPdfNames < ActiveRecord::Migration
+  def self.up
+    [
+      [
+      "http://antcat.org/documents/4361/Espadaler_1997_Sessió_d'Entomologia.pdf",
+      "http://antcat.org/documents/4361/Espadaler_1997_Sessió_dEntomologia.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4179/Hosoishi & Ogata 2009 [A check list of the ant genus Crematogaster in Asia].pdf",
+      "http://antcat.org/documents/4179/Hosoishi & Ogata 2009 [A check list of the ant genus Crematogaster in Asia.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4582/O'Donnell_et_al_2007_J_Anim_Ecol.pdf",
+      "http://antcat.org/documents/4582/ODonnell_et_al_2007_J_Anim_Ecol.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4581/O'Donnell_et_al_2005_Biotropica.pdf",
+      "http://antcat.org/documents/4581/ODonnell_et_al_2005_Biotropica.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4216/Xu et al. 1998 [Seven species of the ant genus Pheidole Westwood newly recorded in China (Hymenoptera Formicidae). [In Chinese.]].PDF",
+      "http://antcat.org/documents/4216/Xu et al. 1998 [Seven species of the ant genus Pheidole Westwood newly recorded in China (Hymenoptera Formicidae). [In Chinese.PDF",
+      ],
+
+      [
+      "http://antcat.org/documents/4217/Xu et al. 1998 [Three Ponerinae species newly recorded in China and new distribution of Ponera sinensis (Hymenoptera Formicidae). [In Chinese.]].PDF",
+      "http://antcat.org/documents/4217/Xu et al. 1998 [Three Ponerinae species newly recorded in China and new distribution of Ponera sinensis (Hymenoptera Formicidae). [In Chinese.PDF",
+      ],
+
+      [
+      "http://antcat.org/documents/4827/Keller_&_Reeve_1994_Evolution [Genetic variability, queen number, and polyandry in social Hymenoptera].pdf",
+      "http://antcat.org/documents/4827/Keller_&_Reeve_1994_Evolution [Genetic variability, queen number, and polyandry in social Hymenoptera.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4828/Gobin_et_al_2006_Cell_Tissue_Res [Queen-worker spermatheca differences].pdf",
+      "http://antcat.org/documents/4828/Gobin_et_al_2006_Cell_Tissue_Res [Queen-worker spermatheca differences.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4829/Gobin_et_al_2008_Naturwissenschaften [Degeneration of spermatheca in worker ants].pdf",
+      "http://antcat.org/documents/4829/Gobin_et_al_2008_Naturwissenschaften [Degeneration of spermatheca in worker ants.pdf",
+      ],
+
+      [
+      "http://antcat.org/documents/4830/Ito_et_al_2010_Naturwissenschaften [Thelytoky in Pyramica membranifera].pdf",
+      "http://antcat.org/documents/4830/Ito_et_al_2010_Naturwissenschaften [Thelytoky in Pyramica membranifera.pdf",
+      ],
+    ].each do |original, fix|
+      ActiveRecord::Base.connection.execute %{UPDATE reference_documents SET url = "#{fix}" WHERE url = "#{original}"}
+      file_file_name_original = original[(original.rindex('/')+1)..-1]
+      file_file_name_fix = fix[(fix.rindex('/')+1)..-1]
+      ActiveRecord::Base.connection.execute %{UPDATE reference_documents SET file_file_name = "#{file_file_name_fix}" WHERE file_file_name = "#{file_file_name_original}"}
+    end
+
+  end
+
+  def self.down
+  end
+end
