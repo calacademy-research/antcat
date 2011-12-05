95f7c32b5db6ed0c16a85d4914333a169a374769
Mark Wilden <mark@mwilden.com>
2011-12-04 16:00:40 -0800

    Help text

--- public/javascripts/bolton_references.js
+++ public/javascripts/bolton_references.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.help_icon').qtip({
-    content: "Enter search words. References matching all words will be displayed.",
+    content: "Enter search words, e.g. 'Bolton 2011'. References matching all words will be displayed.",
     show: 'mouseover',
     hide: 'mouseout',
     position: {
