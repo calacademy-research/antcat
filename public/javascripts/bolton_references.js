2981a6fa33e32942eb78e81074b313372889ccf0
Mark Wilden <mark@mwilden.com>
2011-12-12 12:27:29 -0800

    Simplify help

--- public/javascripts/bolton_references.js
+++ public/javascripts/bolton_references.js
@@ -18,7 +18,7 @@ $(function() {
     }
   });
   $('#match_status_help').qtip({
-    content: "'Auto' matches are those for which the score was >= 0.8. 'None' shows references without matches. Leaving all blank finds all.",
+    content: "'Auto' matches are those for which the score was >= 0.8. 'None' shows references without matches.",
     show: 'mouseover',
     hide: 'mouseout',
     position: {
