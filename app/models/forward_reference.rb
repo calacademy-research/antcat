de9a21afe5ca7083db7b2ce26855ef2565c7cf73
Mark Wilden <mark@mwilden.com>
2011-12-17 13:53:52 -0800

    Set status

--- app/models/forward_reference.rb
+++ app/models/forward_reference.rb
@@ -7,6 +7,6 @@ class ForwardReference < ActiveRecord::Base
 
   def fixup
     source = Taxon.find source_id
-    source.update_attribute :type_taxon, Genus.create!(:name => target_name)
+    source.update_attribute :type_taxon, Genus.create!(:name => target_name, :status => 'valid')
   end
 end
