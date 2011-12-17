70532b580f242b7c1a5360e7e8cf224b9096266b
Mark Wilden <mark@mwilden.com>
2011-12-17 12:24:36 -0800

    Fix misname. Change .import interface.

--- spec/models/protonym_spec.rb
+++ spec/models/protonym_spec.rb
@@ -2,21 +2,26 @@
 require 'spec_helper'
 
 describe Protonym do
+
   it "has an authorship" do
-    authorship = Factory :reference_location
+    authorship = Factory :citation
     protonym = Protonym.create! :authorship => authorship
     protonym.reload.authorship.should == authorship
   end
 
   describe "Importing" do
-    it "works" do
-      reference = Factory :reference, :author_names => [Factory(:author_name, :name => 'Bolton')], :citation_year => '1920'
-      Reference.should_receive(:find_by_bolton_author_year).and_return reference
-      authorship = {:author_names=>["Latreille"], :year=>"1809", :pages=>"124"}
-      protonym = Protonym.import 'Formicariae', authorship
+    it "should create the Protonym and the Citation, which is linked to an existing Reference" do
+      reference = Factory :article_reference, :bolton_key_cache => 'Latreille 1809'
+      data = {
+        :name => "Formicariae",
+        :authorship => [{:author_names => ["Latreille"], :year => "1809", :pages => "124"}]
+      }
+
+      protonym = Protonym.import(data).reload
+
       protonym.name.should == 'Formicariae'
       protonym.authorship.pages.should == '124'
-      protonym.authorship.reference.citation_year.should == '1920'
+      protonym.authorship.reference.should == reference
     end
   end
 end
