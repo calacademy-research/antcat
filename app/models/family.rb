1091f46c222bb1811b9013bf67db4d0e6003c381
Mark Wilden <mark@mwilden.com>
2011-12-17 16:48:30 -0800

    Remove commented-out code

--- app/models/family.rb
+++ app/models/family.rb
@@ -23,26 +23,6 @@ class Family < Taxon
     statistics
   end
 
-  #has_many :tribes, :order => :name
-  #has_many :genera, :class_name => 'Genus', :order => :name
-  #has_many :species, :class_name => 'Species', :order => :name
-  #has_many :subspecies, :class_name => 'Subspecies', :order => :name
-
-  #def children
-    #tribes
-  #end
-
-  #def full_name
-    #name
-  #end
-
-  #def full_label
-    #full_name
-  #end
-
-  #def statistics
-    #get_statistics [:genera, :species, :subspecies]
-  #end
   def full_label
     name
   end
