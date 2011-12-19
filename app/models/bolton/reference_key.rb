e4e9bd4a6a79b752ea864a9fe67f4f8c6c2f8446
Mark Wilden <mark@mwilden.com>
2011-12-19 15:06:50 -0800

    Bolton::ReferenceKey

--- app/models/bolton/reference_key.rb
+++ app/models/bolton/reference_key.rb
@@ -1,13 +1,23 @@
 # coding: UTF-8
 class Bolton::ReferenceKey
+<<<<<<< HEAD
   def initialize authors_string, year
     @authors = authors_string
     @year = year
+=======
+  def initialize authors_string, citation_year
+    @authors = authors_string
+    @citation_year = citation_year
+>>>>>>> Bolton::ReferenceKey
   end
 
   def to_s format
     raise unless format == :db
+<<<<<<< HEAD
     return @year unless @authors.present?
+=======
+    return @citation_year unless @authors.present?
+>>>>>>> Bolton::ReferenceKey
     key = @authors.dup
     # remove initials
     key.gsub! /\b[[:upper:]]\./, ''
@@ -15,7 +25,11 @@ class Bolton::ReferenceKey
     key.gsub! /[,&]/, ''
     # collapse spaces
     key.squish!
+<<<<<<< HEAD
     key << ' ' << @year
+=======
+    key << ' ' << @citation_year
+>>>>>>> Bolton::ReferenceKey
   end
 
 end
