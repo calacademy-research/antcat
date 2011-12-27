db2fd8d9f5241186ecdc1cdd58a0924a6e8a9e91
Mark Wilden <mark@mwilden.com>
2011-12-27 13:19:54 -0800

    Rails 3.1 changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,6 @@ AntCat::Application.configure do
   config.cache_classes = false
   config.whiny_nils = true
   config.consider_all_requests_local = true
-  config.action_view.debug_rjs = true
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
   config.action_dispatch.best_standards_support = :builtin
