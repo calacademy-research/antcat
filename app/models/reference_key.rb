c34c18477847d4053659100308efd69d6e2e2a26
Mark Wilden <mark@mwilden.com>
2011-12-26 18:07:23 -0800

    Icon for jumping to references

--- app/models/reference_key.rb
+++ app/models/reference_key.rb
@@ -14,8 +14,11 @@ class ReferenceKey
   def to_link
     key = @reference.author_names.first.last_name + ', ' + @reference.citation_year
     content_tag(:span) do
-      content_tag(:a, key, :href => "#", :class => :reference_key).html_safe +
-      content_tag(:a, ReferenceFormatter.format(@reference), :href => "#", :class => :reference_key_expansion).html_safe
+      content_tag(:span, key, :class => :reference_key) +
+      content_tag(:span, :class => :reference_key_expansion) do
+        content_tag(:span, ReferenceFormatter.format(@reference)) +
+        content_tag(:img, '', :src => '/images/external_link.png')
+      end
     end.html_safe
   end
 
