e479956e5e524a2127e00e3e2e95b6db60813132
Mark Wilden <mark@mwilden.com>
2011-12-26 17:45:23 -0800

    Fix specs

--- spec/models/reference_key_spec.rb
+++ spec/models/reference_key_spec.rb
@@ -9,8 +9,12 @@ describe ReferenceKey do
 
   describe "Link" do
     it "should create a link to the reference" do
-      reference = Factory :article_reference, :author_names => [Factory(:author_name, :name => 'Latreille, P. A.')], :citation_year => '1809'
-      reference.key.to_link.should ==  "<a class=\"reference_key\" href=\"/references?q=#{reference.id}\">Latreille, 1809</a>"
+      reference = Factory :article_reference, :author_names => [Factory(:author_name, :name => 'Latreille, P. A.')], :citation_year => '1809', :title => "Ants", :journal => Factory(:journal, :name => 'Science'), :series_volume_issue => '(1)', :pagination => '3'
+      reference.key.to_link.should ==
+        "<span>" +
+          "<a class=\"reference_key\" href=\"#\">Latreille, 1809</a>" + 
+          "<a class=\"reference_key_expansion\" href=\"#\">Latreille, P. A. 1809. Ants. Science (1):3.</a>" +
+        "</span>"
     end
   end
 
