9ad32478a5e73639695ee570d14518a115d7bc54
Mark Wilden <mark@mwilden.com>
2011-12-29 15:31:05 -0800

    Force table creation

--- db/migrate/20111228222140_create_taxonomic_history_items.rb
+++ db/migrate/20111228222140_create_taxonomic_history_items.rb
@@ -1,6 +1,6 @@
 class CreateTaxonomicHistoryItems < ActiveRecord::Migration
   def change
-    create_table :taxonomic_history_items do |t|
+    create_table :taxonomic_history_items, :force => true do |t|
       t.text :text
       t.timestamps
     end
