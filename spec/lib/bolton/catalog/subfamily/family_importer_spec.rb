e90eab7fd26f08fbf4446d0ec4c1e61cbdc23ac5
Mark Wilden <mark@mwilden.com>
2011-12-29 11:10:29 -0800

    Import genus

--- spec/lib/bolton/catalog/subfamily/family_importer_spec.rb
+++ spec/lib/bolton/catalog/subfamily/family_importer_spec.rb
@@ -29,9 +29,9 @@ describe Bolton::Catalog::Subfamily::Importer do
 <p>Genera <i>incertae sedis</i> in FORMICIDAE</p>
 
 <p>Genus <i>CONDYLODON</i></p>
-<p><i>Condylodon</i> Lund, 1831a: 131. Type-species: <i>Condylodon audouini</i>, by monotypy. </p>
+<p><i>Condoleeza</i> Lund, 1831a: 131. Type-species: <i>Condylodon audouini</i>, by monotypy. </p>
 <p>Taxonomic history</p>
-<p><i>Condylodon</i> in family Mutillidae?: Swainson &amp; Shuckard, 1840: 173. </p>
+<p><i>Condylodon</i> in family Mutillidae: Swainson &amp; Shuckard, 1840: 173. </p>
 
 <p>Genus references</p><p>Baroni Urbani, 1977c: 482 (review of genus).</p>
 
@@ -46,15 +46,14 @@ describe Bolton::Catalog::Subfamily::Importer do
     }
 
     latreille = Factory :article_reference, :bolton_key_cache => 'Latreille 1809'
-    lund = Factory :unknown_reference, :author_names => [Factory(:author_name, :name => 'Lund, A.')], :citation_year => '1831a', :title => "Ants"
-    swainson = Factory :unknown_reference, :author_names => [Factory(:author_name, :name => 'Swainson, B.'), Factory(:author_name, :name => 'Shuckard, C.')], :citation_year => '1840', :title => "More ants"
-    baroni = Factory :unknown_reference, :author_names => [Factory(:author_name, :name => 'Baroni Urbani, L.')], :citation_year => '1977c', :title => "Yet more ants"
+    lund = Factory :unknown_reference, :bolton_key_cache => 'Lund 1831a'
+    swainson = Factory :unknown_reference, :bolton_key_cache => 'Swainson Shuckard 1840'
+    baroni = Factory :unknown_reference, :bolton_key_cache => 'Baroni Urbani 1977c'
 
     Bolton::Catalog::Subfamily::Importer.new.import_html html
 
     family = Family.first
     family.name.should == 'Formicidae'
-    family.should_not be_nil
     family.should_not be_invalid
     family.should_not be_fossil
     family.type_taxon.name.should == 'Formica'
@@ -63,14 +62,17 @@ describe Bolton::Catalog::Subfamily::Importer do
     ]
 
     genus = Genus.find_by_name 'Condylodon'
-    genus.should_not be_nil
     genus.should_not be_invalid
     genus.should_not be_fossil
     genus.should be_incertae_sedis_in 'family'
     genus.subfamily.should be_nil
-    #genus.taxonomic_history_items.map(&:text).should =~ [
-      #"Condylodon in family Mutillidae?: {ref #{swainson.id}}: 173"
-    #]
+    genus.taxonomic_history_items.map(&:text).should =~ [
+      "Condylodon in family Mutillidae: {ref #{swainson.id}}: 173"
+    ]
+
+    species = genus.type_taxon
+    species.should_not be_invalid
+    species.name.should == 'Condylodon audouini'
   end
 
 end
