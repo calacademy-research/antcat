3dd6c1aa4397c604b704adcd36dd515aaa9af1ae
Mark Wilden <mark@mwilden.com>
2012-01-03 14:50:03 -0800

    Screw the migration

--- db/migrate/20120103210729_merge_updated_bolton_references.rb
+++ db/migrate/20120103210729_merge_updated_bolton_references.rb
@@ -4,27 +4,27 @@ class MergeUpdatedBoltonReferences < ActiveRecord::Migration
     # matched automatically because two out of author, year
     # title changed
 
-    old = Bolton::Reference.find 959
-    raise unless old.title =~ /Revision of the ant genus/
-    neww = Bolton::Reference.find 4548
-    raise unless neww.title =~ /Revision of the ant genus/
-    old.title = neww.title
-    old.citation_year = neww.citation_year
-    old.authors = neww.authors
-    old.original = neww.original
-    old.save!
-    neww.delete
+    #old = Bolton::Reference.find 959
+    #raise unless old.title =~ /Revision of the ant genus/
+    #neww = Bolton::Reference.find 4548
+    #raise unless neww.title =~ /Revision of the ant genus/
+    #old.title = neww.title
+    #old.citation_year = neww.citation_year
+    #old.authors = neww.authors
+    #old.original = neww.original
+    #old.save!
+    #neww.delete
 
-    old = Bolton::Reference.find 3793
-    raise unless old.authors =~ /Zolessi/
-    neww = Bolton::Reference.find 4620
-    raise unless neww.authors =~ /Zolessi/
-    old.title = neww.title
-    old.citation_year = neww.citation_year
-    old.authors = neww.authors
-    old.original = neww.original
-    old.save!
-    neww.delete
+    #old = Bolton::Reference.find 3793
+    #raise unless old.authors =~ /Zolessi/
+    #neww = Bolton::Reference.find 4620
+    #raise unless neww.authors =~ /Zolessi/
+    #old.title = neww.title
+    #old.citation_year = neww.citation_year
+    #old.authors = neww.authors
+    #old.original = neww.original
+    #old.save!
+    #neww.delete
 
   end
 
