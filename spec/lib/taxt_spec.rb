5d997931f6c3869f0f8a1cb263d67f7d2c3beb15
Mark Wilden <mark@mwilden.com>
2012-01-07 17:57:33 -0800

    Test Taxt.reference

--- spec/lib/taxt_spec.rb
+++ spec/lib/taxt_spec.rb
@@ -7,7 +7,7 @@ describe Taxt do
     it "should encode an unparseable string" do
       Taxt.unparseable('foo').should == '{? foo}'
     end
-    it "should encode an unparseable string" do
+    it "should encode a reference" do
       reference = Factory :book_reference
       Taxt.reference(reference).should == "{ref #{reference.id}}"
     end
@@ -20,10 +20,10 @@ describe Taxt do
     end
     it "should format a ref" do
       reference = Factory :article_reference
-      key_stub = stub
-      key_stub.should_receive(:to_link).and_return('foo')
       Reference.should_receive(:find).with(reference.id.to_s).and_return reference
+      key_stub = stub
       reference.should_receive(:key).and_return key_stub
+      key_stub.should_receive(:to_link).and_return('foo')
       Taxt.interpolate("{ref #{reference.id}}").should == 'foo'
     end
     it "should not freak if the ref is malformed" do
