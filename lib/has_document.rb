2634c33c67de7532d281b318f1c1f86a7f4e1e57
Mark Wilden <mark@mwilden.com>
2012-01-09 15:57:47 -0800

    Use #try

--- lib/has_document.rb
+++ lib/has_document.rb
@@ -2,15 +2,15 @@
 module HasDocument
 
   def url
-    document && document.url
+    document.try :url
   end
 
   def downloadable_by? user
-    document && document.downloadable_by?(user)
+    document.try :downloadable_by?, user
   end
 
   def document_host= host
-    document && document.host = host
+    document.host = host if document
   end
 
 end
