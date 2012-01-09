469bf9f8205843354e97d20e80f90045dba09757
Mark Wilden <mark@mwilden.com>
2012-01-09 15:28:01 -0800

    Rename some methods. Format.

--- app/models/unknown_reference.rb
+++ app/models/unknown_reference.rb
@@ -2,7 +2,6 @@
 class UnknownReference < Reference
 
   validates_presence_of :citation
-  before_validation :strip_newlines
 
   def strip_newlines
     citation.gsub! /\n/, ' ' if citation.present?
