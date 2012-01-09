54929f2ae32a0780fb5a2fbe763fe6861c51eba6
Mark Wilden <mark@mwilden.com>
2012-01-09 15:57:38 -0800

    Fix name of overriding method

--- app/models/unknown_reference.rb
+++ app/models/unknown_reference.rb
@@ -3,7 +3,7 @@ class UnknownReference < Reference
 
   validates_presence_of :citation
 
-  def strip_newlines
+  def strip_newlines_from_text_fields
     citation.gsub! /\n/, ' ' if citation.present?
     super
   end
