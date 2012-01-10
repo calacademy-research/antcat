9f25a6cc2f66028251d091d9e01533b7d0d9ef29
Mark Wilden <mark@mwilden.com>
2012-01-09 16:42:39 -0800

    Move search stuff to reference_search

--- app/models/reference_search.rb
+++ app/models/reference_search.rb
@@ -155,4 +155,48 @@ class Reference < ActiveRecord::Base
     end
   end
 
+  def self.find_by_bolton_key author_names, citation_year
+    bolton_key = Bolton::ReferenceKey.new(author_names.join(' '), citation_year).to_s :db
+
+    reference = find_by_bolton_key_cache bolton_key
+    return reference if reference
+
+    bolton_reference = Bolton::Reference.find_by_key_cache bolton_key
+    unless bolton_reference
+      message = "Can't find Bolton reference for '#{bolton_key}'"
+      Progress.error message
+      raise BoltonReferenceNotFound.new message
+    end
+
+    reference = bolton_reference.match
+    unless reference
+      message = "Bolton reference for '#{bolton_key}' was found, but hasn't been matched"
+      Progress.error message
+      raise BoltonReferenceNotMatched.new message
+    end
+
+    reference.update_attribute :bolton_key_cache, bolton_key
+
+    reference
+  end
+
+  def self.find_or_create_by_bolton_key data
+    year = data[:year] || data[:in][:year]
+    bolton_key = Bolton::ReferenceKey.new(data[:author_names].join(' '), year).to_s :db
+
+    reference = find_by_bolton_key_cache bolton_key
+    return reference if reference
+
+    bolton_reference = Bolton::Reference.find_by_key_cache bolton_key
+    if !bolton_reference
+      reference = MissingReference.import 'no Bolton', data
+    else
+      reference = bolton_reference.match || MissingReference.import('no Bolton match', data)
+    end
+
+    reference.update_attribute :bolton_key_cache, bolton_key
+
+    reference
+  end
+
 end
