a5c14dc12ad9cbb630697daf98b959e9946783c6
Mark Wilden <mark@mwilden.com>
2012-01-10 09:04:44 -0800

    Author.find_by_names returns full objects

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,9 +4,6 @@ class Author < ActiveRecord::Base
   scope :sorted_by_name, select('authors.id').joins(:names).group('authors.id').order(:name)
 
   def self.find_by_names names
-    results = Author.select('authors.id').
-      joins(:names).
-      where('name IN (?)', names)
-    results.group('authors.id')
+    Author.joins(:names).where('name IN (?)', names).group('authors.id').to_a
   end
 end
