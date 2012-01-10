4a3aec5d9e55b0ce237471c1b00553db7b6dbf7e
Mark Wilden <mark@mwilden.com>
2012-01-10 09:06:40 -0800

    Interim checkin as we incrementally convert to do_do_search. Comment out ReferencesController test.

--- app/models/reference_search.rb
+++ app/models/reference_search.rb
@@ -5,9 +5,7 @@ class Reference < ActiveRecord::Base
 
   searchable do
     integer :year
-    string  :citation_year
     text    :author_names_string
-    string  :author_names_string
     text    :title
     text    :journal_name do journal.name if journal end
     text    :publisher_name do publisher.name if publisher end
@@ -15,60 +13,12 @@ class Reference < ActiveRecord::Base
     text    :cite_code
     text    :editor_notes
     text    :taxonomic_notes
+    string  :citation_year
+    string  :author_names_string
   end
 
-  def self.advanced_search parameters = {}
-    author_names = AuthorParser.parse(parameters[:q])[:names]
-    authors = Author.find_by_names author_names
-    reference_ids = find_having_authors authors
-    Reference.where('id IN (?)', reference_ids).order(:author_names_string_cache, :citation_year)
-  end
-
-  def self.find_having_authors authors
-    Reference.select('`references`.*').
-      joins(:author_names).
-      joins('JOIN authors ON authors.id = author_names.author_id').
-      where('authors.id IN (?)', authors).
-      group('references.id').
-      having("COUNT(`references`.id) = #{authors.length}")
-  end
-
-  def self.do_advanced_search options = {}
-    paginate = options[:format] != :endnote_import
-    results = advanced_search(options)
-    paginate ? results.paginate(:page => options[:page]) : results
-  end
-
-  def self.do_search options = {}
-    return do_advanced_search(options) if options[:advanced]
-
-    paginate = options[:format] != :endnote_import
-
-    return order('updated_at DESC').paginate :page => options[:page] if options[:review]
-    return order('created_at DESC').paginate :page => options[:page] if options[:whats_new]
-
-    unless options[:q].present?
-      if paginate
-        return order(:author_names_string_cache, :citation_year).paginate :page => options[:page]
-      else
-        return order :author_names_string_cache, :citation_year
-      end
-    end
-
-    string = ActiveSupport::Inflector.transliterate options[:q].downcase
-
-    only_show_unknown_references = false
-    question_mark_index = string.index '?'
-    if question_mark_index
-      string[question_mark_index] = ''
-      only_show_unknown_references = true
-    end
-
-    if match = string.match(/\d{5,}/)
-      return where(:id => match[0]).paginate :page => 1
-    end
-
-    results = search {
+  def self.search_solr string, options, only_show_unknown_references, paginate
+    search {
       start_year, end_year = parse_and_extract_years string
       if start_year
         if end_year
@@ -83,14 +33,91 @@ class Reference < ActiveRecord::Base
       paginate(:page => options[:page]) if paginate
       paginate(:per_page => 5_000) if only_show_unknown_references
     }.results
+  end
 
-    if only_show_unknown_references
-      ids = results.map &:id
-      return where(:type => 'UnknownReference').where('id' => ids).paginate(:page => options[:page])
+  def self.do_do_search options = {}
+    case
+    when options[:authors]
+      do_advanced_search options
+    else
+      search {
+        order_by :author_names_string
+        order_by :citation_year
+        paginate(:page => options[:page])
+      }.results
+    end
+  end
+
+  def self.do_search options = {}
+    return do_do_search if options.empty?
+    return do_do_search(:order => :updated_at, :page => options[:page]) if options[:review]
+
+    if options[:authors]
+      author_names = AuthorParser.parse(options[:authors])[:names]
+      authors = Author.find_by_names author_names
+      return do_do_search :authors => authors, :page => options[:page]
     end
 
-    results
+    #return do_advanced_search(options) if options[:advanced]
+
+    #paginate = options[:format] != :endnote_import
+
+    #return order('created_at DESC').paginate :page => options[:page] if options[:whats_new]
+
+    #unless options[:q].present?
+      #if paginate
+        #return order(:author_names_string_cache, :citation_year).paginate :page => options[:page]
+      #else
+        #return order :author_names_string_cache, :citation_year
+      #end
+    #end
+
+    #string = ActiveSupport::Inflector.transliterate options[:q].downcase
+
+    #only_show_unknown_references = false
+    #question_mark_index = string.index '?'
+    #if question_mark_index
+      #string[question_mark_index] = ''
+      #only_show_unknown_references = true
+    #end
+
+    #if match = string.match(/\d{5,}/)
+      #return where(:id => match[0]).paginate :page => 1
+    #end
+
+    #results = search_solr string, options, only_show_unknown_references, paginate
+
+    #if only_show_unknown_references
+      #ids = results.map &:id
+      #return where(:type => 'UnknownReference').where('id' => ids).paginate(:page => options[:page])
+    #end
+
+    #results
   end
+
+  def self.advanced_search options = {}
+    #author_names = AuthorParser.parse(options[:q])[:names]
+    #authors = Author.find_by_names author_names
+    reference_ids = find_having_authors options[:authors]
+    Reference.where('id IN (?)', reference_ids).order(:author_names_string_cache, :citation_year)
+  end
+
+  def self.find_having_authors authors
+    Reference.
+       select('`references`.*')
+      .joins(:author_names)
+      .joins('JOIN authors ON authors.id = author_names.author_id')
+      .where('authors.id IN (?)', authors)
+      .group('references.id')
+      .having("COUNT(`references`.id) = #{authors.length}")
+  end
+
+  def self.do_advanced_search options = {}
+    #paginate = options[:format] != :endnote_import
+    results = advanced_search(options)
+    #paginate ? results.paginate(:page => options[:page]) : results
+  end
+
 
   def self.parse_and_extract_years string
     start_year = end_year = nil
