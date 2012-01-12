7f730f1c394fd8de42cb525af5b20c7fa91b6c11
Mark Wilden <mark@mwilden.com>
2012-01-12 14:34:02 -0800

    Format. Add spec.

--- app/models/forward_reference.rb
+++ app/models/forward_reference.rb
@@ -8,9 +8,9 @@ class ForwardReference < ActiveRecord::Base
   def fixup
     source = Taxon.find source_id
     target = case source
-    when Family then Genus
-    when Genus then Species
-    else raise
+      when Family then Genus
+      when Genus then Species
+      else raise
     end.create! :name => target_name, :status => 'valid'
     source.update_attribute :type_taxon, target
   end
