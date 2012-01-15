1af9591b6a3121cb3a04fd66325f0de420bdaf31
Mark Wilden <mark@mwilden.com>
2012-01-15 08:03:03 -0800

    Move Antweb formatting into its own module, too.

--- lib/catalog_formatter.rb
+++ lib/catalog_formatter.rb
@@ -5,30 +5,11 @@ class CatalogFormatter
   extend ActionView::Helpers::TextHelper
   extend ActionView::Helpers::NumberHelper
   extend ActionView::Context
+
+  extend Formatter
   extend Catalog::StatisticsFormatter
   extend Catalog::IndexFormatter
-  extend Formatter
-
-  # AntWeb
-  def self.format_taxonomic_history_for_antweb taxon
-    string = taxon.taxonomic_history
-    string << format_homonym_replaced_for_antweb(taxon)
-    string.html_safe if string
-  end
-
-  def self.format_homonym_replaced_for_antweb taxon
-    homonym_replaced = taxon.homonym_replaced
-    return '' unless homonym_replaced
-    label_and_classes = taxon_label_and_css_classes taxon, :uppercase => true
-    span = content_tag('span', label_and_classes[:label], :class => label_and_classes[:css_classes])
-    string = %{<p class="taxon_subsection_header">Homonym replaced by #{span}</p>}
-    string << %{<div id="#{homonym_replaced.id}">#{homonym_replaced.taxonomic_history}</div>}
-    string
-  end
-
-  def self.format_taxonomic_history_with_statistics_for_antweb taxon, options = {}
-    format_taxon_statistics(taxon, options) + format_taxonomic_history_for_antweb(taxon)
-  end
+  extend Catalog::AntwebFormatter
 
   def self.taxon_label_and_css_classes taxon, options = {}
     fossil_symbol = taxon.fossil? ? "&dagger;" : ''
