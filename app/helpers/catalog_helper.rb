78ebce3fbb68e15dad4bbf5ca293f6f51eedf5a6
Mark Wilden <mark@mwilden.com>
2012-01-15 07:44:49 -0800

    Break up CatalogHelper into that and IndexHelper and BrowserHelper

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,57 +1,23 @@
 # coding: UTF-8
 require 'snake'
+require 'catalog/index_helper'
+require 'catalog/browser_helper'
 
 module CatalogHelper
-
-  def index_column_link rank, taxon, selected_taxon, column_selections = {}
-    if taxon == 'none'
-      classes = 'valid'
-      classes << ' selected' if taxon == selected_taxon
-      if rank == :subfamily
-        link_to "(no subfamily)", "/catalog/index?subfamily=none", :class => classes
-      elsif rank == :tribe
-        link_to "(no tribe)", "/catalog/index?subfamily=#{column_selections[:subfamily].id}&tribe=none", :class => classes
-      end
-    else
-      label_and_classes = CatalogFormatter.taxon_label_and_css_classes taxon,
-        :selected => taxon == selected_taxon
-      link_to label_and_classes[:label], index_catalog_path(taxon, column_selections.merge(rank => taxon)),
-        :class => label_and_classes[:css_classes]
-    end
-  end
-
-  def hide_link name, selected, column_selections
-    hide_param = "hide_#{name}".to_sym
-    link_to 'hide', index_catalog_path(selected, column_selections.merge(hide_param => true)), :class => :hide
-  end
-
-  def show_child_link params, name, selected, column_selections
-    hide_child_param = "hide_#{name}".to_sym
-    return unless params[hide_child_param]
-    link_to "show #{name}", index_catalog_path(selected, column_selections.merge(hide_child_param => nil))
-  end
+  include Catalog::IndexHelper
+  include Catalog::BrowserHelper
 
   def status_labels
     CatalogFormatter.status_labels
   end
 
-  def format_statistics statistics, include_invalid = true
-    CatalogFormatter.format_statistics statistics, :include_invalid => include_invalid
+  def search_selector current_search_type
+    select_tag :search_type,
+      options_for_select(['matching', 'beginning with', 'containing'], current_search_type || 'beginning with')
   end
 
-  def snake_taxon_columns items
-    column_count = items.count / 26.0
-    css_class = 'taxon_item'
-    if column_count < 1
-      column_count = 1
-    else
-      column_count = column_count.ceil
-    end
-    if column_count >= 4
-      column_count = 4
-      css_class << ' teensy'
-    end
-    return items.snake(column_count), css_class
+  def format_statistics statistics, include_invalid = true
+    CatalogFormatter.format_statistics statistics, :include_invalid => include_invalid
   end
 
   def make_catalog_search_results_columns items
@@ -59,41 +25,4 @@ module CatalogHelper
     items.snake column_count
   end
 
-  def make_index_groups taxa, max_row_count, abbreviated_length
-    items_per_row = (taxa.count.to_f / max_row_count).ceil
-    return [] if items_per_row.zero?
-    groups = taxa.sort_by(&:name).in_groups_of(items_per_row, false)
-    any_groups_with_more_than_one_member = false
-    groups.inject([]) do |label_groups, group|
-      result = {:id => group.first.id}
-      label_and_classes = CatalogFormatter.taxon_label_and_css_classes group.first
-      any_groups_with_more_than_one_member ||= group.size > 1
-      if any_groups_with_more_than_one_member
-        if group.size > 1
-          result[:label] = "#{group.first.name[0, abbreviated_length]}-#{group.last.name[0, abbreviated_length]}"
-          result[:css_classes] = CatalogFormatter.css_classes_for_rank(group.first).join ' '
-        else
-          result.merge! label_and_classes
-          result[:css_classes] = CatalogFormatter.css_classes_for_rank(group.first).join ' '
-        end
-      else
-        result.merge! label_and_classes
-      end
-      label_groups << result
-    end
-  end
-
-  def taxon_header taxon, options = {}
-    label_and_css_classes = CatalogFormatter.taxon_label_and_css_classes taxon, :uppercase => true
-    if options[:link]
-      (taxon.rank.capitalize + ' ' + link_to(label_and_css_classes[:label], browser_catalog_path(taxon, options[:search_params]), :class => label_and_css_classes[:css_classes])).html_safe
-    else
-      (taxon.rank.capitalize + ' ' + content_tag('span', label_and_css_classes[:label], :class => label_and_css_classes[:css_classes])).html_safe
-    end
-  end
-
-  def search_selector current_search_type
-    select_tag :search_type,
-      options_for_select(['matching', 'beginning with', 'containing'], current_search_type || 'beginning with')
-  end
 end
