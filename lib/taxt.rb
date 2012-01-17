a0cf1cc00d0363ba6ea8505eb124ef9608b63a6c
Mark Wilden <mark@mwilden.com>
2012-01-17 12:33:37 -0800

    wip

--- lib/taxt.rb
+++ lib/taxt.rb
@@ -1,28 +1,34 @@
 module Taxt
 
-  def self.interpolate taxt, user = nil
+  ################################
+  def self.to_string taxt, user = nil
+    decode taxt, user
+  end
+
+  def self.decode taxt, user = nil
     return '' unless taxt
     taxt.gsub /{ref (\d+)}/ do |ref|
       ReferenceFormatter.format_interpolation(Reference.find($1), user) rescue ref
     end
   end
 
-  def self.unparseable string
+  ################################
+  def self.encode_unparseable string
     "{? #{string}}"
   end
 
-  def self.reference reference
+  def self.encode_reference reference
     "{ref #{reference.id}}"
   end
 
-  def self.taxon_name data
+  def self.encode_taxon_name data
     fossil_indicator = data[:fossil] ? '&dagger;' : ''
 
-    if data[:family_name] && data[:suborder]
-      return "#{fossil_indicator}#{data[:family_name]} (#{data[:suborder]})"
+    if data[:family_name] && data[:suborder_name]
+      return "#{fossil_indicator}#{data[:family_name]} (#{data[:suborder_name]})"
     end
 
-    key = [:order_name, :suborder, :family_name, :family_or_subfamily_name, :tribe_name, :subtribe_name, :collective_group_name, :genus_name, :subgenus_name, :species_name, :subspecies_name].find do |key|
+    key = [:order_name, :suborder_name, :family_name, :family_or_subfamily_name, :tribe_name, :subtribe_name, :collective_group_name, :genus_name, :subgenus_name, :species_name, :subspecies_name].find do |key|
       data[key]
     end
     italicize = [:collective_group_name, :genus_name, :subgenus_name, :species_name, :subspecies_name].include? key
--- spec/lib/taxt_spec.rb
+++ spec/lib/taxt_spec.rb
@@ -5,27 +5,27 @@ describe Taxt do
 
   describe "Encodings" do
     it "should encode an unparseable string" do
-      Taxt.unparseable('foo').should == '{? foo}'
+      Taxt.encode_unparseable('foo').should == '{? foo}'
     end
     it "should encode a reference" do
       reference = Factory :book_reference
-      Taxt.reference(reference).should == "{ref #{reference.id}}"
+      Taxt.encode_reference(reference).should == "{ref #{reference.id}}"
     end
     it "should put italics back around taxon names" do
-      Taxt.taxon_name(genus_name: 'Atta').should == "<i>Atta</i>"
+      Taxt.encode_taxon_name(genus_name: 'Atta').should == "<i>Atta</i>"
     end
     it "should put a dagger in front" do
-      Taxt.taxon_name(genus_name:'Atta', fossil:true).should == "<i>&dagger;Atta</i>"
+      Taxt.encode_taxon_name(genus_name:'Atta', fossil:true).should == "<i>&dagger;Atta</i>"
     end
   end
 
   describe "Interpolation" do
 
     it "should leave alone a string without fields" do
-      Taxt.interpolate('foo').should == 'foo'
+      Taxt.to_string('foo').should == 'foo'
     end
     it "should handle nil" do
-      Taxt.interpolate(nil).should == ''
+      Taxt.to_string(nil).should == ''
     end
     it "should format a ref" do
       reference = Factory :article_reference
@@ -33,17 +33,17 @@ describe Taxt do
       key_stub = stub
       reference.should_receive(:key).and_return key_stub
       key_stub.should_receive(:to_link).and_return('foo')
-      Taxt.interpolate("{ref #{reference.id}}").should == 'foo'
+      Taxt.to_string("{ref #{reference.id}}").should == 'foo'
     end
     it "should not freak if the ref is malformed" do
-      Taxt.interpolate("{ref sdf}").should == '{ref sdf}'
+      Taxt.to_string("{ref sdf}").should == '{ref sdf}'
     end
     it "should not freak if the ref points to a reference that doesn't exist" do
-      Taxt.interpolate("{ref 12345}").should == '{ref 12345}'
+      Taxt.to_string("{ref 12345}").should == '{ref 12345}'
     end
     it "should handle a MissingReference" do
       reference = Factory :missing_reference, :citation => 'Latreille, 1809'
-      Taxt.interpolate("{ref #{reference.id}}").should == 'Latreille, 1809'
+      Taxt.to_string("{ref #{reference.id}}").should == 'Latreille, 1809'
     end
 
   end
