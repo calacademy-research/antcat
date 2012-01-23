1196a124af384325004e7d528ec1c4257bdaa6b6
Mark Wilden <mark@mwilden.com>
2012-01-23 08:14:55 -0800

    Import tribe

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -22,4 +22,30 @@ class Tribe < Taxon
     subfamily.tribes
   end
 
+  def self.import data
+    transaction do
+      protonym = Protonym.import data[:protonym]
+
+      attributes = {
+        name: data[:name],
+        fossil: data[:fossil],
+        status: 'valid',
+        protonym: protonym,
+      }
+      if data[:type_genus]
+        type_genus_taxt = Bolton::Catalog::TextToTaxt.convert(data[:type_genus][:texts])
+        attributes[:type_taxon_taxt] = type_genus_taxt
+      end
+      tribe = create! attributes
+      data[:taxonomic_history].each do |item|
+        tribe.taxonomic_history_items.create! taxt: item
+      end
+
+      type_genus = data[:type_genus]
+      ForwardReference.create! source_id: tribe.id, source_attribute: :type_taxon,
+        target_name: type_genus[:genus_name], fossil: type_genus[:fossil] if type_genus
+
+      tribe
+    end
+  end
 end
