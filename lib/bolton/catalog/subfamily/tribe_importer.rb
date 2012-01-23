a8bc372ddc68c5e07b4c1bec5d1c0917f775f952
Mark Wilden <mark@mwilden.com>
2012-01-23 09:16:55 -0800

    Import tribe

--- lib/bolton/catalog/subfamily/tribe_importer.rb
+++ lib/bolton/catalog/subfamily/tribe_importer.rb
@@ -27,48 +27,44 @@ class Bolton::Catalog::Subfamily::Importer < Bolton::Catalog::Importer
   def parse_tribe subfamily
     Progress.method
 
-    name = @parse_result[:name]
-    fossil = @parse_result[:fossil]
+    name = consume(:tribe_header)[:name]
+    headline = consume :family_group_headline
+    fossil = headline[:protonym][:fossil]
+    history = parse_taxonomic_history
 
-    parse_next_line
-    expect :family_group_headline
-    taxonomic_history = @paragraph
-
-    parse_next_line
-    taxonomic_history << parse_tribe_taxonomic_history
-
-    tribe = Tribe.find_by_name(name)
-    raise "Tribe #{name} doesn't exist" unless tribe
+    tribe = Tribe.import(
+      name: name,
+      fossil: fossil,
+      protonym: headline[:protonym],
+      type_genus: headline[:type_genus],
+      taxonomic_history: history,
+    )
+    Progress.info "Created #{tribe.name}"
 
     # genera lists can appear before or after junior synonyms
-    taxonomic_history << parse_genera_lists(:tribe, :subfamily => subfamily, :tribe => tribe)
-    taxonomic_history << parse_junior_synonyms_of_tribe(tribe)
-    taxonomic_history << parse_genera_lists(:tribe, :subfamily => subfamily, :tribe => tribe)
-    taxonomic_history << parse_ichnotaxa_list(:subfamily => subfamily, :tribe => tribe)
-    taxonomic_history << parse_references_sections(:references_section_header, :see_under_references_section_header)
-    taxonomic_history << parse_see_also_references_section
+    parse_genera_lists :tribe, subfamily: subfamily, tribe: tribe
+    parse_junior_synonyms_of_tribe tribe
+    parse_genera_lists :tribe, subfamily: subfamily, tribe: tribe
+    parse_ichnotaxa_list subfamily: subfamily, tribe: tribe
+    parse_references_sections :references_section_header, :see_under_references_section_header
+    parse_see_also_references_section
 
     parse_genera
     parse_genera_incertae_sedis_in_tribe
     parse_ichnotaxa
 
-    tribe.reload.update_attributes :taxonomic_history => clean_taxonomic_history(taxonomic_history)
   end
 
   def parse_ichnotaxa_list parents
-    return '' unless @type == :ichnotaxa_list
-    parsed_text = @paragraph
+    return unless @type == :ichnotaxa_list
     #do something with ichnogenus
     parse_next_line
-    parsed_text
   end
 
   def parse_see_also_references_section
-    return '' unless @type == :see_also_references_section_header
+    return unless @type == :see_also_references_section_header
     Progress.method
-    parsed_text = @paragraph
     parse_next_line
-    parsed_text
   end
 
   def parse_genera_incertae_sedis_in_tribe
@@ -101,28 +97,22 @@ class Bolton::Catalog::Subfamily::Importer < Bolton::Catalog::Importer
   end
 
   def parse_junior_synonyms_of_tribe tribe
-    return '' unless @type == :junior_synonyms_of_tribe_header
+    return unless @type == :junior_synonyms_of_tribe_header
     Progress.method
 
-    parsed_text = @paragraph
     parse_next_line
-
-    parsed_text << parse_junior_synonym_of_tribe(tribe) while @type == :family_group_headline
-
-    parsed_text
+    parse_junior_synonym_of_tribe(tribe) while @type == :family_group_headline
   end
 
   def parse_junior_synonym_of_tribe tribe
     Progress.method
-    parsed_text = ''
-    name = @parse_result[:tribe_name] || @parse_result[:subtribe_name] || @parse_result[:family_or_subfamily_name]
-    fossil = @parse_result[:fossil]
-    taxonomic_history = @paragraph
+    #name = @parse_result[:tribe_name] || @parse_result[:subtribe_name] || @parse_result[:family_or_subfamily_name]
+    #fossil = @parse_result[:fossil]
+    #history = @paragraph
     parse_next_line
-    taxonomic_history << parse_tribe_taxonomic_history
-    tribe = Tribe.create! :name => name, :fossil => fossil, :status => 'synonym', :synonym_of => tribe,
-                          :subfamily => tribe.subfamily, :taxonomic_history => clean_taxonomic_history(taxonomic_history)
-    parsed_text << taxonomic_history
+    parse_tribe_taxonomic_history
+    #tribe = Tribe.create! :name => name, :fossil => fossil, :status => 'synonym', :synonym_of => tribe,
+                          #:subfamily => tribe.subfamily, :taxonomic_history => clean_taxonomic_history(history)
   end
 
 end
