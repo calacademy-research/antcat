b5046eb360345df6a038082272a5384be85f9dc6
Mark Wilden <mark@mwilden.com>
2012-01-23 08:14:34 -0800

    Fixup tribe

--- app/models/forward_reference.rb
+++ app/models/forward_reference.rb
@@ -8,13 +8,22 @@ class ForwardReference < ActiveRecord::Base
   def fixup
     source = Taxon.find source_id
     target = case source
-      when Family, Subfamily
-        subfamily_id = source.kind_of?(Family) ? nil : source_id
-        Genus.create_from_fixup name: target_name, subfamily_id: subfamily_id, fossil: fossil
+
+      when Family
+        Genus.create_from_fixup name: target_name, subfamily_id: nil, fossil: fossil
+
+      when Subfamily
+        Genus.create_from_fixup name: target_name, subfamily_id: source_id, fossil: fossil
+
+      when Tribe
+        Genus.create_from_fixup name: target_name, tribe_id: source_id, fossil: fossil
+
       when Genus
         Species.create_from_fixup name: target_name, genus_id: source_id, fossil: fossil
+
       else raise
       end
+
     source.update_attributes type_taxon: target, type_taxon_name: CatalogFormatter::fossil(target_name, fossil)
   end
 
