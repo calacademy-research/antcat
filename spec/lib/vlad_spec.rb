63ecf47787c8450053eac4ed986f111db9745aaa
Mark Wilden <mark@mwilden.com>
2012-01-23 12:54:38 -0800

    Vlad show genera with tribes but not subfamilies

--- spec/lib/vlad_spec.rb
+++ spec/lib/vlad_spec.rb
@@ -6,4 +6,13 @@ describe Vlad do
     Vlad.idate
   end
 
+  it "should show genera with tribes but not subfamilies" do
+    tribe = Factory :tribe
+    genus_with_tribe_but_not_subfamily = Factory :genus, subfamily: nil, tribe: tribe
+    genus_with_tribe_and_subfamily = Factory :genus, subfamily: tribe.subfamily, tribe: tribe
+    genus_with_subfamily_but_not_tribe = Factory :genus, subfamily: tribe.subfamily, tribe: nil
+    results = Vlad.idate[:genera_with_tribes_but_not_subfamilies]
+    results.count.should == 1
+    results.first.should == genus_with_tribe_but_not_subfamily
+  end
 end
