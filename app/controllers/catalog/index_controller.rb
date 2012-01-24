011956a119d11465d0816bf13f80c8731d9bcc87
Mark Wilden <mark@mwilden.com>
2012-01-23 18:12:53 -0800

    Hide tribes. Rename column_selections.

--- app/controllers/catalog/index_controller.rb
+++ app/controllers/catalog/index_controller.rb
@@ -8,8 +8,13 @@ class Catalog::IndexController < CatalogController
 
     @subfamilies = ::Subfamily.ordered_by_name
 
-    if params[:id].blank?
-      @taxon = Family.first
+    params[:id] = Family.first.id if params[:id].blank?
+
+    @taxon = Taxon.find params[:id]
+
+    case @taxon
+
+    when Family
       @subfamily = params[:subfamily]
       @tribe = params[:tribe]
       if @subfamily == 'none'
@@ -30,54 +35,74 @@ class Catalog::IndexController < CatalogController
       @specieses = nil
       @species = nil
 
-    else
-      @taxon = Taxon.find params[:id]
-      case @taxon
+    when Subfamily
+      @subfamily = @taxon
 
-      when Subfamily
-        @subfamily = @taxon
+      if params[:hide_tribes]
+        @genera = @subfamily.genera
+      else
+        @genera = nil
         @tribe = nil
         @tribes = @subfamily.tribes
-        @genus = nil
-        @genera = nil
-        @species = nil
-        @specieses = nil
-
-      when Tribe
-        @tribe = @taxon
-        @subfamily = @tribe.subfamily
-        @tribes = @tribe.siblings
-        @genus = nil
-        @genera = @tribe.genera
-        @species = nil
-        @specieses = nil
-
-      when Genus
-        @genus = @taxon
-        @subfamily = @genus.subfamily ? @genus.subfamily : 'none'
-        @tribe = @genus.tribe ? @genus.tribe : 'none'
-        @tribes = @genus.tribe ? @tribe.siblings : nil
-        @genera = @genus.siblings
-        @species = nil
-        @specieses = @genus.species
-
-      when Species
-        @species = @taxon
-        @genus = @species.genus
-        @subfamily = @genus.subfamily ? @genus.subfamily : 'none'
-        @tribe = @genus.tribe ? @genus.tribe : 'none'
-        @tribes = @genus.tribe ? @tribe.siblings : nil
-        @genera = @genus.siblings
-        @specieses = @species.siblings
       end
 
+      @genus = nil
+      @species = nil
+      @specieses = nil
+
+    when Tribe
+      @tribe = @taxon
+
+      if params[:hide_tribes]
+        @subfamily = @tribe.subfamily
+        @genera = nil
+      else
+        @subfamily = @tribe.subfamily
+        @tribes = @tribe.siblings
+        @genera = @tribe.genera
+      end
+
+      @genus = nil
+      @species = nil
+      @specieses = nil
+
+    when Genus
+      @genus = @taxon
+      @subfamily = @genus.subfamily ? @genus.subfamily : 'none'
+
+      if params[:hide_tribes]
+        @genera = @subfamily == 'none' ? Genus.without_subfamily : @subfamily.genera
+      else
+        @genera = @genus.siblings
+        @tribe = @genus.tribe ? @genus.tribe : 'none'
+        @tribes = @subfamily == 'none' ? nil : @subfamily.tribes
+      end
+
+      @species = nil
+      @specieses = @genus.species
+
+    when Species
+      @species = @taxon
+      @genus = @species.genus
+      @subfamily = @genus.subfamily ? @genus.subfamily : 'none'
+
+      if params[:hide_tribes]
+        @genera = @subfamily == 'none' ? Genus.without_subfamily : @subfamily.genera
+      else
+        @genera = @genus.siblings
+        @tribe = @genus.tribe ? @genus.tribe : 'none'
+        @tribes = @subfamily == 'none' ? nil : @subfamily.tribes
+      end
+
+      @specieses = @species.siblings
     end
 
-    @column_selections = {
-      subfamily: @subfamily, tribe: @tribe, genus: @genus, species: @species,
-      q: params[:q], search_type: params[:search_type]
+    @page_parameters = {
+      id: params[:id], subfamily: @subfamily, tribe: @tribe, genus: @genus, species: @species,
+      q: params[:q], search_type: params[:search_type],
+      hide_tribes: params[:hide_tribes]
     }
+
   end
 
 end
-
--- app/helpers/catalog/index_helper.rb
+++ app/helpers/catalog/index_helper.rb
@@ -2,31 +2,34 @@
 require 'snake'
 
 module Catalog::IndexHelper
-  def index_column_link rank, taxon, selected_taxon, column_selections = {}
+  def index_column_link rank, taxon, selected_taxon, page_parameters = {}
     if taxon == 'none'
       classes = 'valid'
       classes << ' selected' if taxon == selected_taxon
       if rank == :subfamily
-        link_to "(no subfamily)", "/catalog/index?subfamily=none", :class => classes
+        parameters = "subfamily=none"
+        parameters << "&hide_tribes=true" if page_parameters[:hide_tribes]
+        link_to "(no subfamily)", "/catalog/index?#{parameters}", :class => classes
       elsif rank == :tribe
-        link_to "(no tribe)", "/catalog/index?subfamily=#{column_selections[:subfamily].id}&tribe=none", :class => classes
+        parameters = "subfamily=#{page_parameters[:subfamily].id}&tribe=none"
+        link_to "(no tribe)", "/catalog/index?#{parameters}", :class => classes
       end
     else
       label = CatalogFormatter.taxon_label taxon
       css_classes = CatalogFormatter.taxon_css_classes taxon, :selected => taxon == selected_taxon
-      link_to label, index_catalog_path(taxon, column_selections.merge(rank => taxon)), :class => css_classes
+      link_to label, index_catalog_path(taxon, page_parameters.merge(rank => taxon)), :class => css_classes
     end
   end
 
-  def hide_link name, selected, column_selections
+  def hide_link name, selected, page_parameters
     hide_param = "hide_#{name}".to_sym
-    link_to 'hide', index_catalog_path(selected, column_selections.merge(hide_param => true)), :class => :hide
+    link_to 'hide', index_catalog_path(selected, page_parameters.merge(hide_param => true)), :class => :hide
   end
 
-  def show_child_link params, name, selected, column_selections
+  def show_child_link params, name, selected, page_parameters
     hide_child_param = "hide_#{name}".to_sym
     return unless params[hide_child_param]
-    link_to "show #{name}", index_catalog_path(selected, column_selections.merge(hide_child_param => nil))
+    link_to "show #{name}", index_catalog_path(selected, page_parameters.merge(hide_child_param => nil))
   end
 
   def snake_taxon_columns items
