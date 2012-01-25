64d276ba38d5592c7a389c52e99aa1e8cb99a260
Mark Wilden <mark@mwilden.com>
2012-01-25 09:11:16 -0800

    Refactor

--- app/controllers/catalog/index_controller.rb
+++ app/controllers/catalog/index_controller.rb
@@ -6,11 +6,10 @@ class Catalog::IndexController < CatalogController
 
     #@current_path = index_catalog_path
 
-    @subfamilies = ::Subfamily.ordered_by_name
-
     params[:id] = Family.first.id if params[:id].blank?
-
     @taxon = Taxon.find params[:id]
+
+    @subfamilies = ::Subfamily.ordered_by_name
 
     case @taxon
 
@@ -19,81 +18,40 @@ class Catalog::IndexController < CatalogController
       @tribe = params[:tribe]
       if @subfamily == 'none'
         @genera = Genus.without_subfamily
-        @genus = nil
       elsif @tribe == 'none'
         @subfamily = Taxon.find @subfamily
         @taxon = @subfamily
         @tribes = @subfamily.tribes
         @genera = @subfamily.genera.without_tribe
-        @genus = nil
-      else
-        @tribe = nil
-        @tribes = nil
-        @genus = nil
-        @genera = nil
       end
-      @specieses = nil
-      @species = nil
 
     when Subfamily
       @subfamily = @taxon
-
       if params[:hide_tribes]
         @genera = @subfamily.genera
       else
-        @genera = nil
-        @tribe = nil
         @tribes = @subfamily.tribes
       end
 
-      @genus = nil
-      @species = nil
-      @specieses = nil
-
     when Tribe
       @tribe = @taxon
-
-      if params[:hide_tribes]
-        @subfamily = @tribe.subfamily
-        @genera = nil
-      else
-        @subfamily = @tribe.subfamily
+      @subfamily = @tribe.subfamily
+      unless params[:hide_tribes]
         @tribes = @tribe.siblings
         @genera = @tribe.genera
       end
 
-      @genus = nil
-      @species = nil
-      @specieses = nil
-
     when Genus
       @genus = @taxon
       @subfamily = @genus.subfamily ? @genus.subfamily : 'none'
-
-      if params[:hide_tribes]
-        @genera = @subfamily == 'none' ? Genus.without_subfamily : @subfamily.genera
-      else
-        @genera = @genus.siblings
-        @tribe = @genus.tribe ? @genus.tribe : 'none'
-        @tribes = @subfamily == 'none' ? nil : @subfamily.tribes
-      end
-
-      @species = nil
+      setup_genus_parent_columns
       @specieses = @genus.species
 
     when Species
       @species = @taxon
       @genus = @species.genus
       @subfamily = @genus.subfamily ? @genus.subfamily : 'none'
-
-      if params[:hide_tribes]
-        @genera = @subfamily == 'none' ? Genus.without_subfamily : @subfamily.genera
-      else
-        @genera = @genus.siblings
-        @tribe = @genus.tribe ? @genus.tribe : 'none'
-        @tribes = @subfamily == 'none' ? nil : @subfamily.tribes
-      end
-
+      setup_genus_parent_columns
       @specieses = @species.siblings
     end
 
@@ -103,6 +61,16 @@ class Catalog::IndexController < CatalogController
       hide_tribes: params[:hide_tribes]
     }
 
+  end
+
+  def setup_genus_parent_columns
+    if params[:hide_tribes]
+      @genera = @subfamily == 'none' ? Genus.without_subfamily : @subfamily.genera
+    else
+      @genera = @genus.siblings
+      @tribe = @genus.tribe ? @genus.tribe : 'none'
+      @tribes = @subfamily == 'none' ? nil : @subfamily.tribes
+    end
   end
 
 end
