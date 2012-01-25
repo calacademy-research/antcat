50848d0fac500c2818b5f0c9142dc3c76d9cdd1a
Mark Wilden <mark@mwilden.com>
2012-01-25 09:46:41 -0800

    ReferenceSection migration

---
+++ db/migrate/20120125173943_add_reference_sections.rb
@@ -0,0 +1,16 @@
+class AddReferenceSections < ActiveRecord::Migration
+  def up
+    create_table :reference_sections, force: true do |t|
+      t.integer :taxon_id
+      t.integer :position
+      t.string  :title
+      t.text    :references
+    end
+    add_index :reference_sections, [:taxon_id, :position]
+  end
+
+  def down
+    remove_index :reference_sections, :column => [:taxon_id, :position]
+    drop_table :reference_sections
+  end
+end
