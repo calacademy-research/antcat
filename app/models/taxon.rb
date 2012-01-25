4901fb2b8b5f3cd9f5b7e87ddee7a0233797d1b9
Mark Wilden <mark@mwilden.com>
2012-01-25 09:50:36 -0800

    ReferenceSection model

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -10,6 +10,7 @@ class Taxon < ActiveRecord::Base
   belongs_to  :type_taxon, :class_name => 'Taxon'
   belongs_to  :homonym_replaced_by, :class_name => 'Taxon'
   has_many    :taxonomic_history_items, :order => :position
+  has_many    :reference_sections, :order => :position
 
   validates_presence_of :name
 
