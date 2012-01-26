da344568d00278838329b89729288caeb3f85a63
Mark Wilden <mark@mwilden.com>
2012-01-26 08:08:08 -0800

    Add timestamps (migration)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120125173943) do
+ActiveRecord::Schema.define(:version => 20120126155424) do
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
@@ -142,10 +142,12 @@ ActiveRecord::Schema.define(:version => 20120125173943) do
   add_index "reference_documents", ["reference_id"], :name => "documents_reference_id_idx"
 
   create_table "reference_sections", :force => true do |t|
-    t.integer "taxon_id"
-    t.integer "position"
-    t.string  "title"
-    t.text    "references"
+    t.integer  "taxon_id"
+    t.integer  "position"
+    t.string   "title"
+    t.text     "references"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "reference_sections", ["taxon_id", "position"], :name => "index_reference_sections_on_taxon_id_and_position"
