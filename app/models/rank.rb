bbe16fe0191067a39f5ae954be57cb427b5ecf3c
Mark Wilden <mark@mwilden.com>
2012-01-28 11:06:53 -0800

    More Rank methods

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -2,23 +2,38 @@
 class Rank
   attr_reader :hash
 
-  def initialize(hash) @hash = hash end
+  def initialize hash
+    @hash = hash
+  end
 
   def to_sym *options
     options.include?(:plural) ? @hash[:plural_symbol] : @hash[:symbol]
   end
 
   def to_s *options
+    numeric_argument = options.find {|option| option.kind_of? Numeric}
+    options << :plural if numeric_argument && numeric_argument > 1
+
     s = (options.include?(:plural) ? @hash[:plural_string] : @hash[:string]).dup
     s.downcase! unless options.include? :capitalized
     s
   end
 
-  def to_class; @hash[:klass] end
+  def to_class
+    @hash[:klass]
+  end
 
-  def includes?(identifier) @hash.values.include? identifier end
+  def includes? identifier
+    @hash.values.include? identifier
+  end
 
-  def self.[](identifier) ranks.find {|rank| rank.includes? identifier} end
+  def self.find identifier
+    identifier = identifier.class if identifier.kind_of? Taxon
+    identifier = identifier.first.class if identifier.kind_of? Enumerable
+
+    ranks.find {|rank| rank.includes? identifier}
+  end
+
   def self.ranks
     @_ranks ||= [
       Rank.new(string: 'Tribe', plural_string: 'Tribes', symbol: :tribe, plural_symbol: :tribes, klass: Tribe),
