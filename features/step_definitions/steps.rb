9ceaf27bd5cd232f4eae88bd7ac6179c5f237e5e
Mark Wilden <mark@mwilden.com>
2012-01-27 17:03:03 -0800

    Merge migrations, Bolton edit change, feature/step fix from topic.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -217,7 +217,7 @@ end
 Given 'I log in' do
   step 'I go to the main page'
   User.delete_all
-  @user = Factory :user
+  @user = Factory :user, :email => 'mark@example.com'
   click_link "Login"
   step %{I fill in "user_email" with "#{@user.email}"}
   step %{I fill in "user_password" with "#{@user.password}"}
