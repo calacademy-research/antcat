887a6067b02d43c2094cdbe9882f398690b2d35c
Mark Wilden <mark@mwilden.com>
2012-01-29 12:56:21 -0800

    Subgenus.import

--- spec/models/subgenus_spec.rb
+++ spec/models/subgenus_spec.rb
@@ -17,4 +17,47 @@ describe Subgenus do
     end
   end
 
+  describe "Importing" do
+    it "should work" do
+      genus = Factory :genus
+      reference = Factory :article_reference, :bolton_key_cache => 'Latreille 1809'
+
+      subgenus = Subgenus.import(
+        genus: genus,
+        name: 'Atta',
+        fossil: true,
+        protonym: {subgenus_name: "Atta",
+                   authorship: [{author_names: ["Latreille"], year: "1809", pages: "124"}]},
+        type_species: {subgenus_name: 'Atta', species_epithet: 'major',
+                          texts: [{text: [{phrase: ', by monotypy'}]}]},
+        taxonomic_history: ["Atta as subgenus", "Atta as species"]
+      )
+
+      subgenus.name.should == 'Atta'
+      subgenus.should_not be_invalid
+      subgenus.should be_fossil
+      subgenus.genus.should == genus
+      genus.subgenera.map(&:id).should == [subgenus.id]
+      #subgenus.subfamily.should == genus.subfamily
+      subgenus.taxonomic_history_items.map(&:taxt).should == ['Atta as subgenus', 'Atta as species']
+      subgenus.type_taxon_taxt.should == ', by monotypy'
+
+      protonym = subgenus.protonym
+      protonym.name.should == 'Atta'
+
+      authorship = protonym.authorship
+      authorship.pages.should == '124'
+
+      authorship.reference.should == reference
+
+      ForwardReference.fixup
+
+      type = subgenus.reload.type_taxon
+      type.name.should == 'major'
+      type.subgenus.should == subgenus
+      type.genus.should == subgenus.genus
+      type.subfamily.should == subgenus.genus.subfamily
+    end
+
+  end
 end
