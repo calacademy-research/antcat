f52c4be859f1752f648736f389d4828c124b26c1
Mark Wilden <mark@mwilden.com>
2012-01-28 17:16:10 -0800

    Store lowercase

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -16,7 +16,7 @@ class Rank
     options << :plural if numeric_argument && numeric_argument > 1
 
     s = (options.include?(:plural) ? @hash[:plural_string] : @hash[:string]).dup
-    s.downcase! unless options.include? :capitalized
+    s = s.titleize if options.include? :capitalized
     s
   end
 
@@ -33,6 +33,7 @@ class Rank
     identifier = identifier.class if identifier.kind_of? Taxon
     identifier = identifier.first.class if identifier.kind_of? Enumerable
     identifier = identifier.first.class if identifier.kind_of? ActiveRecord::Relation
+    identifier = identifier.downcase if identifier.kind_of? String
 
     ranks.find {|rank| rank.includes? identifier} or raise "Couldn't find #{identifier}"
   end
@@ -40,13 +41,13 @@ class Rank
 
   def self.ranks
     @_ranks ||= [
-      Rank.new(string: 'Family',     plural_string: 'Families',     symbol: :family,     plural_symbol: :families,    klass: Family),
-      Rank.new(string: 'Subfamily',  plural_string: 'Subfamilies',  symbol: :subfamily,  plural_symbol: :subfamilies, klass: Subfamily),
-      Rank.new(string: 'Tribe',      plural_string: 'Tribes',       symbol: :tribe,      plural_symbol: :tribes,      klass: Tribe),
-      Rank.new(string: 'Genus',      plural_string: 'Genera',       symbol: :genus,      plural_symbol: :genera,      klass: Genus),
-      Rank.new(string: 'Subgenus',   plural_string: 'Subgenera',    symbol: :subgenus,   plural_symbol: :subgenera,   klass: Subgenus),
-      Rank.new(string: 'Species',    plural_string: 'Species',      symbol: :species,    plural_symbol: :species,     klass: Species),
-      Rank.new(string: 'Subspecies', plural_string: 'Subspecies',   symbol: :subspecies, plural_symbol: :subspecies,  klass: Subspecies),
+      Rank.new(string: 'family',     plural_string: 'families',     symbol: :family,     plural_symbol: :families,    klass: Family),
+      Rank.new(string: 'subfamily',  plural_string: 'subfamilies',  symbol: :subfamily,  plural_symbol: :subfamilies, klass: Subfamily),
+      Rank.new(string: 'tribe',      plural_string: 'tribes',       symbol: :tribe,      plural_symbol: :tribes,      klass: Tribe),
+      Rank.new(string: 'genus',      plural_string: 'genera',       symbol: :genus,      plural_symbol: :genera,      klass: Genus),
+      Rank.new(string: 'subgenus',   plural_string: 'subgenera',    symbol: :subgenus,   plural_symbol: :subgenera,   klass: Subgenus),
+      Rank.new(string: 'species',    plural_string: 'species',      symbol: :species,    plural_symbol: :species,     klass: Species),
+      Rank.new(string: 'subspecies', plural_string: 'subspecies',   symbol: :subspecies, plural_symbol: :subspecies,  klass: Subspecies),
     ]
   end
 end
