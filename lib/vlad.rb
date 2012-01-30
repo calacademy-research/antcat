7b0fabce50b1d8107e616b37825b3c5e0ab73cf9
Mark Wilden <mark@mwilden.com>
2012-01-30 10:00:16 -0800

    Vlad that synonym_of_id matches status

--- lib/vlad.rb
+++ lib/vlad.rb
@@ -2,28 +2,53 @@
 class Vlad
 
   def self.idate show_progress = false
-    Progress.new_init show_progress: show_progress
+    Progress.new_init show_progress: true
+
     results = {}
     results.merge! record_counts
     results.merge! genera_with_tribes_but_not_subfamilies
+    results.merge! taxa_with_mismatched_synonym_and_status
+
     display results
     results
   end
 
   def self.display results
-    results[:record_counts].each do |model_class, count|
+    results_section = results[:record_counts]
+    results_section.each do |model_class, count|
       Progress.puts "#{count.to_s.rjust(7)} #{model_class}"
     end
 
-    results = results[:genera_with_tribes_but_not_subfamilies]
-    unless results.blank?
-      Progress.puts "Genera with tribes but not subfamilies"
-      results.map do |genus|
+    results_section = results[:genera_with_tribes_but_not_subfamilies]
+    Progress.print "Genera with tribes but not subfamilies:"
+    if results_section.blank?
+      Progress.puts " none"
+    else
+      Progress.puts
+      results_section.map do |genus|
         "#{genus.name} (tribe #{genus.tribe.name})"
       end.sort.each do |name|
         Progress.puts name
       end
     end
+
+    results_section = results[:taxa_with_mismatched_synonym_and_status]
+    Progress.print "Taxa with mismatched synonym status and synonym_of_id:"
+    if results_section.blank?
+      Progress.puts " none"
+    else
+      Progress.puts
+      results_section.map do |taxon|
+        "#{taxon.name} #{taxon.status} #{taxon.synonym_of_id}"
+      end.sort.each do |line|
+        Progress.puts line
+      end
+    end
+
+  end
+
+  def self.taxa_with_mismatched_synonym_and_status
+    {taxa_with_mismatched_synonym_and_status: Taxon.where("(status = 'synonym') = (synonym_of_id IS NULL)")}
   end
 
   def self.genera_with_tribes_but_not_subfamilies
