0e05df3c457e15c0f68dfa2cd8a8edc92be1dc10
Mark Wilden <mark@mwilden.com>
2012-01-30 10:00:16 -0800

    Vlad that synonym_of_id matches status

--- spec/lib/vlad_spec.rb
+++ spec/lib/vlad_spec.rb
@@ -15,4 +15,15 @@ describe Vlad do
     results.count.should == 1
     results.first.should == genus_with_tribe_but_not_subfamily
   end
+
+  it "should show taxa whose status doesn't match their synonym_of" do
+    tribe = Factory :tribe
+    no_synonym = Factory :genus, status: 'synonym'
+    no_status = Factory :tribe, synonym_of: tribe
+    ok = Factory :species, synonym_of: Factory(:species), status: 'synonym'
+
+    results = Vlad.idate[:taxa_with_mismatched_synonym_and_status]
+    results.map(&:id).should =~ [no_synonym.id, no_status.id]
+  end
+
 end
