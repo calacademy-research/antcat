2d9ab3bd16d8f309e0e6770ecd25645cca7a9d62
Mark Wilden <mark@mwilden.com>
2012-01-30 17:18:50 -0800

    Add hong to Taxon (MIGRATION)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120129204913) do
+ActiveRecord::Schema.define(:version => 20120131011753) do
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(:version => 20120129204913) do
     t.text     "headline_notes_taxt"
     t.text     "type_taxon_name"
     t.integer  "subgenus_id"
+    t.boolean  "hong"
   end
 
   add_index "taxa", ["genus_id"], :name => "taxa_genus_id_idx"
