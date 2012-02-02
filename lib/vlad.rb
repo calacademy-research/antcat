16445680f6a60b4e7036ca25adf65a34b185753e
Mark Wilden <mark@mwilden.com>
2012-02-01 16:28:43 -0800

    Merge Vlad from topic

--- lib/vlad.rb
+++ lib/vlad.rb
@@ -2,31 +2,74 @@
 class Vlad
 
   def self.idate show_progress = false
-    Progress.new_init show_progress: show_progress,
-                      log_file_directory: 'data/vlad',
-                      log_file_name: 'vlad',
-                      append_to_log_file: !Rails.env.test?
-    results = {}
-    results.merge! record_counts
-    results.merge! genera_with_tribes_but_not_subfamilies
-    display results
-    results
+    Progress.new_init show_progress: show_progress
+
+    @results = {}
+    @results.merge! record_counts
+    @results.merge! genera_with_tribes_but_not_subfamilies
+    @results.merge! taxa_with_mismatched_synonym_and_status
+    @results.merge! taxa_with_mismatched_homonym_and_status
+    @results.merge! duplicates
+
+    display
+    @results
   end
 
-  def self.display results
-    results[:record_counts].each do |model_class, count|
-      Progress.log "#{count.to_s.rjust(7)} #{model_class}"
+  def self.display_results_section section, options = {}
+    options = options.reverse_merge sort: true
+    results_section = @results[section]
+    return unless results_section.present?
+
+    Progress.puts section.to_s.gsub(/_/, ' ').capitalize
+
+    lines = results_section.map do |item|
+      yield item
     end
 
-    results = results[:genera_with_tribes_but_not_subfamilies]
-    unless results.blank?
-      Progress.puts "Genera with tribes but not subfamilies"
-      results.map do |genus|
-        "#{genus.name} (tribe #{genus.tribe.name})"
-      end.sort.each do |name|
-        Progress.puts name
-      end
+    lines.sort! if options[:sort]
+
+    for line in lines
+      Progress.puts '  ' + line
     end
+
+    Progress.puts
+  end
+
+  def self.display
+    Progress.puts
+
+    display_results_section :record_counts, :sort => false do |count|
+      "#{count[:count].to_s.rjust(7)} #{count[:table]}"
+    end
+
+    display_results_section :genera_with_tribes_but_not_subfamilies do |genus|
+      "#{genus.name} (tribe #{genus.tribe.name})"
+    end
+
+    display_results_section :taxa_with_mismatched_synonym_and_status do |taxon|
+      "#{taxon.name} #{taxon.status} #{taxon.synonym_of_id}"
+    end
+
+    display_results_section :taxa_with_mismatched_homonym_and_status do |taxon|
+      "#{taxon.name} #{taxon.status} #{taxon.homonym_replaced_by_id}"
+    end
+
+    display_results_section :duplicates do |duplicate|
+      "#{duplicate[:name]} #{duplicate[:count]}"
+    end
+
+  end
+
+  def self.duplicates
+    {duplicates: Taxon.select('name, COUNT(name) AS count').group(:name, :genus_id).having('COUNT(name) > 1')}
+  end
+
+  def self.taxa_with_mismatched_synonym_and_status
+    {taxa_with_mismatched_synonym_and_status: Taxon.where("(status = 'synonym') = (synonym_of_id IS NULL)")}
+  end
+
+  def self.taxa_with_mismatched_homonym_and_status
+    {taxa_with_mismatched_homonym_and_status: Taxon.where("(status = 'homonym') = (homonym_replaced_by_id IS NULL)")}
   end
 
   def self.genera_with_tribes_but_not_subfamilies
@@ -51,9 +94,8 @@ class Vlad
       Taxon,
       TaxonomicHistoryItem,
       User,
-    ].inject({}) do |counts, table|
-      counts[table] = table.count
-      counts
+    ].map do |table|
+      {table: table.to_s, count: table.count}
     end}
   end
 
