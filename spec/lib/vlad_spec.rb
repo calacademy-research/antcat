1802a13386c3776422af3b268e26b8a96f2c4e9c
Mark Wilden <mark@mwilden.com>
2012-02-01 16:28:43 -0800

    Merge Vlad from topic

--- spec/lib/vlad_spec.rb
+++ spec/lib/vlad_spec.rb
@@ -15,4 +15,31 @@ describe Vlad do
     results.count.should == 1
     results.first.should == genus_with_tribe_but_not_subfamily
   end
+
+  it "should show taxa whose status doesn't match their synonym_of" do
+    tribe = Factory :tribe
+    no_synonym = Factory :genus, status: 'synonym'
+    no_status = Factory :tribe, synonym_of: tribe
+    ok = Factory :species, synonym_of: Factory(:species), status: 'synonym'
+
+    results = Vlad.idate[:taxa_with_mismatched_synonym_and_status]
+    results.map(&:id).should =~ [no_synonym.id, no_status.id]
+  end
+
+  describe "Duplicate checking" do
+
+    it "should show duplicate names" do
+      Factory :genus, :name => 'Eciton'
+      Factory :genus, :name => 'Atta'
+      Factory :genus, :name => 'Atta'
+      Vlad.idate[:duplicates].map {|e| [e.name, e.count]}.should =~ [['Atta', 2]]
+    end
+    it "should be cool with same species name if genus is different" do
+      Factory :species, :name => 'niger'
+      Factory :species, :name => 'major'
+      Factory :species, :name => 'major'
+      Vlad.idate[:duplicates].should be_empty
+    end
+  end
+
 end
