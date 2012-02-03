7e3d852334a09f962ddf59f13f04ea4e7f56b461
Mark Wilden <mark@mwilden.com>
2012-02-03 06:26:37 -0800

    Authors page. Old page -> authors/all.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,11 @@
 # coding: UTF-8
 class AuthorsController < ApplicationController
+
   def index
+    @name = 'Bolton, B.' if params[:q]
+  end
+
+  def all
     respond_to do |format|
       format.json {render :json => AuthorName.search(params[:term]).to_json}
       format.html
