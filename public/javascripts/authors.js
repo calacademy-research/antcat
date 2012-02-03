7adba565c405932fbfd0d84be8c74ba535f36df4
Mark Wilden <mark@mwilden.com>
2012-02-03 08:25:41 -0800

    Help

--- public/javascripts/authors.js
+++ public/javascripts/authors.js
@@ -1,7 +1,15 @@
 $(function() {
+  setupHelp();
   setupAuthorAutocomplete();
 })
 
+function setupHelp() {
+  $('.q.help_icon').qtip({
+    content: "Start entering an author name, then press Enter to choose from the list. Then press Enter to search for the chosen name",
+    show: 'mouseover',
+    hide: 'mouseout',
+  });
+}
 
 function setupAuthorAutocomplete() {
   if (testing)
