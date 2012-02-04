e3c17105de3c2154fa168481d61bbe232d665108
Mark Wilden <mark@mwilden.com>
2012-02-04 09:47:33 -0800

    Help text

--- public/javascripts/authors.js
+++ public/javascripts/authors.js
@@ -5,7 +5,7 @@ $(function() {
 
 function setupHelp() {
   $('.q.help_icon').qtip({
-    content: "Start entering an author name, then press Enter to choose from the list. Then press Enter to search for the chosen name",
+    content: "Type some letters in the author's name, then choose the name from the list.",
     show: 'mouseover',
     hide: 'mouseout',
   });
