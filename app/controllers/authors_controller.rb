dce6f923a81c9dc2640e410251f6f2e25bad14b1
Mark Wilden <mark@mwilden.com>
2012-02-04 16:55:05 -0800

    Close panel, show if author open in another panel. Finished 'multiple author panels' story.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,29 +2,25 @@
 class AuthorsController < ApplicationController
 
   def index
-    # create the other panels
     @panels = []
     @panels = params[:other_terms].inject([]) {|panels, term| panels << {q: term}} if params[:other_terms].present?
-    @panels << {q: params[:q].strip} if params[:q].present?
+    @panels << {q: params[:q].strip} if params[:q].present? if params[:commit] == 'Go'
 
-    # gather the search terms
     all_terms = @panels.map{|panel| panel[:q]}
 
-    # find the author for each panel
     blank_panel_added = false
     for panel in @panels
-      panel[:author] = Author.find_by_names(panel[:q]).first
+      author = Author.find_by_names(panel[:q]).first
+      if @panels.find {|a_panel| a_panel != panel && a_panel[:author] == author}
+        panel[:already_open] = true
+      else
+        panel[:author] = author
+      end
       blank_panel_added ||= !panel[:author]
     end
-
-    # add a blank panel unless there was already a panel with a failed search
     @panels << {} unless blank_panel_added
 
-    # save the other panels' terms in each panel
-    for panel in @panels
-      panel[:other_terms] = all_terms - [panel[:q]]
-    end
-
+    @panels.each {|panel| panel[:other_terms] = all_terms - [panel[:q]]}
   end
 
   def all
