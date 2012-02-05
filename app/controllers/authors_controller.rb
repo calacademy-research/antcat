3b54b3ea98e2c6745c777834ba4693fe0fd55346
Mark Wilden <mark@mwilden.com>
2012-02-05 09:34:16 -0800

    "Merge these authors" UI

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -21,6 +21,18 @@ class AuthorsController < ApplicationController
     @panels << {} unless blank_panel_added
 
     @panels.each {|panel| panel[:other_terms] = all_terms - [panel[:q]]}
+
+    all_names = @panels.select{|panel| panel[:author]}.collect{|panel| panel[:author].names}
+    all_names = all_names.flatten.uniq.map{|name| '"' + name.name + '"'}.sort
+    @all_names_string = case
+    when all_names.count == 0
+    when all_names.count == 1
+      all_names.first
+    when all_names.count == 2
+      all_names.first + ' and ' + all_names.second
+    else
+      all_names[0..-2].join(', ') + ' and ' + all_names.last
+    end
   end
 
   def all
