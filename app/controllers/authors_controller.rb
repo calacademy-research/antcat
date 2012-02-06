74c2d26a53654760e0087c4589687c80c5dfc69a
Mark Wilden <mark@mwilden.com>
2012-02-06 08:00:48 -0800

    Refactor

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,44 +1,45 @@
 # coding: UTF-8
 class AuthorsController < ApplicationController
 
+  def index
+    create_panels
+    add_blank_panel_if_necessary
+  end
+
   def create_panels
-    # default to an empty collection of terms
     params[:terms] ||= []
 
     # if closing a panel, remove its term
     params[:terms].delete params[:term] unless params[:commit]
 
-    # make a panel for each search term and find its author
-    @panels = params[:terms].map do |term|
-      panel = {term: term.strip, author: Author.find_by_names(term).first}
+    @panels = []
+    @authors = []
+    for term in params[:terms]
+      panel = OpenStruct.new term: term.strip, author: Author.find_by_names(term).first
+      @panels << panel
+      next unless panel.author
+      panel.already_open = @authors.include? panel.author
+      panel.author = nil if panel.already_open
+      @authors << panel.author if panel.author
     end
-    @panels_with_authors = @panels.select{|panel| panel[:author]}
-    @authors = @panels_with_authors.map{|panel| panel[:author]}
     @names = @authors.map{|author| author.names}.flatten.map{|author_name| author_name.name}.uniq
   end
 
-  def index
-    create_panels
-
-    # recognize duplicate panels
-    for panel in @panels
-      panel[:already_open] = panel[:author] && @panels.find {|a_panel| a_panel != panel && a_panel[:author] == panel[:author]}
-      panel[:author] = nil if panel[:already_open]
-    end
-
-    # add a blank panel if necessary
-    @panels << {term: ''} unless @panels.find {|panel| !panel[:author]}
+  def add_blank_panel_if_necessary
+    @panels << OpenStruct.new(term: '') unless @panels.find{|panel| !panel.author}
   end
 
+  #######
   def merge
+    term = params[:terms].first
     create_panels
-    term = @panels_with_authors.first[:term]
     Author.merge @authors
     params[:terms] = [term]
     index
     render :index
   end
 
+  #######
   def all
     respond_to do |format|
       format.json {render :json => AuthorName.search(params[:term]).to_json}
