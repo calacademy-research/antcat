314cd797e156c332a613bd4126fdbd57b314bbcb
Mark Wilden <mark@mwilden.com>
2012-02-05 16:18:10 -0800

    Show the names to merge

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -14,6 +14,21 @@ class AuthorsController < ApplicationController
     end
     @panels_with_authors = @panels.select{|panel| panel[:author]}
     @authors = @panels_with_authors.map{|panel| panel[:author]}
+    @names = @authors.map{|author| author.names}.flatten.map{|author_name| author_name.name}.uniq
+  end
+
+  def format_conjuncted_list items, css_class
+    items = items.flatten.uniq.map{|item| "<span class='#{css_class}'>" + item + '</span>'}.sort
+    case
+    when items.count == 0
+      ''
+    when items.count == 1
+      items.first
+    when items.count == 2
+      items.first + ' and ' + items.second
+    else
+      items[0..-2].join(', ') + ' and ' + items.last
+    end.html_safe
   end
 
   def index
@@ -28,27 +43,7 @@ class AuthorsController < ApplicationController
     # add a blank panel if necessary
     @panels << {term: ''} unless @panels.find {|panel| !panel[:author]}
 
-    #@all_authors = []
-    #blank_panel_added = false
-    #for panel in @panels
-      #if @panels.find {|a_panel| a_panel != panel && a_panel[:author] == panel[:author]}
-        #panel[:already_open] = true
-      #end
-      #blank_panel_added ||= !panel[:author]
-    #end
-    #@panels << {} unless blank_panel_added
-
-    #all_names = @panels.select{|panel| panel[:author]}.collect{|panel| panel[:author].names}
-    #all_names = all_names.flatten.uniq.map{|name| '"' + name.name + '"'}.sort
-    #@all_names_string = case
-    #when all_names.count == 0
-    #when all_names.count == 1
-      #all_names.first
-    #when all_names.count == 2
-      #all_names.first + ' and ' + all_names.second
-    #else
-      #all_names[0..-2].join(', ') + ' and ' + all_names.last
-    #end
+    @all_names_string = format_conjuncted_list @names, 'name'
   end
 
   def merge
