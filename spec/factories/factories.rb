413304445a84ce43bd8c092bd6045aad5cd689f7
Mark Wilden <mark@mwilden.com>
2012-02-10 07:41:31 -0800

    Factories

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,131 +1,159 @@
 # coding: UTF-8
-Factory.define :journal do |journal|
-  journal.sequence(:name) {|n| "Ants#{n}"}
-end
-
-Factory.define :author do |_|
-end
-
-Factory.define :author_name do |author_name|
-  author_name.sequence(:name) {|n| "Fisher#{n}, B.L."}
-  author_name.association :author
-end
-
-Factory.define :publisher do |row|
-  row.name  'Wiley'
-  row.association :place
-end
-
-Factory.define :place do |row|
-  row.name  'New York'
-end
-
-Factory.define :reference do |reference|
-  reference.sequence(:title)  {|n| "Ants are my life#{n}"}
-  reference.sequence(:citation_year)       {|n| "201#{n}d"}
-  reference.author_names      {[Factory(:author_name)]}
-end
-
-Factory.define :article_reference do |reference|
-  reference.author_names        {[Factory(:author_name)]}
-  reference.sequence(:title)    {|n| "Ants are my life#{n}"}
-  reference.sequence(:citation_year)       {|n| "201#{n}d"}
-  reference.association         :journal
-  reference.sequence(:series_volume_issue) {|n| n}
-  reference.sequence(:pagination) {|n| n}
-end
-
-Factory.define :book_reference do |reference|
-  reference.author_names        {[Factory(:author_name)]}
-  reference.sequence(:title)    {|n| "Ants are my life#{n}"}
-  reference.sequence(:citation_year)       {|n| "201#{n}d"}
-  reference.association         :publisher
-  reference.pagination          '22 pp.'
-end
-
-Factory.define :unknown_reference do |reference|
-  reference.author_names        {[Factory(:author_name)]}
-  reference.sequence(:title)    {|n| "Ants are my life#{n}"}
-  reference.sequence(:citation_year)       {|n| "201#{n}d"}
-  reference.citation            'New York'
-end
-
-Factory.define :nested_reference do |reference|
-  reference.author_names        {[Factory(:author_name)]}
-  reference.sequence(:title)    {|n| "Ants are my life#{n}"}
-  reference.sequence(:citation_year)       {|n| "201#{n}d"}
-  reference.pages_in            'In: '
-  reference.nested_reference    {Factory :book_reference}
-end
-
-Factory.define :missing_reference do |reference|
-  reference.citation "Bolton, 2005"
-  reference.citation_year '2001'
-  reference.title '(missing)'
-end
-
 def reference_factory attributes = {}
   author_name = Factory(:author_name, :name => attributes.delete(:author_name))
   reference = Factory(:reference, attributes.merge(:author_names => [author_name]))
   reference
 end
 
-Factory.define :user do |user|
-  user.sequence(:email) {|n| "mark#{n}@example.com"}
-  user.password  'secret'
-end
+FactoryGirl.define do
 
-Factory.define :bolton_reference, :class => Bolton::Reference do |reference|
-  reference.title 'New General Catalog'
-  reference.citation_year '2011'
-  reference.authors 'Fisher, B.L.'
-end
+  factory :author
 
-Factory.define :bolton_match, :class => Bolton::Match do |bolton_match|
-  bolton_match.association :bolton_reference
-  bolton_match.association :reference
-  bolton_match.similarity 0.9
-end
+  factory :author_name do
+    sequence(:name) {|n| "Fisher#{n}, B.L."}
+    author
+  end
 
-Factory.define :reference_document do |_|
-end
+  factory :journal do
+    sequence(:name) {|n| "Ants#{n}"}
+  end
 
-Factory.define :taxon do |taxon|
-  taxon.status  'valid'
-end
+  factory :publisher do
+    name  'Wiley'
+    place
+  end
 
-Factory.define :subfamily do |subfamily|
-  subfamily.sequence(:name) {|n| "Subfamily#{n}"}
-  subfamily.status  'valid'
-end
+  factory :place do
+    name  'New York'
+  end
 
-Factory.define :tribe do |tribe|
-  tribe.sequence(:name) {|n| "Tribe#{n}"}
-  tribe.association :subfamily
-  tribe.status  'valid'
-end
+  factory :reference do
+    sequence(:title)          {|n| "Ants are my life#{n}"}
+    sequence(:citation_year)  {|n| "201#{n}d"}
+    author_names              {[Factory(:author_name)]}
+  end
 
-Factory.define :genus do |genus|
-  genus.sequence(:name) {|n| "Genus#{n}"}
-  genus.association :tribe
-  genus.subfamily   {|a| a.tribe && a.tribe.subfamily}
-  genus.status  'valid'
-end
+  factory :article_reference do
+    author_names                    {[Factory(:author_name)]}
+    sequence(:title)                {|n| "Ants are my life#{n}"}
+    sequence(:citation_year)        {|n| "201#{n}d"}
+    journal
+    sequence(:series_volume_issue)  {|n| n}
+    sequence(:pagination)           {|n| n}
+  end
 
-Factory.define :subgenus do |subgenus|
-  subgenus.sequence(:name) {|n| "Subgenus#{n}"}
-  subgenus.association :genus
-  subgenus.status  'valid'
-end
+  factory :book_reference do
+    author_names                    {[Factory(:author_name)]}
+    sequence(:title)                {|n| "Ants are my life#{n}"}
+    sequence(:citation_year)        {|n| "201#{n}d"}
+    publisher
+    pagination                      '22 pp.'
+  end
 
-Factory.define :species do |species|
-  species.sequence(:name) {|n| "Species#{n}"}
-  species.association :genus
-  species.status  'valid'
-end
+  factory :unknown_reference do
+    author_names                    {[Factory(:author_name)]}
+    sequence(:title)                {|n| "Ants are my life#{n}"}
+    sequence(:citation_year)        {|n| "201#{n}d"}
+    citation                        'New York'
+  end
 
-Factory.define :subspecies do |subspecies|
-  subspecies.sequence(:name) {|n| "Subspecies#{n}"}
-  subspecies.status  'valid'
-  subspecies.association :species
+  factory :missing_reference do
+    title         '(missing)'
+    citation_year '2009'
+    citation      'Latreille, 2009'
+  end
+
+  factory :nested_reference do
+    author_names                    {[Factory(:author_name)]}
+    sequence(:title)                {|n| "Ants are my life#{n}"}
+    sequence(:citation_year)        {|n| "201#{n}d"}
+    pages_in                        'In: '
+    nested_reference                {Factory :book_reference}
+  end
+
+  factory :user do
+    sequence(:email) {|n| "mark#{n}@example.com"}
+    password  'secret'
+  end
+
+  factory :bolton_reference, :class => Bolton::Reference do
+    title 'New General Catalog'
+    citation_year '2011'
+    authors 'Fisher, B.L.'
+  end
+
+  factory :bolton_match, :class => Bolton::Match do
+    bolton_reference
+    reference
+    similarity 0.9
+  end
+
+  factory :reference_document do
+  end
+
+  ####################################################
+  factory :taxon do
+    protonym
+    status  'valid'
+  end
+
+  factory :family do
+    name     'Formicidae'
+    protonym
+    status  'valid'
+  end
+
+  factory :subfamily do
+    sequence(:name) {|n| "Subfamily#{n}"}
+    protonym
+    status  'valid'
+  end
+
+  factory :tribe do
+    sequence(:name) {|n| "Tribe#{n}"}
+    subfamily
+    protonym
+    status  'valid'
+  end
+
+  factory :genus do
+    sequence(:name) {|n| "Genus#{n}"}
+    tribe
+    subfamily   {|a| a.tribe && a.tribe.subfamily}
+    protonym
+    status  'valid'
+  end
+
+  factory :subgenus do
+    sequence(:name) {|n| "Subgenus#{n}"}
+    genus
+    protonym
+    status  'valid'
+  end
+
+  factory :species do
+    sequence(:name) {|n| "Species#{n}"}
+    genus
+    protonym
+    status  'valid'
+  end
+
+  factory :subspecies do
+    sequence(:name) {|n| "Subspecies#{n}"}
+    species
+    protonym
+    status  'valid'
+  end
+
+  ####################################################
+  factory :citation do
+    reference :factory => :article_reference
+    pages '49'
+  end
+
+  factory :protonym do
+    authorship :factory => :citation 
+    sequence(:name) {|n| "Protonym#{n}"}
+  end
+
 end
