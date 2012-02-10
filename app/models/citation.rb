c0ed71a164c1e5d644438f9b82dc68b563706de5
Mark Wilden <mark@mwilden.com>
2012-02-09 16:00:04 -0800

    Update Citation, Protonym

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,3 +1,11 @@
 # coding: UTF-8
 class Citation < ActiveRecord::Base
+  belongs_to :reference
+
+  def self.import data
+    reference = Reference.find_by_bolton_key data
+    notes_taxt = data[:notes] ? Bolton::Catalog::TextToTaxt.notes(data[:notes]) : nil
+    create! reference: reference, pages: data[:pages], notes_taxt: notes_taxt
+  end
+
 end
--- app/models/protonym.rb
+++ app/models/protonym.rb
@@ -1,3 +1,28 @@
 # coding: UTF-8
 class Protonym < ActiveRecord::Base
+  belongs_to :authorship, :class_name => 'Citation'
+
+  def self.import data
+    transaction do
+      authorship = Citation.import data[:authorship].first
+
+      case 
+      when data[:family_or_subfamily_name]
+        name = data[:family_or_subfamily_name]
+        rank = 'family_or_subfamily'
+      when data[:genus_name]
+        name = data[:genus_name]
+        rank = 'genus'
+      when data[:subgenus_name]
+        name = data[:subgenus_name]
+        rank = 'subgenus'
+      when data[:tribe_name]
+        name = data[:tribe_name]
+        rank = 'tribe'
+      end
+
+      create! name: name, rank: rank, sic: data[:sic], fossil: data[:fossil], authorship: authorship
+
+    end
+  end
 end
