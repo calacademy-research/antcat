5a75ff1c166c5ec5f6279371515ce4e4984d42a2
Mark Wilden <mark@mwilden.com>
2012-02-11 12:49:06 -0800

    Formatter is a module, and is tested via CatalogFormatter. Add pluralize_with_delimiters.

--- lib/formatters/formatter.rb
+++ lib/formatters/formatter.rb
@@ -1,13 +1,21 @@
 # coding: UTF-8
-class Formatters::Formatter
+module Formatters::Formatter
+ extend ActionView::Helpers::NumberHelper
 
-  def self.count_and_noun collection, noun
+  def pluralize_with_delimiters count, word, plural = nil
+    if count != 1
+      word = plural ? plural : word.pluralize
+    end
+    "#{number_with_delimiter(count)} #{word}"
+  end
+
+  def count_and_noun collection, noun
     quantity = collection.present? ? collection.count.to_s : 'no'
     noun << 's' unless collection.count == 1
     "#{quantity} #{noun}"
   end
 
-  def self.conjuncted_list items, css_class
+  def conjuncted_list items, css_class
     items = items.flatten.uniq.map{|item| %{<span class="#{css_class}">#{item}</span>}}.sort
     case
     when items.count == 0
