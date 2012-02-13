021e191ee2cd6ce2cffde4c2b4676797e6dc1fc1
Mark Wilden <mark@mwilden.com>
2012-02-13 14:38:04 -0800

    wip

--- features/step_definitions/catalog_steps.rb
+++ features/step_definitions/catalog_steps.rb
@@ -25,3 +25,43 @@ And /I hide tribes/ do
   step %{I follow "hide"}
 end
 
+When /I follow "All" in the subfamilies list/ do
+  step %{I follow "All" within ".subfamilies"}
+end
+
+Then /^"([^"]+)" should be selected(?: in (.*))?$/ do |word, location|
+  with_scope location || 'the page' do
+    page.should have_css ".selected", :text => word
+  end
+end
+
+Then /^I should (not )?see the reference key "([^"]+)"$/ do |should_not, text|
+  selector = should_not ? :should_not : :should
+  find(".reference_key", :text => text).send(selector, be_visible)
+end
+
+Then /^I should (not )?see the reference key expansion$/ do |should_not|
+  selector = should_not ? :should_not : :should
+  find(".reference_key_expansion").send(selector, be_visible)
+end
+
+And /^I click the reference key$/ do
+  find(".reference_key").click
+end
+
+And /^I click the reference key expansion$/ do
+  find(".reference_key_expansion").click
+end
+
+Then /^I should (not )?see the tribes index$/ do |should_not|
+  selector = should_not ? :should_not : :should
+  page.send selector, have_css('.index .tribes')
+end
+
+Given /^there is a missing reference$/ do
+  Factory :missing_reference, :citation => 'Adventures among Ants'
+end
+
+And /^I should not see the missing reference$/ do
+  step 'I should not see "Adventures among Ants"'
+end
