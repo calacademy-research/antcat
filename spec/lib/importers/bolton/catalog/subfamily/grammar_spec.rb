aa14c990fff909b37e6beddff1247fe4f84fee90
Mark Wilden <mark@mwilden.com>
2012-02-13 11:49:36 -0800

    wip

---
+++ spec/lib/importers/bolton/catalog/subfamily/grammar_spec.rb
@@ -0,0 +1,27 @@
+# coding: UTF-8
+require 'spec_helper'
+
+describe Importers::Bolton::Catalog::Subfamily::Importer do
+  before do
+    @importer = Importers::Bolton::Catalog::Subfamily::Importer.new
+  end
+
+  it "should recognize the usual supersubfamily header" do
+    @importer.parse(%{
+<b><span lang=EN-GB>THE DOLICHODEROMORPHS: SUBFAMILIES ANEURETINAE AND DOLICHODERINAE<o:p></o:p></span></b>
+    }).should == {:type => :supersubfamily_header}
+  end
+
+  it "should recognize the supersubfamily header when there's only one subfamily" do
+    @importer.parse(%{
+<b><span lang=EN-GB>THE MYRMICOMORPHS: SUBFAMILY MYRMICINAE<o:p></o:p></span></b></p>
+    }).should == {:type => :supersubfamily_header}
+  end
+
+  it "should recognize the supersubfamily header for extinct subfamilies" do
+    @importer.parse(%{
+<b><span lang=EN-GB>EXTINCT SUBFAMILIES OF FORMICIDAE: *ARMANIINAE, *BROWNIMECIINAE, *FORMICIINAE, *SPHECOMYRMINAE<o:p></o:p></span></b>
+    }).should == {:type => :supersubfamily_header}
+  end
+
+end
