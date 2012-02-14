a0f47a10d963ec6a345204243680675a2b2f4c8f
Mark Wilden <mark@mwilden.com>
2012-02-13 17:10:11 -0800

    Add innocuous steps from topic

--- features/step_definitions/catalog_steps.rb
+++ features/step_definitions/catalog_steps.rb
@@ -25,3 +25,29 @@ And /I hide tribes/ do
   step %{I follow "hide"}
 end
 
+When /I follow "All" in the subfamilies list/ do
+  step %{I follow "All" within ".subfamilies"}
+end
+
+Then /^I should (not )?see the reference key "([^"]+)"$/ do |should_not, text|
+  selector = should_not ? :should_not : :should
+  find(".reference_key", :text => text).send(selector, be_visible)
+end
+
+Then /^I should (not )?see the reference key expansion$/ do |should_not|
+  selector = should_not ? :should_not : :should
+  find(".reference_key_expansion").send(selector, be_visible)
+end
+
+And /^I click the reference key$/ do
+  find(".reference_key").click
+end
+
+And /^I click the reference key expansion$/ do
+  find(".reference_key_expansion").click
+end
+
+Then /^I should (not )?see the tribes index$/ do |should_not|
+  selector = should_not ? :should_not : :should
+  page.send selector, have_css('.index .tribes')
+end
