ac1deb4254c4468620aa74e8d8d0fe39c7f12c51
Mark Wilden <mark@mwilden.com>
2012-02-15 15:08:30 -0800

    ReleaseType.user_can_edit?, user_can_not_edit?

--- lib/release_type.rb
+++ lib/release_type.rb
@@ -3,6 +3,8 @@
 class ReleaseTypeBase 
   include ActionView::Helpers::TagHelper
   def title; 'AntCat' end
+  def user_can_edit?; !!user_signed_in? end
+  def user_can_not_edit?; !user_can_edit? end
   def method_missing(*) end
 end
 
@@ -12,6 +14,7 @@ class PreviewReleaseType < ReleaseTypeBase
   def banner; content_tag :div, "preview", class: :preview end
   def title; 'Preview of AntCat' end
   def preview?; true end
+  def user_can_edit?; true end
 end
 
 ReleaseType = (defined?($release_type) && $release_type == :preview ? PreviewReleaseType : ProductionReleaseType).new
