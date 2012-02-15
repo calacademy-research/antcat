19613b85ab65330702755a74950f742c59983a09
Mark Wilden <mark@mwilden.com>
2012-02-15 13:15:18 -0800

    ReleaseType

---
+++ lib/release_type.rb
@@ -0,0 +1,17 @@
+# coding: UTF-8
+
+class ReleaseTypeBase 
+  include ActionView::Helpers::TagHelper
+  def title; 'AntCat' end
+  def method_missing(*) end
+end
+
+class ProductionReleaseType < ReleaseTypeBase; end
+
+class PreviewReleaseType < ReleaseTypeBase
+  def banner; content_tag :div, "preview", class: :preview end
+  def title; 'Preview of AntCat' end
+end
+
+ReleaseType = (defined?($release_type) && $release_type == :preview ? PreviewReleaseType : ProductionReleaseType).new
+
