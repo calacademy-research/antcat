aa5d25c794d156404a5cc2b2ff9e96e416813c05
Mark Wilden <mark@mwilden.com>
2012-02-15 15:51:34 -0800

    Comment out browser feature

--- spec/helpers/catalog/browser_helper_spec.rb
+++ spec/helpers/catalog/browser_helper_spec.rb
@@ -4,13 +4,13 @@ require 'spec_helper'
 describe Catalog::BrowserHelper do
 
   describe 'taxon header' do
-    it "should return the header and CSS class based on the type of the taxon, its status and whether or not it's a fossil" do
-      taxon = Factory :genus, :name => 'Atta', :fossil => true
-      taxon_header = helper.browser_taxon_header taxon, :link => true
-      taxon_header.should ==
-        %{Genus <a href="#{browser_catalog_path taxon}" class="genus taxon valid">&dagger;ATTA</a>}
-      taxon_header.should be_html_safe
-    end
+    #it "should return the header and CSS class based on the type of the taxon, its status and whether or not it's a fossil" do
+      #taxon = Factory :genus, :name => 'Atta', :fossil => true
+      #taxon_header = helper.browser_taxon_header taxon, :link => true
+      #taxon_header.should ==
+        #%{Genus <a href="#{browser_catalog_path taxon}" class="genus taxon valid">&dagger;ATTA</a>}
+      #taxon_header.should be_html_safe
+    #end
     it "should be able to not include a link" do
       taxon = Factory :genus, :name => 'Atta', :fossil => true
       taxon_header = helper.browser_taxon_header taxon
