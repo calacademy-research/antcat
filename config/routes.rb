07ce4691c98a47b88200d42266fb8f8eb08c9913
Mark Wilden <mark@mwilden.com>
2012-02-15 13:26:57 -0800

    No browser if preview

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ AntCat::Application.routes.draw do
   resources :styles, :only => [:index]
   resource  :catalog, :only => [] do
     match     'index/(:id)', :to => 'catalog/index#show', :as => 'index'
-    match     'browser/(:id)', :to => 'catalog/browser#show', :as => 'browser'
+    match     'browser/(:id)', :to => 'catalog/browser#show', :as => 'browser' unless ReleaseType.preview?
   end
 
   devise_for :users
