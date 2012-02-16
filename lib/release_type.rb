725d520b80d609efbf5e84de37869a6d224efe54
Mark Wilden <mark@mwilden.com>
2012-02-16 14:47:59 -0800

    Have sunspot.yml use the right WebSolr URL for preview or production

--- lib/release_type.rb
+++ lib/release_type.rb
@@ -18,5 +18,18 @@ class PreviewReleaseType < ReleaseType
   def user_can_edit?(_); true end
 end
 
-$ReleaseType = (defined?($release_type) && $release_type == :preview ? PreviewReleaseType : ProductionReleaseType).new
+# The preview-or-production environment detection mechanism detects the
+# presence of any preview file with 'preview' in its name in
+# config/initializers/release_type. The reason for this crude approach
+# is that sunspot.yml needs to know what the release type is, and that's
+# loaded before initializers
+
+# With this approach, each branch can have its own release type, simply 
+# by including or not including a file with 'preview' in its name.
+# If each branch has its own 'branch.preview' file (or not), then they
+# won't be affected by each others' presence.
+
+preview_files = Rails.root + 'config/initializers/release_type/*preview*'
+is_preview = Dir.glob(preview_files).present?
+$ReleaseType = (is_preview ? PreviewReleaseType : ProductionReleaseType).new
 
