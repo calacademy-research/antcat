2849114a2d29739c0eb317d06229e7603d44fc11
Mark Wilden <mark@mwilden.com>
2012-02-15 17:55:51 -0800

    Use a global instead of a constant for ReleaseType, so can switch it for testing

--- lib/release_type.rb
+++ lib/release_type.rb
@@ -1,6 +1,6 @@
 # coding: UTF-8
 
-class ReleaseTypeBase 
+class ReleaseType 
   include ActionView::Helpers::TagHelper
   def title; 'AntCat' end
   def user_can_edit?; !!user_signed_in? end
@@ -8,14 +8,15 @@ class ReleaseTypeBase
   def method_missing(*) end
 end
 
-class ProductionReleaseType < ReleaseTypeBase; end
+class ProductionReleaseType < ReleaseType
+end
 
-class PreviewReleaseType < ReleaseTypeBase
+class PreviewReleaseType < ReleaseType
   def banner; content_tag :div, "preview", class: :preview end
   def title; 'Preview of AntCat' end
   def preview?; true end
   def user_can_edit?; true end
 end
 
-ReleaseType = (defined?($release_type) && $release_type == :preview ? PreviewReleaseType : ProductionReleaseType).new
+$ReleaseType = (defined?($release_type) && $release_type == :preview ? PreviewReleaseType : ProductionReleaseType).new
 
