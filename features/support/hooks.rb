33cafa1baff71854fe86c4c67c8c77c16a40694d
Mark Wilden <mark@mwilden.com>
2012-02-15 17:57:19 -0800

    Change $ReleaseType for @preview scenarios

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,15 +1,17 @@
 # coding: UTF-8
+require 'release_type'
 
 Before do
   Family.delete_all
   Factory :family, protonym: nil
 end
 
+Before('@preview') do
+  $ReleaseType = PreviewReleaseType.new
+end
+
+After('@preview') do
+  $ReleaseType = ProductionReleaseType.new
+end
+
 After {Sunspot.remove_all!}
-
-#at_exit do
-  #Sunspot.session = Sunspot::Rails::StubSessionProxy.new(Sunspot.session) if ENV['DRB'] != 'true'
-#end
-
-
-
