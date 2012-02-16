85553c088a1159a00cf7bbd9f0e4f768fcdc2085
Mark Wilden <mark@mwilden.com>
2012-02-16 14:54:11 -0800

    Sentinel file for preview environment

--- config/initializers/preview.rb
+++
@@ -1,7 +0,0 @@
-# The presence of this file, setting $preview to true, is the only
-# difference between the preview branch, which gets pushed to the
-# preview server, and the main and topic branches, which always
-# think they're on the production server - these two branches don't
-# include this file
-
-$preview = true
