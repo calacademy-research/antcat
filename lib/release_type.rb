7acdeed89e87b6066364d705f22a0540ed585006
Mark Wilden <mark@mwilden.com>
2012-02-15 17:56:44 -0800

    Fix

--- lib/release_type.rb
+++ lib/release_type.rb
@@ -3,19 +3,20 @@
 class ReleaseType 
   include ActionView::Helpers::TagHelper
   def title; 'AntCat' end
-  def user_can_edit?; !!user_signed_in? end
-  def user_can_not_edit?; !user_can_edit? end
+  def user_can_edit?(user) user || preview? end
+  def user_can_not_edit?(user); !user_can_edit user end
   def method_missing(*) end
 end
 
 class ProductionReleaseType < ReleaseType
+  def production?; true end
 end
 
 class PreviewReleaseType < ReleaseType
   def banner; content_tag :div, "preview", class: :preview end
   def title; 'Preview of AntCat' end
   def preview?; true end
-  def user_can_edit?; true end
+  def user_can_edit?(_); true end
 end
 
 $ReleaseType = (defined?($release_type) && $release_type == :preview ? PreviewReleaseType : ProductionReleaseType).new
