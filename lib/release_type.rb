f1925a1bdb71bc8e8ddf123fb9893de52660e801
Mark Wilden <mark@mwilden.com>
2012-02-17 04:20:21 -0800

    ReleaseType (unused in this branch)

---
+++ lib/release_type.rb
@@ -0,0 +1,22 @@
+# coding: UTF-8
+
+class ReleaseType 
+  include ActionView::Helpers::TagHelper
+  def title; 'AntCat' end
+  def user_can_edit?(user) user || preview? end
+  def user_can_not_edit?(user); !user_can_edit user end
+  def method_missing(*) end
+end
+
+class ProductionReleaseType < ReleaseType
+  def production?; true end
+end
+
+class PreviewReleaseType < ReleaseType
+  def title; 'Preview of AntCat' end
+  def preview?; true end
+  def user_can_edit?(_); true end
+end
+
+$ReleaseType = PreviewReleaseType.new
+
