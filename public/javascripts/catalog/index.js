1d72762f9c7ec51a9235e45d1cf1e60258570576
Mark Wilden <mark@mwilden.com>
2012-02-18 18:07:25 -0800

    Hover edit icon

--- public/javascripts/catalog/index.js
+++ public/javascripts/catalog/index.js
@@ -1,10 +1,11 @@
 $(function() {
   setupPage();
   setupHelp();
-  $('.reference_key').live('click', expandReferenceKey);
-  $('.reference_key_expansion_text').live('click', expandReferenceKey);
+  setupIcons();
+  setupReferenceKeys();
 })
 
+////////////////////////////////////////
 function setupPage() {
   setDimensions();
   $(window).resize(function() {
@@ -34,12 +35,66 @@ function setWidth() {
   $("#catalog .content").width($('#page').width());
 }
 
+////////////////////////////////////////
+function setupReferenceKeys() {
+  $('.reference_key').live('click', expandReferenceKey);
+  $('.reference_key_expansion_text').live('click', expandReferenceKey);
+}
+
 function expandReferenceKey() {
   $('.reference_key',           $(this).closest('.reference_key_and_expansion')).toggle();
   $('.reference_key_expansion', $(this).closest('.reference_key_and_expansion')).toggle();
 }
 
+////////////////////////////////////////
 function setupHelp() {
   setupQtip('.document_link', "Click to download and view the document");
   setupQtip('.goto_reference_link', "Click to view/edit this reference on its own page");
+}
+
+////////////////////////////////////////
+function isEditing() {
+  return false;
+}
+
+function setupIcons() {
+  setupIconVisibility();
+  if (user_can_edit) {
+    setupIconHighlighting();
+    setupIconClickHandlers();
+  }
+}
+
+function setupIconVisibility() {
+  if (!testing || !user_can_edit)
+    $('.icon').hide();
+
+  if (!user_can_edit)
+    return
+
+  $('.history_item').live('mouseenter',
+    function() {
+      if (!isEditing())
+        $('.icon', $(this)).show();
+    }).live('mouseleave',
+    function() {
+      $('.icon').hide();
+    });
+}
+
+function setupIconHighlighting() {
+  $('.icon img').live('mouseenter',
+    function() {
+      this.src = this.src.replace('off', 'on');
+    }).live('mouseleave',
+    function() {
+      this.src = this.src.replace('on', 'off');
+    });
+}
+
+function setupIconClickHandlers() {
+  $('.icon.edit').live('click', editHistoryItem);
+}
+
+function editHistoryItem() {
 }
