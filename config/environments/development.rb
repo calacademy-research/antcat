1d1108dbf30b6ee2688bda4f20b14b89d248eadf
Mark Wilden <mark@mwilden.com>
2012-02-19 06:29:57 -0800

    Guess Sass::Plugin.options has to go in environment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,11 @@
 # coding: UTF-8
 AntCat::Application.configure do
-
   config.action_controller.perform_caching = false
-
   config.cache_classes = false
   config.whiny_nils = true
   config.consider_all_requests_local = true
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
   config.action_dispatch.best_standards_support = :builtin
-  config.action_mailer.default_url_options = { :host => 'antcat.local' }
+  config.action_mailer.default_url_options = {host: 'antcat.local'}
 end
-
