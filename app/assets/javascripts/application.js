b6375b5741c44022d1c1af331a521ecb7004d6ab
Mark Wilden <mark@mwilden.com>
2012-02-22 10:57:36 -0800

    Don't bother preloading images - that way don't have to erbize this file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 document.documentElement.className = 'js'; // FOUC fix
 
-preloadImages();
+//preloadImages();
 
 $(function() {
   setupLogin();
@@ -8,13 +8,13 @@ $(function() {
   $("input[type=text]:visible:first").focus();
 })
 
-function preloadImages() {
-  var images = new Array('/images/header_bg.png', '/images/antcat_logo.png', '/images/site_header_ant_5.png')
-  for (var i = 0; i < images.length; i++) {
-    var image = new Image();
-    image.src = images[i];
-  }
-}
+//function preloadImages() {
+  //var images = new Array('/images/header_bg.png', '/images/antcat_logo.png', '/images/site_header_ant_5.png')
+  //for (var i = 0; i < images.length; i++) {
+    //var image = new Image();
+    //image.src = images[i];
+  //}
+//}
 
 function setupLogin() {
   $('#login .form').hide();
