43e8b19820045e1fe7694d8631dd3d3c87d98362
Mark Wilden <mark@mwilden.com>
2012-03-14 06:02:55 -0700

    Format, dehashrocketize

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,22 @@ require 'release_type'
 
 AntCat::Application.routes.draw do
 
-  root :to => "catalog/index#show"
+  root to: "catalog/index#show"
 
-  resources :authors, :only => [:index, :all, :merge]
-  match     '/authors/all', :to => 'authors#all', :via => :get
-  match     '/authors/merge', :to => 'authors#merge', :via => :post
+  resources :authors, only: [:index, :all, :merge]
+  match     '/authors/all', to: 'authors#all', via: :get
+  match     '/authors/merge', to: 'authors#merge', via: :post
   resources :bolton_references
-  match     '/documents/:id/:file_name', :to => 'references#download', :file_name => /.+/, :via => :get
+  resource  :catalog, only: [] do
+    match     'index/(:id)', to: 'catalog/index#show', as: 'index'
+    match     'browser/(:id)', to: 'catalog/browser#show', as: 'browser'
+  end
+  match     '/documents/:id/:file_name', to: 'references#download', file_name: /.+/, via: :get
   resources :journals
-  resources :publishers, :only => [:index]
-  resources :references, :only => [:index, :update, :create, :destroy]
-  match     '/antcat_references.utf8.endnote_import', :to => 'references#index', :format => :endnote_import, :as => :endnote_import
-  resources :styles, :only => [:index]
-  resource  :catalog, :only => [] do
-    match     'index/(:id)', :to => 'catalog/index#show', :as => 'index'
-    match     'browser/(:id)', :to => 'catalog/browser#show', :as => 'browser'
+  resources :publishers, only: [:index]
+  resources :references, only: [:index, :update, :create, :destroy]
+  match     '/antcat_references.utf8.endnote_import', to: 'references#index', format: :endnote_import, as: :endnote_import
+  resources :styles, only: [:index]
   end
 
   devise_for :users
