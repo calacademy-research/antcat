45f5ee1c5b10f1ad58e9f7cfdd2e12d9e0dc3e96
Mark Wilden <mark@mwilden.com>
2012-03-15 14:10:01 -0700

    Format

--- features/step_definitions/history_item_steps.rb
+++ features/step_definitions/history_item_steps.rb
@@ -3,12 +3,12 @@ Then /^the history should be "(.*)"$/ do |history|
   page.should have_css '.history .display', text: history
 end
 
-When /^I edit the history item to "([^"]*)"$/ do |history|
-  step %{I fill in "taxt_editor" with "#{history}"}
-end
-
 When /^I click the edit icon$/ do
   step 'I follow "edit"'
+end
+
+When /^I edit the history item to "([^"]*)"$/ do |history|
+  step %{I fill in "taxt_editor" with "#{history}"}
 end
 
 When /^I save my changes$/ do
