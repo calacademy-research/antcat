0556e565c81f1ffec1d31029c404890673b00b36
Mark Wilden <mark@mwilden.com>
2012-03-16 18:38:32 -0700

    Add resource for each Taxon subtype

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,24 @@ AntCat::Application.routes.draw do
   resources :families, controller: :catalog do
     resources :taxonomic_history_items
   end
+  resources :subfamilies, controller: :catalog do
+    resources :taxonomic_history_items
+  end
+  resources :tribes, controller: :catalog do
+    resources :taxonomic_history_items
+  end
+  resources :genera, controller: :catalog do
+    resources :taxonomic_history_items
+  end
+  resources :subgenera, controller: :catalog do
+    resources :taxonomic_history_items
+  end
+  resources :species, controller: :catalog do
+    resources :taxonomic_history_items
+  end
+  resources :subspecies, controller: :catalog do
+    resources :taxonomic_history_items
+  end
 
   devise_for :users
 
