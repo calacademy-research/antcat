7c402aee5a5229548acb569579e0ffb3d2dae1cd
Mark Wilden <mark@mwilden.com>
2012-03-17 13:09:19 -0700

    TaxonomicHistoryItem#update_taxt_from_editable, factory

--- app/models/taxonomic_history_item.rb
+++ app/models/taxonomic_history_item.rb
@@ -2,4 +2,9 @@
 class TaxonomicHistoryItem < ActiveRecord::Base
   belongs_to :taxon
   acts_as_list :scope => :taxon
+
+  def update_taxt_from_editable editable_taxt
+    update_attribute :taxt, Taxt.from_editable(editable_taxt)
+  end
+
 end
