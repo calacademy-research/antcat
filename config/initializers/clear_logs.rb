37b70aaf898bfcf16ad76cd3c4caf2511295d3c5
Mark Wilden <mark@mwilden.com>
2012-03-17 09:15:42 -0700

    Let the logs get big, so can see why

--- config/initializers/clear_logs.rb
+++ config/initializers/clear_logs.rb
@@ -1,9 +1,9 @@
-if Rails.env.development?
-  MAX_LOG_SIZE = 50.megabytes
+#if Rails.env.development?
+  #MAX_LOG_SIZE = 50.megabytes
   
-  logs = File.join(Rails.root, 'log', '*.log')
-  if Dir[logs].any? {|log| File.size?(log).to_i > MAX_LOG_SIZE }
-    $stdout.puts "Clearing log files..."
-    `rake log:clear`
-  end 
-end
+  #logs = File.join(Rails.root, 'log', '*.log')
+  #if Dir[logs].any? {|log| File.size?(log).to_i > MAX_LOG_SIZE }
+    #$stdout.puts "Clearing log files..."
+    #`rake log:clear`
+  #end 
+#end
