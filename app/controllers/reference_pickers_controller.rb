8e44d9773d273aa3f49c62422f8d17d1c7f5de52
Mark Wilden <mark@mwilden.com>
2012-04-05 14:54:14 -0700

    Alway send current reference id to server

--- app/controllers/reference_pickers_controller.rb
+++ app/controllers/reference_pickers_controller.rb
@@ -3,18 +3,19 @@ class ReferencePickersController < ApplicationController
 
   def show
     params[:search_selector] ||= 'Search for author(s)'
+
     if params[:id].present?
       @references = Reference.perform_search id: params[:id]
-    else
-      params[:search_selector] ||= 'Search for'
-      params[:q].strip! if params[:q]
-      params[:is_author_search] = params[:search_selector] == 'Search for author(s)'
+      @selected_reference = @references.first
+    end
 
+    if params[:q].present?
+      params[:q].strip!
+      params[:is_author_search] = params[:search_selector] == 'Search for author(s)'
       @references = Reference.do_search params
     end
 
     render partial: 'show'
-    raise if @references.size == Reference.count
   end
 
 end
