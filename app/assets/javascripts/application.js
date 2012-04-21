e9350eab425694f6f1423fb4719e6fafd9da02ef
Mark Wilden <mark@mwilden.com>
2012-04-21 14:25:14 -0700

    Move stuff from application.js to utility.coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,15 +5,7 @@
 //= require utility
 //= require_tree ./ext
 
-document.documentElement.className = 'js'; // FOUC fix
-
 //preloadImages();
-
-$(function() {
-  setupLogin();
-  spaceOutImages();
-  $("input[type=text]:visible:first").focus();
-})
 
 //function preloadImages() {
   //var images = new Array('/images/header_bg.png', '/images/antcat_logo.png', '/images/site_header_ant_5.png')
@@ -22,22 +14,3 @@ $(function() {
     //image.src = images[i];
   //}
 //}
-
-function setupLogin() {
-  $('#login .form').hide();
-  $('#login a.link').click(function() {
-    $('#login div').toggle();
-  });
-}
-
-$(window).resize(function() {
-  spaceOutImages();
-});
-
-function spaceOutImages() {
-  var totalImageWidth = 379 + 154 + 124;
-  var imageCount = 7;
-  var availableWidth = $('#site_footer .images').width();
-  var marginInBetween = (availableWidth - totalImageWidth) / (imageCount - 1);
-  $("#site_footer .spacer").width(marginInBetween);
-}
