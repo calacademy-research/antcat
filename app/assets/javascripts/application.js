2dfd16bec7fe02e68adb7a11f567093dc96ac617
Mark Wilden <mark@mwilden.com>
2012-04-21 14:37:07 -0700

    Preload images, rename to application_coffee.coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,15 +2,5 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require qtip
-//= require utility
+//= require application_coffee
 //= require_tree ./ext
-
-//preloadImages();
-
-//function preloadImages() {
-  //var images = new Array('/images/header_bg.png', '/images/antcat_logo.png', '/images/site_header_ant_5.png')
-  //for (var i = 0; i < images.length; i++) {
-    //var image = new Image();
-    //image.src = images[i];
-  //}
-//}
