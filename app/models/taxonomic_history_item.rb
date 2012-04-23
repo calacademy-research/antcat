9531879f7d6cc7b0903b76a9a0085c958f569d96
Mark Wilden <mark@mwilden.com>
2012-04-23 16:29:28 -0700

    Have to call valid? to populate errors

--- app/models/taxonomic_history_item.rb
+++ app/models/taxonomic_history_item.rb
@@ -6,6 +6,7 @@ class TaxonomicHistoryItem < ActiveRecord::Base
 
   def update_taxt_from_editable editable_taxt
     update_attribute :taxt, Taxt.from_editable(editable_taxt)
+    valid?
   rescue Taxt::ReferenceNotFound => e
     errors.add :base, "The reference '#{e}' could not be found. Was the ID changed?"
   end
