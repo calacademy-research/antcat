84788e89c0d25940d76dd57dd15e913cc2e3ea1c
Mark Wilden <mark@mwilden.com>
2012-04-28 17:53:53 -0700

    simulated_form

--- app/controllers/widget_tests_controller.rb
+++ app/controllers/widget_tests_controller.rb
@@ -1,7 +1,16 @@
 # coding: UTF-8
 class WidgetTestsController < ApplicationController
+
   def reference_picker
   end
+
   def reference_field
   end
+
+  def simulated_form
+    lll {"params['Name']"}
+    lll{%q{request.method}}
+    head 200 if request.method == 'POST'
+  end
+
 end
