0e082ecb8b67e2bb58ce9efc3d4ca4865bce5611
Mark Wilden <mark@mwilden.com>
2012-05-22 17:58:34 -0700

    Fix synonym fixup

--- app/models/forward_reference.rb
+++ app/models/forward_reference.rb
@@ -21,8 +21,8 @@ class ForwardReference < ActiveRecord::Base
       when Species
         senior_synonym = Taxon.find_by_genus_id_and_name target_parent, target_name
         Progress.error "Couldn't find species '#{target_name}' for genus #{target_parent}" unless senior_synonym
-        source.update_attributes synonym_of: senior_synonym
-
+        source.update_attributes status: 'synonym', synonym_of: senior_synonym
+        return
       else raise
       end
     source.update_attributes type_taxon_rank: rank, type_taxon_name: Formatters::CatalogFormatter::fossil(target_name, fossil)
