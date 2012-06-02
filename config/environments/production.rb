fc6c6ac6867e8a28032e57f019deebf6761982cd
Mark Wilden <mark@mwilden.com>
2012-06-02 09:44:28 -0700

    Just precompile every damn thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,10 @@ AntCat::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile = [->(path) {
+    true
+  }]
+
   # Generate digests for assets URLs
   config.assets.digest = true
 end
