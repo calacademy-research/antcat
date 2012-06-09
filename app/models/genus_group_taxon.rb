0dc667fa9e7712763b90c626110e914a41391cbe
Mark Wilden <mark@mwilden.com>
2012-06-08 18:49:25 -0700

    Refactor: GenusGroupTaxon base class for Genus and Subgenus

---
+++ app/models/genus_group_taxon.rb
@@ -0,0 +1,51 @@
+# coding: UTF-8
+class GenusGroupTaxon < Taxon
+  belongs_to :subfamily
+  belongs_to :tribe
+
+  def children
+    species
+  end
+
+  def full_label
+    "<i>#{full_name}</i>"
+  end
+
+  def self.import data, name_key, attributes = {}
+    transaction do
+      protonym = Protonym.import data[:protonym]
+      name = Name.import data[:name]
+
+      headline_notes_taxt = Importers::Bolton::Catalog::TextToTaxt.convert(data[:note])
+      attributes.merge!(
+        name_object:          name,
+        fossil:               data[:fossil] || false,
+        status:               data[:status] || 'valid',
+        synonym_of:           data[:synonym_of],
+        protonym:             protonym,
+        headline_notes_taxt:  headline_notes_taxt,
+      )
+      attributes.merge! data[:attributes] if data[:attributes]
+      if data[:type_species]
+        type_species_taxt = Importers::Bolton::Catalog::TextToTaxt.convert(data[:type_species][:texts])
+        attributes[:type_taxon_taxt] = type_species_taxt
+      end
+      genus_group_taxon = create! attributes
+      data[:taxonomic_history].each do |item|
+        genus_group_taxon.taxonomic_history_items.create! taxt: item if item.present?
+      end
+
+      type_species = data[:type_species]
+      if type_species
+        target_name = type_species[name_key] || type_species[:genus_name]
+        target_name << ' (' << type_species[:subgenus_epithet] + ')' if type_species[:subgenus_epithet]
+        target_name << ' '  << type_species[:species_epithet]
+        ForwardReference.create! source_id: genus_group_taxon.id, target_name: target_name,
+          fossil: type_species[:fossil]
+      end
+
+      genus_group_taxon
+    end
+  end
+
+end
--- app/models/subgenus.rb
+++ app/models/subgenus.rb
@@ -1,54 +1,15 @@
 # coding: UTF-8
-class Subgenus < Taxon
+class Subgenus < GenusGroupTaxon
   belongs_to :genus
-  belongs_to :tribe
-  belongs_to :subfamily
-  has_many :species
   validates_presence_of :genus
+  belongs_to :tribe
+  has_many :species
 
-  def children
-    species
+  def self.import data
+    super data, :subgenus_name, genus: data[:genus]
   end
 
   def statistics
-  end
-
-  def self.import data
-    transaction do
-      protonym = Protonym.import data[:protonym]
-      name = Name.import data[:name]
-
-      headline_notes_taxt = Importers::Bolton::Catalog::TextToTaxt.convert(data[:note])
-      attributes = {
-        genus:                data[:genus],
-        name_object:          name,
-        fossil:               data[:fossil] || false,
-        status:               data[:status] || 'valid',
-        synonym_of:           data[:synonym_of],
-        protonym:             protonym,
-        headline_notes_taxt:  headline_notes_taxt,
-      }
-      attributes.merge! data[:attributes] if data[:attributes]
-      if data[:type_species]
-        type_species_taxt = Importers::Bolton::Catalog::TextToTaxt.convert(data[:type_species][:texts])
-        attributes[:type_taxon_taxt] = type_species_taxt
-      end
-      subgenus = create! attributes
-      data[:taxonomic_history].each do |item|
-        subgenus.taxonomic_history_items.create! taxt: item if item.present?
-      end
-
-      type_species = data[:type_species]
-      if type_species
-        target_name = type_species[:subgenus_name] || type_species[:genus_name]
-        target_name << ' (' << type_species[:subgenus_epithet] + ')' if type_species[:subgenus_epithet]
-        target_name << ' '  << type_species[:species_epithet]
-        ForwardReference.create! source_id: subgenus.id, target_name: target_name,
-          fossil: type_species[:fossil]
-      end
-
-      subgenus
-    end
   end
 
 end
