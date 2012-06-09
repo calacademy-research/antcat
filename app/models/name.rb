64de9baf8a0201e0319c92c89f261bfcfdb0bad4
Mark Wilden <mark@mwilden.com>
2012-06-09 07:47:13 -0700

    Import SubgenusName. Add import instance method for overriding.

--- app/models/name.rb
+++ app/models/name.rb
@@ -2,15 +2,24 @@ class Name < ActiveRecord::Base
 
   validates :name, presence: true
 
-  def self.import name, data = {}
-    name_object = find_by_name name
-    return name_object if name_object
+  def full_name
+    name
+  end
 
-    subclass = case
+  def self.import name, data = {}
+    case
     when data[:genus_name] then GenusName
+    when data[:subgenus_name] then SubgenusName
     else Name
-    end
-    subclass.create! name: name
+    end.new.import name, data
+  end
+
+  def import name, data
+    name_object = self.class.find_by_name name
+    return name_object if name_object
+    self.name = name
+    save!
+    self
   end
 
 end
