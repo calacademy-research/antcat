e9c9275a2ed82bd5449102695e42c7c24d06a448
Mark Wilden <mark@mwilden.com>
2012-06-09 12:01:12 -0700

    Remove unused methods

--- app/models/name.rb
+++ app/models/name.rb
@@ -2,10 +2,6 @@ class Name < ActiveRecord::Base
 
   validates :name, presence: true
 
-  def full_name
-    name
-  end
-
   def self.import name, data = {}
     SpeciesName.import(name, data)  or
     SubgenusName.import(name, data) or
@@ -13,14 +9,6 @@ class Name < ActiveRecord::Base
     SubtribeName.import(name, data) or
     TribeName.import(name, data)    or
     FamilyOrSubfamilyName.import(name, data)
-  end
-
-  def import name, data
-    name_object = self.class.find_by_name name
-    return name_object if name_object
-    self.name = name
-    save!
-    self
   end
 
 end
--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -6,38 +6,4 @@ describe Name do
     Name.new(name:'Name').name.should == 'Name'
   end
 
-  describe "Importing" do
-    it "should work" do
-      name = Name.import 'Atta'
-      name.reload.name.should == 'Atta'
-    end
-    it "should re-use an existing name" do
-      existing_name = FactoryGirl.create :name, name: 'Atta'
-      new_name = Name.import 'Atta'
-      new_name.should == existing_name
-    end
-
-    describe "Creating the right subclass" do
-      it "should create a regular Name for most things" do
-        name = Name.import 'Formicidae', family_name: 'Formicidae'
-        name.should be_kind_of Name
-      end
-      it "should create a GenusName" do
-        name = Name.import 'Atta', genus_name: 'Atta'
-        name.should be_kind_of GenusName
-      end
-      it "should create a SubgenusName" do
-        name = Name.import 'Atta', subgenus_name: 'Atta'
-        name.should be_kind_of SubgenusName
-      end
-      it "should create a SpeciesName" do
-        name = Name.import 'major', species_epithet: 'major', genus_name: 'Atta'
-        name.should be_kind_of SpeciesName
-        name.full_name.should == 'Atta major'
-        name.genus_name.should == GenusName.find_by_name('Atta')
-      end
-    end
-
-  end
-
 end
