9e1706540861ef79b9d8ba1adaab5cce5af47039
Mark Wilden <mark@mwilden.com>
2012-06-08 19:00:52 -0700

    Create a GenusName

--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -16,6 +16,10 @@ describe Name do
       new_name = Name.import 'Atta'
       new_name.should == existing_name
     end
+    it "should create an object of the appropriate subclass" do
+      name = Name.import 'Atta', genus_name: 'Atta'
+      name.should be_kind_of GenusName
+    end
   end
 
 end
