443c80e8330ce9753c9fb2c5fd25e47fbe7b4d07
Mark Wilden <mark@mwilden.com>
2012-06-10 15:54:26 -0700

    Import genus name with subgenus name

--- spec/models/subgenus_name_spec.rb
+++ spec/models/subgenus_name_spec.rb
@@ -6,18 +6,23 @@ describe SubgenusName do
   describe "Importing" do
 
     it "should recognize its key and set its name appropriately" do
-      name = Name.import subgenus_name: 'Atta'
-      SubgenusName.find(name).name.should == 'Atta'
+      name = Name.import genus_name: 'Atta', subgenus_epithet: 'Subatta'
+      name = SubgenusName.find(name)
+      name.name.should == 'Atta (Subatta)'
+      name.epithet.should == 'Subatta'
     end
     it "should reuse names" do
-      FactoryGirl.create :name, name: 'Subatta'
-      Name.import subgenus_name: 'Subatta'
-      Name.count.should == 1
+      Name.import genus_name: 'Atta', subgenus_epithet: 'Subatta'
+      Name.count.should == 2
+      Name.import genus_name: 'Atta', subgenus_epithet: 'Subatta'
+      Name.count.should == 2
     end
-
+    it "should not reuse name for another genus" do
+      Name.import genus_name: 'Eciton', subgenus_epithet: 'Subatta'
+      Name.count.should == 2
+      Name.import genus_name: 'Atta', subgenus_epithet: 'Subatta'
+      Name.count.should == 4
+    end
   end
 
 end
-
-
-
