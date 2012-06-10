e8945504f4782a3e794be1b45c271d9a67f5f6b6
Mark Wilden <mark@mwilden.com>
2012-06-09 17:21:21 -0700

    wip

--- app/models/family_name.rb
+++ app/models/family_name.rb
@@ -1,6 +1,6 @@
 class FamilyName < FamilyOrSubfamilyName
 
-  def self.import data
+  def import data
     return unless name = data[:family_name]
     create! name: name
   end
--- app/models/species_name.rb
+++ app/models/species_name.rb
@@ -1,19 +1,26 @@
 class SpeciesName < Name
 
-  belongs_to :genus_name
-  validates :genus_name, presence: true
+  belongs_to :genus_name_object
+  validates :genus_name_object, presence: true
 
-    genus_name = GenusName.find_or_create_by_name data[:genus_name]
-    name_object = self.class.find_by_genus_name_id_and_name genus_name.id, name
-    return name_object if name_object
-  def self.import data
+  def import data
+    #genus_name_object = GenusName.find_by_name data[:genus_name_object]
+    #if genus_name_object
+      #name_object = self.class.find_by_genus_name_id_and_name genus_name_object.id, data[:species_epithet]
+      #return name_object if name_object
+    #else
+      #genus_name_object = GenusName.import(data)
+    #end
 
-    self.genus_name = GenusName.find_or_create_by_name data[:genus_name]
-    self.epithet = data[:species_epithet]
-    self.name = genus_name.name + ' ' + epithet
-    save!
-
+    lll{%q{data}}
+    create! genus_name: Name.import(genus_name: data[:genus_name]),
+            epithet:    data[:species_epithet],
+            name:       data[:species_epithet]
     self
+  end
+
+  def full_name
+    genus_name.name + ' ' + epithet
   end
 
 end
