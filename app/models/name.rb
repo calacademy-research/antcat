3d095a68cf081939cca3a4844a64abba809512c9
Mark Wilden <mark@mwilden.com>
2012-06-12 06:48:43 -0700

    SubspeciesName (WIP)

--- app/models/name.rb
+++ app/models/name.rb
@@ -3,13 +3,14 @@ class Name < ActiveRecord::Base
   validates :name, presence: true
 
   def self.import data
-    SpeciesName.import(data)  or
-    SubgenusName.import(data) or
-    GenusName.import(data)    or
-    SubtribeName.import(data) or
-    TribeName.import(data)    or
-    SubfamilyName.import(data)or
-    FamilyName.import(data)   or
+    SubspeciesName.import(data) or
+    SpeciesName.import(data)    or
+    SubgenusName.import(data)   or
+    GenusName.import(data)      or
+    SubtribeName.import(data)   or
+    TribeName.import(data)      or
+    SubfamilyName.import(data)  or
+    FamilyName.import(data)     or
     FamilyOrSubfamilyName.import(data) or
     raise "No Name subclass wanted #{data}"
   end
