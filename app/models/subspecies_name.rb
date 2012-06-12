267b221f5f81f152a3ae1e05727570b0bb629da2
Mark Wilden <mark@mwilden.com>
2012-06-12 06:48:43 -0700

    SubspeciesName (WIP)

--- app/models/subspecies_name.rb
+++ app/models/subspecies_name.rb
@@ -1,3 +1,32 @@
 class SubspeciesName < Name
 
+  belongs_to :species_name
+  validates :species_name, presence: true
+
+  def self.import data
+    return unless data[:subspecies]
+
+    epithet = data[:subspecies].first[:species_group_epithet]
+    raise unless data[:genus]
+
+    if data[:species]
+      species_name = data[:species].name_object
+    else
+      species_name = SpeciesName.import data
+    end
+
+    name = Name.find_by_species_name_id_and_epithet species_name.id, epithet
+    return name if name
+
+    create!({
+      name:             genus_group_name.name + ' ' + species_name.name + ' ' + epithet,
+      epithet:          epithet,
+      genus_group_name: data[:genus],
+    })
+  end
+
+  def rank
+    'species'
+  end
+
 end
---
+++ spec/models/subspecies_name_spec.rb
@@ -0,0 +1,35 @@
+# coding: UTF-8
+require 'spec_helper'
+
+describe SubspeciesName do
+
+  describe "Importing" do
+
+    it "should recognize its key and set its name appropriately" do
+      name = Name.import genus_name: 'Atta', species_epithet: 'major', subspecies: [{species_group_epithet: 'alpina'}]
+      name = SubspeciesName.find name
+      name.epithet.should == 'alpina'
+      name.name.should == 'Atta major alpina'
+    end
+    #it "should reuse names" do
+      #Name.import genus_name: 'Atta', species_epithet: 'major'
+      #Name.count.should == 2
+      #Name.import genus_name: 'Atta', species_epithet: 'major'
+      #Name.count.should == 2
+    #end
+    #it "should not reuse name for another genus" do
+      #Name.import genus_name: 'Eciton', species_epithet: 'major'
+      #Name.count.should == 2
+      #Name.import genus_name: 'Atta', species_epithet: 'major'
+      #Name.count.should == 4
+    #end
+    #it "should import a species name with a subgenus name" do
+      #name = Name.import genus_name: 'Atta', subgenus_epithet: 'Subatta', species_epithet: 'major'
+      #name = SpeciesName.find name
+      #name.epithet.should == 'major'
+      #name.name.should == 'Atta (Subatta) major'
+    #end
+
+  end
+
+end
