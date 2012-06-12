f350b49862d4d20d3d31ddeb2eef8e53a734e312
Mark Wilden <mark@mwilden.com>
2012-06-11 17:47:12 -0700

    Search only GenusGroupNames for a subgenus

--- app/models/subgenus_name.rb
+++ app/models/subgenus_name.rb
@@ -11,9 +11,8 @@ class SubgenusName < GenusGroupName
       genus_group_name = GenusName.import genus_name: data[:genus_name]
     end
 
-    name_object = Name.find_by_genus_group_name_id_and_epithet genus_group_name.id, epithet
+    name_object = GenusGroupName.find_by_genus_group_name_id_and_epithet genus_group_name.id, epithet
     return name_object if name_object
-
     name = genus_group_name.name + ' (' + epithet + ')'
     create! name: name, epithet: epithet, genus_group_name: genus_group_name
   end
