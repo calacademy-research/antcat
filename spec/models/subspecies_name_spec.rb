e7f6d2b7f3f3132f07ccfbe6608574d5d2145fad
Mark Wilden <mark@mwilden.com>
2012-06-12 18:16:51 -0700

    Subspecies name with multiple parts

--- spec/models/subspecies_name_spec.rb
+++ spec/models/subspecies_name_spec.rb
@@ -11,25 +11,50 @@ describe SubspeciesName do
       name.epithet.should == 'alpina'
       name.name.should == 'Atta major alpina'
     end
-    #it "should reuse names" do
-      #Name.import genus_name: 'Atta', species_epithet: 'major'
-      #Name.count.should == 2
-      #Name.import genus_name: 'Atta', species_epithet: 'major'
-      #Name.count.should == 2
-    #end
-    #it "should not reuse name for another genus" do
-      #Name.import genus_name: 'Eciton', species_epithet: 'major'
-      #Name.count.should == 2
-      #Name.import genus_name: 'Atta', species_epithet: 'major'
-      #Name.count.should == 4
-    #end
-    #it "should import a species name with a subgenus name" do
-      #name = Name.import genus_name: 'Atta', subgenus_epithet: 'Subatta', species_epithet: 'major'
-      #name = SpeciesName.find name
-      #name.epithet.should == 'major'
-      #name.name.should == 'Atta (Subatta) major'
-    #end
+    it "should reuse names" do
+      attributes = {genus_name: 'Atta', species_epithet: 'major', subspecies: [{species_group_epithet: 'alpina'}]}
+      Name.import attributes
+      Name.count.should == 3
+      Name.import attributes
+      Name.count.should == 3
+    end
+    it "should not reuse name for another species" do
+      Name.import genus_name: 'Atta', species_epithet: 'major', subspecies: [{species_group_epithet: 'alpina'}]
+      Name.count.should == 3
+      Name.import genus_name: 'Atta', species_epithet: 'minor', subspecies: [{species_group_epithet: 'alpina'}]
+      Name.count.should == 5
+    end
 
+    it "should handle a subspecies qualifiers" do
+      name = Name.import genus_name: 'Atta', species_epithet: 'major', subspecies: [{:type => 'r.', species_group_epithet: 'alpina'}]
+      name = SubspeciesName.find name
+      name.epithet.should == 'alpina'
+      name.name.should == 'Atta major r. alpina'
+      #name.label.should == '<i>Atta major</i> r. <i>alpina</i>'
+    end
+
+    it "should handle multiple subspecies epithets" do
+      name = Name.import genus_name: 'Atta', species_epithet: 'major', subspecies: [
+        {species_group_epithet: 'alpina'},
+        {species_group_epithet: 'superba', :type => 'r.'}
+      ]
+      name = SubspeciesName.find name
+      name.epithet.should == 'alpina'
+      name.name.should == 'Atta major alpina r. superba'
+    end
+
+    it "multiple subspecies epithets should create separate names for each epithet" do
+      name = Name.import genus_name: 'Atta', species_epithet: 'major', subspecies: [
+        {species_group_epithet: 'alpina'},
+        {species_group_epithet: 'superba', :type => 'r.'}
+      ]
+      alpina = Name.find_by_epithet 'alpina'
+      superba = Name.find_by_epithet 'superba'
+      alpina.next_subspecies_name.should == superba
+      superba.prior_subspecies_name.should == alpina
+    end
+
+    it "should handle subspecies with subgenera"
   end
 
 end
