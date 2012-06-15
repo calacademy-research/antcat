509546e2a2bc161f6085e3a405fdb61f3313a2f8
Mark Wilden <mark@mwilden.com>
2012-06-14 17:59:50 -0700

    No need for target parent. Also, just use name as key

--- app/models/forward_reference.rb
+++ app/models/forward_reference.rb
@@ -9,8 +9,8 @@ class ForwardReference < ActiveRecord::Base
     source = Taxon.find source_id
     case source
     when Species
-      senior_synonym = Taxon.find_by_genus_id_and_name target_parent, target_name
-      Progress.error "Couldn't find species '#{target_name}' for genus #{target_parent}" unless senior_synonym
+      senior_synonym = Taxon.find_by_name target_name
+      Progress.error "Couldn't find species '#{target_name}'" unless senior_synonym
       source.update_attributes status: 'synonym', synonym_of: senior_synonym
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120614221159) do
+ActiveRecord::Schema.define(:version => 20120615005351) do
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
@@ -77,7 +77,6 @@ ActiveRecord::Schema.define(:version => 20120614221159) do
     t.integer "source_id"
     t.string  "target_name"
     t.boolean "fossil"
-    t.integer "target_parent"
   end
 
   create_table "journals", :force => true do |t|
