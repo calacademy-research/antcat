d9300ff760f1032c5b3eede9bcd29e80e3c2179b
Mark Wilden <mark@mwilden.com>
2012-06-16 08:17:49 -0700

    Change FK requirements

--- spec/models/subspecies_spec.rb
+++ spec/models/subspecies_spec.rb
@@ -2,36 +2,28 @@
 require 'spec_helper'
 
 describe Subspecies do
+  before do
+    @genus = create_genus 'Atta'
+  end
 
   it "has no statistics" do
     Subspecies.new.statistics.should be_nil
   end
 
-  it "must have a species" do
-    subspecies = FactoryGirl.build :subspecies, name: FactoryGirl.create(:name, name: 'Colobopsis'), species: nil
+  it "does not have to have a species (before being fixed up, e.g.)" do
+    subspecies = create_subspecies 'Atta major colobopsis', genus: @genus, species: nil
+    subspecies.should be_valid
+  end
+
+  it "must have a genus" do
+    subspecies = create_subspecies 'Atta major colobopsis', genus: nil, species: nil
     subspecies.should_not be_valid
-    subspecies.species = FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'christi')
+  end
+
+  it "has its subfamily assigned from its genus" do
+    subspecies = create_subspecies 'Atta major colobopsis', genus: @genus
     subspecies.save!
-    subspecies.reload.species.name.to_s.should == 'christi'
-  end
-
-  it "should have a subfamily" do
-    species = FactoryGirl.create :species
-    subspecies = FactoryGirl.create :subspecies, :species => species
-    subspecies.subfamily.should == species.subfamily
-  end
-
-  it "should have a genus" do
-    species = FactoryGirl.create :species
-    subspecies = FactoryGirl.create :subspecies, :species => species
-    subspecies.genus.should == species.genus
-  end
-
-  describe "Full name" do
-    it "is the the genus, the species, and the subspecies name" do
-      taxon = FactoryGirl.create :subspecies, name: FactoryGirl.create(:name, name: 'Atta emeryi biggus'), :species => FactoryGirl.create(:species, name: FactoryGirl.create(:name, name: 'emeryi'), :genus => FactoryGirl.create(:genus, name: FactoryGirl.create(:name, name: 'Atta'), :subfamily => FactoryGirl.create(:subfamily, name: FactoryGirl.create(:name, name: 'Dolichoderinae'))))
-      taxon.name.to_s.should == 'Atta emeryi biggus'
-    end
+    subspecies.subfamily.should == @genus.subfamily
   end
 
 end
