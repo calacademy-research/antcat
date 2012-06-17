b048ad563323841bd72cea3093bc3aaf2d986f49
Mark Wilden <mark@mwilden.com>
2012-06-16 18:36:08 -0700

    Use create_xxx helpers, and refactor those. Dehashrocketize.

--- spec/models/species_spec.rb
+++ spec/models/species_spec.rb
@@ -4,32 +4,32 @@ require 'spec_helper'
 describe Species do
 
   it "should have a genus" do
-    genus = FactoryGirl.create :genus, name: FactoryGirl.create(:name, name: 'Atta')
-    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'Atta championi'), :genus => genus
+    genus = create_genus 'Afropone'
+    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'Atta championi'), genus: genus
     Species.find_by_name('Atta championi').genus.should == genus
   end
 
   it "can have a subgenus" do
-    subgenus = FactoryGirl.create :subgenus, name: FactoryGirl.create(:name, name: 'Atta')
-    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'Atta championi'), :subgenus => subgenus
+    subgenus = create_subgenus 'Atta (Subatta)'
+    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'Atta championi'), subgenus: subgenus
     Species.find_by_name('Atta championi').subgenus.should == subgenus
   end
 
   it "should have a subfamily" do
-    genus = FactoryGirl.create :genus, name: FactoryGirl.create(:name, name: 'Atta')
-    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'championi'), :genus => genus
+    genus = create_genus 'Afropone'
+    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'championi'), genus: genus
     Species.find_by_name('championi').subfamily.should == genus.subfamily
   end
 
   it "doesn't need a genus" do
-    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'championi'), :genus => nil
+    FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'championi'), genus: nil
     Species.find_by_name('championi').genus.should be_nil
   end
 
   it "should have subspecies, which are its children" do
     species = FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'chilensis')
-    FactoryGirl.create :subspecies, name: FactoryGirl.create(:name, name: 'robusta'), :species => species
-    FactoryGirl.create :subspecies, name: FactoryGirl.create(:name, name: 'saltensis'), :species => species
+    FactoryGirl.create :subspecies, name: FactoryGirl.create(:name, name: 'robusta'), species: species
+    FactoryGirl.create :subspecies, name: FactoryGirl.create(:name, name: 'saltensis'), species: species
     species = Species.find_by_name 'chilensis'
     species.subspecies.map(&:name).map(&:to_s).should =~ ['robusta', 'saltensis']
     species.children.should == species.subspecies
@@ -38,8 +38,8 @@ describe Species do
   describe "Name" do
     it "should handle it" do
       subfamily = FactoryGirl.create :subfamily, name: FactoryGirl.create(:subfamily_name, name: 'Dolichoderinae')
-      genus = FactoryGirl.create :genus, name: FactoryGirl.create(:genus_name, name: 'Myrmicium'), :subfamily => subfamily
-      species = FactoryGirl.create :species, name: FactoryGirl.create(:species_name, name: 'Myrmicium shattucki', epithet: 'shattucki'), :genus => genus
+      genus = FactoryGirl.create :genus, name: FactoryGirl.create(:genus_name, name: 'Myrmicium'), subfamily: subfamily
+      species = FactoryGirl.create :species, name: FactoryGirl.create(:species_name, name: 'Myrmicium shattucki', epithet: 'shattucki'), genus: genus
       species.name.to_s.should == 'Myrmicium shattucki'
     end
   end
@@ -52,35 +52,35 @@ describe Species do
 
     it "should handle 1 valid subspecies" do
       species = FactoryGirl.create :species
-      subspecies = FactoryGirl.create :subspecies, :species => species
-      species.statistics.should == {:extant => {:subspecies => {'valid' => 1}}}
+      subspecies = FactoryGirl.create :subspecies, species: species
+      species.statistics.should == {extant: {subspecies: {'valid' => 1}}}
     end
 
     it "should differentiate between extant and fossil subspecies" do
       species = FactoryGirl.create :species
-      subspecies = FactoryGirl.create :subspecies, :species => species
-      FactoryGirl.create :subspecies, :species => species, :fossil => true
+      subspecies = FactoryGirl.create :subspecies, species: species
+      FactoryGirl.create :subspecies, species: species, fossil: true
       species.statistics.should == {
-        :extant => {:subspecies => {'valid' => 1}},
-        :fossil => {:subspecies => {'valid' => 1}},
+        extant: {subspecies: {'valid' => 1}},
+        fossil: {subspecies: {'valid' => 1}},
       }
     end
 
     it "should differentiate between extant and fossil subspecies" do
       species = FactoryGirl.create :species
-      subspecies = FactoryGirl.create :subspecies, :species => species
-      FactoryGirl.create :subspecies, :species => species, :fossil => true
+      subspecies = FactoryGirl.create :subspecies, species: species
+      FactoryGirl.create :subspecies, species: species, fossil: true
       species.statistics.should == {
-        :extant => {:subspecies => {'valid' => 1}},
-        :fossil => {:subspecies => {'valid' => 1}},
+        extant: {subspecies: {'valid' => 1}},
+        fossil: {subspecies: {'valid' => 1}},
       }
     end
 
     it "should handle 1 valid subspecies and 2 synonyms" do
       species = FactoryGirl.create :species
-      FactoryGirl.create :subspecies, :species => species
-      2.times {FactoryGirl.create :subspecies, :species => species, :status => 'synonym'}
-      species.statistics.should == {:extant => {:subspecies => {'valid' => 1, 'synonym' => 2}}}
+      FactoryGirl.create :subspecies, species: species
+      2.times {FactoryGirl.create :subspecies, species: species, status: 'synonym'}
+      species.statistics.should == {extant: {subspecies: {'valid' => 1, 'synonym' => 2}}}
     end
 
   end
@@ -89,8 +89,8 @@ describe Species do
     it "should return itself and its subfamily's other tribes" do
       FactoryGirl.create :tribe
       subfamily = FactoryGirl.create :subfamily
-      tribe = FactoryGirl.create :tribe, :subfamily => subfamily
-      another_tribe = FactoryGirl.create :tribe, :subfamily => subfamily
+      tribe = FactoryGirl.create :tribe, subfamily: subfamily
+      another_tribe = FactoryGirl.create :tribe, subfamily: subfamily
       tribe.siblings.should =~ [tribe, another_tribe]
     end
   end
@@ -100,7 +100,7 @@ describe Species do
     it "should work" do
       subfamily = FactoryGirl.create :subfamily
       genus = FactoryGirl.create :genus, name: FactoryGirl.create(:genus_name, name: 'Fiona'), subfamily: subfamily
-      reference = FactoryGirl.create :article_reference, :bolton_key_cache => 'Latreille 1809'
+      reference = FactoryGirl.create :article_reference, bolton_key_cache: 'Latreille 1809'
 
       species = Species.import(
         genus: genus,
@@ -137,7 +137,7 @@ describe Species do
 
     describe "Importing subspecies" do
       it "should work" do
-        genus = FactoryGirl.create :genus, name: FactoryGirl.create(:genus_name, name: 'Camponotus')
+        genus = create_genus 'Camponotus'
         taxon = Species.import(
           genus:                  genus,
           species_group_epithet:  'refectus',
@@ -185,15 +185,15 @@ describe Species do
       ferox = FactoryGirl.create :species, name: FactoryGirl.create(:name, name: 'Atta texanus'), genus: genus
       species = FactoryGirl.create :species, genus: genus
       history = 
-        [{:combinations_in=>
-          [{:genus_name=>"Acanthostichus",
-            :subgenus_epithet=>"Ctenopyga",
-            :references=>
-              [{:author_names=>["Emery"],
-                :year=>"1911d",
-                :pages=>"14",
-                :matched_text=>"Emery, 1911d: 14"}]}],
-          :matched_text=>
+        [{combinations_in:
+          [{genus_name:"Acanthostichus",
+            subgenus_epithet:"Ctenopyga",
+            references:
+              [{author_names:["Emery"],
+                year:"1911d",
+                pages:"14",
+                matched_text:"Emery, 1911d: 14"}]}],
+          matched_text:
           " Combination in <i>Acanthostichus (Ctenopyga)</i>: Emery, 1911d: 14."},
         {:synonym_ofs=>
           [{:species_epithet=>"texanus",
