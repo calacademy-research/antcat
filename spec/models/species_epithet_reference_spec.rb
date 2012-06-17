ee8bdd417af8798feed1fbfed2a0365188a3cb15
Mark Wilden <mark@mwilden.com>
2012-06-16 19:54:01 -0700

    SpeciesEpithetReference

---
+++ spec/models/species_epithet_reference_spec.rb
@@ -0,0 +1,23 @@
+# coding: UTF-8
+require 'spec_helper'
+
+describe SpeciesEpithetReference do
+
+  describe "Fixing up a reference" do
+    it "should look for and find the referenced species" do
+      genus = create_genus
+      species = create_species 'Atta major', genus: genus
+      subspecies = create_subspecies 'Atta major splendens', genus: genus, species: nil
+      attributes = {
+        fixee:            subspecies,
+        fixee_table:      'taxa',
+        fixee_attribute: 'species_id',
+        genus:            genus,
+        epithet:          'major',
+      }
+      SpeciesEpithetReference.new.fixup
+      Subspecies.find(subspecies).species.name.to_s.should == 'Atta major'
+    end
+  end
+
+end
