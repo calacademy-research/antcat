371cf3da211f9e133d4a372ee83a1dba6b668b65
Mark Wilden <mark@mwilden.com>
2012-06-18 11:29:17 -0700

    Refactor

--- app/models/name.rb
+++ app/models/name.rb
@@ -39,17 +39,54 @@ class Name < ActiveRecord::Base
     self.class.name[0, self.class.name.rindex('Name')].underscore
   end
 
+  def self.add_epithets epithet, epithets, prefix, variant1, variant2
+    if epithet =~ /[#{prefix}]#{variant1}$/
+      epithets << epithet.gsub(/([#{prefix}])#{variant1}$/, "\\1#{variant2}")
+      return true
+    end
+    if epithet =~ /[#{prefix}]#{variant2}$/
+      epithets << epithet.gsub(/([#{prefix}])#{variant2}$/, "\\1#{variant1}")
+      return true
+    end
+  end
+
+  def self.replace_suffix epithet, prefix, suffix, new_suffix
+    epithet.gsub /([#{prefix}])#{suffix}$/, "\\1#{new_suffix}"
+  end
+
+  def self.has_suffix epithet, prefix, suffix
+    epithet =~ /[#{prefix}]#{suffix}$/
+  end
+
+  def self.add_ii_epithets epithet, epithets
+    consonants = 'bcdfghjklmnprstvxyz'
+    if has_suffix epithet, consonants, 'ii'
+      epithets << replace_suffix(epithet, consonants, 'ii', 'i')
+      epithets << replace_suffix(epithet, consonants, 'ii', 'iae')
+      return true
+    end
+    if has_suffix epithet, consonants, 'iae'
+      epithets << replace_suffix(epithet, consonants, 'iae', 'ii')
+      return true
+    end
+    if has_suffix epithet, consonants, 'i'
+      epithets << replace_suffix(epithet, consonants, 'i', 'ii')
+      epithets << replace_suffix(epithet, consonants, 'i', 'ae')
+      return true
+    end
+    if has_suffix epithet, consonants, 'ae'
+      epithets << replace_suffix(epithet, consonants, 'ae', 'i')
+      return true
+    end
+  end
+
   def self.make_epithet_set epithet
     consonants = 'bcdfghjklmnprstvxyz'
     epithets = [epithet]
-    [["[#{consonants}]",  'a',  'us'],
-     ["[#{consonants}q]", 'ua', 'uus'],
-     ["[#{consonants}]",  'ii', 'i'],
-     ["[#{consonants}]",  'eus', 'ea'],
-    ].each do |prefix, first_variant, second_variant|
-      epithets << epithet.gsub(/(#{prefix})#{first_variant}$/, "\\1#{second_variant}")
-      epithets << epithet.gsub(/(#{prefix})#{second_variant}$/,"\\1#{first_variant}")
-    end
-    epithets.uniq
+    add_ii_epithets(epithet, epithets) or
+    add_epithets(epithet, epithets, consonants, 'a',  'us') or
+    add_epithets(epithet, epithets, consonants + 'q', 'ua', 'uus') or
+    add_epithets(epithet, epithets, consonants, 'ea', 'eus')
+    epithets
   end
 end
--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -13,23 +13,29 @@ describe Name do
       Name.make_epithet_set('maga').should == ['maga', 'magus']
     end
 
-    def should_be_same_epithet *epithets
-      epithets.each do |epithet|
-        Name.make_epithet_set(epithet).should =~ epithets
+    def epithet_set_for_one_should_include_other *epithets
+      epithets.each do |search_epithet|
+        epithets.each do |target_epithet|
+          if search_epithet != target_epithet
+            Name.make_epithet_set(search_epithet).find(target_epithet).should_not be_nil
+          end
+        end
       end
     end
 
     describe "Masculine-feminine" do
       it "should convert between these" do
-        should_be_same_epithet 'magus', 'maga'
-        should_be_same_epithet 'diabolicus', 'diabolica'
-        should_be_same_epithet 'equus', 'equa'
-        should_be_same_epithet 'aneus', 'anea'
+        epithet_set_for_one_should_include_other 'subterranea', 'subterraneus'
+        epithet_set_for_one_should_include_other 'magus', 'maga'
+        epithet_set_for_one_should_include_other 'diabolicus', 'diabolica'
+        epithet_set_for_one_should_include_other 'equus', 'equa'
+        epithet_set_for_one_should_include_other 'aneus', 'anea'
+        epithet_set_for_one_should_include_other 'eugeniae', 'eugenii'
       end
     end
 
     it "should convert names deemed identical" do
-      should_be_same_epithet 'lundi', 'lundii'
+      epithet_set_for_one_should_include_other 'lundi', 'lundii'
     end
 
   end
