4e974823c77e42c084a8147270b626d7de2be871
Mark Wilden <mark@mwilden.com>
2012-06-17 17:50:56 -0700

    Report when a fixup turns out to be a subspecies instead of a species

--- app/models/species_epithet_reference.rb
+++ app/models/species_epithet_reference.rb
@@ -6,7 +6,11 @@ class SpeciesEpithetReference < ForwardReference
   def fixup
     species = Taxon.find_by_genus_id_and_epithet genus_id, epithet
     if species
-      fixee.update_attribute :species, species
+      if species.kind_of? Subspecies
+        Progress.error "Fixup for genus '#{genus.name}', epithet '#{epithet}' turned out to be a subspecies: #{species}"
+      else
+        fixee.update_attribute :species, species
+      end
     else
       Progress.error "Couldn't find species for genus '#{genus.name}', epithet '#{epithet}'"
     end
