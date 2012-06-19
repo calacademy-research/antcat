26e958c8e9e0ed178d62492eac0c98ab5642e857
Mark Wilden <mark@mwilden.com>
2012-06-19 06:03:11 -0700

    Don't try to fixup to a Subspecies

--- app/models/species_epithet_reference.rb
+++ app/models/species_epithet_reference.rb
@@ -4,13 +4,9 @@ class SpeciesEpithetReference < ForwardReference
   belongs_to :genus; validates :genus, presence: true
 
   def fixup
-    species = Taxon.find_by_genus_id_and_epithet genus_id, epithet
+    species = Species.find_by_genus_id_and_epithet genus_id, epithet
     if species
-      if species.kind_of? Subspecies
-        Progress.error "Fixup for genus '#{genus.name}', epithet '#{epithet}' turned out to be a subspecies: #{species}"
-      else
-        fixee.update_attribute :species, species
-      end
+      fixee.update_attribute :species, species
     else
       Progress.error "Couldn't find species for genus '#{genus.name}', epithet '#{epithet}'"
     end
