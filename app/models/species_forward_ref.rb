a66574ecebd970a1038c6bb50a6b0e56dc4c6906
Mark Wilden <mark@mwilden.com>
2012-06-25 11:33:20 -0700

    SpeciesForwardRef.fixup

--- app/models/species_forward_ref.rb
+++ app/models/species_forward_ref.rb
@@ -6,4 +6,21 @@ class SpeciesForwardRef < ActiveRecord::Base
   belongs_to :genus; validates :genus, presence: true
   validates  :epithet, presence: true 
 
+  def self.fixup
+    all.each {|e| e.fixup}
+  end
+
+  def fixup
+    results = Taxon.find_by_genus_id_and_epithet genus.id, epithet
+    value = nil
+    if results.nil?
+      Progress.error "Couldn't find species '#{epithet}' in genus '#{genus.name}' when fixing up '#{fixee_attribute}' in '#{fixee.name}'"
+    elsif results.size > 1
+      Progress.error "More than one result found for species '#{epithet}' in genus '#{genus.name}' when fixing up '#{fixee_attribute}' in '#{fixee.name}'"
+    else
+      value = results.first.id
+    end
+    fixee.update_attributes fixee_attribute.to_sym => value
+  end
+
 end
