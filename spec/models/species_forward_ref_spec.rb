2989c88663a9b210479d625dcfd631a27cbcc6ae
Mark Wilden <mark@mwilden.com>
2012-06-25 11:33:20 -0700

    SpeciesForwardRef.fixup

--- spec/models/species_forward_ref_spec.rb
+++ spec/models/species_forward_ref_spec.rb
@@ -32,4 +32,70 @@ describe SpeciesForwardRef do
       SpeciesForwardRef.new(@valid_attributes).should_not be_valid
     end
   end
+
+  describe "Fixing up" do
+
+    describe "Fixing up all forward references" do
+      it "should call each's fixup method" do
+        first = mock
+        second = mock
+        SpeciesForwardRef.should_receive(:all).and_return [first, second]
+        first.should_receive :fixup
+        second.should_receive :fixup
+        SpeciesForwardRef.fixup
+      end
+    end
+
+    describe "Fixing up one forward reference" do
+
+      it "should set the synonym_of attribute to a taxon matching a name" do
+        genus = create_genus 'Atta'
+        fixee = create_species
+        forward_ref = SpeciesForwardRef.create!({
+          fixee: fixee, fixee_attribute: 'synonym_of_id',
+          genus: genus, epithet: 'major'
+        })
+        fixer = create_species genus: genus,
+          name: FactoryGirl.create(:species_name, name: 'Atta major', epithet: 'major')
+
+        forward_ref.fixup
+
+        fixee = Taxon.find fixee
+        fixee.synonym_of.should == fixer
+      end
+
+      it "clear the attribute and record an error if there are no results" do
+        genus = create_genus 'Atta'
+        fixee = create_species
+        forward_ref = SpeciesForwardRef.create!({
+          fixee: fixee, fixee_attribute: 'synonym_of_id',
+          genus: genus, epithet: 'major'
+        })
+        Progress.should_receive :error
+
+        forward_ref.fixup
+
+        fixee = Taxon.find fixee
+        fixee.synonym_of.should be_nil
+      end
+
+      it "clear the attribute and record an error if there is more than one results" do
+        genus = create_genus 'Atta'
+        fixee = create_species
+        forward_ref = SpeciesForwardRef.create!({
+          fixee: fixee, fixee_attribute: 'synonym_of_id',
+          genus: genus, epithet: 'major'
+        })
+        name = FactoryGirl.create :species_name, name: 'Atta major', epithet: 'major'
+        2.times {create_species genus: genus, name: name}
+        Progress.should_receive :error
+
+        forward_ref.fixup
+
+        fixee = Taxon.find fixee
+        fixee.synonym_of.should be_nil
+      end
+
+    end
+  end
 end
