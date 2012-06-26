b9c07762678518d6f3215e5355fdc6e7f7200676
Mark Wilden <mark@mwilden.com>
2012-06-26 16:09:41 -0700

    SpeciesForwardRef.pick_valid_target

--- app/models/species_forward_ref.rb
+++ app/models/species_forward_ref.rb
@@ -11,41 +11,27 @@ class SpeciesForwardRef < ActiveRecord::Base
   end
 
   def fixup
-    results = Taxon.find_epithet_in_genus genus.id, epithet
-    value = nil
-    if results.nil?
+    candidates = Species.find_epithet_in_genus genus_id, epithet
+    species = self.class.pick_validest candidates
+    if species.nil?
       Progress.error "Couldn't find species '#{epithet}' in genus '#{genus.name}' when fixing up '#{fixee_attribute}' in '#{fixee.name}'"
-    elsif results.size > 1
-      Progress.error "More than one result found for species '#{epithet}' in genus '#{genus.name}' when fixing up '#{fixee_attribute}' in '#{fixee.name}'"
     else
-      value = results.first.id
+      fixee.update_attributes fixee_attribute.to_sym => species
     end
-    fixee.update_attributes fixee_attribute.to_sym => value
   end
 
 
-  #belongs_to :genus; validates :genus, presence: true
-
-  #def fixup
-    #species = self.class.pick_validest Species.find_epithet_in_genus genus_id, epithet
-    #if species
-      #fixee.update_attribute :species, species
-    #else
-      #Progress.error "Couldn't find species for genus '#{genus.name}', epithet '#{epithet}'"
-    #end
-  #end
-
-  #def self.pick_validest targets
-    #return unless targets
-    #valid_targets = targets.select {|target| target.status == 'valid'}
-    #other_targets = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
-    #case
-    #when valid_targets.size > 1
-      #Progress.error "Found multiple valid targets among #{targets.map(&:name).map(&:to_s).join(', ')}"
-      #return nil
-    #when valid_targets.empty? then other_targets.first
-    #else valid_targets.first
-    #end
-  #end
+  def self.pick_validest targets
+    return unless targets
+    valid_targets = targets.select {|target| target.status == 'valid'}
+    other_targets = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
+    case
+    when valid_targets.size > 1
+      Progress.error "Found multiple valid targets among #{targets.map(&:name).map(&:to_s).join(', ')}"
+      return nil
+    when valid_targets.empty? then other_targets.first
+    else valid_targets.first
+    end
+  end
 
 end
