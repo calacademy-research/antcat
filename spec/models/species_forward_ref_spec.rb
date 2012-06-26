1038d6b9818514714d262d7b332db1a364a31c34
Mark Wilden <mark@mwilden.com>
2012-06-26 14:06:22 -0700

    Format, move commented code

--- spec/models/species_forward_ref_spec.rb
+++ spec/models/species_forward_ref_spec.rb
@@ -33,69 +33,130 @@ describe SpeciesForwardRef do
     end
   end
 
-  describe "Fixing up" do
+  describe "Fixing up all forward references" do
+    it "should call each's fixup method" do
+      first = mock
+      second = mock
+      SpeciesForwardRef.should_receive(:all).and_return [first, second]
+      first.should_receive :fixup
+      second.should_receive :fixup
+      SpeciesForwardRef.fixup
+    end
+  end
 
-    describe "Fixing up all forward references" do
-      it "should call each's fixup method" do
-        first = mock
-        second = mock
-        SpeciesForwardRef.should_receive(:all).and_return [first, second]
-        first.should_receive :fixup
-        second.should_receive :fixup
-        SpeciesForwardRef.fixup
-      end
+  describe "Fixing up one forward reference" do
+
+    it "should set the synonym_of attribute to a taxon matching a name" do
+      genus = create_genus 'Atta'
+      fixee = create_species
+      forward_ref = SpeciesForwardRef.create!({
+        fixee: fixee, fixee_attribute: 'synonym_of_id',
+        genus: genus, epithet: 'major'
+      })
+      fixer = create_species genus: genus,
+        name: FactoryGirl.create(:species_name, name: 'Atta major', epithet: 'major')
+
+      forward_ref.fixup
+
+      fixee = Taxon.find fixee
+      fixee.synonym_of.should == fixer
     end
 
-    describe "Fixing up one forward reference" do
+    it "clear the attribute and record an error if there are no results" do
+      genus = create_genus 'Atta'
+      fixee = create_species
+      forward_ref = SpeciesForwardRef.create!({
+        fixee: fixee, fixee_attribute: 'synonym_of_id',
+        genus: genus, epithet: 'major'
+      })
+      Progress.should_receive :error
 
-      it "should set the synonym_of attribute to a taxon matching a name" do
-        genus = create_genus 'Atta'
-        fixee = create_species
-        forward_ref = SpeciesForwardRef.create!({
-          fixee: fixee, fixee_attribute: 'synonym_of_id',
-          genus: genus, epithet: 'major'
-        })
-        fixer = create_species genus: genus,
-          name: FactoryGirl.create(:species_name, name: 'Atta major', epithet: 'major')
+      forward_ref.fixup
 
-        forward_ref.fixup
-
-        fixee = Taxon.find fixee
-        fixee.synonym_of.should == fixer
-      end
-
-      it "clear the attribute and record an error if there are no results" do
-        genus = create_genus 'Atta'
-        fixee = create_species
-        forward_ref = SpeciesForwardRef.create!({
-          fixee: fixee, fixee_attribute: 'synonym_of_id',
-          genus: genus, epithet: 'major'
-        })
-        Progress.should_receive :error
-
-        forward_ref.fixup
-
-        fixee = Taxon.find fixee
-        fixee.synonym_of.should be_nil
-      end
-
-      it "clear the attribute and record an error if there is more than one result" do
-        genus = create_genus 'Atta'
-        fixee = create_species
-        forward_ref = SpeciesForwardRef.create!({
-          fixee: fixee, fixee_attribute: 'synonym_of_id',
-          genus: genus, epithet: 'major'
-        })
-        name = FactoryGirl.create :species_name, name: 'Atta major', epithet: 'major'
-        2.times {create_species genus: genus, name: name}
-        Progress.should_receive :error
-
-        forward_ref.fixup
-
-        fixee = Taxon.find fixee
-        fixee.synonym_of.should be_nil
-      end
-
+      fixee = Taxon.find fixee
+      fixee.synonym_of.should be_nil
     end
+
+    it "clear the attribute and record an error if there is more than one result" do
+      genus = create_genus 'Atta'
+      fixee = create_species
+      forward_ref = SpeciesForwardRef.create!({
+        fixee: fixee, fixee_attribute: 'synonym_of_id',
+        genus: genus, epithet: 'major'
+      })
+      name = create_name 'Atta major'
+      2.times {create_species genus: genus, name: name}
+      Progress.should_receive :error
+
+      forward_ref.fixup
+
+      fixee = Taxon.find fixee
+      fixee.synonym_of.should be_nil
+    end
+
+    it "should use declension rules to find Atta magnus when the synonym is to Atta magna" do
+      genus = create_genus 'Atta'
+      fixee = create_species genus: genus
+      forward_ref = SpeciesForwardRef.create!({
+        fixee: fixee, fixee_attribute: 'synonym_of_id',
+        genus: genus, epithet: 'magna'
+      })
+      fixer = create_species 'Atta magnus', genus: genus
+
+      forward_ref.fixup
+
+      fixee = Taxon.find fixee
+      fixee.synonym_of.should == fixer
+    end
+
+    #it "should look for and find the referenced species" do
+      #genus = create_genus
+      #species = create_species 'Atta major', genus: genus
+      #subspecies = create_subspecies 'Atta major splendens', genus: genus, species: nil
+      #attributes = {
+        #fixee:           subspecies,
+        #fixee_table:     'taxa',
+        #fixee_attribute: 'species_id',
+        #genus:           genus,
+        #epithet:         'major',
+      #}
+      #SpeciesEpithetReference.should_receive(:pick_validest).and_return species
+      #SpeciesEpithetReference.new(attributes).fixup
+      #Subspecies.find(subspecies).species.name.to_s.should == 'Atta major'
+    #end
+
+    #describe "Picking the validest target" do
+      #it "should return nil if there is none" do
+        #targets = []
+        #SpeciesEpithetReference.pick_validest(targets).should be_nil
+      #end
+
+      #it "should return nil if there is none" do
+        #targets = nil
+        #SpeciesEpithetReference.pick_validest(targets).should be_nil
+      #end
+
+      #it "should pick the best target, if there is more than one" do
+        #invalid_species = create_species status: 'homonym'
+        #valid_species = create_species status: 'valid'
+        #targets = [invalid_species, valid_species]
+        #SpeciesEpithetReference.pick_validest(targets).should == valid_species
+      #end
+
+      #it "should raise log an error and return nil if there is more than one valid" do
+        #valid_species = create_species
+        #another_valid_species = create_species
+        #targets = [another_valid_species, valid_species]
+        #Progress.should_receive :error
+        #SpeciesEpithetReference.pick_validest(targets).should be_nil
+      #end
+
+      #it "should not pick the homonym, no matter what" do
+        #homonym_species = create_species status: 'homonym'
+        #targets = [homonym_species]
+        #SpeciesEpithetReference.pick_validest(targets).should be_nil
+      #end
+
+    #end
   end
 end
