08bb9ebe40184ddf923d195f9a412438360757f7
Mark Wilden <mark@mwilden.com>
2012-06-26 14:06:22 -0700

    Format, move commented code

--- app/models/species_forward_ref.rb
+++ app/models/species_forward_ref.rb
@@ -23,4 +23,29 @@ class SpeciesForwardRef < ActiveRecord::Base
     fixee.update_attributes fixee_attribute.to_sym => value
   end
 
+
+  #belongs_to :genus; validates :genus, presence: true
+
+  #def fixup
+    #species = self.class.pick_validest Species.find_epithet_in_genus genus_id, epithet
+    #if species
+      #fixee.update_attribute :species, species
+    #else
+      #Progress.error "Couldn't find species for genus '#{genus.name}', epithet '#{epithet}'"
+    #end
+  #end
+
+  #def self.pick_validest targets
+    #return unless targets
+    #valid_targets = targets.select {|target| target.status == 'valid'}
+    #other_targets = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
+    #case
+    #when valid_targets.size > 1
+      #Progress.error "Found multiple valid targets among #{targets.map(&:name).map(&:to_s).join(', ')}"
+      #return nil
+    #when valid_targets.empty? then other_targets.first
+    #else valid_targets.first
+    #end
+  #end
+
 end
