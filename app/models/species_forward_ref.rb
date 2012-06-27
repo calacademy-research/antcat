9960f40cf86671284468be6fa75ffcece807de6c
Mark Wilden <mark@mwilden.com>
2012-06-26 17:05:32 -0700

    Fix SpeciesForwardRef

--- app/models/species_forward_ref.rb
+++ app/models/species_forward_ref.rb
@@ -12,12 +12,12 @@ class SpeciesForwardRef < ActiveRecord::Base
 
   def fixup
     specieses = Species.find_validest_for_epithet_in_genus epithet, genus
-    if specieses.empty?
+    if specieses.blank?
       Progress.error "Couldn't find species '#{epithet}' in genus '#{genus.name}' when fixing up '#{fixee_attribute}' in '#{fixee.name}'"
     elsif specieses.count > 1
       Progress.error "Found multiple valid targets among #{specieses.map(&:name).map(&:to_s).join(', ')}"
     else
-      fixee.update_attributes fixee_attribute.to_sym => specieses.first
+      fixee.update_attributes fixee_attribute.to_sym => specieses.first.id
     end
   end
 
