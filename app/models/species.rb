ced71668266eb20b13a56c2f6fdf1101b9f21364
Mark Wilden <mark@mwilden.com>
2012-06-27 10:31:17 -0700

    Move find_validest_for_epithet_in_genus to SpeciesGroupTaxon

--- app/models/species.rb
+++ app/models/species.rb
@@ -14,17 +14,4 @@ class Species < SpeciesGroupTaxon
     get_statistics [:subspecies]
   end
 
-  def self.find_validest_for_epithet_in_genus epithet, genus
-    pick_validest find_epithet_in_genus epithet, genus
-  end
-
-  def self.pick_validest targets
-    return unless targets
-    validest = targets.select {|target| target.status == 'valid'}
-    if validest.empty?
-      validest = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
-    end
-    validest.presence
-  end
-
 end
--- app/models/species_group_taxon.rb
+++ app/models/species_group_taxon.rb
@@ -10,8 +10,20 @@ class SpeciesGroupTaxon < Taxon
   end
 
   ##################################################
-  # importing
+  def self.find_validest_for_epithet_in_genus epithet, genus
+    pick_validest find_epithet_in_genus epithet, genus
+  end
 
+  def self.pick_validest targets
+    return unless targets
+    validest = targets.select {|target| target.status == 'valid'}
+    if validest.empty?
+      validest = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
+    end
+    validest.presence
+  end
+
+  ##################################################
   def self.import data
     transaction do
       protonym = import_protonym data
