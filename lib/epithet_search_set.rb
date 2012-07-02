8fd9572767d4a097934c189c5a9e831769f33131
Mark Wilden <mark@mwilden.com>
2012-07-02 07:18:10 -0700

    Refactor

--- lib/epithet_search_set.rb
+++ lib/epithet_search_set.rb
@@ -3,30 +3,29 @@ class EpithetSearchSet
   attr_reader :epithets
 
   def initialize epithet
-    epithets = [epithet]
-    epithets = frequent_misspellings epithets
-    epithets = declensions epithets
-    epithets = orthographic epithets
-    epithets = deemed_identical epithets
-    @epithets = epithets.uniq
+    @epithets = [epithet]
+    frequent_misspellings
+    declensions
+    orthographic
+    deemed_identical
   end
 
-  def frequent_misspellings epithets
-    epithets.concat(epithets.inject([]) do |epithets, epithet|
+  ####################
+  def frequent_misspellings
+    @epithets.concat(@epithets.inject([]) do |epithets, epithet|
       epithets << 'alfari' if epithet == 'alfaroi'
       epithets << epithet.gsub(/^columbic/, 'colombic') if epithet =~ /^columbic/
       epithets
-    end).uniq
+    end).uniq!
   end
 
-  def declensions epithets
-    epithets.concat(epithets.inject([]) do |epithets, epithet|
-      epithets << epithet
+  ####################
+  def declensions
+    @epithets.concat(@epithets.inject([]) do |epithets, epithet|
       epithets.concat first_declension_nominative_singular epithet
       epithets.concat first_declension_genitive_singular epithet
       epithets.concat third_declension_nominative_singular epithet
-      epithets
-    end).uniq
+    end).uniq!
   end
 
   def first_declension_nominative_singular epithet
@@ -47,14 +46,14 @@ class EpithetSearchSet
     endings.map {|ending| epithet.gsub(/(#{stem})#{endings_regexp}$/, "\\1#{ending}")}
   end
 
-  def orthographic epithets
-    epithets = ae_and_e epithets
-    epithets.concat p_and_ph epithets
-    epithets.uniq
+  ####################
+  def orthographic
+    ae_and_e
+    p_and_ph
   end
 
-  def ae_and_e epithets
-    epithets.concat(epithets.inject([]) do |epithets, epithet|
+  def ae_and_e
+    @epithets.concat(@epithets.inject([]) do |epithets, epithet|
       consonants = "(?:[#{CONSONANTS}][ei]?|qu)"
       epithets << epithet.gsub(/(#{consonants})e(#{consonants})/) do |string|
         if ['ter', 'del'].include?(string)
@@ -67,28 +66,26 @@ class EpithetSearchSet
         $1 + 'e' + $2
       end
       epithets
-    end).uniq
+    end).uniq!
   end
 
-  def p_and_ph epithets
-    epithets.concat(epithets.inject([]) do |epithets, epithet|
+  def p_and_ph
+    @epithets.concat(@epithets.inject([]) do |epithets, epithet|
       epithets << epithet.gsub(/ph/, 'p')
       epithets << epithet.gsub(/p([^h])/, 'ph\1')
-      epithets
-    end).uniq
+    end).uniq!
   end
 
-  def deemed_identical epithets
-    lll('start'){%q{epithets}}
-    v = epithets.concat(epithets.inject([]) do |epithets, epithet|
+  ####################
+  def deemed_identical
+    @epithets.concat(@epithets.inject([]) do |epithets, epithet|
       if ends_with epithet, 'i'
         epithets << replace_ending(epithet, 'i', 'ii')
       elsif ends_with epithet, 'ii'
         epithets << replace_ending(epithet, 'ii', 'i')
       end
       epithets
-    end).uniq
-    lll('end'){%q{epithets}}
+    end).uniq!
   end
 
   def ends_with epithet, ending
@@ -97,10 +94,6 @@ class EpithetSearchSet
 
   def replace_ending epithet, old_ending, new_ending
     epithet.gsub /([#{CONSONANTS}])#{old_ending}$/, "\\1#{new_ending}"
-  end
-
-  def replace_stem epithet, new_stem, stem = "[#{CONSONANTS}]"
-    epithet.gsub /^#{stem}/, new_stem
   end
 
   CONSONANTS = 'bcdfghjklmnprstvxyz'
