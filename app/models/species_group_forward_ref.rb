46c59b3c51733e9d35c9637db1131ae288c2b13a
Mark Wilden <mark@mwilden.com>
2012-07-12 10:55:49 -0700

    If a species forward reference turns out to be a subspecies, use its species

--- app/models/species_group_forward_ref.rb
+++ app/models/species_group_forward_ref.rb
@@ -17,6 +17,7 @@ class SpeciesGroupForwardRef < ActiveRecord::Base
       Progress.error "Found multiple valid targets among #{specieses.map(&:name).map(&:to_s).join(', ')}"
     else
       species = specieses.first
+      species = species.species if species.respond_to? :species
       # updating the id instead of the association doesn't do anything
       fixee.junior_synonym.update_attributes synonym_of: species if fixee.kind_of? Synonym
       fixee.update_attributes fixee_attribute.to_sym => species
