baf140ed18469023cf0e35ef4714fb055b9efc47
Mark Wilden <mark@mwilden.com>
2012-07-17 13:25:37 -0700

    Refactor

--- app/models/family.rb
+++ app/models/family.rb
@@ -3,21 +3,18 @@ class Family < Taxon
 
   def self.import data
     transaction do
-      name = Name.import family_name: 'Formicidae'
-      protonym = Protonym.import data[:protonym]
-      type_name = Name.import data[:type_genus]
-      type_taxt = Importers::Bolton::Catalog::TextToTaxt.convert(data[:type_genus][:texts])
-      headline_notes_taxt = Importers::Bolton::Catalog::TextToTaxt.convert(data[:note])
-
-      family = create! name:         name,
-                       status:              'valid',
-                       protonym:            protonym,
-                       type_name:           type_name,
-                       type_taxt:           type_taxt,
-                       headline_notes_taxt: headline_notes_taxt
-
-      data[:taxonomic_history].each {|item| family.taxonomic_history_items.create! :taxt => item}
-
+      attributes = {
+        name:                Name.import(family_name: 'Formicidae'),
+        fossil:              false,
+        status:              'valid',
+        protonym:            Protonym.import(data[:protonym]),
+        headline_notes_taxt: Importers::Bolton::Catalog::TextToTaxt.convert(data[:note]),
+      }
+      attributes.merge! get_type_attributes :type_genus, data
+      family = create! attributes
+      data[:taxonomic_history].each do |item|
+        family.taxonomic_history_items.create! taxt: item
+      end
       family
     end
   end
