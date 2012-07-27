81b8b43b8127bc5156f6dee736877a7984cf7fd9
Mark Wilden <mark@mwilden.com>
2012-07-27 13:02:02 -0700

    Return to prior page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
+  before_filter :save_location
 
   def preview?
     $ReleaseType.preview?
+  end
+
+  def save_location
+    session[:user_return_to] = request.url
   end
 
 end
