efd1e098d233b188e3f31ef34f0c3ed204da222d
Mark Wilden <mark@mwilden.com>
2012-08-02 15:41:49 -0700

    Name.to_html_with_fossil, html_epithet_with_fossil

--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -6,4 +6,16 @@ describe Name do
     Name.new(name:'Name').name.should == 'Name'
   end
 
+  it "should format the fossil symbol" do
+    SpeciesName.new(html_epithet: '<i>major</i>').html_epithet_with_fossil(true).should == '<i>&dagger;</i><i>major</i>'
+    SpeciesName.new(html_epithet: '<i>major</i>').html_epithet_with_fossil(false).should == '<i>major</i>'
+    GenusName.new(html_epithet: '<i>Atta</i>').html_epithet_with_fossil(true).should == '<i>&dagger;</i><i>Atta</i>'
+    GenusName.new(html_epithet: '<i>Atta</i>').html_epithet_with_fossil(false).should == '<i>Atta</i>'
+    SubfamilyName.new(html_epithet: 'Attanae').html_epithet_with_fossil(true).should == '&dagger;Attanae'
+    SubfamilyName.new(html_epithet: 'Attanae').html_epithet_with_fossil(false).should == 'Attanae'
+
+    SpeciesName.new(html_name: '<i>Atta major</i>').to_html_with_fossil(false).should == '<i>Atta major</i>'
+    SpeciesName.new(html_name: '<i>Atta major</i>').to_html_with_fossil(true).should == '<i>&dagger;</i><i>Atta major</i>'
+  end
+
 end
