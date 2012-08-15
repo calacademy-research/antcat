98206266e13513f81635e56cfdcb507a4ea2600b
Mark Wilden <mark@mwilden.com>
2012-08-15 09:56:28 -0700

    Insert species name correctly when creating subspecies name (GREEN)

--- app/models/subspecies.rb
+++ app/models/subspecies.rb
@@ -9,7 +9,16 @@ class Subspecies < SpeciesGroupTaxon
     name_data = data[:protonym]
     name_data[:genus] = data[:genus]
     name_data[:subspecies_epithet] = data[:species_group_epithet] || data[:species_epithet]
+    adjust_species_when_differs_from_protonym name_data, data[:raw_history]
     Name.import name_data
+  end
+
+  def self.adjust_species_when_differs_from_protonym name_data, history
+    currently_subspecies_of = get_currently_subspecies_of_from_history history
+    return unless currently_subspecies_of
+    name_data[:subspecies] ||= []
+    name_data[:subspecies].unshift subspecies_epithet: name_data[:species_epithet]
+    name_data[:species_epithet] = currently_subspecies_of
   end
 
   def self.after_creating taxon, data
