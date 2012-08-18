9bda149e218bcac5c1558e23e3486d0c954d199d
Mark Wilden <mark@mwilden.com>
2012-08-17 18:58:15 -0700

    Handle being revived and raised

--- lib/importers/bolton/catalog/species/history.rb
+++ lib/importers/bolton/catalog/species/history.rb
@@ -183,7 +183,9 @@ class Importers::Bolton::Catalog::Species::History
   end
 
   def item_raised_to_species? item
-    item[:raised_to_species] or item[:matched_text] =~ /Raised to species/
+    item[:raised_to_species] or
+    item[:revived_from_synonymy].try(:[], :raised_to_species) or
+    item[:matched_text] =~ /Raised to species/
   end
 
   def item_revived_as_subspecies? item
