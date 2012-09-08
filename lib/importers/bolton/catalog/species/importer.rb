fa3847e6e71ac1fe040064495a6a84de4e88b4cd
Mark Wilden <mark@mwilden.com>
2012-09-08 10:28:35 -0700

    Debug

--- lib/importers/bolton/catalog/species/importer.rb
+++ lib/importers/bolton/catalog/species/importer.rb
@@ -150,7 +150,7 @@ class Importers::Bolton::Catalog::Species::Importer < Importers::Bolton::Catalog
     if AntCat::ImportAllFiles
       super Dir.glob("#{$BOLTON_DATA_DIRECTORY}/NGC-Sp*.htm")
     else
-      ['cam1'].map {|e| "#{$BOLTON_DATA_DIRECTORY}/NGC-Sp#{e}.htm"}
+      ['cam1', 'cam2'].map {|e| "#{$BOLTON_DATA_DIRECTORY}/NGC-Sp#{e}.htm"}
     end
   end
 
