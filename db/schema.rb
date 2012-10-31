76d5a84ffe3a14e39b737200ffffd3af8a6d9495
Mark Wilden <mark@mwilden.com>
2012-10-31 16:50:21 -0700

    Add id and result columns to antwiki_valid_taxa

--- db/schema.rb
+++ db/schema.rb
@@ -13,26 +13,28 @@
 
 ActiveRecord::Schema.define(:version => 20121029232853) do
 
-  create_table "antwiki_valid_taxa", :id => false, :force => true do |t|
-    t.string   "name"
-    t.string   "subfamilia"
-    t.string   "tribus"
-    t.string   "genus"
-    t.string   "species"
-    t.string   "binomial"
-    t.string   "binomial_authority"
-    t.string   "subspecies"
-    t.string   "trinomial"
-    t.string   "trinomial_authority"
-    t.string   "author"
-    t.string   "year"
-    t.string   "changed_comb"
-    t.string   "type_locality_country"
-    t.string   "source"
-    t.string   "images"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "antwiki_valid_taxa", :force => true do |t|
+    t.string "name"
+    t.string "subfamilia"
+    t.string "tribus"
+    t.string "genus"
+    t.string "species"
+    t.string "binomial"
+    t.string "binomial_authority"
+    t.string "subspecies"
+    t.string "trinomial"
+    t.string "trinomial_authority"
+    t.string "author"
+    t.string "year"
+    t.string "changed_comb"
+    t.string "type_locality_country"
+    t.string "source"
+    t.string "images"
+    t.string "result"
   end
+
+  add_index "antwiki_valid_taxa", ["name"], :name => "antwiki_valid_taxa_name_index", :unique => true
+  add_index "antwiki_valid_taxa", ["name"], :name => "antwiki_valid_taxa_name_index2", :unique => true
 
   create_table "author_names", :force => true do |t|
     t.string   "name"
