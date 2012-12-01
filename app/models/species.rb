3256f86e882b52998f3ba1525ef8fee8a666ccc4
Mark Wilden <mark@mwilden.com>
2012-12-01 13:29:17 -0800

    Species#become_subspecies_of

--- app/models/species.rb
+++ app/models/species.rb
@@ -14,6 +14,23 @@ class Species < SpeciesGroupTaxon
     get_statistics [:subspecies]
   end
 
+  def become_subspecies_of species
+    new_name_string = species.genus.name.to_s + ' ' + species.name.epithet + ' ' + name.epithet
+    raise if Subspecies.find_by_name new_name_string
+    new_name = SubspeciesName.find_by_name new_name_string
+    new_name ||= SubspeciesName.new
+    new_name.update_attributes({
+      name:           new_name_string,
+      name_html:      Formatters::Formatter.italicize(new_name_string),
+      epithet:        name.epithet,
+      epithet_html:   name.epithet_html,
+      epithets:       species.name.epithet + ' ' + name.epithet,
+      protonym_html:  name.protonym_html,
+    })
+    Species.connection.execute "UPDATE taxa SET type = 'Subspecies' WHERE id = '#{id}'"
+    Subspecies.find(id).update_attributes name: new_name, species: species
+  end
+
   def self.import_myrmicium_heerii
     parse_result = Importers::Bolton::Catalog::Species::Grammar.parse(
       '*<i>heerii</i>. *<i>Myrmicium heerii</i> Westwood, 1854: 396, pl. 18, fig. 21 (wing) GREAT BRITAIN. Transferred to *Myrmiciidae (Symphyta): Maa, 1949: 17; Rasnitsyn, 1969: 17; to *Pseudosiricidae (Symphyta): Handlirsch, 1906: 577; Abe & Smith, D.R. 1991: 53. Excluded from Formicidae.', root: :species_group_record).value
