24ac17157f85db6a686471ee3466ec1c80d62f9b
Mark Wilden <mark@mwilden.com>
2012-12-13 11:56:47 -0800

    Don't require authorization

--- app/controllers/taxon_pickers_controller.rb
+++ app/controllers/taxon_pickers_controller.rb
@@ -1,9 +1,6 @@
 # coding: UTF-8
 class TaxonPickersController < ApplicationController
 
-  before_filter :authenticate_user!
-  skip_before_filter :authenticate_user!, :if => :preview?
-
   def index
     respond_to do |format|
       format.json {render :json => Taxon.names_and_authorships(params[:term]).to_json}
