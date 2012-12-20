4a1713147f6de1f35c6f0ae0940333fb320a0399
Mark Wilden <mark@mwilden.com>
2012-12-19 17:00:32 -0800

    Use real IDs instead of just the dummy parameter name

--- app/controllers/reference_pickers_controller.rb
+++ app/controllers/reference_pickers_controller.rb
@@ -4,15 +4,12 @@ class ReferencePickersController < ApplicationController
   def show
     params[:search_selector] ||= 'Search for author(s)'
 
-    if params[:id].present?
-      @references = Reference.perform_search id: params[:id]
-      @current_reference = @references.first
-    end
+    reference = Reference.find params[:id] if params[:id].present?
 
     if params[:q].present?
       params[:q].strip!
       params[:is_author_search] = params[:search_selector] == 'Search for author(s)'
-      @references = Reference.do_search params
+      references = Reference.do_search params
     end
 
     render partial: 'show', locals: {references: references, reference: reference}
