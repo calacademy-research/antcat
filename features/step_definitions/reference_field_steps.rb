399ced98798022f9e582f515f09045c79bdde88e
Mark Wilden <mark@mwilden.com>
2012-12-22 10:58:38 -0800

    Refine selectors

--- features/step_definitions/reference_field_steps.rb
+++ features/step_definitions/reference_field_steps.rb
@@ -1,16 +1,16 @@
 # coding: UTF-8
 When /^I click the first search result$/ do
-  step 'I click ".search_results .display:first"'
+  step 'I click "#authorship_picker .search_results .display:first"'
 end
 
 Then /^the field should contain the reference by (\w+)$/ do |author|
-  page.find('.current .display').text.should =~ /#{author}.*/
+  page.find('#authorship_picker .current .display').text.should =~ /#{author}.*/
 end
 
 Then /^the field should contain "([^"]*)"$/ do |contents|
-  page.find('.current .display').text.should == contents
+  page.find('#authorship_picker .current .display').text.should == contents
 end
 
 When /^I click the expand icon$/ do
-  step 'I click ".current .expand_collapse_icon"'
+  step 'I click "#authorship_picker .current .expand_collapse_icon"'
 end
