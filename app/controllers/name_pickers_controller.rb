d5e9eea799fa4e13bbd79f6e0a44885f673adbe6
Mark Wilden <mark@mwilden.com>
2012-12-24 17:40:52 -0800

    Fix parameter name

--- app/controllers/name_pickers_controller.rb
+++ app/controllers/name_pickers_controller.rb
@@ -8,14 +8,14 @@ class NamePickersController < ApplicationController
   end
 
   def lookup
-    taxon = Taxon.find_by_name params[:taxon_name]
+    taxon = Taxon.find_by_name params[:name]
     if taxon
       taxt = Taxt.to_editable_taxon taxon
       error_message = nil
       success = true
     else
       taxt = nil
-      error_message = "The taxon '#{params[:taxon_name]}' was not found"
+      error_message = "The taxon '#{params[:name]}' was not found"
       success = false
     end
     send_back_json {{taxt: taxt, success: success, error_message: error_message}}
