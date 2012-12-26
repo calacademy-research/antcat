644edaf1e0ddab65b6b9f18c96cd681e25197494
Mark Wilden <mark@mwilden.com>
2012-12-25 21:18:15 -0800

    #authorship_picker -> #picker, for generalizing step definitions.

--- features/step_definitions/reference_field_steps.rb
+++ features/step_definitions/reference_field_steps.rb
@@ -1,16 +1,16 @@
 # coding: UTF-8
 When /^I click the first search result$/ do
-  step 'I click "#authorship_picker .search_results .display:first"'
+  step 'I click "#picker .search_results .display:first"'
 end
 
 Then /^the field should contain the reference by (\w+)$/ do |author|
-  page.find('#authorship_picker .current .display').text.should =~ /#{author}.*/
+  page.find('#picker .current .display').text.should =~ /#{author}.*/
 end
 
 Then /^the field should contain "([^"]*)"$/ do |contents|
-  page.find('#authorship_picker .current .display').text.should == contents
+  page.find('#picker .display').text.should == contents
 end
 
 When /^I click the expand icon$/ do
-  step 'I click "#authorship_picker .current .expand_collapse_icon"'
+  step 'I click "#picker .expand_collapse_icon"'
 end
