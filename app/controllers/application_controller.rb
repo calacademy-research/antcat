efee1af0a51048f03dc4cdd1a7fc7dbbde869066
Mark Wilden <mark@mwilden.com>
2012-12-25 16:20:49 -0800

    Simplify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
     session[:user_return_to] = request.url unless request.url =~ %r{/users/}
   end
 
-  def send_back_json
-    json = yield.to_json
+  def send_back_json hash
+    json = hash.to_json
     json = '<textarea>' + json + '</textarea>' unless
       params[:picker].present? || Rails.env.test?
     render json: json, content_type: 'text/html'
