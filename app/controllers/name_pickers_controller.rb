0d55f7370f84a5c4285fb92d14f2c3bea072d3b2
Mark Wilden <mark@mwilden.com>
2012-12-27 20:15:43 -0800

    Send back taxt and taxon_id

--- app/controllers/name_pickers_controller.rb
+++ app/controllers/name_pickers_controller.rb
@@ -17,15 +17,21 @@ class NamePickersController < ApplicationController
     if name
       id = name.id
       name = name.name
-      #taxt = Taxt.to_editable_taxon name
       error_message = nil
       success = true
+      taxon = Taxon.find_by_name name
+      if taxon
+        taxon_id = taxon.id
+        taxt = Taxt.to_editable_taxon taxon
+      else
+        taxon_id = taxt = nil
+      end
     else
-      id = taxt = name = nil
+      id = taxt = name = taxon_id = nil
       error_message = "The name '#{params[:name_string]}' was not found"
       success = false
     end
-    send_back_json id: id, name: name, success: success, error_message: error_message
+    send_back_json id: id, name: name, taxt: taxt, taxon_id: taxon_id, success: success, error_message: error_message
   end
 
 end
