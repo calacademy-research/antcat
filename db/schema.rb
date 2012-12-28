813eb92624c44c6725cc76033a7d5ca7f513c589
Mark Wilden <mark@mwilden.com>
2012-12-27 17:26:31 -0800

    Name name index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121209204617) do
+ActiveRecord::Schema.define(:version => 20121228011608) do
 
   create_table "antwiki_valid_taxa", :id => false, :force => true do |t|
     t.string   "name"
@@ -136,6 +136,8 @@ ActiveRecord::Schema.define(:version => 20121209204617) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "names", ["name"], :name => "name_name_index"
 
   create_table "places", :force => true do |t|
     t.string   "name"
