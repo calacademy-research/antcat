6721763d4b1174726c11efe49ca7820785f454c7
Mark Wilden <mark@mwilden.com>
2013-01-05 12:09:47 -0800

    Add _item to names

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -4,7 +4,7 @@ class Citation < ActiveRecord::Base
 
   def self.import data
     reference = Reference.find_by_bolton_key data
-    notes_taxt = data[:notes] ? Importers::Bolton::Catalog::TextToTaxt.notes(data[:notes]) : nil
+    notes_taxt = data[:notes] ? Importers::Bolton::Catalog::TextToTaxt.notes_item(data[:notes]) : nil
     create! reference: reference, pages: data[:pages], forms: data[:forms], notes_taxt: notes_taxt
   end
 
