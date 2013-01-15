5aa95619ca83980e8567c2c22427784c10b4a0ef
Mark Wilden <mark@mwilden.com>
2013-01-14 17:13:50 -0800

    Citation.reference update

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -11,10 +11,21 @@ class Citation < ActiveRecord::Base
 
   def update_data data
     attributes = {}
+    update_reference data
     update_field 'pages', data[:pages], attributes
     update_field 'forms', data[:forms], attributes
     update_notes_taxt data, attributes
     update_attributes attributes
+  end
+
+  def update_reference data
+    before = reference
+    after = Reference.find_by_bolton_key data
+    if before != after
+      Update.create! class_name: self.class.to_s, record_id: id, field_name: 'reference',
+        before: before.key.to_s, after: after.key.to_s
+      self.reference = after
+    end
   end
 
 end
