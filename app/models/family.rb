d6a02c610c788a72918e47786c3436f431547273
Mark Wilden <mark@mwilden.com>
2013-01-15 10:51:53 -0800

    Format

--- app/models/family.rb
+++ app/models/family.rb
@@ -27,14 +27,18 @@ class Family < Taxon
 
   def update_data data
     attributes = {}
-    update_field 'fossil', data[:fossil], attributes
-    update_field 'status', data[:status], attributes
+
+    update_field      'fossil',              data[:fossil], attributes
+    update_field      'status',              data[:status], attributes
     update_taxt_field 'headline_notes_taxt', data[:note], attributes
+
     type_attributes = self.class.get_type_attributes :type_genus, data
-    update_name_field 'type_name', type_attributes[:type_name], attributes
-    update_field 'type_taxt', type_attributes[:type_taxt], attributes
-    update_field 'type_fossil', type_attributes[:type_fossil], attributes
+    update_name_field 'type_name',    type_attributes[:type_name], attributes
+    update_field      'type_taxt',    type_attributes[:type_taxt], attributes
+    update_field      'type_fossil',  type_attributes[:type_fossil], attributes
+
     update_attributes attributes
+
     protonym.update_data data[:protonym]
     update_history data[:history]
   end
