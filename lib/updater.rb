df6740b246ed24a3e83f3a01094a3bfece54ae3e
Mark Wilden <mark@mwilden.com>
2013-01-15 10:51:42 -0800

    Move update methods to class that uses them

--- lib/updater.rb
+++ lib/updater.rb
@@ -1,37 +1,14 @@
 # coding: UTF-8
 module Updater
 
-  def update_history history_data
-    # compare and update common subset
-    i = 0
-    while i < history_items.count && i < history_data.count
-      item = history_items.all[i]
-      before = item.taxt
-      after = history_data[i]
-      if before != after
-        Update.create! class_name: 'TaxonHistoryItem', record_id: item.id, field_name: 'taxt',
-          before: before, after: after
-        item.update_attributes taxt: after
-      end
-      i += 1
+  def update_field field_name, new_value, attributes
+    before = self[field_name]
+    after = new_value
+    if before != after
+      Update.create! class_name: self.class.to_s, record_id: id, field_name: field_name,
+        before: before, after: after
+      attributes[field_name] = after
     end
-    # add new ones
-    while i < history_data.count
-      new_taxt = history_data[i]
-      new_item = history_items.create! taxt: new_taxt
-      Update.create! class_name: 'TaxonHistoryItem', record_id: new_item.id,
-        field_name: 'taxt', before: nil, after: new_taxt
-      i += 1
-    end
-    # delete deleted ones
-    items_to_delete = []
-    while i < history_items.count
-      items_to_delete << history_items[i].id
-      Update.create! class_name: 'TaxonHistoryItem', record_id: history_items[i].id,
-        field_name: 'taxt', before: nil, after: nil
-      i += 1
-    end
-    items_to_delete.each {|item| TaxonHistoryItem.delete item}
   end
 
   def update_name_field field_name, new_name, attributes
@@ -44,28 +21,8 @@ module Updater
     end
   end
 
-  def update_field field_name, new_value, attributes
-    before = self[field_name]
-    after = new_value
-    if before != after
-      Update.create! class_name: self.class.to_s, record_id: id, field_name: field_name,
-        before: before, after: after
-      attributes[field_name] = after
-    end
-  end
-
   def update_taxt_field field_name, new_value, attributes
     update_field field_name, Importers::Bolton::Catalog::TextToTaxt.convert(new_value), attributes
-  end
-
-  def update_notes_taxt data, attributes
-    before = self['notes_taxt']
-    after = data[:notes] ? Importers::Bolton::Catalog::TextToTaxt.notes_item(data[:notes]) : nil
-    if before != after
-      Update.create! class_name: self.class.to_s, record_id: id, field_name: 'notes_taxt',
-        before: before, after: after
-      attributes['notes_taxt'] = after
-    end
   end
 
 end
