d56f42184597e753c7db1b82a2b8160372621e0f
Mark Wilden <mark@mwilden.com>
2013-01-15 20:45:49 -0800

    Refactor

--- app/models/subfamily.rb
+++ app/models/subfamily.rb
@@ -31,21 +31,7 @@ class Subfamily < Taxon
   end
 
   def update_data data
-    attributes = {}
-
-    update_boolean_field  'fossil',              data[:fossil], attributes
-    update_field          'status',              data[:status] || 'valid', attributes
-    update_taxt_field     'headline_notes_taxt', data[:note], attributes
-
-    type_attributes = self.class.get_type_attributes :type_genus, data
-    update_name_field     'type_name',    type_attributes[:type_name], attributes
-    update_field          'type_taxt',    type_attributes[:type_taxt], attributes
-    update_boolean_field  'type_fossil',  type_attributes[:type_fossil], attributes
-
-    update_attributes attributes
-
-    protonym.update_data data[:protonym]
-    update_history data[:history]
+    update_family_or_subfamily data
   end
 
   #########
