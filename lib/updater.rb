9d11f194be1cf0aa515018dd3da14b5e0605ae77
Mark Wilden <mark@mwilden.com>
2013-01-15 20:45:49 -0800

    Refactor

--- lib/updater.rb
+++ lib/updater.rb
@@ -76,4 +76,22 @@ module Updater
     items_to_delete.each {|item| TaxonHistoryItem.delete item}
   end
 
+  def update_family_or_subfamily data
+    attributes = {}
+
+    update_boolean_field  'fossil',              data[:fossil], attributes
+    update_field          'status',              data[:status] || 'valid', attributes
+    update_taxt_field     'headline_notes_taxt', data[:note], attributes
+
+    type_attributes = self.class.get_type_attributes :type_genus, data
+    update_name_field     'type_name',    type_attributes[:type_name], attributes
+    update_field          'type_taxt',    type_attributes[:type_taxt], attributes
+    update_boolean_field  'type_fossil',  type_attributes[:type_fossil], attributes
+
+    update_attributes attributes
+
+    protonym.update_data data[:protonym]
+    update_history data[:history]
+  end
+
 end
