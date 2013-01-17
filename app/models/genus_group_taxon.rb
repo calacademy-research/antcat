ceb193875bc103090e748d9eeba7335710d3f1b0
Mark Wilden <mark@mwilden.com>
2013-01-17 12:01:37 -0800

    Format

--- app/models/genus_group_taxon.rb
+++ app/models/genus_group_taxon.rb
@@ -3,6 +3,30 @@ class GenusGroupTaxon < Taxon
   belongs_to :subfamily
   belongs_to :tribe
 
+  def self.import data, attributes = {}
+    transaction do
+      attributes.merge!(
+        name:                 Name.import(data),
+        fossil:               data[:fossil] || false,
+        status:               data[:status] || 'valid',
+        protonym:             Protonym.import(data[:protonym]),
+        headline_notes_taxt:  Importers::Bolton::Catalog::TextToTaxt.convert(data[:note]),
+      ).merge! parent_attributes data, attributes
+      attributes.merge! data[:attributes] if data[:attributes]
+      attributes.merge! get_type_attributes :type_species, data
+      senior = attributes.delete :synonym_of
+      taxon = create! attributes
+      taxon.import_synonyms senior
+      taxon.import_history data
+      taxon
+    end
+  end
+
+  def self.parent_attributes data, attributes
+    {subfamily: data[:subfamily], tribe: data[:tribe]}
+  end
+
+  ###########
   def children
     species
   end
@@ -25,35 +49,6 @@ class GenusGroupTaxon < Taxon
     string << ')' if subfamily || tribe
     string << " #{id}"
     string
-  end
-
-  def self.parent_attributes data, attributes
-    {subfamily: data[:subfamily], tribe: data[:tribe]}
-  end
-
-  def self.import data, attributes = {}
-    transaction do
-      attributes.merge!(
-        name:                 Name.import(data),
-        fossil:               data[:fossil] || false,
-        status:               data[:status] || 'valid',
-        protonym:             Protonym.import(data[:protonym]),
-        headline_notes_taxt:  Importers::Bolton::Catalog::TextToTaxt.convert(data[:note]),
-      ).merge! parent_attributes data, attributes
-      attributes.merge! data[:attributes] if data[:attributes]
-      attributes.merge! get_type_attributes :type_species, data
-      senior = attributes.delete :synonym_of
-      taxon = create! attributes
-      taxon.import_synonyms senior
-      taxon.import_history data
-      taxon
-    end
-  end
-
-  def import_history data
-    for item in data[:history].select &:present?
-      history_items.create! taxt: item
-    end
   end
 
 end
