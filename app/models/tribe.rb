f0c4d5d74bf8305b7a337b1c596f1c45527ed8e3
Mark Wilden <mark@mwilden.com>
2013-01-16 20:39:23 -0800

    Move ill-placed code

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -27,36 +27,18 @@ class Tribe < Taxon
     end
   end
 
+
   def update_data data
     update_synonyms do
-      previous_junior_synonyms = junior_synonyms
-      previous_senior_synonyms = senior_synonyms
+      senior = data.delete :synonym_of
 
       update_taxon data
 
+      import_synonyms senior
+
       current_junior_synonyms = junior_synonyms
       current_senior_synonyms = senior_synonyms
-
-      new_junior_synonyms = current_junior_synonyms - previous_junior_synonyms
-      for junior_synonym in new_junior_synonyms
-        Update.create! class_name: 'Tribe', record_id: id, field_name: 'senior_synonym_of',
-          before: nil, after: junior_synonym.name.name
-      end
-
-      deleted_junior_synonyms = previous_junior_synonyms - current_junior_synonyms
-      for junior_synonym in deleted_junior_synonyms
-        Update.create! class_name: 'Tribe', record_id: id, field_name: 'senior_synonym_of',
-          before: junior_synonym.name.name, after: nil
-        Synonym.where([junior_synonym_id: junior_synonym, senior_synonym_id: id]).destroy_all
-      end
-
-      #new_senior_synonyms = current_senior_synonyms - previous_senior_synonyms
-      #for senior_synonym in new_senior_synonyms
-        #Update.create! class_name: 'Tribe', record_id: id, field_name: 'senior_synonym_of',
-          #before: nil, after: senior_synonym.senior_synonym
-      #end
     end
-
   end
 
   #########
