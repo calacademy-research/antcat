e7525dac5a65d6996bffafd24ec284876dc6deae
Mark Wilden <mark@mwilden.com>
2013-01-17 09:50:31 -0800

    update_synonyms

--- spec/lib/importers/bolton/catalog/updater_spec.rb
+++ spec/lib/importers/bolton/catalog/updater_spec.rb
@@ -11,6 +11,7 @@ describe Importers::Bolton::Catalog::Updater do
       type_genus: {genus_name: 'Atta'},
       history: ['Attini history'],
     }
+    # create the tribe from scratch
     @attini = Tribe.import @data
     @ecitonini = create_tribe 'Ecitonini'
     @bacerosini = create_tribe 'Baserosini'
@@ -21,20 +22,23 @@ describe Importers::Bolton::Catalog::Updater do
     it "should handle the addition of a synonym" do
       @data[:status] = 'synonym'
       @data[:synonym_of] = @bacerosini
+      # update the existing tribe
       Tribe.import @data
       @attini.reload.should be_junior_synonym_of @bacerosini
+      @bacerosini.reload.should be_senior_synonym_of @attini
 
       Update.count.should == 2
 
-      update = Update.find_by_record_id_and_field_name @attini.id, :junior_synonym_of
+      update = Update.find_by_record_id_and_field_name @attini.id, 'status'
+      update.class_name.should == 'Tribe'
+      update.before.should == 'valid'
+      update.after.should == 'synonym'
+
+      update = Update.find_by_record_id_and_field_name @attini.id, 'junior_synonym_of'
       update.class_name.should == 'Tribe'
       update.before.should == nil
       update.after.should == 'Baserosini'
 
-      update = Update.find_by_record_id_and_field_name @attini.id, :status
-      update.class_name.should == 'Tribe'
-      update.before.should == 'valid'
-      update.after.should == 'synonym'
     end
   end
 
