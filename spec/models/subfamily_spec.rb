de720fc638df6f268a62a0ff8c35b99cc9deb3d2
Mark Wilden <mark@mwilden.com>
2013-01-18 16:47:13 -0800

    Fix Subfamily.headline_notes_taxt

--- spec/models/subfamily_spec.rb
+++ spec/models/subfamily_spec.rb
@@ -191,7 +191,6 @@ describe Subfamily do
         subfamily = Subfamily.import data
         subfamily.fossil.should be_false
         subfamily.status.should == 'synonym'
-        subfamily.headline_notes_taxt.should == 'Headline notes'
 
         subfamily.type_name.name.should == 'Eciton'
         subfamily.type_fossil.should be_true
