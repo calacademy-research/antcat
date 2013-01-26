2fe9ff6c16448e4ae6ff9b9503bb34ba9b52b711
Mark Wilden <mark@mwilden.com>
2013-01-26 10:13:36 -0800

    Format

--- app/models/species_group_taxon.rb
+++ app/models/species_group_taxon.rb
@@ -6,25 +6,6 @@ class SpeciesGroupTaxon < Taxon
   belongs_to :subgenus
   before_create :set_subfamily
 
-  def set_subfamily
-    self.subfamily = genus.subfamily if genus
-  end
-
-  ##################################################
-  def self.find_validest_for_epithet_in_genus epithet, genus
-    pick_validest find_epithet_in_genus epithet, genus
-  end
-
-  def self.pick_validest targets
-    return unless targets
-    validest = targets.select {|target| target.status == 'valid'}
-    if validest.empty?
-      validest = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
-    end
-    validest.presence
-  end
-
-  ##################################################
   def self.import data
     transaction do
       protonym = import_protonym data
@@ -121,4 +102,24 @@ class SpeciesGroupTaxon < Taxon
   end
 
   class NoProtonymError < StandardError; end
+
+  def set_subfamily
+    self.subfamily = genus.subfamily if genus
+  end
+
+  ##################################################
+  def self.find_validest_for_epithet_in_genus epithet, genus
+    pick_validest find_epithet_in_genus epithet, genus
+  end
+
+  def self.pick_validest targets
+    return unless targets
+    validest = targets.select {|target| target.status == 'valid'}
+    if validest.empty?
+      validest = targets.select {|target| target.status != 'valid' and target.status != 'homonym'}
+    end
+    validest.presence
+  end
+
+  ##################################################
 end
