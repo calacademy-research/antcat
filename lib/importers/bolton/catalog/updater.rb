19a238a9485d82b0bea6eac8b352dd476e9237e7
Mark Wilden <mark@mwilden.com>
2013-01-26 12:55:37 -0800

    Better messages

--- lib/importers/bolton/catalog/updater.rb
+++ lib/importers/bolton/catalog/updater.rb
@@ -156,7 +156,7 @@ module Importers::Bolton::Catalog::Updater
       before = item.taxt
       after = history_data[i]
       if before != after
-        Update.create! name: 'History', class_name: 'TaxonHistoryItem', record_id: item.id, field_name: 'taxt',
+        Update.create! name: item.taxon.name.name, class_name: 'TaxonHistoryItem', record_id: item.id, field_name: 'taxt',
           before: before, after: after
         item.update_attributes taxt: after
       end
@@ -166,7 +166,7 @@ module Importers::Bolton::Catalog::Updater
     while i < history_data.count
       new_taxt = history_data[i]
       new_item = history_items.create! taxt: new_taxt
-      Update.create! name: 'History', class_name: 'TaxonHistoryItem', record_id: new_item.id,
+      Update.create! name: item.taxon.name.name, class_name: 'TaxonHistoryItem', record_id: new_item.id,
         field_name: 'taxt', before: nil, after: new_taxt
       i += 1
     end
@@ -175,7 +175,7 @@ module Importers::Bolton::Catalog::Updater
     while i < history_items.count
       items_to_delete << history_items[i].id
       Update.create! name: 'History', class_name: 'TaxonHistoryItem', record_id: history_items[i].id,
-        field_name: 'taxt', before: history_items[i].taxt, after: nil
+        field_name: 'delete', before: history_items[i].taxt, after: nil
       i += 1
     end
     items_to_delete.each {|item| TaxonHistoryItem.delete item}
