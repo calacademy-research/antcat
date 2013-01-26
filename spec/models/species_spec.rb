392368e521c8fc4f517175ada1afe31eba7bc116
Mark Wilden <mark@mwilden.com>
2013-01-26 10:13:53 -0800

    Format

--- spec/models/species_spec.rb
+++ spec/models/species_spec.rb
@@ -222,39 +222,25 @@ describe Species do
     end
     it "should not change the history when going through twice" do
       atta = create_genus 'Atta'
-      data =
-      {:type=>:species_record,
-       genus: atta,
-      :species_group_epithet=>"major",
-      :protonym=>
-        {:genus_name=>"Atta",
-        :species_epithet=>"major",
-        :authorship=>
-          [{:author_names=>["Latreille"],
-            :year=>"1809",
-            :pages=>"152",
-            :notes=>
-            [[{:phrase=>"w", :delimiter=>"."},
-              {:phrase=>", not m", :delimiter=>". "},
-              {:phrase=>"as stated"}]],
-            :matched_text=>"Santschi, 1925c: 152 (w., not m. as stated)"}],
-        :locality=>"Tanzania"},
-      :history=>
-        [{:text=>
-          [{:opening_bracket=>"["},
-            {:genus_name=>"Atta",
-            :species_epithet=>"major",
-            :authorship=>
-              [{:author_names=>["Latreille"],
-                :year=>"1809",
-                :pages=>"116",
-                :matched_text=>""}],
-            :delimiter=>". "},
-            {:genus_name=>"Nomen", :species_epithet=>"nudum"},
-            {:phrase=>", attributed to Stitz", :delimiter=>"."},
-            {:closing_bracket=>"]"}],
-          :matched_text=>
-          ""}]}
+      data = {
+        :type=>:species_record,
+        genus: atta,
+        :species_group_epithet=>"major",
+        :protonym=>
+          {:genus_name=>"Atta",
+          :species_epithet=>"major",
+          :authorship=>
+            [{:author_names=>["Latreille"],
+              :year=>"1809",
+              :pages=>"152",
+              :notes=>
+              [[{:phrase=>"w", :delimiter=>"."},
+                {:phrase=>", not m", :delimiter=>". "},
+                {:phrase=>"as stated"}]],
+              :matched_text=>"Santschi, 1925c: 152 (w., not m. as stated)"}],
+          :locality=>"Tanzania"},
+        :history=> ['A history item']
+      }
       taxon = Species.import data
       taxon.history_items.count.should == 1
       taxon = Species.import data
