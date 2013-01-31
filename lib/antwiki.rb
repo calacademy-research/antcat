85fd175c3f4cbd647ef4b2fa3700265470eee718
Mark Wilden <mark@mwilden.com>
2013-01-30 16:54:54 -0800

    Comment

--- lib/antwiki.rb
+++ lib/antwiki.rb
@@ -1,12 +1,12 @@
 # coding: UTF-8
 # To get data from AntWiki to compare with AntCat:
+#
 # 1. Download http://ants.csiro.au/Ant_Wiki/images/9/9e/AntWiki_Valid_Species.txt
-# 2. Delete header row
-# 3. Save it in Excel as Tab-Delimited Text
-# 3. Truncate antwiki_valid_taxa
-# 3. In MySql, run "LOAD DATA LOCAL INFILE '/Users/mwilden/antcat/data/antwiki/AntWiki_Valid_Species.txt' REPLACE INTO TABLE antwiki_valid_taxa FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r' IGNORE 1 LINES;
-# 4. Add VARCHAR column 'result'
-# 4. Add PRIMARY KEY AUTOINCREMENT column 'result'
+# 1. Open in Excel, and save as Tab-Delimited Text
+# 3. In MySql, truncate antwiki_valid_taxa
+# 3. run "LOAD DATA LOCAL INFILE '/Users/mwilden/antcat/data/antwiki/AntWiki_Valid_Species.txt' REPLACE INTO TABLE antwiki_valid_taxa FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r' IGNORE 1 LINES;"
+# 4. Add VARCHAR column 'result', if necessary
+# 4. Add PRIMARY KEY AUTOINCREMENT column 'result', if necessary
 
 module Antwiki
 
