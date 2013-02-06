b9cdcd2e53b663590c9dade1425a8890f7f8e519
Mark Wilden <mark@mwilden.com>
2013-02-06 11:36:36 -0800

    Headline notes

--- app/controllers/widget_tests_controller.rb
+++ app/controllers/widget_tests_controller.rb
@@ -19,7 +19,7 @@ class WidgetTestsController < ApplicationController
   end
 
   def taxon_form_test
-    @taxon = Genus.first
+    @taxon = Genus.where('headline_notes_taxt != ""').first
   end
 
 end
