1031bb00f9fffd4a7d8a419b6e26292011fef45d
Mark Wilden <mark@mwilden.com>
2013-02-07 10:35:16 -0800

    Update devise, devise_invitable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130131012013) do
+ActiveRecord::Schema.define(:version => 20130207183019) do
 
   create_table "antwiki_valid_taxa", :force => true do |t|
     t.string   "name"
@@ -312,21 +312,22 @@ ActiveRecord::Schema.define(:version => 20130131012013) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                              :default => "", :null => false
-    t.string   "encrypted_password",                 :default => ""
-    t.string   "password_salt",                      :default => ""
+    t.string   "email",                                :default => "", :null => false
+    t.string   "encrypted_password",                   :default => ""
+    t.string   "password_salt",                        :default => ""
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      :default => 0
+    t.integer  "sign_in_count",                        :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "invitation_token",     :limit => 20
+    t.string   "invitation_token",       :limit => 20
     t.datetime "invitation_sent_at"
+    t.datetime "reset_password_sent_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
