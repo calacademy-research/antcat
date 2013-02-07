ada1b89d89f7fe00fcd01cb14409146693398de4
Mark Wilden <mark@mwilden.com>
2013-02-07 10:35:16 -0800

    Update devise, devise_invitable

---
+++ db/migrate/20130207183019_upgrade_devise.rb
@@ -0,0 +1,9 @@
+class UpgradeDevise < ActiveRecord::Migration
+  def up
+    add_column :users, :reset_password_sent_at, :datetime
+  end
+
+  def down
+    remove_column :users, :reset_password_sent_at
+  end
+end
