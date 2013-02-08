d8cd40306dea2416bd82746813a4c4ef36da11bf
Mark Wilden <mark@mwilden.com>
2013-02-08 13:36:09 -0800

    Organize step definitions

---
+++ features/step_definitions/edit_steps.rb
@@ -0,0 +1,13 @@
+# coding: UTF-8
+When /^I save my changes$/ do
+  step 'I press "Save"'
+  step 'I wait for a bit'
+end
+
+When /^I save the form$/ do
+  step 'I save my changes'
+end
+
+When /^I set the genus name to "([^"]*)"$/ do |name|
+  step %{I fill in "genus_name" with "#{name}"}
+end
