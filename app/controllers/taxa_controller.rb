802ac6f93d409d99d46d5a04dd9cc679f7e9b0d1
Mark Wilden <mark@mwilden.com>
2013-02-08 09:53:58 -0800

    Start updating from taxon form

--- app/controllers/taxa_controller.rb
+++ app/controllers/taxa_controller.rb
@@ -13,6 +13,23 @@ class TaxaController < ApplicationController
 
   def update
     @taxon = Taxon.find params[:id] 
+
+    unless update_name
+      render :edit and return
+    end
+  end
+
+  ###################
+  def update_name
+    begin
+      name = Name.import genus_name: params[:genus][:name_attributes][:name]
+    rescue ActiveRecord::RecordInvalid
+      @taxon.name.name = params[:genus][:name_attributes][:name]
+      @taxon.errors.add :base, "Name can't be blank"
+      return
+    end
+    @taxon.update_attributes name: name
+    redirect_to catalog_path @taxon
   end
 
 end
