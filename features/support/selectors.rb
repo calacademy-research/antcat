040efa34bf91191612b5a3620ee2e844800b5c27
Mark Wilden <mark@mwilden.com>
2013-02-20 11:02:32 -0800

    Remove comments. Format.

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -1,11 +1,5 @@
 # coding: UTF-8
 module HtmlSelectorsHelpers
-  # Maps a name to a selector. Used primarily by the
-  #
-  #   When /^(.+) within (.+)$/ do |step, scope|
-  #
-  # step definitions in web_steps.rb
-  #
   def selector_for(locator)
     case locator
 
@@ -45,29 +39,17 @@ module HtmlSelectorsHelpers
     when /the headline/
       '.headline'
 
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #  when /the (notice|error|info) flash/
-    #    ".flash.#{$1}"
     when /the name field/
       '.antcat_name_picker .display'
 
-    # You can also return an array to use a different selector
-    # type, like:
-    #
-    #  when /the header/
-    #    [:xpath, "//header"]
-
-    # This allows you to provide a quoted selector as the scope
-    # for "within" steps as was previously the default for the
-    # web steps:
     when /"(.+)"/
       $1
+
     else
       raise "Can't find mapping from \"#{locator}\" to a selector"
     end
+
   end
 end
 
-World(HtmlSelectorsHelpers)
+World HtmlSelectorsHelpers
