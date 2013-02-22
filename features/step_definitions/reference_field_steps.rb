768a86853bc47bb1efbf00d06596fd43e1d58cd1
Mark Wilden <mark@mwilden.com>
2013-02-22 11:43:16 -0800

    No more expand icon

--- features/step_definitions/reference_field_steps.rb
+++ features/step_definitions/reference_field_steps.rb
@@ -10,7 +10,3 @@ end
 Then /^the field should contain "([^"]*)"$/ do |contents|
   page.find('#picker .display').text.should == contents
 end
-
-When /^I click the expand icon$/ do
-  step 'I click "#picker .expand_collapse_icon"'
-end
