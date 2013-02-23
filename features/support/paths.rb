5605d66cfc9f1701f090f681ed066c2d0f127dc6
Mark Wilden <mark@mwilden.com>
2013-02-22 21:26:07 -0800

    Cleanup

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -2,39 +2,23 @@
 module NavigationHelpers
   def path_to page_name
     case page_name
+
     when /the main page/
       root_path
-    when /the references page/
-      references_path
-    when /the Bolton references page/
-      bolton_references_path
-    when /the page for that reference/
-      reference_path(@reference || Reference.first)    
-    when /the edit user page/
-      '/users/edit'
-    when /the "Edit journals" page/
-      journals_path
-    when /the Authors page/
-      authors_path
-    when /the (catalog entry|page) for "([^"]*)"/
-      taxon = Taxon.find_by_name $2
-      "/catalog/#{taxon.id}"
-    when /the catalog/
-      catalog_path
-    when /the forgot password page/
-      '/users/password/new'
-    when /the login page/
-      '/users/sign_in'
-    when /the reference field widget test page, opened to the first reference/
+
+    when /the reference field test page, opened to the first reference/
       "/widget_tests/reference_field_test?id=#{Reference.first.id}"
+    when /the reference field test page/
+      '/widget_tests/reference_field_test'
+
     when /the reference popup widget test page, opened to the first reference/
       "/widget_tests/reference_popup_test?id=#{Reference.first.id}"
     when /the reference popup widget test page/
       '/widget_tests/reference_popup_test'
-    when /the reference field widget test page/
-      '/widget_tests/reference_field_test'
+
     when /the taxt editor test page/
       '/widget_tests/taxt_editor_test'
+
     when /the name picker test page/
       '/widget_tests/name_picker_test'
     when /the name popup test page/
@@ -43,8 +27,36 @@ module NavigationHelpers
       "/widget_tests/name_field_test?id=#{Name.first.id}"
     when /the name field test page/
       '/widget_tests/name_field_test'
+
     when /the edit page for "(.*)"/
       "/taxa/#{Taxon.find_by_name($1).id}/edit"
+
+    when /the references page/
+      references_path
+    when /the page for that reference/
+      reference_path(@reference || Reference.first)
+    when /the Bolton references page/
+      bolton_references_path
+
+    when /the "Edit journals" page/
+      journals_path
+
+    when /the Authors page/
+      authors_path
+
+    when /the (catalog entry|page) for "([^"]*)"/
+      taxon = Taxon.find_by_name $2
+      "/catalog/#{taxon.id}"
+    when /the catalog/
+      catalog_path
+
+    when /the edit user page/
+      '/users/edit'
+    when /the forgot password page/
+      '/users/password/new'
+    when /the login page/
+      '/users/sign_in'
+
     else
       raise "#{page_name} not found"
     end
