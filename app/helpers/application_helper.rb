d1dc8677cf41b1fc4fd858b698c69ba0036b788e
Mark Wilden <mark@mwilden.com>
2013-03-06 10:15:00 -0800

    Style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
 
   def make_link_menu *items
     content_tag :span, class: 'link_menu' do |content|
-      items.flatten.inject("".html_safe) do |string, item|
+      items.flatten.inject(''.html_safe) do |string, item|
         string << ' | '.html_safe unless string.empty?
         string << item
       end
