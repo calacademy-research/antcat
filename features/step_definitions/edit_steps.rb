615f5e3ca77861bf9b5ee4e2169be3e8aaa6922b
Mark Wilden <mark@mwilden.com>
2013-03-10 11:44:08 -0700

    Move step defs

--- features/step_definitions/edit_steps.rb
+++ features/step_definitions/edit_steps.rb
@@ -48,3 +48,20 @@ end
 When /I click the type name field/ do
   find('#type_name_field .display_button').click
 end
+
+Then /^the history should be "(.*)"$/ do |history|
+  page.find('div.display').text.should =~ /#{history}\.?/
+end
+
+When /^I edit the history item to "([^"]*)"$/ do |history|
+  step %{I fill in "taxt_editor" with "#{history}"}
+end
+
+Given /^I edit the history item to include that reference$/ do
+  key = Taxt.id_for_editable @reference.id
+  step %{I edit the history item to "{#{key}}"}
+end
+
+Then /^I should see an error message about the unfound reference$/ do
+  step %{I should see "The reference '{123}' could not be found. Was the ID changed?"}
+end
