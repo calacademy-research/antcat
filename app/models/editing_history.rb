b58c288ba0ef1ca7d9c201138a4c4f590d919bb1
Mark Wilden <mark@mwilden.com>
2013-03-25 22:00:19 -0700

    Don't require user for editing history

--- app/models/editing_history.rb
+++ app/models/editing_history.rb
@@ -1,6 +1,6 @@
 # coding: UTF-8
 class EditingHistory < ActiveRecord::Base
   self.table_name = 'editing_history'
-  belongs_to :user; validates :user, presence: true
+  belongs_to :user
   belongs_to :taxon
 end
