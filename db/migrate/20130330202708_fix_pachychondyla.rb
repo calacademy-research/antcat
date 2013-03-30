4a02042bd64106e5bc8bc702dfa87b6ee5c83fd2
Mark Wilden <mark@mwilden.com>
2013-03-30 14:18:28 -0700

    Fix Pachycondyla darwinii indica. Delete duplicate names.

--- db/migrate/20130330202708_fix_pachychondyla.rb
+++ db/migrate/20130330202708_fix_pachychondyla.rb
@@ -1,7 +1,7 @@
 class FixPachychondyla < ActiveRecord::Migration
   def up
     name = Taxon.find_by_name_cache 'Pachycondyla darwinii indica'
-    Taxon.update_all({name_id: name.id}, name_cache: 'Pachycondyla darwinii indica')
+    Taxon.update_all({name_id: name.name.id}, name_cache: 'Pachycondyla darwinii indica')
   end
 
   def down
