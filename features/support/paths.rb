443d58aeb669a0ffc2401af2266c89b8d9c4adc6
Mark Wilden <mark@mwilden.com>
2013-04-03 23:31:52 -0700

    Rationalize paths
    Fix path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -3,50 +3,17 @@ module NavigationHelpers
   def path_to page_name
     case page_name
 
-    when /the main page/
+    when /^the main page$/
       root_path
-
-    when /the reference field test page, opened to the first reference/
-      "/widget_tests/reference_field_test?id=#{Reference.first.id}"
-    when /the reference field test page/
-      '/widget_tests/reference_field_test'
-
-    when /the reference popup widget test page, opened to the first reference/
-      "/widget_tests/reference_popup_test?id=#{Reference.first.id}"
-    when /the reference popup widget test page/
-      '/widget_tests/reference_popup_test'
-
-    when /the taxt editor test page/
-      '/widget_tests/taxt_editor_test'
-
-    when /the name popup test page/
-      '/widget_tests/name_popup_test'
-    when /the name field test page for a name/
-      "/widget_tests/name_field_test?id=#{Name.first.id}"
-    when /the name field test page/
-      '/widget_tests/name_field_test'
-
-    when /the edit page for "(.*)"/
-      "/taxa/#{Taxon.find_by_name($1).id}/edit"
-
-    when /the references page/
-      references_path
-    when /the page for that reference/
-      reference_path(@reference || Reference.first)
-    when /the Bolton references page/
-      bolton_references_path
-
-    when /the "Edit journals" page/
-      journals_path
-
-    when /the Authors page/
-      authors_path
 
     when /^the catalog (entry|page) for "([^"]*)"$/
       taxon = Taxon.find_by_name $2
       "/catalog/#{taxon.id}"
     when /^the catalog$/
       catalog_path
+
+    when /^the edit page for "(.*)"$/
+      "/taxa/#{Taxon.find_by_name($1).id}/edit"
 
     when /^the reverse synonymy page for "([^"]*)"$/
       taxon = Taxon.find_by_name $1
@@ -56,16 +23,49 @@ module NavigationHelpers
       taxon = Taxon.find_by_name $1
       "/taxa/#{taxon.id}/elevate_to_species"
 
-    when /the edit user page/
+    when /^the references page$/
+      references_path
+    when /^the page for that reference$/
+      reference_path(@reference || Reference.first)
+    when /^the Bolton references page$/
+      bolton_references_path
+
+    when /^the "Edit journals" page$/
+      journals_path
+
+    when /^the Authors page$/
+      authors_path
+
+    when /^the edit user page$/
       '/users/edit'
-    when /the forgot password page/
+    when /^the forgot password page$/
       '/users/password/new'
-    when /the login (page|form)/
+    when /^the login (page|form)$/
       '/users/sign_in'
+
+    when /^the reference field test page, opened to the first reference$/
+      "/widget_tests/reference_field_test?id=#{Reference.first.id}"
+    when /^the reference field test page/
+      '/widget_tests/reference_field_test'
+
+    when /^the reference popup widget test page, opened to the first reference$/
+      "/widget_tests/reference_popup_test?id=#{Reference.first.id}"
+    when /^the reference popup widget test page$/
+      '/widget_tests/reference_popup_test'
+
+    when /^the taxt editor test page$/
+      '/widget_tests/taxt_editor_test'
+    when /^the name popup test page$/
+      '/widget_tests/name_popup_test'
+    when /^the name field test page for a name$/
+      "/widget_tests/name_field_test?id=#{Name.first.id}"
+    when /^the name field test page$/
+      '/widget_tests/name_field_test'
 
     else
       raise "#{page_name} not found"
     end
+
   end
 end
 
