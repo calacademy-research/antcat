7b233e51cf78c76f4adab14a0a33aa093a9dad09
Mark Wilden <mark@mwilden.com>
2013-04-08 15:30:31 -0700

    Refactor

--- app/controllers/synonyms_controller.rb
+++ app/controllers/synonyms_controller.rb
@@ -2,22 +2,22 @@
 class SynonymsController < ApplicationController
 
   def create
-    @taxon = Taxon.find params[:taxa_id]
+    taxon = Taxon.find params[:taxa_id]
     synonym = Taxon.find_by_name params[:name]
-    unless synonym
-      error_message = 'Taxon not found'
-    else
-      error_message = ''
-      if Synonym.find_by_senior_synonym_id_and_junior_synonym_id params[:taxa_id], synonym.id
+    error_message = ''
+    if synonym
+      if Synonym.find_by_senior_synonym_id_and_junior_synonym_id taxon.id, synonym.id
         error_message = 'This taxon is already a junior synonym'
       else
-        Synonym.create! senior_synonym_id: params[:taxa_id], junior_synonym_id: synonym.id
+        Synonym.create! senior_synonym_id: taxon.id, junior_synonym_id: synonym.id
       end
+    else
+      error_message = 'Taxon not found'
     end
 
     json = {
       content: render_to_string(partial: 'taxa/synonyms_section', locals: {
-        taxon: @taxon, title: 'Junior synonyms', association_selector: :synonyms_as_senior,
+        taxon: taxon, title: 'Junior synonyms', association_selector: :synonyms_as_senior,
         synonym_field_selector: :junior_synonym}),
       success: error_message.blank?,
       error_message: error_message
