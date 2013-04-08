9f9373fd06858d3896d2c89d4e6fbc39eee47f9b
Mark Wilden <mark@mwilden.com>
2013-04-08 15:24:17 -0700

    Return and replace synonym section on success

--- app/controllers/synonyms_controller.rb
+++ app/controllers/synonyms_controller.rb
@@ -4,14 +4,21 @@ class SynonymsController < ApplicationController
   def create
     @taxon = Taxon.find params[:taxa_id]
     synonym = Taxon.find_by_name params[:name]
-    error_message = ''
     unless synonym
       error_message = 'Taxon not found'
     else
+      error_message = ''
       Synonym.create! senior_synonym_id: params[:taxa_id], junior_synonym_id: synonym.id
-
     end
-    json = {success: !!synonym, error_message: error_message}.to_json
+
+    json = {
+      content: render_to_string(partial: 'taxa/synonyms_section', locals: {
+        taxon: @taxon, title: 'Junior synonyms', association_selector: :synonyms_as_senior,
+        synonym_field_selector: :junior_synonym}),
+      success: !!synonym,
+      error_message: error_message
+    }.to_json
+
     render json: json, content_type: 'text/html'
   end
 
