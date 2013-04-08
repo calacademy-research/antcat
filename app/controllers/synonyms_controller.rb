67996dfdabfae98568fbdc3ed4aceb3a6ac82651
Mark Wilden <mark@mwilden.com>
2013-04-07 18:11:38 -0700

    Make synonyms routes more RESTful

--- app/controllers/synonyms_controller.rb
+++ app/controllers/synonyms_controller.rb
@@ -2,7 +2,7 @@
 class SynonymsController < ApplicationController
 
   def destroy
-    Synonym.find(params[:id]).destroy
+    Synonym.find(params[:synonym_id]).destroy
     json = {success: true}.to_json
     render json: json, content_type: 'text/html'
   end
