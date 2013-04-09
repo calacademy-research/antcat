c98e37018b63dd501972c8a2f71fd7879407e8cc
Mark Wilden <mark@mwilden.com>
2013-04-08 20:13:30 -0700

    Only show taxa

--- app/controllers/name_pickers_controller.rb
+++ app/controllers/name_pickers_controller.rb
@@ -3,7 +3,9 @@ class NamePickersController < ApplicationController
 
   def search
     respond_to do |format|
-      format.json {render json: Name.picklist_matching(params[:term]).to_json}
+      options = {}
+      options = params[:taxa_only] ? {taxa_only: true} : {}
+      format.json {render json: Name.picklist_matching(params[:term], options).to_json}
     end
   end
 
