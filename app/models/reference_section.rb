04c684f206d13d767b9df077ada45b67a68e5325
Mark Wilden <mark@mwilden.com>
2013-04-12 10:45:58 -0700

    Dedupe reference sections

--- app/models/reference_section.rb
+++ app/models/reference_section.rb
@@ -6,4 +6,11 @@ class ReferenceSection < ActiveRecord::Base
 
   has_paper_trail
 
+  def self.dedupe
+    all.each do |reference_section|
+      where('references_taxt = ? AND position > ?',
+        reference_section.references_taxt, reference_section.position).destroy_all
+    end
+  end
+
 end
