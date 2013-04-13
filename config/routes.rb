ed03c5fdf4e381f469d681f18f69d78973b33811
Mark Wilden <mark@mwilden.com>
2013-04-12 18:26:48 -0700

    Fix route (GREEN)

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ AntCat::Application.routes.draw do
 
   match     '/antcat_references.utf8.endnote_import', to: 'references#index', format: :endnote_import, as: :endnote_import
 
-  match '/taxa/:id' => redirect('/catalog/%{id}')
+  match '/taxa/:id' => redirect('/catalog/%{id}'), via: :get
   resources :taxa do
     resources 'taxon_history_items', only: [:update, :create, :destroy]
     resources 'synonyms', only: [:create, :destroy] do
