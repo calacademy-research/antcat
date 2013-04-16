e3fa2d86a4bb4282478f3d7d6722c17993651ae4
Mark Wilden <mark@mwilden.com>
2013-04-16 14:00:29 -0700

    Validate reference section

--- app/models/reference_section.rb
+++ app/models/reference_section.rb
@@ -1,10 +1,9 @@
 # coding: UTF-8
 class ReferenceSection < ActiveRecord::Base
   belongs_to :taxon
-
   acts_as_list scope: :taxon
-
   has_paper_trail
+  validates_presence_of :references_taxt
 
   def self.dedupe
     all.each do |reference_section|
