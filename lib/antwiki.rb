b99743b77c6daa53d9f29f46ddbdd64a3ff53924
Mark Wilden <mark@mwilden.com>
2013-04-30 10:04:04 -0700

    Fix comment

--- lib/antwiki.rb
+++ lib/antwiki.rb
@@ -3,10 +3,10 @@
 #
 # 1. Download http://ants.csiro.au/Ant_Wiki/images/9/9e/AntWiki_Valid_Species.txt
 # 1. Open in Excel, and save as Tab-Delimited Text
-# 3. In MySql, truncate antwiki_valid_taxa
-# 3. run "LOAD DATA LOCAL INFILE '/Users/mwilden/antcat/data/antwiki/AntWiki_Valid_Species.txt' REPLACE INTO TABLE antwiki_valid_taxa FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r' IGNORE 1 LINES;"
-# 4. Add VARCHAR column 'result', if necessary
-# 4. Add PRIMARY KEY AUTOINCREMENT column 'result', if necessary
+# 3. In MySql, "TRUNCATE antwiki_valid_taxa"
+# 4. "LOAD DATA LOCAL INFILE '/Users/mwilden/antcat/data/antwiki/AntWiki_Valid_Species.txt' REPLACE INTO TABLE antwiki_valid_taxa FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r' IGNORE 1 LINES;"
+# 5. "ALTER TABLE antwiki_valid_taxa ADD COLUMN result VARCHAR(255);"
+# 6. "ALTER TABLE antwiki_valid_taxa ADD column id int(10) PRIMARY KEY AUTO_INCREMENT;"
 
 module Antwiki
 
