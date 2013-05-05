007e6a91a4efe43be628b56646192cf7fdfbe9aa
Mark Wilden <mark@mwilden.com>
2013-05-04 17:30:24 -0700

    name_field.feature passes

--- app/controllers/name_fields_controller.rb
+++ app/controllers/name_fields_controller.rb
@@ -2,47 +2,60 @@
 class NameFieldsController < NamePickersController
 
   def find
-    data = {}
+    data = {success: true}
+
     name_string = params[:name_string]
     allow_blank = params[:allow_blank].present?
     new_or_homonym = params[:new_or_homonym].present?
+    confirming_adding_name = params[:confirm_add_name].present?
+    require_existing = params[:require_existing].present?
 
-    if name_string.empty? and allow_blank
-      success = true
-      id = nil
+    if confirming_adding_name
+      name = Name.parse name_string
+      data[:success] = true
+      data[:id] = name.id
+
+    elsif name_string.empty? and allow_blank
+      data[:success] = true
+      data[:id] = nil
+
     elsif new_or_homonym
       name = Name.find_by_name name_string
       if !name
         name = add_name name_string, data
-        success = true
-        id = name.id
+        data[:success] = true
+        data[:id] = name.id
+      else
+        data[:success] = false
+        data[:id] = name.id
+        data[:reason] = 'homonym'
+        data[:submit_button_text] = 'Save Homonym'
+        data[:error_message] = "This name is in use by another taxon. To create a homonym, click \"Save Homonym\"."
       end
+
     else
-      adding_name = params[:add_name] == 'true'
-      require_existing = params[:require_existing].present?
-
-      if adding_name
-        name = add_name name_string, data
+      name = Name.find_by_name name_string
+      if !name
+        if require_existing
+          data[:success] = false
+          data[:error_message] = 'This must be the name of an existing taxon'
+          data[:reason] = 'not found'
+        else
+          data[:success] = false
+          data[:reason] = 'not found'
+          data[:submit_button_text] = 'Add this name'
+          data[:error_message] = "Do you want to add the name #{name_string}? You can attach it to a taxon later, if desired."
+        end
       else
-        name = find_name name_string, require_existing, data
-      end
-
-      id = name.try :id
-      name_string = name.try(:name) || name_string
-
-      if name
-        success = name.errors.empty?
-      else
-        success = false
+        if require_existing
+          data[:id] = name.id
+          data[:success] = true
+        end
       end
     end
-
-    data.merge!(
-      content: render_to_string(partial: 'name_fields/panel', locals: {name_string: name_string}),
-      success: success,
-      id: id)
+  
+    data[:content] = render_to_string(partial: 'name_fields/panel', locals: {name_string: name_string})
     json = data.to_json
-
     render json: json, content_type: 'text/html'
   end
 
@@ -53,9 +66,6 @@ class NameFieldsController < NamePickersController
     if name
       data[:success] = true
     elsif require_existing
-      data[:success] = false
-      data[:error_message] = 'This must be the name of an existing taxon'
-      data[:reason_for_error] = 'taxon not found'
     else
       ask_whether_to_add_name name_string, data
     end
