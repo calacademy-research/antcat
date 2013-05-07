3bdbe32cdf99e2179c0d218bb2d159a3d105f996
Mark Wilden <mark@mwilden.com>
2013-05-06 17:59:59 -0700

    Protection

--- app/controllers/name_fields_controller.rb
+++ app/controllers/name_fields_controller.rb
@@ -11,31 +11,25 @@ class NameFieldsController < NamePickersController
     require_existing = params[:require_existing].present?
 
     if confirming_adding_name
-      name = Name.parse name_string
-      data[:success] = true
-      data[:id] = name.id
+      add_name name_string, data
 
     elsif name_string.empty? and allow_blank
-      data[:success] = true
-      data[:id] = nil
+      clear_name data
 
     elsif new_or_homonym
       name = Name.find_by_name name_string
-      if !name
-        name = Name.parse name_string
-        data[:success] = true
-        data[:id] = name.id
+      if name
+        ask_about_homonym name, data
       else
-        data[:success] = false
-        data[:id] = name.id
-        data[:reason] = 'homonym'
-        data[:submit_button_text] = 'Save homonym'
-        data[:error_message] = "This name is in use by another taxon. To create a homonym, click \"Save homonym\"."
+        add_name name_string, data
       end
 
     else
       name = Name.find_by_name name_string
-      if !name
+      if name
+        data[:success] = true
+        data[:id] = name.id
+      else
         if require_existing
           data[:success] = false
           data[:error_message] = 'This must be the name of an existing taxon'
@@ -46,14 +40,30 @@ class NameFieldsController < NamePickersController
           data[:submit_button_text] = 'Add this name'
           data[:error_message] = "Do you want to add the name #{name_string}? You can attach it to a taxon later, if desired."
         end
-      else
-        data[:success] = true
-        data[:id] = name.id
       end
     end
   
     data[:content] = render_to_string(partial: 'name_fields/panel', locals: {name_string: name_string})
     render json: data.to_json, content_type: 'text/html'
+  end
+
+  def add_name name_string, data
+    name = Name.parse name_string
+    data[:success] = true
+    data[:id] = name.id
+  end
+
+  def clear_name data
+    data[:success] = true
+    data[:id] = nil
+  end
+
+  def ask_about_homonym name, data
+    data[:success] = false
+    data[:id] = name.id
+    data[:reason] = 'homonym'
+    data[:submit_button_text] = 'Save homonym'
+    data[:error_message] = "This name is in use by another taxon. To create a homonym, click \"Save homonym\"."
   end
 
 end
