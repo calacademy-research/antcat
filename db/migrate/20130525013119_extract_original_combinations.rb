956db779b4ca914b6ba3a1c18a40a9b8af9c90a4
Mark Wilden <mark@mwilden.com>
2013-05-24 18:33:16 -0700

    Put call to extract original combinations in migration

---
+++ db/migrate/20130525013119_extract_original_combinations.rb
@@ -0,0 +1,9 @@
+class ExtractOriginalCombinations < ActiveRecord::Migration
+  def up
+    PaperTrail.enabled = false
+    Taxon.extract_original_combinations true
+  end
+
+  def down
+  end
+end
--- lib/tasks/utility.rake
+++
@@ -1,5 +0,0 @@
-# coding: UTF-8
-desc "Extract original combinations"
-task :extract_original_combinations => :environment do
-  Taxon.extract_original_combinations true
-end
