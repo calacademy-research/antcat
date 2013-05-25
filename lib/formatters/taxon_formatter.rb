3c9ce6e924e3813d231782fd3b1f8edb1358d0df
Mark Wilden <mark@mwilden.com>
2013-05-25 12:32:24 -0700

    Show homonym replaced by in header

--- lib/formatters/taxon_formatter.rb
+++ lib/formatters/taxon_formatter.rb
@@ -88,7 +88,7 @@ class Formatters::TaxonFormatter
     labels = []
     labels << "<i>incertae sedis</i> in #{Rank[@taxon.incertae_sedis_in].to_s}" if @taxon.incertae_sedis_in
     if @taxon.homonym? && @taxon.homonym_replaced_by
-      labels << "homonym replaced by #{Formatters::CatalogFormatter.taxon_label_span(@taxon.homonym_replaced_by, ignore_status: true)}"
+      labels << "homonym replaced by #{self.class.link_to_taxon(@taxon.homonym_replaced_by)}"
     elsif @taxon.unidentifiable
       labels << 'unidentifiable'
     elsif @taxon.unresolved_homonym?
