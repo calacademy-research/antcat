6f0a1dd6358b5e08071d9faae68e3e7b040662fb
Mark Wilden <mark@mwilden.com>
2013-06-14 18:53:31 -0700

    Taxt.references

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -270,6 +270,50 @@ class Taxon < ActiveRecord::Base
   end
 
   ###############################################
+  def references
+    references = []
+    references.concat references_in_taxa
+    references.concat references_in_taxt
+    references.concat references_in_synonyms
+  end
+
+  def references_in_taxa
+    references = []
+    [:subfamily_id, :tribe_id, :genus_id, :subgenus_id, :species_id, :homonym_replaced_by_id, :current_valid_taxon_id].each do |field|
+      Taxon.where(field => id).each do |taxon|
+        references << {table: 'taxa', field: field, id: taxon.id}
+      end
+    end
+    references
+  end
+
+  def references_in_synonyms
+    references = []
+    synonyms_as_senior.each do |synonym|
+      references << {table: 'synonyms', field: :senior_synonym_id, id: synonym.junior_synonym_id}
+    end
+    synonyms_as_junior.each do |synonym|
+      references << {table: 'synonyms', field: :junior_synonym_id, id: synonym.senior_synonym_id}
+    end
+    references
+  end
+
+  def references_in_taxt
+    references = []
+    Taxt.taxt_fields.each do |klass, fields|
+      for record in klass.send :all
+        for field in fields
+          next unless record[field]
+          if record[field] =~ /{tax #{id}}/
+            references << {table: klass.table_name, field: field, id: record[:id]}
+          end
+        end
+      end
+    end
+    references
+  end
+
+  ###############################################
   # import
 
   def import_synonyms senior
