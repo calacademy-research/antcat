0d9d5066cb6a7e5af91902d6f0a6fb6fdd99f248
Mark Wilden <mark@mwilden.com>
2013-06-21 17:38:52 -0700

    Remove '_a_'

--- db/migrate/20121209204617_fix_creightonidris.rb
+++ db/migrate/20121209204617_fix_creightonidris.rb
@@ -5,10 +5,10 @@ class FixCreightonidris < ActiveRecord::Migration
     creightonidris = Taxon.find_by_name 'Creightonidris'
     aspididris = Taxon.find_by_name 'Aspididris'
 
-    creightonidris.become_not_a_junior_synonym_of ceratobasis
+    creightonidris.become_not_junior_synonym_of ceratobasis
     creightonidris.become_junior_synonym_of basiceros
 
-    aspididris.become_not_a_junior_synonym_of ceratobasis
+    aspididris.become_not_junior_synonym_of ceratobasis
     aspididris.become_junior_synonym_of basiceros
   end
 
