d02084775dd8380f1379961f02167e44f379d0a3
Mark Wilden <mark@mwilden.com>
2013-06-28 10:06:41 -0700

    Format

--- features/step_definitions/widget_steps.rb
+++ features/step_definitions/widget_steps.rb
@@ -14,38 +14,32 @@ Then /I should (not )?see the reference field$/ do |should_not|
   selector = should_not ? :should_not : :should
   find('.antcat_reference_field').send(selector, be_visible)
 end
-
 When /^I edit the reference$/ do
   within ".current" do
     step 'I follow "edit"'
   end
 end
-
 When /^I save my changes to the current reference$/ do
   within first('.current') do
     step 'I press "Save"'
     step 'I wait for a bit'
   end
 end
-
 When /^I add a reference$/ do
   within ".expansion" do
     step 'I follow "Add"'
   end
 end
-
 When /^I set the authors to "([^"]*)"$/ do |names|
   within ".current div.edit" do
     step %{I fill in "reference[author_names_string]" with "#{names}"}
   end
 end
-
 When /^I set the title to "([^"]*)"$/ do |title|
   within ".current div.edit" do
     step %{I fill in "reference[title]" with "#{title}"}
   end
 end
-
 When /^I add a reference by Brian Fisher$/ do
   step 'I press "Add"'
   within '.current' do
@@ -60,10 +54,10 @@ When /^I add a reference by Brian Fisher$/ do
   end
 end
 
+# Search
 When /In the search box, I press "Go"/ do
   step 'I press "Go" within ".expansion"'
 end
-
 When /^I search for "([^"]*)"$/ do |search_term|
   within '.expansion' do
     # this is to make the selectmenu work
@@ -73,7 +67,6 @@ When /^I search for "([^"]*)"$/ do |search_term|
     step %{In the search box, I press "Go"}
   end
 end
-
 When /^I search for the authors? "([^"]*)"$/ do |authors|
   within '.expansion' do
     step %{I follow "Search for author(s)"}
@@ -116,18 +109,15 @@ Then /in the name popup display I should see the first name/ do
     step %{I should see "#{Name.first.name}"}
   end
 end
-
 Then /in the name popup display I should see "([^"]*)"/ do |text|
   within "#popup .display" do
     step %{I should see "#{text}"}
   end
 end
-
 Then /^I should (not )?see the name popup edit interface$/ do |should_not|
   selector = should_not ? :should_not : :should
   find('#popup .controls').send(selector, be_visible)
 end
-
 Then /^I should (not )?see the name popup$/ do |should_not|
   selector = should_not ? :should_not : :should
   find('.antcat_name_popup').send(selector, be_visible)
@@ -139,19 +129,16 @@ Then /in the results section I should see the editable taxt for the name "([^"]*
     step %{I should see "#{Taxt.to_editable_name(Name.find_by_name(text))}"}
   end
 end
-
 Then /in the results section I should see the id for "([^"]*)"/ do |text|
   within "#results" do
     step %{I should see "#{Name.find_by_name(text).id}"}
   end
 end
-
 Then /in the results section I should see the id for the name "([^"]*)"/ do |text|
   within "#results" do
     step %{I should see "#{Name.find_by_name(text).id}"}
   end
 end
-
 Then /in the results section I should see the editable taxt for "([^"]*)"/ do |text|
   within "#results" do
     step %{I should see "#{Taxt.to_editable_taxon(Taxon.find_by_name(text))}"}
