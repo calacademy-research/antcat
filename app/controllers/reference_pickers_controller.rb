4976e9032f4be5c5e4d2752f35c6ba1675ab3b4a
Mark Wilden <mark@mwilden.com>
2013-07-02 09:57:08 -0700

    Refactor into base class

---
+++ app/controllers/reference_pickers_controller.rb
@@ -0,0 +1,18 @@
+# coding: UTF-8
+class ReferencePickersController < ApplicationController
+
+  def show
+    params[:search_selector] ||= 'Search for author(s)'
+
+    reference = Reference.find params[:id] if params[:id].present?
+
+    if params[:q].present?
+      params[:q].strip!
+      params[:is_author_search] = params[:search_selector] == 'Search for author(s)'
+      references = Reference.do_search params
+    end
+
+    render partial: 'show', locals: {references: references, reference: reference}
+  end
+
+end
