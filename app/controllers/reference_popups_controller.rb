26154ad3765c4ea6e108d9cc95f25f008885c2b5
Mark Wilden <mark@mwilden.com>
2013-07-02 09:57:08 -0700

    Refactor into base class

--- app/controllers/reference_popups_controller.rb
+++ app/controllers/reference_popups_controller.rb
@@ -1,18 +1,4 @@
 # coding: UTF-8
-class ReferencePopupsController < ApplicationController
-
-  def show
-    params[:search_selector] ||= 'Search for author(s)'
-
-    reference = Reference.find params[:id] if params[:id].present?
-
-    if params[:q].present?
-      params[:q].strip!
-      params[:is_author_search] = params[:search_selector] == 'Search for author(s)'
-      references = Reference.do_search params
-    end
-
-    render partial: 'show', locals: {references: references, reference: reference}
-  end
-
+class ReferencePopupsController < ReferencePickersController
 end
+
