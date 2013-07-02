1b8dc317b77e9b1b90d6ef10ec0f91a8a85ccebb
Mark Wilden <mark@mwilden.com>
2013-07-02 11:04:22 -0700

    Another render fix

--- app/controllers/reference_fields_controller.rb
+++ app/controllers/reference_fields_controller.rb
@@ -1,4 +1,6 @@
 # coding: UTF-8
 class ReferenceFieldsController < ReferencePickersController
+  def picker_type
+    'field'
+  end
 end
-
--- app/controllers/reference_popups_controller.rb
+++ app/controllers/reference_popups_controller.rb
@@ -1,4 +1,6 @@
 # coding: UTF-8
 class ReferencePopupsController < ReferencePickersController
+  def picker_type
+    'popup'
+  end
 end
-
