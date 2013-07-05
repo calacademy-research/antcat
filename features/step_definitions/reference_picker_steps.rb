a82afe975442b22a4e428d4684ea140bbc828366
Mark Wilden <mark@mwilden.com>
2013-07-04 18:53:29 -0700

    wip

--- features/step_definitions/reference_picker_steps.rb
+++ features/step_definitions/reference_picker_steps.rb
@@ -15,6 +15,15 @@ Then /^the current reference should be "([^"]*)"$/ do |contents|
   page.find('#popup .current .display').text.should == contents
 end
 
+Then /^the widget results should be the ID for "([^"]*)"$/ do |key|
+  reference = find_reference_by_key key
+  page.find('#results').text.should == reference.id.to_s
+end
+
+Then /^the current reference should be the first reference$/ do
+  page.find('#popup .current .display').text.should == ''
+end
+
 Then /^I should not see the default reference button$/ do
   page.should_not have_css('button.default_reference')
 end
