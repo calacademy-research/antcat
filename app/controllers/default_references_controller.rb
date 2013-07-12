213f7f8b1f01a8b0080704e898480b5ad930a710
Mark Wilden <mark@mwilden.com>
2013-07-12 11:35:19 -0700

    Redirect back to new references unless Ajax request

--- app/controllers/default_references_controller.rb
+++ app/controllers/default_references_controller.rb
@@ -3,7 +3,11 @@ class DefaultReferencesController < ApplicationController
 
   def update
     DefaultReference.set session, Reference.find(params[:id])
-    render nothing: true
+    if request.xhr?
+      render nothing: true
+    else
+      redirect_to '/references?commit=new'
+    end
   end
 
 end
