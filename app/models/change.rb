e8cbad433232ae7232ff9c071d6723fc3f868c98
Mark Wilden <mark@mwilden.com>
2013-07-19 10:07:36 -0700

    Change.reify

--- app/models/change.rb
+++ app/models/change.rb
@@ -1,4 +1,16 @@
 # coding: UTF-8
 class Change < ActiveRecord::Base
   belongs_to :paper_trail_version, class_name: 'PaperTrail::Version'
+
+  def reify
+    # this dodgy code is from paper_trail_manager's changes_helper.rb
+    current = paper_trail_version.next.try :reify
+    previous = paper_trail_version.reify rescue nil
+    begin
+      paper_trail_version.item_type.constantize.find paper_trail_version.item_id
+    rescue ActiveRecord::RecordNotFound
+      previous || current
+    end
+  end
+
 end
