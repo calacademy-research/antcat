c3b965e75287dd3bb421e6dc6494927efb468abd
Mark Wilden <mark@mwilden.com>
2013-07-19 16:22:20 -0700

    ChangesHelper

---
+++ app/helpers/changes_helper.rb
@@ -0,0 +1,17 @@
+# coding: UTF-8
+module ChangesHelper
+
+  def format_change_attributes taxon
+    string = []
+    string << 'Fossil' if taxon.fossil?
+    string << 'Hong' if taxon.hong?
+    string << '<i>nomen nudum</i>' if taxon.nomen_nudum?
+    string << 'unresolved homonym' if taxon.unresolved_homonym?
+    string << 'ichnotaxon' if taxon.ichnotaxon?
+    string.join(', ').html_safe
+  end
+
+  def format_taxt taxt
+    Taxt.to_string taxt, current_user
+  end
+end
