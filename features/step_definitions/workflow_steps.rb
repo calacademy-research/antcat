dd8c792f21ebc713f3cfaf0b302278b397b45d53
Mark Wilden <mark@mwilden.com>
2013-07-19 14:33:25 -0700

    wip

--- features/step_definitions/workflow_steps.rb
+++ features/step_definitions/workflow_steps.rb
@@ -2,8 +2,11 @@
 When /^version tracking is (not)?enabled$/ do |is_not|
   PaperTrail.enabled = !is_not
 end
-
-Then /^I should see the name "(.*?)" in the changes$/ do |value|
-  page.should have_css '.name', text: value
+Then /^I should see the name "(.*?)" in the changes$/ do |subfamily|
+  page.should have_css '.name', text: subfamily
+end
+Then /^I should see the subfamily "(.*?)" in the changes$/ do |subfamily|
+  page.should have_css '.parent_rank', text: 'Subfamily'
+  page.should have_css '.parent', text: subfamily
 end
 
