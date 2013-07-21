e389ecfa1c870b6ac8fd5ab7cd1bb3390e293d3c
Mark Wilden <mark@mwilden.com>
2013-07-20 17:03:20 -0700

    Pull out Debugger

--- config/initializers/debugger.rb
+++ config/initializers/debugger.rb
@@ -1,10 +1,12 @@
 # coding: UTF-8
-class Debugger::QuitCommand
-  def execute
-    # patch so debugger doesn't prompt to quit
-    #if @match[1] or confirm(pr("quit.confirmations.really"))
-      @state.interface.finalize
-      exit! # exit -> exit!: No graceful way to stop threads...
-    #end
+module Debugger
+  class QuitCommand
+    def execute
+      # patch so debugger doesn't prompt to quit
+      #if @match[1] or confirm(pr("quit.confirmations.really"))
+        @state.interface.finalize
+        exit! # exit -> exit!: No graceful way to stop threads...
+      #end
+    end
   end
 end
