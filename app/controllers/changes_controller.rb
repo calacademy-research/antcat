1c6b16802c94b364181d8ed90281a14b6870f881
Mark Wilden <mark@mwilden.com>
2013-07-21 11:36:00 -0700

    Only show create changes

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -1,9 +1,11 @@
 class ChangesController < ApplicationController
 
   def index
-    @changes = Change.order('created_at DESC').
-      paginate(page: params[:page]).
-      all
+    @changes = Change.
+      joins(:paper_trail_version).
+      where('versions.event' => 'create').
+      order('created_at DESC').
+      paginate(page: params[:page])
   end
 
 end
