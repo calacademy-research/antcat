dbbbbd7f6e9da3e17e40aa9065394daca82327ff
Mark Wilden <mark@mwilden.com>
2013-07-25 16:19:28 -0700

    Finish first phase of Brian's report

--- spec/lib/exporters/taxon_list/exporter_spec.rb
+++ spec/lib/exporters/taxon_list/exporter_spec.rb
@@ -23,16 +23,40 @@ describe Exporters::TaxonList::Exporter do
     end
 
     describe "Outputting taxa" do
-      it "should export a species correctly" do
-        reference = FactoryGirl.create(:article_reference, date: '1840', author_names: [FactoryGirl.create(:author_name, name: 'Shuckard')])
+      before do
+        reference = FactoryGirl.create(:article_reference, citation_year: '1840', author_names: [FactoryGirl.create(:author_name, name: 'Shuckard')])
         authorship = FactoryGirl.create :citation, reference: reference
         name = FactoryGirl.create :species_name, name: 'Atta robusta'
-        protonym = FactoryGirl.create :protonym, authorship: authorship, name: name
-        @species = create_species name: name, protonym: protonym
+        @protonym = FactoryGirl.create :protonym, authorship: authorship, name: name
+        @species = create_species name: name, protonym: @protonym
+        @homonym = create_species 'Atta major', status: 'homonym', protonym: @protonym
+        @synonym = create_species 'Eciton major', status: 'synonym', protonym: @protonym
+        @original_combination = create_species 'Furris major', status: 'original combination', protonym: @protonym
+      end
 
-        @exporter.get_data(@species).should == ['Atta robusta', 1840, 'Shuckard', 'valid']
+      it "should export a valid species correctly" do
+        @exporter.get_data(@species).should == ['Atta robusta', 'Shuckard', 1840, 'valid']
+      end
+
+      it "should export an invalid species correctly" do
+        @exporter.get_data(@synonym).should == ['Eciton major', 'Shuckard', 1840, 'invalid']
+      end
+
+      describe "Outputting a number of taxa" do
+        it "should sort its output by names in ranks" do
+          file = double
+          File.stub(:open).and_yield file
+
+          @exporter.should_receive(:write).with(file, "name\tauthors\tyear\tvalid").ordered
+          @exporter.should_receive(:write).with(file, "Atta robusta\t" + "Shuckard\t" + "1840\t" + "valid").ordered
+          @exporter.should_receive(:write).with(file, "Atta major\t"   + "Shuckard\t" + "1840\t" + "invalid").ordered
+          @exporter.should_receive(:write).with(file, "Eciton major\t" + "Shuckard\t" + "1840\t" + "invalid").ordered
+          @exporter.should_not_receive(:write).with(file, "Furris major\t" + "Shuckard\t" + "1840\t" + "valid").ordered
+
+          @exporter.export
+        end
       end
     end
-  end
 
+  end
 end
