425a7660fed7e2f7f6e5685101bd9bd28be7e0ef
Mark Wilden <mark@mwilden.com>
2013-07-30 19:09:19 -0700

    User.can_edit_catalog, can_approve_changes

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+# coding: UTF-8
+require 'spec_helper'
+describe User do
+
+  specify "A user is an editor" do
+    User.new.is_editor?.should be_true
+  end
+
+  it "knows whether it can edit the catalog" do
+    User.new.can_edit_catalog.should be_false
+    User.new(can_edit_catalog: true).can_edit_catalog.should be_true
+  end
+
+  it "knows whether it can approve changes" do
+    User.new.can_approve_changes.should be_false
+    User.new(can_approve_changes: true).can_approve_changes.should be_true
+  end
+
+end
