0b866ab09541297c825b7b23ebe9c92286e1f963
Mark Wilden <mark@mwilden.com>
2013-07-30 18:24:25 -0700

    Back-fill authorization specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,15 +4,40 @@ require 'spec_helper'
 describe ApplicationHelper do
 
   describe 'Making a link menu' do
-
     it "should put bars between them and be html safe" do
       result = helper.make_link_menu(['a', 'b'])
       result.should == '<span class="link_menu">a | b</span>'
     end
-
     it "should always be html safe" do
       helper.make_link_menu('a'.html_safe, 'b'.html_safe).should be_html_safe
       helper.make_link_menu(['a'.html_safe, 'b']).should be_html_safe
+    end
+  end
+
+  describe "Authorization" do
+    before do
+      @current_user = double
+      helper.stub(:current_user).and_return @current_user
+    end
+
+    specify "reference editing authorization depends on the milieu and current user" do
+      $Milieu.should_receive(:user_can_edit_references?).with @current_user
+      helper.user_can_edit_references?
+    end
+
+    specify "catalog editing authorization depends on the milieu and current user" do
+      $Milieu.should_receive(:user_can_edit_catalog?).with @current_user
+      helper.user_can_edit_catalog?
+    end
+
+    specify "editing authorization depends on the milieu and current user" do
+      $Milieu.should_receive(:user_can_edit_catalog?).with @current_user
+      helper.user_can_edit_catalog?
+    end
+
+    specify "updloading PDF authorization depends on the milieu and current user" do
+      $Milieu.should_receive(:user_can_edit_catalog?).with @current_user
+      helper.user_can_edit_catalog?
     end
 
   end
