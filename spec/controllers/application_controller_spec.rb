8b82bc34e25824e9f9af7f6d9ccaba9cbfed4366
Mark Wilden <mark@mwilden.com>
2013-07-30 20:37:08 -0700

    Spec ApplicationController

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  describe "Authentication and authorization" do
+    before do
+      @current_user = double
+      controller.stub(:current_user).and_return @current_user
+    end
+    describe "Authentication and authorization to edit the catalog" do
+      it "should ask the milieu" do
+        $Milieu.should_receive(:user_can_edit_catalog?).with @current_user
+        controller.should_receive(:authenticate_user!).and_return true
+        controller.authenticate_catalog_editor
+      end
+    end
+  end
+end
