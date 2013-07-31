7e95924389e1b99dd533e3af279b8ff6620a100e
Mark Wilden <mark@mwilden.com>
2013-07-30 19:09:19 -0700

    User.can_edit_catalog, can_approve_changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :registerable,
          :rememberable, :trackable, :validatable, :invitable
 
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :can_edit_catalog, :can_approve_changes
 
   def is_editor?
     true
