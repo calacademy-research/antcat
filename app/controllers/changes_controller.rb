8807ddf986c0f4866ceead4e98821ac8f9938775
Mark Wilden <mark@mwilden.com>
2013-08-18 09:38:07 -0700

    No need to redirect after approval

--- app/controllers/changes_controller.rb
+++ app/controllers/changes_controller.rb
@@ -12,7 +12,6 @@ class ChangesController < ApplicationController
     @change = Change.find params[:id]
     @change.taxon.approve!
     @change.update_attributes! approver_id: current_user.id, approved_at: @change.updated_at
-    redirect_to changes_path
   end
 
 end
