e7a18bc0044e22e227c22c9d264e71d31f3698e2
Mark Wilden <mark@mwilden.com>
2013-08-18 09:36:01 -0700

    Supposedly show Capybara exceptions

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,7 +4,7 @@ AntCat::Application.configure do
   config.whiny_nils                                 = true
   config.consider_all_requests_local                = true
   config.action_controller.perform_caching          = false
-  config.action_dispatch.show_exceptions            = false
+  config.action_dispatch.show_exceptions            = true
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.delivery_method              = :test
   config.active_support.deprecation                 = :stderr
