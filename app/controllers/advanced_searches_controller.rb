c4aef5d8a531ed00c1470f3094d5298852479afe
Mark Wilden <mark@mwilden.com>
2013-08-20 13:00:10 -0700

    Remove unneeded code

--- app/controllers/advanced_searches_controller.rb
+++ app/controllers/advanced_searches_controller.rb
@@ -3,7 +3,6 @@ class AdvancedSearchesController < ApplicationController
 
   def show
     @taxa = Taxon.advanced_search(params[:rank], params[:year]).paginate page: params[:page]
-    render 'advanced_search/index'
   end
 
 end
