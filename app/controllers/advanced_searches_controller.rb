ca4307a46223d99f0a770a84282b333359656d48
Mark Wilden <mark@mwilden.com>
2013-08-21 18:55:25 -0700

    Don't say "No results found" when just starting

--- app/controllers/advanced_searches_controller.rb
+++ app/controllers/advanced_searches_controller.rb
@@ -2,9 +2,10 @@
 class AdvancedSearchesController < ApplicationController
 
   def show
-    taxa = Taxon.advanced_search params[:rank], params[:year], params[:valid_only]
-    @taxa = taxa.paginate page: params[:page]
-    @taxa_count = taxa.count
+    if params[:rank].present?
+      @taxa = Taxon.advanced_search params[:rank], params[:year], params[:valid_only]
+      @taxa_count = @taxa.count
+    end
   end
 
 end
