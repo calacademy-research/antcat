311d64c48e88b36e51f72a1278b0a108b03985ab
Mark Wilden <mark@mwilden.com>
2013-08-22 16:29:22 -0700

    Setup for multiple kinds of search

--- app/models/taxon_advanced_search.rb
+++ app/models/taxon_advanced_search.rb
@@ -2,10 +2,15 @@
 class Taxon < ActiveRecord::Base
 
   def self.advanced_search params
-    query = joins(protonym: [{authorship: :reference}])
-    query = query.where(type: params[:rank]) unless params[:rank] == 'All'
-    query = query.where('references.year' => params[:year]).order(:name_cache)
-    query = query.where(status: 'valid') if params[:valid_only].present?
+    query = joins(protonym: [{authorship: :reference}]).order(:name_cache)
+    if params[:year].present? && params[:author_name].blank?
+      query = query.where(type: params[:rank]) unless params[:rank] == 'All'
+      query = query.where('references.year' => params[:year])
+      query = query.where(status: 'valid') if params[:valid_only].present?
+    elsif params[:author_name].present?
+    else
+      query = []
+    end
     query
   end
 
