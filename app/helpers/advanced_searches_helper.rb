89ac55a1502ff8d54a325e16f485dec699b39b0f
Mark Wilden <mark@mwilden.com>
2013-08-23 17:22:54 -0700

    AdvancedSearchesHelper

--- app/helpers/advanced_searches_helper.rb
+++ app/helpers/advanced_searches_helper.rb
@@ -1,6 +1,22 @@
 # coding: UTF-8
 module AdvancedSearchesHelper
 
+  def format_name taxon
+    Formatters::CatalogTaxonFormatter.link_to_taxon taxon
+  end
+
+  def format_status taxon
+    if taxon.original_combination?
+      format_original_combination_status taxon
+    end
+  end
+
+  def format_original_combination_status taxon
+    string = 'see '.html_safe
+    string << Formatters::CatalogTaxonFormatter.link_to_taxon(taxon.current_valid_taxon)
+    string
+  end
+
   def format_protonym taxon
     reference = taxon.protonym.authorship.reference
     string = ''.html_safe
