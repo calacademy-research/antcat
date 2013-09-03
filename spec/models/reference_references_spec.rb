c11cb33f4cb74abde60df9cc532339df446d9415
Mark Wilden <mark@mwilden.com>
2013-09-03 10:17:08 -0700

    Reference.references

--- spec/models/reference_references_spec.rb
+++ spec/models/reference_references_spec.rb
@@ -9,73 +9,32 @@ describe Reference do
       reference.should have(0).references
     end
 
-    #describe "references in reference fields" do
-      #it "should have a reference if it's a reference's genus" do
-        #genus = create_genus
-        #species = create_species genus: genus
-        #genus.references.should =~ [
-          #{table: 'taxa', field: :genus_id, id: species.id},
-        #]
-      #end
-      #it "should have a reference if it's a reference's subfamily" do
-        #genus = create_genus
-        #genus.subfamily.references.should =~ [
-          #{table: 'taxa', field: :subfamily_id, id: genus.id},
-          #{table: 'taxa', field: :subfamily_id, id: genus.tribe.id},
-        #]
-      #end
-    #end
+    describe "References in reference fields" do
+      it "should have a reference if it's a protonym's authorship's reference" do
+        genus = create_genus
+        species = create_species genus: genus
+        genus.references.should =~ [
+          {table: 'taxa', field: :genus_id, id: species.id},
+        ]
+        reference = FactoryGirl.create :article_reference
+        eciton = create_genus 'Eciton'
+        eciton.protonym.authorship.update_attributes! reference_id: reference.id
+        reference.references.should =~ [
+          {table: 'citations', field: :reference_id, id: eciton.id},
+        ]
+      end
+    end
 
-    #describe "references in taxt" do
-      #it "should return references in taxt" do
-        #atta = create_genus 'Atta'
-        #eciton = create_genus 'Eciton'
-        #eciton.update_attribute :type_taxt, "{tax #{atta.id}}"
-        #atta.references.should =~ [
-          #{table: 'taxa', field: :type_taxt, id: eciton.id},
-        #]
-      #end
-      #it "should not return references in its own taxt" do
-        #eciton = create_genus 'Eciton'
-        #eciton.update_attribute :type_taxt, "{tax #{eciton.id}}"
-        #eciton.references.should be_empty
-      #end
+    describe "References in taxt" do
+      it "should return references in taxt" do
+        reference = FactoryGirl.create :article_reference
+        eciton = create_genus 'Eciton'
+        eciton.update_attribute :type_taxt, "{ref #{reference.id}}"
+        reference.references.should =~ [
+          {table: 'taxa', field: :type_taxt, id: eciton.id},
+        ]
+      end
+    end
 
-    #end
-
-    #describe "Reference in its authorship taxt" do
-      #it "should not consider this an external reference" do
-        #eciton = create_genus 'Eciton'
-        #eciton.protonym.authorship.update_attribute :notes_taxt, "{tax #{eciton.id}}"
-        #eciton.references.should be_empty
-      #end
-    #end
-
-    #describe "references as synonym" do
-      #it "should work" do
-        #atta = create_genus 'Atta'
-        #eciton = create_genus 'Eciton'
-        #eciton.become_junior_synonym_of atta
-        #atta.references.should =~ [
-          #{table: 'synonyms', field: :senior_synonym_id, id: eciton.id},
-        #]
-        #eciton.references.should =~ [
-          #{table: 'synonyms', field: :junior_synonym_id, id: atta.id},
-        #]
-      #end
-    #end
-
-    #describe "Nontaxt references" do
-      #it "should return only nontaxt references" do
-        #atta = create_genus 'Atta'
-        #eciton = create_genus 'Eciton'
-        #eciton.update_attribute :type_taxt, "{tax #{atta.id}}"
-        #eciton.update_attribute :homonym_replaced_by, atta
-        #atta.nontaxt_references.should =~ [
-          #{table: 'taxa', field: :homonym_replaced_by_id, id: eciton.id},
-        #]
-      #end
-    #end
   end
-
 end
