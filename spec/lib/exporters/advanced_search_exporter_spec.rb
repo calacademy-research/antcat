84ceef5bca336651db5ef18f9fda4e074dc697f3
Mark Wilden <mark@mwilden.com>
2013-09-05 15:22:37 -0700

    Fix spec

--- spec/lib/exporters/advanced_search_exporter_spec.rb
+++ spec/lib/exporters/advanced_search_exporter_spec.rb
@@ -2,11 +2,16 @@
 require 'spec_helper'
 
 describe Exporters::AdvancedSearchExporter do
+
   it "should format a taxon" do
-    reference = FactoryGirl.create :article_reference, author_names: [AuthorName
-    create_genus 'Atta'
+    reference = FactoryGirl.create :article_reference, title: 'Ants are my life', author_names: [FactoryGirl.create(:author_name, name: 'Fred')],
+      journal: (FactoryGirl.create :journal, name: 'Ants'), series_volume_issue: '13', pagination: '12', citation_year: '2011d'
+    subfamily = create_subfamily 'Dolichoderinae'
+    subfamily.protonym.authorship.update_attributes! reference: reference
+    subfamily.update_attributes! incertae_sedis_in: :family
     exporter = Exporters::AdvancedSearchExporter.new
-    exporter.export(nil).should == %{Atta Fisher, B.L. 2013d. Ants are my life. Ants 3:3.}
+    exporter.export.should == %{Dolichoderinae <i>incertae sedis</i> in family\nFred 2011d. Ants are my life. Ants 13:12.\n}
   end
+
 end
 
