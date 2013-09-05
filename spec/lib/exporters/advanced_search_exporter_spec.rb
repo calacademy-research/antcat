ab8959adceeb7ebfee7b0ed93d8935d94e3ca80a
Mark Wilden <mark@mwilden.com>
2013-09-04 18:12:21 -0700

    Start filling in tests

--- spec/lib/exporters/advanced_search_exporter_spec.rb
+++ spec/lib/exporters/advanced_search_exporter_spec.rb
@@ -3,10 +3,10 @@ require 'exporters/advanced_search_exporter'
 
 describe Exporters::AdvancedSearchExporter do
   it "should format a taxon" do
+    reference = FactoryGirl.create :article_reference, author_names: [AuthorName
+    create_genus 'Atta'
     exporter = Exporters::AdvancedSearchExporter.new
-    debugger
-
-    exporter.export(nil).should == 'asdf'
+    exporter.export(nil).should == %{Atta Fisher, B.L. 2013d. Ants are my life. Ants 3:3.}
   end
 end
 
