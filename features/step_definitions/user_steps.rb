5322dbf35a048ff71b515071024074d4eaaf0970
Mark Wilden <mark@mwilden.com>
2013-09-06 20:44:09 -0700

    Login more than one user in a scenario

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -21,9 +21,10 @@ end
 
 def login can_edit_catalog, use_web_interface = false, user_name = nil
   user_name ||= 'Mark Wilden'
+  user = User.find_by_name 'user_name'
+  user.destroy if user
   step 'I go to the main page'
-  User.delete_all
-  attributes = {email: 'mark@example.com'}
+  attributes = {email: "mark@#{rand}example.com"}
   attributes[:can_edit_catalog] = true if can_edit_catalog
   attributes[:name] = user_name if user_name
   @user = FactoryGirl.create :user, attributes
