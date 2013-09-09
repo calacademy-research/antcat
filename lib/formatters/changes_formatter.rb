7a3e75d2a684055e7dd771c2ce2d0f115db57469
Mark Wilden <mark@mwilden.com>
2013-09-08 17:09:30 -0700

    Fix

--- lib/formatters/changes_formatter.rb
+++ lib/formatters/changes_formatter.rb
@@ -1,5 +1,6 @@
 # coding: UTF-8
 module Formatters::ChangesFormatter
+  include Formatters::Formatter
   extend self
   def format_approver_name user
     "#{format_doer_name(user)} approved this change".html_safe
