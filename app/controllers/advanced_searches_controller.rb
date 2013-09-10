5cf1597305832777c4265ebe3bc32ee7c2cd5404
Mark Wilden <mark@mwilden.com>
2013-09-10 16:05:02 -0700

    Finish AdvancedSearchesController

--- app/controllers/advanced_searches_controller.rb
+++ app/controllers/advanced_searches_controller.rb
@@ -5,20 +5,27 @@ class AdvancedSearchesController < ApplicationController
 
   def show
     if params[:rank].present?
-      @taxa = Taxon.advanced_search author_name: params[:author_name], rank: params[:rank], year: params[:year], valid_only: params[:valid_only]
-      @taxa_count = @taxa.count
+      get_taxa
       set_search_results_message
     end
     respond_to do |format|
-      format.json {render json: AuthorName.search(params[:term]).to_json}
-      format.html {@taxa = @taxa.paginate page: params[:page] if @taxa}
+      format.json {send_author_name_picklist}
+      format.html {send_html}
       format.txt  {send_text}
     end
   end
 
+  def send_author_name_picklist
+    render json: AuthorName.search(params[:term]).to_json
+  end
+
+  def send_html
+    @taxa = @taxa.paginate page: params[:page] if @taxa
+  end
+
   def send_text
-    data = Exporters::AdvancedSearchExporter.new.export
-    send_data data, filename: 'taxa.txt', type: 'text/plain'
+    text = Exporters::AdvancedSearchExporter.new.export @taxa
+    send_data text, filename: 'taxa.txt', type: 'text/plain'
   end
 
   def set_search_results_message
@@ -37,4 +44,11 @@ class AdvancedSearchesController < ApplicationController
     AuthorName.find_by_name(name).nil?
   end
 
+  def get_taxa
+    @taxa = Taxon.advanced_search author_name: params[:author_name],
+                                  rank: params[:rank],
+                                  year: params[:year],
+                                  valid_only: params[:valid_only]
+    @taxa_count = @taxa.count
+  end
 end
