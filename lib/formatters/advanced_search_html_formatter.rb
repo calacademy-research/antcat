9b7d3a378a4089fad99369e15f42cc3d9f037536
Mark Wilden <mark@mwilden.com>
2013-09-11 09:38:08 -0700

    Remove duplicate code

--- lib/formatters/advanced_search_html_formatter.rb
+++ lib/formatters/advanced_search_html_formatter.rb
@@ -1,13 +1,10 @@
 # coding: UTF-8
 module Formatters::AdvancedSearchHtmlFormatter
+  include Formatters::Formatter
   include Formatters::AdvancedSearchFormatter
 
   def format_name taxon
     Formatters::CatalogTaxonFormatter.link_to_taxon taxon
-  end
-
-  def italicize string
-    content_tag :i, string
   end
 
   def reference_id reference
