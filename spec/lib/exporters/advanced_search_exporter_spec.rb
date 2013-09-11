48b932bbec6fe93b6b6ff3153f2edd467733f4ee
Mark Wilden <mark@mwilden.com>
2013-09-10 20:13:26 -0700

    Move spec

--- spec/lib/exporters/advanced_search_exporter_spec.rb
+++ spec/lib/exporters/advanced_search_exporter_spec.rb
@@ -22,13 +22,5 @@ describe Exporters::AdvancedSearchExporter do
     exporter.export(Subfamily.all).should have(15).lines
   end
 
-  it "should format a taxon" do
-    subfamily = create_subfamily 'Dolichoderinae'
-    subfamily.protonym.authorship.update_attributes! reference: @reference
-    subfamily.update_attributes! incertae_sedis_in: :family
-    exporter = Exporters::AdvancedSearchExporter.new
-    exporter.export([subfamily]).should == %{Dolichoderinae <i>incertae sedis</i> in family\nFred 2011d. Ants are my life. Ants 13:12.\n}
-  end
-
 end
 
--- spec/lib/formatters/advanced_search_html_formatter_spec.rb
+++ spec/lib/formatters/advanced_search_html_formatter_spec.rb
@@ -10,9 +10,14 @@ describe Formatters::AdvancedSearchFormatter do
     @formatter = FormattersAdvancedSearchHtmlFormatterTestClass.new
   end
 
-  describe "Formatting search results" do
-    it "should know how to do it" do
-    end
+  it "should format a taxon" do
+      latreille = FactoryGirl.create :author_name, name: 'Latreille, P. A.'
+      science = FactoryGirl.create :journal, name: 'Science'
+      reference = FactoryGirl.create :article_reference, author_names: [latreille], citation_year: '1809', title: "*Atta*", journal: science, series_volume_issue: '(1)', pagination: '3'
+      taxon = create_genus 'Atta', incertae_sedis_in: 'genus', nomen_nudum: true
+      taxon.protonym.authorship.update_attributes reference: reference
+      string = @formatter.format taxon
+      string.should == "<i>Atta</i> <i>incertae sedis</i> in genus, nomen nudum\nLatreille, P. A. 1809. Atta. Science (1):3.<span class=\"reference_id\">#{reference.id}</span>\n\n"
   end
-end
 
+end
