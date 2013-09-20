03c24a1c3042dd2bd9c26944d436c45259d8c167
Mark Wilden <mark@mwilden.com>
2013-09-20 13:41:10 -0700

    Subfamily name methods

--- app/models/subfamily_name.rb
+++ app/models/subfamily_name.rb
@@ -1,8 +1,24 @@
 # coding: UTF-8
 class SubfamilyName < FamilyOrSubfamilyName
 
+  def self.parse_words words
+    return unless words.size == 1
+    create! make_import_attributes words[0]
+  end
+
   def self.get_name data
     data[:subfamily_name]
+  end
+
+  def self.make_import_attributes name, _ = nil
+    name_html = name
+    {
+      name:         name,
+      name_html:    name_html,
+      epithet:      name,
+      epithet_html: name_html,
+      protonym_html:name_html,
+    }
   end
 
 end
