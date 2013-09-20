c93f8d3a33a8b543c7971d4f28d1a068a7c19c68
Mark Wilden <mark@mwilden.com>
2013-09-19 17:13:00 -0700

    Default FactoryGirl :family_name to Formicidae

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -124,7 +124,7 @@ FactoryGirl.define do
   end
 
   factory :family_name do
-    name 'Family'
+    name            'Formicidae'
     name_html       {name}
     epithet         {name}
     epithet_html    {name_html}
