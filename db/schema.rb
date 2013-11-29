11739d3263d7dcc90a57ab5b09f30327af15bf50
Mark Wilden <mark@mwilden.com>
2013-11-29 08:54:12 -0800

    Fix glabrior (protonym deleted)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131125192812) do
+ActiveRecord::Schema.define(:version => 20131127234313) do
 
   create_table "antwiki_valid_taxa", :id => false, :force => true do |t|
     t.string   "name"
