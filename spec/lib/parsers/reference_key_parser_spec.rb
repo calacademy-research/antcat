ecbd566df6a6efb321556e3d1a460bc25860c5dc
Mark Wilden <mark@mwilden.com>
2014-01-01 19:55:22 -0800

    Handle various types of reference key text to parse

--- spec/lib/parsers/reference_key_parser_spec.rb
+++ spec/lib/parsers/reference_key_parser_spec.rb
@@ -13,6 +13,15 @@ describe Parsers::ReferenceKeyParser do
     it "should return two authors" do
       @parser.parse('Bolton & Fisher, 1975').should == {author_last_names: ['Bolton', 'Fisher'], nester_last_names: nil, year: '1975', year_ordinal: nil}
     end
+    it "should handle et al." do
+      @parser.parse('Bolton, <i>et al.</i> 1975').should == {author_last_names: ['Bolton'], nester_last_names: nil, year: '1975', year_ordinal: nil}
+    end
+    it "should handle three or more items" do
+      @parser.parse('Bolton, Fisher & Ward, 1975').should == {author_last_names: ['Bolton', 'Fisher', 'Ward'], nester_last_names: nil, year: '1975', year_ordinal: nil}
+    end
+    it "should handle it when the year is missing" do
+      @parser.parse('Bolton').should == {author_last_names: ['Bolton'], nester_last_names: nil, year: nil, year_ordinal: nil}
+    end
   end
 
 end
