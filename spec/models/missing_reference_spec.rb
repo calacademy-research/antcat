ab5fc980f9e97652114cebe2d5ef802162c77fc1
Mark Wilden <mark@mwilden.com>
2014-01-03 10:17:01 -0800

    Rationalize code

--- spec/models/missing_reference_spec.rb
+++ spec/models/missing_reference_spec.rb
@@ -3,25 +3,28 @@ require 'spec_helper'
 
 describe MissingReference do
 
-  describe "Replacing all missing references" do
-    it "should replace a reference" do
-      missing_reference = FactoryGirl.create :missing_reference, citation: 'Borowiec, 2010'
-      protonym = FactoryGirl.create :protonym, authorship: FactoryGirl.create(:citation, reference: missing_reference)
-      taxon = create_genus protonym: protonym
-      taxon.protonym.authorship.reference.should == missing_reference
+  describe "Replacing" do
+    describe "Replacing all missing references" do
+      it "should replace a reference" do
+        missing_reference = FactoryGirl.create :missing_reference, citation: 'Borowiec, 2010'
+        protonym = FactoryGirl.create :protonym, authorship: FactoryGirl.create(:citation, reference: missing_reference)
+        taxon = create_genus protonym: protonym
+        taxon.protonym.authorship.reference.should == missing_reference
+        nonmissing_reference = FactoryGirl.create :article_reference, key_cache: 'Borowiec, 2010'
 
-      nonmissing_reference = FactoryGirl.create :article_reference, key_cache: 'Borowiec, 2010'
-      MissingReference.replace_all
-      MissingReference.count.should == 0
-      taxon.reload.protonym.authorship.reference.should == nonmissing_reference
-    end
-    it "should report the MissingReferences that can't be found" do
-      missing_reference = FactoryGirl.create :missing_reference, citation: 'Borowiec, 2010'
-      MissingReference.replace_all.should == ['Borowiec, 2010']
-      MissingReference.count.should == 1
+        MissingReference.replace_all
+
+        MissingReference.count.should == 0
+        taxon.reload.protonym.authorship.reference.should == nonmissing_reference
+      end
+      it "should report the MissingReferences that can't be found" do
+        missing_reference = FactoryGirl.create :missing_reference, citation: 'Borowiec, 2010'
+        MissingReference.replace_all.should == ['Borowiec, 2010']
+        MissingReference.count.should == 1
+      end
     end
 
-    describe "when a MissingReference is found" do
+    describe "Replacing one missing reference" do
       before do
         @found_reference = FactoryGirl.create :article_reference
         @missing_reference = FactoryGirl.create :missing_reference
@@ -44,27 +47,26 @@ describe MissingReference do
         @missing_reference.replace_with @found_reference
         citation.reload.reference.should == @found_reference
       end
+    end
 
-      describe "Reporting" do
-        it "should report the unfound MissingReferences" do
-        end
+    describe "Batch processing a number of replacements in one pass" do
+      before do
+        @found_reference = FactoryGirl.create :article_reference
+        @missing_reference = FactoryGirl.create :missing_reference
       end
-
-      describe "Batch processing a number of replacements in one pass" do
-        it "should replace references in taxt to the MissingReference to the found reference" do
-          item = TaxonHistoryItem.create! taxt: "{ref #{@missing_reference.id}}"
-          Reference.replace_with_batch [{replace: @missing_reference, with: @found_reference}]
-          item.reload.taxt.should == "{ref #{@found_reference.id}}"
-        end
-        it "should replace references in citations" do
-          citation = Citation.create! reference: @missing_reference
-          Reference.replace_with_batch [{replace: @missing_reference, with: @found_reference}]
-          citation.reload.reference.should == @found_reference
-        end
-
+      it "should replace references in taxt to the MissingReference to the found reference" do
+        item = TaxonHistoryItem.create! taxt: "{ref #{@missing_reference.id}}"
+        Reference.replace_with_batch [{replace: @missing_reference, with: @found_reference}]
+        item.reload.taxt.should == "{ref #{@found_reference.id}}"
+      end
+      it "should replace references in citations" do
+        citation = Citation.create! reference: @missing_reference
+        Reference.replace_with_batch [{replace: @missing_reference, with: @found_reference}]
+        citation.reload.reference.should == @found_reference
       end
     end
   end
+
   describe "Optional year" do
     it "should permit a missing year (unlike other references)" do
       MissingReference.new(title: 'missing', citation: 'Bolton').should be_valid
