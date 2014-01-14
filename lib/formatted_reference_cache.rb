bb85febecb0f78d99bef0a5c51ea1d2adfdd0dc0
Mark Wilden <mark@mwilden.com>
2014-01-13 22:05:01 -0800

    FormattedReferenceCache

--- lib/formatted_reference_cache.rb
+++ lib/formatted_reference_cache.rb
@@ -1,7 +1,15 @@
 # coding: UTF-8
 class FormattedReferenceCache
-  def self.cache reference, formatted_reference
+
+  def self.get reference, formatted_reference
+    return unless reference
+    reference.formatted_cache
+  end
+
+  def self.set reference, formatted_reference
+    return unless reference
     reference.formatted_cache = formatted_reference
     reference.save!
   end
+
 end
