81f28de09b891952b62fb3e3a8e7d84706bb0eb2
Mark Wilden <mark@mwilden.com>
2014-01-15 09:40:27 -0800

    Use internal methods, format

--- app/models/reference_formatter_cache.rb
+++ app/models/reference_formatter_cache.rb
@@ -1,22 +1,27 @@
 # coding: UTF-8
 class ReferenceFormatterCache
   include Singleton
+
   def invalidate reference
     return unless reference.formatted_cache?
-    reference.update_column :formatted_cache, nil unless reference.new_record?
+    set reference, nil unless reference.new_record?
     Reference.where("nested_reference_id = ?", reference.id).each do |nestee|
       nestee.invalidate_formatted_reference_cache
     end
   end
+
   def get reference
     Reference.find(reference.id).formatted_cache
   end
+
   def set reference, value
     reference.update_column :formatted_cache, value
   end
+
   def populate reference
-    value = Formatters::ReferenceFormatter.format(reference, false)
-    self.set reference, value
+    value = Formatters::ReferenceFormatter.format reference, false
+    set reference, value
     value
   end
+
 end
