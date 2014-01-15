cd963d1910aac0cc8007aea76a767231d34f32fa
Mark Wilden <mark@mwilden.com>
2014-01-15 09:57:20 -0800

    Remove unneeded code

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,6 @@
 class Place < ActiveRecord::Base
   validates_presence_of :name
   has_paper_trail
-  before_update :invalidate_formatted_reference_cache
 
   def self.import name
     place = find_or_create_by_name name
