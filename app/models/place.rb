890d4db92cae20197349f737a0cd8609f15c049b
Mark Wilden <mark@mwilden.com>
2014-01-14 18:04:12 -0800

    Invalidate cache when place changes

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,11 +2,18 @@
 class Place < ActiveRecord::Base
   validates_presence_of :name
   has_paper_trail
+  before_update :invalidate_formatted_reference_cache
 
   def self.import name
     place = find_or_create_by_name name
     raise unless place.valid?
     place
+  end
+
+  def invalidate_formatted_reference_cache
+    Reference.joins(publisher: [:place]).where('places.id = ?', id).each do |reference|
+      reference.invalidate_formatted_reference_cache
+    end
   end
 
 end
