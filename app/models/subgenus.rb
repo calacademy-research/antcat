7985a1dcbcc0069fcaf2d27598510f8c0ddd5db5
Mark Wilden <mark@mwilden.com>
2014-02-19 19:05:47 -0800

    No need to update a subgenus's parent, as that capability isn't offered

--- app/models/subgenus.rb
+++ app/models/subgenus.rb
@@ -4,11 +4,6 @@ class Subgenus < GenusGroupTaxon
   validates_presence_of :genus
   has_many :species
 
-  def update_parent new_parent
-    super
-    genus = new_parent.genus
-  end
-
   def species_group_descendants
     Taxon.where(subgenus_id: id).where('taxa.type != ?', 'subgenus').includes(:name).order('names.epithet')
   end
