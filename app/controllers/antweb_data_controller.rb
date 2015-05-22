d26cf88fbee70027bda37f816058306a1f2b9188
Joe Russack <joe@nothingugly.com>
2015-05-22 16:44:31 -0700

    Yay for committing testing code

--- app/controllers/antweb_data_controller.rb
+++ app/controllers/antweb_data_controller.rb
@@ -1,8 +1,7 @@
 class AntwebDataController < ApplicationController
 
   def index
-    render file: "/Users/joe/antcat/boo.txt", layout: false, content_type: 'text/plain'
-    # render file: "/data/antcat/shared/antcat.antweb.txt", layout: false, content_type: 'text/plain'
+    render file: "/data/antcat/shared/antcat.antweb.txt", layout: false, content_type: 'text/plain'
 
   end
 end
