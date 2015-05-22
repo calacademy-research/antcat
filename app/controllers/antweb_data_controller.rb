b6d3471113b79e956c0a1f747472e710220bef19
Joe Russack <joe@nothingugly.com>
2015-05-22 16:11:57 -0700

    Bugfixes and controller to pull static file

---
+++ app/controllers/antweb_data_controller.rb
@@ -0,0 +1,8 @@
+class AntwebDataController < ApplicationController
+
+  def index
+    render file: "/data/antcat/shared/antcat.antweb.txt", layout: false, content_type: 'text/plain'
+  end
+end
+
+
