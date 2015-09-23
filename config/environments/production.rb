afbcac1c32c0ac84b28c0699a780b23ffe5863a8
Joe Russack <joe@nothingugly.com>
2015-09-23 16:52:00 -0700

    diagnose failed emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'antcat.org' }
 
 
