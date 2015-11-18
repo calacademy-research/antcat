8f78f51c154e2cd5012373de17ee5e48ccd89144
jonkerz <jonkerz@gmail.com>
2015-11-18 01:59:12 +0100

    lower log level in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.log_level = :info
+  
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
