ab33c9bf751c5b75907036b961e7266e680ce9a7
jonkerz <jonkerz@gmail.com>
2015-11-22 18:45:48 +0100

    added failing test before refactoring

--- spec/lib/formatters/snake_spec.rb
+++ spec/lib/formatters/snake_spec.rb
@@ -18,4 +18,15 @@ describe "array snaking" do
   it "should handle empty cells" do
     expect([1,2,3,4,5].snake(2)).to eq([[1,4], [2,5], [3,nil]])
   end
+
+  it "should put all nil padding items at the end" do
+    pending "current method cannot handle all items/columns combinations"
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    manually_snaked = [
+      [1, 4, 6, 8, 10],
+      [2, 5, 7, 9, 11],
+      [3, nil, nil, nil, nil]
+    ]
+    expect(array.snake(5)).to eq(manually_snaked)
+  end
 end
