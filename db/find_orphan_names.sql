8b6500373e5874a2e6505a17a0597ed4cee48082
jonkerz <jonkerz@gmail.com>
2015-12-04 14:25:18 +0100

    remove unused/stale/outdated

--- db/find_orphan_names.sql
+++ db/find_orphan_names.sql
@@ -1,1 +1,1 @@
-select names.id, names.name from names left outer join taxa on taxa.name_id = names.id where taxa.id IS NUL
+select names.id, names.name from names left outer join taxa on taxa.name_id = names.id where taxa.id IS NULL;
