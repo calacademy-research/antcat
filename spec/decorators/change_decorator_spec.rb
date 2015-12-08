76c6b152bc1ad364cd95afbe348895de0227c7a8
jonkerz <jonkerz@gmail.com>
2015-12-08 04:29:04 +0100

    start cleanup decorator specs

--- spec/decorators/change_decorator_spec.rb
+++ spec/decorators/change_decorator_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe ChangeDecorator do
+
+  describe "Formatting approver name" do
+    it "should call Formatters::CatalogFormatter" do
+      approver = FactoryGirl.create :editor, name: 'Brian Fisher'
+      change = FactoryGirl.create :change, approver: approver
+      string = change.decorate.format_approver_name
+      expect(string).to match(/Brian Fisher/)
+      expect(string).to match(/approved this change/)
+    end
+  end
+  
 end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe UserDecorator do
+  let(:user) do
+    FactoryGirl.create :user, name: "First Last", email: "email@example.com"
+  end
+  let(:decorated_user) { user.decorate }
+
+  describe "#format_name_linking_to_email" do
+    it "formats email and name" do
+      expect(decorated_user.format_doer_name)
+        .to eq '<a href="mailto:email@example.com">First Last</a>'
+    end
+  end
+
 end
