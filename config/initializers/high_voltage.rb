653666a14f348cd34e4d64d9bf5bf4df098bae5f
jonkerz <jonkerz@gmail.com>
2015-12-27 19:15:39 +0100

    Put static pages at the root and organized help pages in a subdir.
    That is, help pages are now found nested under antcat.org/help/xzy,
    and all other static pages (there's only one actually, antcat.org/about) are at the root instead of antcat.org/pages/about.
    The directory structure has not changes (all high_voltage pages are sill in /app/view/pages).

---
+++ config/initializers/high_voltage.rb
@@ -0,0 +1,8 @@
+# Removes the top-level prefix 'pages' from the URL
+#   '/about' instead of '/pages/about'
+#   '/help/tooltips' instead of '/pages/help/tooltips', etc, etc
+# The views are till located in 'app/views/pages'. For some reason the extensions
+# must include the html suffix '.html.haml", ie not just '.haml'.
+HighVoltage.configure do |config|
+  config.route_drawer = HighVoltage::RouteDrawers::Root
+end
