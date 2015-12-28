aa95cecab217e8b073bf17e69a3d49c397e11179
jonkerz <jonkerz@gmail.com>
2015-12-28 20:50:46 +0100

    General: tweaked active_admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,12 @@
 ActiveAdmin.register User do
-  permit_params :email, :is_superadmin, :password, :password_confirmation
+  permit_params :email, :can_edit, :is_superadmin, :password, :password_confirmation
 
   index do
     selectable_column
-    # id_column
+    id_column
+    column :name
     column :email
+    column :can_edit
     column :is_superadmin
     column :current_sign_in_at
     column :sign_in_count
@@ -12,15 +14,18 @@ ActiveAdmin.register User do
     actions
   end
 
+  filter :name
   filter :email
+  filter :can_edit
   filter :is_superadmin
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs "User Details" do
       f.input :email
+      f.input :can_edit
       f.input :is_superadmin
       f.input :password
       f.input :password_confirmation
