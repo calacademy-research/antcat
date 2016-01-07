0379ea5bf086e5125814fe5e0d3660829da8d668
jonkerz <jonkerz@gmail.com>
2016-01-07 14:36:14 +0100

    Removed SandboxMilieu from the specs (class already removed)

--- spec/lib/milieu_spec.rb
+++ spec/lib/milieu_spec.rb
@@ -53,47 +53,5 @@ describe Milieu do
         expect(@milieu.user_can_approve_changes?(@user)).to be_falsey
       end
     end
-
-    describe "Production milieu" do
-      before do
-        @user = double
-        @milieu = SandboxMilieu.new
-      end
-
-      specify "Uploading PDFs" do
-        expect(@milieu.user_can_upload_pdfs?(nil)).to be_falsey
-        expect(@milieu.user_can_upload_pdfs?(@user)).to be_falsey
-      end
-
-      specify "Being an editor" do
-        expect(@milieu.user_is_editor?(nil)).to be_truthy
-        expect(@milieu.user_is_editor?(@user)).to be_truthy
-      end
-
-      specify "Editing references" do
-        expect(@milieu.user_can_edit?(nil)).to be_truthy
-        expect(@milieu.user_can_edit?(@user)).to be_truthy
-        expect(@milieu.user_can_edit?(@user)).to be_truthy
-      end
-
-      specify "Editing the catalog" do
-        expect(@milieu.user_can_edit?(nil)).to be_truthy
-        expect(@milieu.user_can_edit?(@user)).to be_truthy
-        expect(@milieu.user_can_edit?(@user)).to be_truthy
-      end
-
-      specify "Reviewing changes" do
-        expect(@milieu.user_can_review_changes?(nil)).to be_truthy
-        expect(@milieu.user_can_approve_changes?(@user)).to be_truthy
-        expect(@milieu.user_can_approve_changes?(@user)).to be_truthy
-      end
-
-      specify "Approving changes" do
-        expect(@milieu.user_can_approve_changes?(nil)).to be_truthy
-        expect(@milieu.user_can_approve_changes?(@user)).to be_truthy
-        expect(@milieu.user_can_approve_changes?(@user)).to be_truthy
-      end
-
-    end
   end
 end
