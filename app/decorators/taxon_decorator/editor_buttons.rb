calacademy-research/antcat

commit c8e6ac97238e0b3b8960ff19ce2534dd802f2fff

More explicit, and does not depend on jQuery UI or ButtonHelper.

app/decorators/taxon_decorator/editor_buttons.rb
@@ -29,8 +29,8 @@ module TaxonDecorator::EditorButtons
 
     def link_to_delete_taxon
       if get_current_user.try :is_superadmin?
-        parameters = { 'data-delete-location' => helpers.taxa_path(taxon), 'data-taxon-id' => "#{taxon.id}" }
-        helpers.button 'Delete', 'delete_button', parameters, ["btn-delete"]
+        helpers.link_to 'Delete', "#", id: "delete_button", class: "btn-delete",
+          data: { 'data-delete-location' => helpers.taxa_path(taxon), "taxon-id" => taxon.id }
       end
     end
 end

app/assets/javascripts/catalog.coffee
@@ -19,7 +19,6 @@ $ ->
   splitter = new AntCat.Splitter $('#splitter'), on_splitter_change
 
   $('#delete_button')
-    .button()
     .click =>
       taxon_id = $('#delete_button').data('taxon-id')
       url = "/catalog/delete_impact_list/" + taxon_id

features/editing/edit_authorization.feature
@@ -13,7 +13,7 @@ Feature: Editing a taxon with authorization constraints
     And I log in as a bibliography editor
     When I go to the catalog page for "Calyptites"
     Then I should not see a "Edit" button
-    And I should not see a "Delete" button
+    And I should not see "Delete"
 
   Scenario: Trying to edit a taxon that's waiting for approval
     Given I log in as a catalog editor
@@ -21,14 +21,14 @@ Feature: Editing a taxon with authorization constraints
     When I go to the catalog page for "Calyptites"
     Then I should see an "Edit" button
     And I should see "Review change"
-    And I should not see a "Delete" button
+    And I should not see "Delete"
 
   Scenario: Seeing the delete button as superadmin
     Given there is a genus "Calyptites"
     And I log in as a superadmin
     When I go to the catalog page for "Calyptites"
     Then I should see an "Edit" button
-    And I should see a "Delete" button
+    And I should see "Delete"
 
   Scenario: Trying to edit a taxon that's waiting for approval
     Given I log in as a catalog editor

commit c3708cb9e2698aafde5a10ed71904f530925b98e

(superadmin delete button) Fix obvious bug introduced in c8e6ac97238e0b3b8960ff19ce2534dd802f2fff

app/decorators/taxon_decorator/editor_buttons.rb
@@ -30,7 +30,7 @@ module TaxonDecorator::EditorButtons
     def link_to_delete_taxon
       if get_current_user.try :is_superadmin?
         helpers.link_to 'Delete', "#", id: "delete_button", class: "btn-delete",
-          data: { 'data-delete-location' => helpers.taxa_path(taxon), "taxon-id" => taxon.id }
+          data: { 'delete-location' => helpers.taxa_path(taxon), "taxon-id" => taxon.id }
       end
     end
 end