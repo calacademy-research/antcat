1f310f41dd039e55e279a3ba34f35a8576ee3782
jonkerz <jonkerz@gmail.com>
2016-01-27 04:48:07 +0100

    Use Rails model predicate methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :recoverable, :registerable,
          :rememberable, :trackable, :validatable, :invitable
 
-  def is_editor?
-    can_edit
-  end
-
   def can_approve_changes?
-    can_edit
+    can_edit?
   end
 
   def can_review_changes?
-    can_edit
+    can_edit?
   end
 
   def can_upload_pdfs?
-    can_edit
-  end
-
-  def is_superadmin?
-    is_superadmin
+    can_edit?
   end
 
 end
