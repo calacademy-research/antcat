b1d447fc6bf6076bf70839ea8213da3b1432bf2e
jonkerz <jonkerz@gmail.com>
2016-01-28 07:40:22 +0100

    Added dev-specific CSS.
    Makes it easier to not accidentally delete everything in prod.
    
    Suffix the url with ?prod=pizza to disable.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,7 @@ Rails.application.config.assets.precompile += %w( *.js )
 
 Rails.application.config.assets.precompile += %w( active_admin.css )
 Rails.application.config.assets.precompile += %w( bootstrap_custom.css )
+Rails.application.config.assets.precompile += %w( dev_env.css )
 Rails.application.config.assets.precompile += %w( jquery_overrides.css )
 
 Rails.application.config.assets.precompile += %w( widgets/*.css )
