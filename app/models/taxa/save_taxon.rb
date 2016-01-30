9362be26f3c54021347649f9bdb71374e4eb1944
jonkerz <jonkerz@gmail.com>
2016-01-30 07:08:38 +0100

    Marked as private without modifying the code

--- app/models/taxa/save_taxon.rb
+++ app/models/taxa/save_taxon.rb
@@ -12,30 +12,6 @@ class TaxonMother
     @taxon
   end
 
-  def remove_auto_generated
-    @taxon.auto_generated = false
-    name = @taxon.name
-    if name.auto_generated
-      name.auto_generated = false
-      name.save
-    end
-    synonyms = Synonym.where(senior_synonym_id: @taxon.id)
-    synonyms.each do |synonym|
-      if synonym.auto_generated
-        synonym.auto_generated = false
-        synonym.save
-      end
-    end
-
-    synonyms = Synonym.where(junior_synonym_id: @taxon.id)
-    synonyms.each do |synonym|
-      if synonym.auto_generated
-        synonym.auto_generated = false
-        synonym.save
-      end
-    end
-  end
-
   # previous_combination will be a pointer to the species taxon record
   # if non-nil
   def save_taxon taxon, params, previous_combination = nil
@@ -106,90 +82,114 @@ class TaxonMother
     end
   end
 
-  def update_elements taxon, params, taxon_to_update, status_string, change_id
-    taxon_to_update.status = status_string
-    taxon_to_update.current_valid_taxon = @taxon
-    TaxonHistoryItem.where(taxon_id: taxon_to_update.id).update_all(taxon_id: @taxon.id)
-    Synonym.where(senior_synonym_id: taxon_to_update.id).update_all(senior_synonym_id: @taxon.id)
-    Synonym.where(junior_synonym_id: taxon_to_update.id).update_all(junior_synonym_id: @taxon.id)
-    taxon_to_update.save
-  end
-
-  def get_status_string taxon_to_update
-    if Status[taxon_to_update] == Status['original combination']
-      Status['original combination'].to_s
-    else
-      Status['obsolete combination'].to_s
+  private
+    def update_elements taxon, params, taxon_to_update, status_string, change_id
+      taxon_to_update.status = status_string
+      taxon_to_update.current_valid_taxon = @taxon
+      TaxonHistoryItem.where(taxon_id: taxon_to_update.id).update_all(taxon_id: @taxon.id)
+      Synonym.where(senior_synonym_id: taxon_to_update.id).update_all(senior_synonym_id: @taxon.id)
+      Synonym.where(junior_synonym_id: taxon_to_update.id).update_all(junior_synonym_id: @taxon.id)
+      taxon_to_update.save
     end
-  end
 
-  ####################################
-  def update_name attributes
-    attributes[:name_id] = attributes.delete :id
-    gender = attributes.delete :gender
-    @taxon.name.gender = gender.blank? ? nil : gender
-    @taxon.attributes = attributes
-  end
-
-  def update_name_status_flags attributes
-    attributes[:incertae_sedis_in] = nil unless attributes[:incertae_sedis_in].present?
-    @taxon.attributes = attributes
-    @taxon.headline_notes_taxt = Taxt.from_editable attributes.delete :headline_notes_taxt
-    if attributes[:type_taxt]
-      @taxon.type_taxt = Taxt.from_editable attributes.delete :type_taxt
+    def get_status_string taxon_to_update
+      if Status[taxon_to_update] == Status['original combination']
+        Status['original combination'].to_s
+      else
+        Status['obsolete combination'].to_s
+      end
     end
-  end
 
-  def update_parent attributes
-    return unless attributes
-    @taxon.update_parent Taxon.find_by_name_id attributes[:id]
-  rescue Taxon::TaxonExists
-    @taxon.errors[:base] = "This name is in use by another taxon"
-    raise
-  end
-
-  def update_homonym_replaced_by attributes
-    replacement_id = attributes[:id]
-    replacement = replacement_id.present? ? Taxon.find_by_name_id(replacement_id) : nil
-    @taxon.homonym_replaced_by = replacement
-  end
-
-  def update_current_valid_taxon attributes
-    replacement_id = attributes[:id]
-    replacement = replacement_id.present? ? Taxon.find_by_name_id(replacement_id) : nil
-    @taxon.current_valid_taxon = replacement
-  end
-
-  def update_protonym attributes
-    attributes[:name_id] = attributes.delete(:name_attributes)[:id]
-    update_protonym_authorship attributes.delete :authorship_attributes
-    @taxon.protonym.attributes = attributes
-  end
-
-  def update_protonym_authorship attributes
-    return unless @taxon.protonym.authorship
-    attributes[:reference_id] = attributes.delete(:reference_attributes)[:id]
-    return if attributes[:reference_id].blank? and @taxon.protonym.authorship.reference.blank?
-    if attributes[:notes_taxt]
-      @taxon.protonym.authorship.notes_taxt = Taxt.from_editable attributes.delete :notes_taxt
-    end
-    @taxon.protonym.authorship.attributes = attributes
-  end
-
-  def update_type_name attributes
-    # ugly way to handle optional, but possibly pre-built, subobject
-    if @taxon.type_name && @taxon.type_name.new_record? && (!attributes or attributes[:id] == '')
-      @taxon.type_name = nil
-      return
-    end
-    # Why do we hit this case?
-    if !attributes.nil?
-      attributes[:type_name_id] = attributes.delete :id
+    ####################################
+    def update_name attributes
+      attributes[:name_id] = attributes.delete :id
+      gender = attributes.delete :gender
+      @taxon.name.gender = gender.blank? ? nil : gender
       @taxon.attributes = attributes
     end
-  end
 
-  private
+    def update_name_status_flags attributes
+      attributes[:incertae_sedis_in] = nil unless attributes[:incertae_sedis_in].present?
+      @taxon.attributes = attributes
+      @taxon.headline_notes_taxt = Taxt.from_editable attributes.delete :headline_notes_taxt
+      if attributes[:type_taxt]
+        @taxon.type_taxt = Taxt.from_editable attributes.delete :type_taxt
+      end
+    end
+
+    def update_parent attributes
+      return unless attributes
+      @taxon.update_parent Taxon.find_by_name_id attributes[:id]
+    rescue Taxon::TaxonExists
+      @taxon.errors[:base] = "This name is in use by another taxon"
+      raise
+    end
+
+    def update_homonym_replaced_by attributes
+      replacement_id = attributes[:id]
+      replacement = replacement_id.present? ? Taxon.find_by_name_id(replacement_id) : nil
+      @taxon.homonym_replaced_by = replacement
+    end
+
+    def update_current_valid_taxon attributes
+      replacement_id = attributes[:id]
+      replacement = replacement_id.present? ? Taxon.find_by_name_id(replacement_id) : nil
+      @taxon.current_valid_taxon = replacement
+    end
+
+    def update_protonym attributes
+      attributes[:name_id] = attributes.delete(:name_attributes)[:id]
+      update_protonym_authorship attributes.delete :authorship_attributes
+      @taxon.protonym.attributes = attributes
+    end
+
+    def update_protonym_authorship attributes
+      return unless @taxon.protonym.authorship
+      attributes[:reference_id] = attributes.delete(:reference_attributes)[:id]
+      return if attributes[:reference_id].blank? and @taxon.protonym.authorship.reference.blank?
+      if attributes[:notes_taxt]
+        @taxon.protonym.authorship.notes_taxt = Taxt.from_editable attributes.delete :notes_taxt
+      end
+      @taxon.protonym.authorship.attributes = attributes
+    end
+
+    def update_type_name attributes
+      # ugly way to handle optional, but possibly pre-built, subobject
+      if @taxon.type_name && @taxon.type_name.new_record? && (!attributes or attributes[:id] == '')
+        @taxon.type_name = nil
+        return
+      end
+      # Why do we hit this case?
+      if !attributes.nil?
+        attributes[:type_name_id] = attributes.delete :id
+        @taxon.attributes = attributes
+      end
+    end
+
+    def remove_auto_generated
+      @taxon.auto_generated = false
+      name = @taxon.name
+      if name.auto_generated
+        name.auto_generated = false
+        name.save
+      end
+      synonyms = Synonym.where(senior_synonym_id: @taxon.id)
+      synonyms.each do |synonym|
+        if synonym.auto_generated
+          synonym.auto_generated = false
+          synonym.save
+        end
+      end
+
+      synonyms = Synonym.where(junior_synonym_id: @taxon.id)
+      synonyms.each do |synonym|
+        if synonym.auto_generated
+          synonym.auto_generated = false
+          synonym.save
+        end
+      end
+    end
+
     def save_taxon_children taxon
       return if taxon.kind_of?(Family) || taxon.kind_of?(Subspecies)
       taxon.children.each do |c|
