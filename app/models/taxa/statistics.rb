3ff83bb2f0516b8ae8ae8eddf9f83c22e2828a38
jonkerz <jonkerz@gmail.com>
2016-01-31 11:38:18 +0100

    Extracted statistics into a concern

---
+++ app/models/taxa/statistics.rb
@@ -0,0 +1,41 @@
+module Taxa::Statistics
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def massage_count count, rank, statistics
+      count.keys.each do |fossil, status|
+        value = count[[fossil, status]]
+        extant_or_fossil = fossil ? :fossil : :extant
+        statistics[extant_or_fossil] ||= {}
+        statistics[extant_or_fossil][rank] ||= {}
+        statistics[extant_or_fossil][rank][status] = value
+      end
+    end
+  end
+
+  def get_statistics ranks
+    statistics = {}
+    ranks.each do |rank|
+      count = send(rank).group('fossil', 'status').count
+      delete_original_combinations count
+      self.class.massage_count count, rank, statistics
+    end
+    statistics
+  end
+
+  def delete_original_combinations count
+    count.delete [true, 'original combination']
+    count.delete [false, 'original combination']
+  end
+
+  def child_list_query children_selector, conditions = {}
+    children = send children_selector
+    children = children.where(fossil: !!conditions[:fossil]) if conditions.key? :fossil
+    incertae_sedis_in = conditions[:incertae_sedis_in]
+    children = children.where(incertae_sedis_in: incertae_sedis_in) if incertae_sedis_in
+    children = children.where(hong: !!conditions[:hong]) if conditions.key? :hong
+    children = children.where(status: 'valid')
+    children = children.ordered_by_name
+    children
+  end
+end
--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -4,6 +4,7 @@ class Taxon < ActiveRecord::Base
   include UndoTracker
   include Taxa::CallbacksAndValidators
   include Taxa::PredicateMethods
+  include Taxa::Statistics
 
   class TaxonExists < StandardError; end
 
@@ -310,41 +311,7 @@ class Taxon < ActiveRecord::Base
 
   ###############################################
   # statistics
-  def get_statistics ranks
-    statistics = {}
-    ranks.each do |rank|
-      count = send(rank).group('fossil', 'status').count
-      delete_original_combinations count
-      self.class.massage_count count, rank, statistics
-    end
-    statistics
-  end
 
-  def delete_original_combinations count
-    count.delete [true, 'original combination']
-    count.delete [false, 'original combination']
-  end
-
-  def self.massage_count count, rank, statistics
-    count.keys.each do |fossil, status|
-      value = count[[fossil, status]]
-      extant_or_fossil = fossil ? :fossil : :extant
-      statistics[extant_or_fossil] ||= {}
-      statistics[extant_or_fossil][rank] ||= {}
-      statistics[extant_or_fossil][rank][status] = value
-    end
-  end
-
-  def child_list_query children_selector, conditions = {}
-    children = send children_selector
-    children = children.where(fossil: !!conditions[:fossil]) if conditions.key? :fossil
-    incertae_sedis_in = conditions[:incertae_sedis_in]
-    children = children.where(incertae_sedis_in: incertae_sedis_in) if incertae_sedis_in
-    children = children.where(hong: !!conditions[:hong]) if conditions.key? :hong
-    children = children.where(status: 'valid')
-    children = children.ordered_by_name
-    children
-  end
 
   ###############################################
   def references options = {}
