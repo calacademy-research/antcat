4945bcb8ad64a44eb308c28f39953cd04b5999d2
jonkerz <jonkerz@gmail.com>
2016-02-18 22:25:16 +0100

    Added "AntCat" breadcrumbs (users/login/etc)

---
+++ config/breadcrumbs/antcat.rb
@@ -0,0 +1,23 @@
+crumb :antcat do
+ link "AntCat"
+end
+
+crumb :about do
+  link "About", page_path('about')
+  parent :antcat
+end
+
+crumb :users do
+  link "Users", users_path
+  parent :antcat
+end
+
+crumb :login do
+  link "Login"
+  parent :antcat
+end
+
+crumb :forgot_password do
+  link "Forgot Password"
+  parent :antcat
+end
