b80b125636cb9b04578e3298e92ff138a143ee9d
jonkerz <jonkerz@gmail.com>
2016-02-22 12:18:56 +0100

    Tweaked

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -11,19 +11,22 @@ module CatalogHelper
 
   # The "(no subfamily/tribe)"/"?child=none" links
   def incertae_sedis_column_link rank, taxon, selected_taxon, parent_taxon
-    classes = 'valid'
-    classes << ' selected' if taxon == selected_taxon
+    classes = ['valid']
+    classes << 'selected' if taxon == selected_taxon
+
     link_to "(no #{rank})", catalog_path(parent_taxon, child: "none"), class: classes
   end
 
   def taxon_column_link taxon, selected_taxon
-    classes = taxon_css_classes taxon, selected: taxon == selected_taxon
-    label = taxon_label taxon
-    link_to label, catalog_path(taxon), class: classes
+    classes = css_classes_for_rank(taxon)
+    classes = css_classes_for_status(taxon)
+    classes << 'selected' if taxon == selected_taxon
+
+    link_to taxon_label(taxon), catalog_path(taxon), class: classes
   end
 
   def taxon_label_span taxon
-    content_tag :span, class: taxon_css_classes(taxon, ignore_status: true) do
+    content_tag :span, class: css_classes_for_rank(taxon) do
       taxon_label(taxon).html_safe
     end
   end
@@ -53,17 +56,12 @@ module CatalogHelper
       "?#{hash.to_query}" if hash.present?
     end
 
-    def taxon_css_classes(taxon, selected: nil, ignore_status: false)
-      css_classes = css_classes_for_rank taxon
-
-      unless ignore_status
-        css_classes << taxon.status.downcase.gsub(/ /, '_')
-        css_classes << 'nomen_nudum' if taxon.nomen_nudum?
-        css_classes << 'collective_group_name' if taxon.collective_group_name?
-      end
-
-      css_classes << 'selected' if selected
-      css_classes.sort.join ' '
+    def css_classes_for_status taxon
+      css_classes = []
+      css_classes << taxon.status.downcase.gsub(/ /, '_')
+      css_classes << 'nomen_nudum' if taxon.nomen_nudum?
+      css_classes << 'collective_group_name' if taxon.collective_group_name?
+      css_classes
     end
 
 end
