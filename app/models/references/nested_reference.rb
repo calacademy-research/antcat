6eed261362f6b49b81c5169bd574bc504e33cdd6
jonkerz <jonkerz@gmail.com>
2016-02-28 07:33:59 +0100

    Marked as private

--- app/models/references/nested_reference.rb
+++ app/models/references/nested_reference.rb
@@ -8,19 +8,20 @@ class NestedReference < Reference
 
   def self.requires_title; false end
 
-  def validate_nested_reference_exists
-    errors.add(:nesting_reference_id, 'does not exist') if nesting_reference_id && !Reference.find_by_id(nesting_reference_id)
-  end
-
-  def validate_nested_reference_doesnt_point_to_itself
-    comparison = self
-    while comparison && comparison.nesting_reference_id
-      if comparison.nesting_reference_id == id
-        errors.add(:nesting_reference_id, "can't point to itself")
-        break
-      end
-      comparison = Reference.find_by_id comparison.nesting_reference_id
+  private
+    def validate_nested_reference_exists
+      errors.add(:nesting_reference_id, 'does not exist') if nesting_reference_id && !Reference.find_by_id(nesting_reference_id)
     end
-  end
+
+    def validate_nested_reference_doesnt_point_to_itself
+      comparison = self
+      while comparison && comparison.nesting_reference_id
+        if comparison.nesting_reference_id == id
+          errors.add(:nesting_reference_id, "can't point to itself")
+          break
+        end
+        comparison = Reference.find_by_id comparison.nesting_reference_id
+      end
+    end
 
 end
