aa79df25c2eb6c3faba01a8b89560a3cb54b17ae
jonkerz <jonkerz@gmail.com>
2016-03-07 20:09:48 +0100

    Removed Twitter Bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,13 +8,12 @@ Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif 
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.ico)
 
 Rails.application.config.assets.precompile += %w( *.js )
-#Rails.application.config.assets.precompile += %w( app/assets/javascripts/taxon_browser/*.js )
 
 Rails.application.config.assets.precompile += %w( active_admin.css )
 Rails.application.config.assets.precompile += %w( foundation_and_overrides.css )
-Rails.application.config.assets.precompile += %w( bootstrap_custom.css )
 Rails.application.config.assets.precompile += %w( dev_env.css )
 Rails.application.config.assets.precompile += %w( jquery_overrides.css )
 
+Rails.application.config.assets.precompile += %w( site/*.css )
 Rails.application.config.assets.precompile += %w( widgets/*.css )
 Rails.application.config.assets.precompile += %w( controllers/*.css )
