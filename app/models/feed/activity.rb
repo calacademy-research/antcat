ae7fde75e063ab82d6515b2757c42f5bb72417fb
jonkerz <jonkerz@gmail.com>
2016-03-21 12:40:51 +0100

    (feed) globally enabling/disabling

--- app/models/feed/activity.rb
+++ app/models/feed/activity.rb
@@ -1,7 +1,5 @@
 # Upcoming class methods:
 #   create_activity (without a trackable)
-#   enabled?
-#   enabled=
 #   without_tracking
 #   with_tracking
 
@@ -11,10 +9,17 @@ class Feed::Activity < ActiveRecord::Base
 
   serialize :parameters, Hash
 
+  class_attribute :enabled
+
   class << self
     def create_activity_for_trackable trackable, action, parameters = {}
+      return unless enabled?
       self.create! trackable: trackable, action: action,
         user: User.current_user, parameters: parameters
+    end
+
+    def enabled?
+      self.enabled != false
     end
   end
 end
--- spec/models/feed/activity_spec.rb
+++ spec/models/feed/activity_spec.rb
@@ -1,5 +1,39 @@
 require "spec_helper"
 
-describe Activity do
-  # TODO
+Activity = Feed::Activity
+
+describe Feed::Activity do
+  describe ".create_activity_for_trackable" do
+    context "globally enabled" do
+      it "creates activities" do
+        expect do
+          Activity.create_activity_for_trackable nil, nil
+        end.to change{ Activity.count }.by 1
+      end
+    end
+
+    context "globally disabled" do
+      it "doesn't create activities" do
+        Activity.enabled = false
+        Activity.create_activity_for_trackable nil, nil
+        expect(Activity.count).to eq 0
+        Activity.enabled = true
+      end
+    end
+  end
+
+  describe "globally enabling and disabling tracking" do
+    it "is enabled by default" do
+      expect(Activity.enabled?).to be true
+    end
+
+    it "can be disabled and enabled" do
+      Activity.enabled = false
+      expect(Activity.enabled?).to be false
+
+      Activity.enabled = true
+      expect(Activity.enabled?).to be true
+    end
+  end
+
 end
