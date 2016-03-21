12915d5c34d657a20bbf160e0e7afed3da3b671d
jonkerz <jonkerz@gmail.com>
2016-03-21 13:43:02 +0100

    Prettier
    I was worried that implicit returns would not work due to the `ensure` in #_with_or_without_tracking, but it does! http://blog.leshill.org/blog/2009/11/17/ensure-with-explicit-return.html
    
    Ruby saves Ruby, again... so pretty..

--- features/step_definitions/feed_steps.rb
+++ features/step_definitions/feed_steps.rb
@@ -7,37 +7,31 @@ When /^I add a journal for the feed$/ do
 end
 
 When /^I edit a journal for the feed$/ do
-  journal = "outside of block"
-  Feed::Activity.without_tracking do
-    journal = Journal.create name: "Archibald Bulletin"
+  journal = Feed::Activity.without_tracking do
+    Journal.create name: "Archibald Bulletin"
   end
   journal.name = "New Journal Name"
   journal.save!
 end
 
 When /^I delete a journal for the feed$/ do
-  journal = "outside of block"
-  Feed::Activity.without_tracking do
-    journal = Journal.create name: "Archibald Bulletin"
+  journal = Feed::Activity.without_tracking do
+    Journal.create name: "Archibald Bulletin"
   end
   journal.destroy
 end
 
 # TaxonHistoryItem
 When /^I add a taxon history item for the feed$/ do
-  taxon = "outside of block"
-  Feed::Activity.without_tracking do
-    taxon = create_subfamily
-  end
+  taxon = Feed::Activity.without_tracking { create_subfamily }
 
   TaxonHistoryItem.create taxt: "as a subfamily: {ref 123}",
     taxon: taxon
 end
 
 When /^I edit a taxon history item for the feed$/ do
-  item = "outside of block"
-  Feed::Activity.without_tracking do
-    item = TaxonHistoryItem.create taxt: "as a subfamily: {ref 123}",
+  item = Feed::Activity.without_tracking do
+    TaxonHistoryItem.create taxt: "as a subfamily: {ref 123}",
       taxon: create_subfamily
   end
   item.taxt = "as a genus: {ref 123}"
@@ -45,9 +39,8 @@ When /^I edit a taxon history item for the feed$/ do
 end
 
 When /^I delete a taxon history item for the feed$/ do
-  item = "outside of block"
-  Feed::Activity.without_tracking do
-    item = TaxonHistoryItem.create taxt: "as a subfamily: {ref 123}",
+  item = Feed::Activity.without_tracking do
+    TaxonHistoryItem.create taxt: "as a subfamily: {ref 123}",
       taxon: create_subfamily
   end
   item.destroy
