ab7b618ee1ef2105afbaf91fde4ea4b1882747f7
jonkerz <jonkerz@gmail.com>
2016-03-27 15:43:52 +0200

    (feedback) limit comment to 10,000 characters
    More than enough even for crazy people.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,6 @@
 class Feedback < ActiveRecord::Base
   belongs_to :user
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { maximum: 10_000 }
 
   scope :recently_created, ->(time_ago = 5.minutes.ago) {
     where('created_at >= :time_ago', time_ago: time_ago)
