f08a8f107ad2d273366430cc616255e79e25e324
jonkerz <jonkerz@gmail.com>
2016-03-27 16:48:18 +0200

    (feedback) tweaked templates
    We need to use local soon, so, locals.

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,12 +1,13 @@
 class FeedbackMailer < ApplicationMailer
   def feedback_email feedback
-    @feedback = feedback
-    @user = feedback.user
-
-    @feedback.update_attributes email_recipients: emails_with_names
+    feedback.update_attributes email_recipients: emails_with_names
 
     subject = "AntCat Feedback ##{feedback.id}"
-    mail to: @feedback.email_recipients, subject: subject
+    mail to: feedback.email_recipients, subject: subject do |format|
+      format.html {
+        render locals: { feedback: feedback }
+      }
+    end
   end
 
   private
