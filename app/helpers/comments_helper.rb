202987af0677c94c418bd73b5fa47c8a17fb4d60
jonkerz <jonkerz@gmail.com>
2016-04-14 09:56:43 +0200

    (feed) added anchor links for the comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,4 +3,8 @@ module CommentsHelper
     label = commentable.comment_threads.count
     link_to label, url_for(commentable) + "#comments"
   end
+
+  def link_comment comment, label
+    link_to label, url_for(comment.commentable) + "#comment-#{comment.id}"
+  end
 end
