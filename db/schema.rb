1a9a09aa939e50ece75a7ab9aee000beb3032c06
jonkerz <jonkerz@gmail.com>
2016-07-21 06:02:40 +0200

    Fix `OrderName` data issue (#155)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503220908) do
+ActiveRecord::Schema.define(version: 20160721034349) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
