5d62a2e710569dff9a2851bb02108df7413f2cdd
jonkerz <jonkerz@gmail.com>
2016-10-21 16:37:41 +0200

    Removed 'tasks.yml' seed.
    It's example data, not a seed. Also, crashes when there are no users in the db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,3 @@
 tooltips_seed_file = Rails.root.join('db', 'seeds', 'tooltips.yml')
 tooltips = YAML.load_file tooltips_seed_file
 Tooltip.create! tooltips
-
-tasks_seed_file = Rails.root.join('db', 'seeds', 'tasks.yml')
-tasks = YAML.load_file tasks_seed_file
-Task.create! tasks
