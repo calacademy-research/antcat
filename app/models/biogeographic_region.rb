efaeb1ed83bf51b125dc4bb31f8ca53cb577eb5d
jonkerz <jonkerz@gmail.com>
2016-10-22 06:39:44 +0200

    Db migration: cleanup biogeographic regions. Closes #170.

--- app/models/biogeographic_region.rb
+++ app/models/biogeographic_region.rb
@@ -1,8 +1,5 @@
 module BiogeographicRegion
-  # Note that there are two spellings of "Palearctic" (American English) in the
-  # database. All occurrences of "Palaearctic" (BrE) should probably be replaced.
-  # TODO migrate and remove HACK in app/models/taxa/search.rb
   REGIONS = %w( Nearctic Neotropic Palearctic Afrotropic
                 Malagasy Indomalaya Australasia Oceania
-                Palaearctic Antarctic)
+                Antarctic)
 end
