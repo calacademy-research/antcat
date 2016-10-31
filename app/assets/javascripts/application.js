f08050be3c9f83835ba78cace9ce2087b60b440f
jonkerz <jonkerz@gmail.com>
2016-10-31 23:27:57 +0100

    Performance: less Foundation JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,16 @@
 // gems
 //= require jquery
 //= require jquery_ujs
-//= require foundation
+//= require foundation.core
+//= require foundation.accordion
+//= require foundation.dropdown
+//= require foundation.responsiveMenu
+//= require foundation.responsiveToggle
+//= require foundation.reveal
+//= require foundation.tabs
+//= require foundation.util.box
+//= require foundation.util.mediaQuery
+//= require foundation.util.triggers
 //= require jquery-ui/core
 //= require jquery-ui/autocomplete
 //= require jquery-ui/dialog
