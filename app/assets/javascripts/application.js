dd1e814cf27d5e3df72a2eeb951eaa693d14b0cd
jonkerz <jonkerz@gmail.com>
2016-10-31 22:52:40 +0100

    Performance: tweak JavaScript inclusion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,20 +8,12 @@
 //= require jquery-ui/tabs
 //= require jquery-ui/tooltip
 
-// vendor/assets/javascripts
-//= require jquery.a-tools-1.5.2
-//= require jquery.form
-
 // app/assets/javascripts
 //= require catalog_autocompletion
-//= require comments
 //= require debugging
 //= require feedback
 //= require site
 //= require spinner
-//= require unread_site_notices_flash_message
 
 // gems
 //= require twitter/typeahead
-//= require best_in_place
-//= require js.cookie
