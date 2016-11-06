733c68a16e799a3fc313f632623096f9d7cc068f
jonkerz <jonkerz@gmail.com>
2016-11-06 13:30:33 +0100

    Taxa::SaveTaxon: extract handling of previous combinations

---
+++ app/models/taxa/handle_previous_combination.rb
@@ -0,0 +1,93 @@
+# Note: some of the comments here may be incorrect, as I'm still trying to
+# understand what's going on.
+
+# TODO see if we can convert this to a callback?
+class Taxa::HandlePreviousCombination
+  def initialize taxon
+    @taxon = taxon # The new combination.
+  end
+
+  # The taxon that was just saved is a new combination. Update the
+  # previous combination's status, associated it with the new
+  # combination, and transfer all taxon history and synonyms.
+  def handle_it! previous_combination
+    # Find all taxa that list `previous_combination.id` as
+    # `current_valid_taxon_id`, and update them.
+    Taxon.where(current_valid_taxon_id: previous_combination.id).each do |taxon_to_update|
+      update_elements taxon_to_update, status_string_for_crazy(taxon_to_update)
+    end
+
+    # Since the previous doesn't have a pointer to `current_valid_taxon`,
+    # it won't show up in the above search. If it's the protonym, set it propertly.
+    new_status = status_string_for_previous_combination previous_combination
+    update_elements previous_combination, new_status
+  end
+
+  private
+    # TODO find proper name.
+    def status_string_for_crazy taxon_to_update
+      if Status[taxon_to_update] == Status['original combination']
+        Status['original combination'].to_s
+      else
+        Status['obsolete combination'].to_s
+      end
+    end
+
+    def status_string_for_previous_combination previous_combination
+      if previous_combination.id == @taxon.protonym.id
+        Status['original combination'].to_s
+      else
+        Status['obsolete combination'].to_s
+      end
+    end
+
+    # TODO rename. Working name: `set_current_valid_taxon_to_the_current_taxon_and_update_stuff`
+    # Or, move it to `SpeciesGroupTaxon` (only species and subspecies can be combinations).
+    # And then somewhere: `Species#make_combination_of!(taxon)`. Would have private methods
+    # such as `transfer_history_items` and `retarget_synonyms`.
+    #
+    # So, this method is called on the previous combination and any taxa where `current_valid_taxon`
+    # is pointing to the previous combination. "PC and it's Friends", for short.
+    # Some comments may make it sound like PC and Friends are updated at the same time,
+    # but that's just me lying while trying to wrap my head around it.
+    def update_elements taxon_to_update, status_string
+      taxon_to_update.current_valid_taxon = @taxon
+
+      # The new status will be either 'original combination' or 'obsolete combination'.
+      taxon_to_update.status = status_string
+
+      # Transfer the original/obsolete combination's history items to `@taxon`.
+      taxon_to_update.history_items.update_all taxon_id: @taxon.id
+
+      # Update the PC and Friends' junior synonyms (if there are any) to point to `@taxon`.
+      # This makes sense, because taxa that are/were junior synonyms of PC and its
+      # Friends are now junior synonyms of `@taxon`, so update.
+      taxon_to_update.junior_synonyms_objects.update_all senior_synonym_id: @taxon.id
+
+      # THIS IS THE TRICKY PART!!!
+      # Take all synonym relationships where PC or one of its Friends is listed as
+      # the `junior_synonym` (ie, ignore anything but senior synonyms of PC and Friends),
+      # and change `junior_synonym` to point to `@taxon`.
+      #
+      # So, now our new `@taxon` may have senior synonyms that were transferred from
+      # other taxa. This makes sense because........... trying to think... . ... .
+      # .... ... so... I guess this case is for when the new `@taxon` is a synonym?
+      # Then it would make sense to inherit the seniors. But is doesn't make sense
+      # because setting PC and Friends' `current_valid_taxon` to point to `@taxon`
+      # implies that `@taxon` is valid, no?
+      #
+      # A thought: all these "Friends", do they have PC as their senior synonym?
+      #
+      # Another train of thought: if any seniors were transferred, that implies that PC or
+      # a Friend was a synonym of a third taxon that is valid, and that taxon is now
+      # `@taxon`'s senior synonym, which means `@taxon` is a junior synonym of that
+      # third taxon, which means PC and Friends' `current_valid_taxon` should not had
+      # been changed to `@taxon`...
+      #
+      # I'm sure there's something I'm not seeing here, perhaps this can never happen,
+      # and if it does due to incorrect data, this is less incorrect that not doing anything.
+      taxon_to_update.senior_synonyms_objects.update_all junior_synonym_id: @taxon.id
+
+      taxon_to_update.save
+    end
+end
--- app/models/taxa/save_taxon.rb
+++ app/models/taxa/save_taxon.rb
@@ -1,28 +1,4 @@
-# Note: some of the comments here may be incorrect, as I'm still trying to
-# understand what's going on.
-#
-# `#save_from_form` (the only public method) is responsible for, in -ish this order:
-#   1) Parse the `params` to prepare them for ActiveRecord's `#save`.
-#   2) Update the `TaxonState`, or create it.
-#   3) Create a `Change`.
-#   4) Ask PaperTrail to create a version.
-#   5) Remove `auto_generated` flags from various tables.
-#   6) Run `Taxon#save`.
-#   7) If `previous_combination` was passed in, that means we're have just saved a
-#      new taxon with attributes inherited from that previous combination.
-#      `#handle_previous_combination` will take care of updating the `previous_combination`
-#      and transfer some of its associations to this new taxon.
-#   8) Finally, call `#save_taxon_children`, which recursively saves all children
-#      via ActiveRecord's `#save`, presumably to create new PaperTrail versions
-#      and to trigger callbacks; for example if a species is saved with a different
-#      genus, this makes sure that all the species' subspecies also are updated
-#      so they belong to that genus.
-#
-# `#save_from_form` expects `params` as they are formatted by the 'edit taxa form',
-# which is why we have code such as `params[:name_attributes][:id]` that must be
-# parsed/modified up. Also, the `params` are not "pure", as in it's more like the
-# method expects `params` from the form + changes JavaScripts made to them +
-# stuff in `TaxaController` = it's hard to use this outside of `TaxaController`.
+# This class is responsible for saving taxa from `TaxaController` (from the edit form).
 
 class Taxa::SaveTaxon
   include UndoTracker
@@ -65,7 +41,9 @@ class Taxa::SaveTaxon
         change.save
       end
 
-      handle_previous_combination previous_combination if previous_combination
+      if previous_combination
+        Taxa::HandlePreviousCombination.new(@taxon).handle_it! previous_combination
+      end
     end
   end
 
@@ -150,95 +128,5 @@ class Taxa::SaveTaxon
       if attributes[:type_taxt]
         @taxon.type_taxt = Taxt.from_editable attributes.delete :type_taxt
       end
-    end
-
-    # TODO extract to a new class? I do not fully understand what's going on,
-    # but I believe that if `TaxaController` decides that the taxon being saved
-    # is a combination, then we get here without the actual taxon. Instead: 1) we get
-    # a copy of it, 2) we do the `update_name` stuff etc and save it, resulting in a
-    # taxon with a new id, 3) we update the passed in `previous_combination`, ie, update
-    # its status, etc, and transfer its associated items to the new taxon id.
-    #
-    # The taxon that was just saved is a new combination. Update the
-    # previous combination's status, associated it with the new
-    # combination, and transfer all taxon history and synonyms.
-    def handle_previous_combination previous_combination
-      # Find all taxa that list `previous_combination.id` as
-      # `current_valid_taxon_id`, and update them.
-      Taxon.where(current_valid_taxon_id: previous_combination.id).each do |taxon_to_update|
-        update_elements taxon_to_update, status_string_for_crazy(taxon_to_update)
-      end
-
-      # Since the previous doesn't have a pointer to `current_valid_taxon`,
-      # it won't show up in the above search. If it's the protonym, set it propertly.
-      new_status = status_string_for_previous_combination previous_combination
-      update_elements previous_combination, new_status
-    end
-
-    # TODO find proper name.
-    def status_string_for_crazy taxon_to_update
-      if Status[taxon_to_update] == Status['original combination']
-        Status['original combination'].to_s
-      else
-        Status['obsolete combination'].to_s
-      end
-    end
-
-    def status_string_for_previous_combination previous_combination
-      if previous_combination.id == @taxon.protonym.id
-        Status['original combination'].to_s
-      else
-        Status['obsolete combination'].to_s
-      end
-    end
-
-    # TODO rename. Working name: `set_current_valid_taxon_to_the_current_taxon_and_update_stuff`
-    # Or, move it to `SpeciesGroupTaxon` (only species and subspecies can be combinations).
-    # And then somewhere: `Species#make_combination_of!(taxon)`. Would have private methods
-    # such as `transfer_history_items` and `retarget_synonyms`.
-    #
-    # So, this method is called on the previous combination and any taxa where `current_valid_taxon`
-    # is pointing to the previous combination. "PC and it's Friends", for short.
-    # Some comments may make it sound like PC and Friends are updated at the same time,
-    # but that's just me lying while trying to wrap my head around it.
-    def update_elements taxon_to_update, status_string
-      taxon_to_update.current_valid_taxon = @taxon
-
-      # The new status will be either 'original combination' or 'obsolete combination'.
-      taxon_to_update.status = status_string
-
-      # Transfer the original/obsolete combination's history items to `@taxon`.
-      taxon_to_update.history_items.update_all taxon_id: @taxon.id
-
-      # Update the PC and Friends' junior synonyms (if there are any) to point to `@taxon`.
-      # This makes sense, because taxa that are/were junior synonyms of PC and its
-      # Friends are now junior synonyms of `@taxon`, so update.
-      taxon_to_update.junior_synonyms_objects.update_all senior_synonym_id: @taxon.id
-
-      # THIS IS THE TRICKY PART!!!
-      # Take all synonym relationships where PC or one of its Friends is listed as
-      # the `junior_synonym` (ie, ignore anything but senior synonyms of PC and Friends),
-      # and change `junior_synonym` to point to `@taxon`.
-      #
-      # So, now our new `@taxon` may have senior synonyms that were transferred from
-      # other taxa. This makes sense because........... trying to think... . ... .
-      # .... ... so... I guess this case is for when the new `@taxon` is a synonym?
-      # Then it would make sense to inherit the seniors. But is doesn't make sense
-      # because setting PC and Friends' `current_valid_taxon` to point to `@taxon`
-      # implies that `@taxon` is valid, no?
-      #
-      # A thought: all these "Friends", do they have PC as their senior synonym?
-      #
-      # Another train of thought: if any seniors were transferred, that implies that PC or
-      # a Friend was a synonym of a third taxon that is valid, and that taxon is now
-      # `@taxon`'s senior synonym, which means `@taxon` is a junior synonym of that
-      # third taxon, which means PC and Friends' `current_valid_taxon` should not had
-      # been changed to `@taxon`...
-      #
-      # I'm sure there's something I'm not seeing here, perhaps this can never happen,
-      # and if it does due to incorrect data, this is less incorrect that not doing anything.
-      taxon_to_update.senior_synonyms_objects.update_all junior_synonym_id: @taxon.id
-
-      taxon_to_update.save
     end
 end
