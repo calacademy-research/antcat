faf755f4d5ac22051fc20dc6f5862d6daadfa12f
jonkerz <jonkerz@gmail.com>
2016-11-10 15:04:31 +0100

    Taxt: cleanup specs

--- spec/lib/taxt_converter_spec.rb
+++ spec/lib/taxt_converter_spec.rb
@@ -1,5 +1,73 @@
 require "spec_helper"
 
 describe TaxtConverter do
-  # TODO
+  describe "#to_editor_format" do
+    context "references" do
+      it "uses the inline citation format followed by the id, with type number" do
+        decorated = double 'keey'
+        reference = double 'reference', id: 36
+        expect(reference).to receive(:decorate).and_return decorated
+        expect(decorated).to receive(:keey).and_return 'Fisher, 1922'
+        expect(Reference).to receive(:find).and_return reference
+        editable_keey = TaxtConverter.send :id_for_editable, reference.id, 1
+
+        expect(TaxtConverter["{ref #{reference.id}}"].to_editor_format).to eq "{Fisher, 1922 #{editable_keey}}"
+      end
+
+      it "handles missing references" do
+        reference = create :missing_reference, citation: 'Fisher, 2011'
+        editable_keey = TaxtConverter.send :id_for_editable, reference.id, 1
+        expect(TaxtConverter["{ref #{reference.id}}"].to_editor_format).to eq "{Fisher, 2011 #{editable_keey}}"
+      end
+
+      it "handles references we don't even know are missing" do
+        expect(TaxtConverter["{ref 123}"].to_editor_format).to eq "{Rt}"
+      end
+    end
+
+    context "taxa" do
+      it "uses the taxon's name followed by its id" do
+        genus = create_genus 'Atta'
+        editable_keey = TaxtConverter.send :id_for_editable, genus.id, 2
+        expect(TaxtConverter["{tax #{genus.id}}"].to_editor_format).to eq "{Atta #{editable_keey}}"
+      end
+    end
+
+    context "names" do
+      it "uses the name followed by its id" do
+        genus = create_genus 'Atta'
+        editable_keey = TaxtConverter.send :id_for_editable, genus.name.id, 3
+        expect(TaxtConverter["{nam #{genus.name.id}}"].to_editor_format).to eq "{Atta #{editable_keey}}"
+      end
+    end
+  end
+
+  describe "#from_editor_format" do
+    context "references" do
+      it "uses the inline citation format followed by the id" do
+        reference = create :article_reference
+        editable_keey = TaxtConverter.send :id_for_editable, reference.id, 1
+        expect(TaxtConverter["{Fisher, 1922 #{editable_keey}}"].from_editor_format).to eq "{ref #{reference.id}}"
+      end
+
+      it "handles more than one reference" do
+        reference = create :article_reference
+        other_reference = create :article_reference
+        editable_keey = TaxtConverter.send :id_for_editable, reference.id, 1
+        other_editable_keey = TaxtConverter.send :id_for_editable, other_reference.id, 1
+
+        taxt = "{Fisher, 1922 #{editable_keey}}, also {Bolton, 1970 #{other_editable_keey}}"
+        results = TaxtConverter[taxt].from_editor_format
+        expect(results).to eq "{ref #{reference.id}}, also {ref #{other_reference.id}}"
+      end
+    end
+
+    context "taxa" do
+      it "uses the taxon's name followed by its id" do
+        genus = create_genus 'Atta'
+        editable_keey = TaxtConverter.send :id_for_editable, genus.id, 2
+        expect(TaxtConverter["{Atta #{editable_keey}}"].from_editor_format).to eq "{tax #{genus.id}}"
+      end
+    end
+  end
 end
--- spec/lib/taxt_presenter_spec.rb
+++ spec/lib/taxt_presenter_spec.rb
@@ -1,5 +1,101 @@
 require "spec_helper"
 
 describe TaxtPresenter do
-  # TODO
+  describe "#to_text" do
+    it "renders text without links or HTML (except <i> tags)" do
+      taxt = "{tax #{create(:family).id}}"
+      expect(TaxtPresenter[taxt].to_text).to eq "Formicidae."
+    end
+
+    context "names that should be italicized" do
+      let(:genus) { create_genus "Atta" }
+
+      it "includes HTML <i> tags" do
+        taxt = "{tax #{genus.id}}"
+        expect(TaxtPresenter[taxt].to_text).to eq "<i>Atta</i>."
+      end
+    end
+  end
+
+  describe "#to_html" do
+    #it "escapes its input" do
+      #string = TaxtPresenter['<script>'].to_html
+      #string.should == '&lt;script&gt;'
+      #string.should be_html_safe
+    #end
+
+    it "handles nil" do
+      expect(TaxtPresenter[nil].to_html).to eq ''
+    end
+
+    context "references" do
+      context "when the ref is malformed" do
+        it "doesn't freak" do
+          expect(TaxtPresenter["{ref sdf}"].to_html).to eq '{ref sdf}'
+        end
+      end
+
+      context "when the ref points to a reference that doesn't exist" do
+        it "doesn't freak" do
+          expect(TaxtPresenter["{ref 12345}"].to_html).to eq '{ref 12345}'
+        end
+      end
+
+      it "handles a MissingReference" do
+        reference = create :missing_reference, citation: 'Latreille, 1809'
+        expect(TaxtPresenter["{ref #{reference.id}}"].to_html).to eq 'Latreille, 1809'
+      end
+
+      #it "escapes input" do
+        #reference = create :missing_reference, citation: 'Latreille, 1809 <script>'
+        #TaxtPresenter["{ref #{reference.id}}"].to_html.should == 'Latreille, 1809 &lt;script&gt;'
+      #end
+    end
+
+    context "names" do
+      it "returns the HTML version of the name" do
+        name = create :subspecies_name, name_html: '<i>Atta major minor</i>'
+        expect(TaxtPresenter["{nam #{name.id}}"].to_html).to eq '<i>Atta major minor</i>'
+      end
+
+      context "when the name can't be found" do
+        it "doesn't freak" do
+          expect(TaxtPresenter["{nam 12345}"].to_html).to eq '{nam 12345}'
+        end
+      end
+    end
+
+    describe "taxa" do
+      it "uses the HTML version of the taxon's name" do
+        genus = create_genus name: create(:genus_name, name_html: '<i>Atta</i>')
+        expect(TaxtPresenter["{tax #{genus.id}}"].to_html)
+          .to eq %{<a href="/catalog/#{genus.id}"><i>Atta</i></a>}
+      end
+
+      context "when the taxon is a fossil" do
+        it "includes the fossil symbol" do
+          genus = create_genus name: create(:genus_name, name_html: '<i>Atta</i>'), fossil: true
+          expect(TaxtPresenter["{tax #{genus.id}}"].to_html)
+            .to eq %{<a href="/catalog/#{genus.id}"><i>&dagger;</i><i>Atta</i></a>}
+        end
+      end
+
+      context "when the taxon can't be found" do
+        it "doesn't freak" do
+          expect(TaxtPresenter["{tax 12345}"].to_html).to eq '{tax 12345}'
+        end
+      end
+    end
+  end
+
+  describe "#to_antweb_export" do
+    before { $use_ant_web_formatter = true }
+    after { $use_ant_web_formatter = nil }
+
+    it "uses a different link formatter" do
+      taxt = "{tax #{create(:family).id}}"
+      expect_any_instance_of(TaxtPresenter).to receive(:link_to_antcat_from_antweb).and_call_original
+      TaxtPresenter[taxt].to_html
+    end
+  end
 end
