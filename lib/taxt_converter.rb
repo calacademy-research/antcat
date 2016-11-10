bff60de427c9c60bc2c05ef0b99ae49b160f719e
jonkerz <jonkerz@gmail.com>
2016-11-10 19:03:40 +0100

    TaxtConverter: refactor

--- lib/taxt_converter.rb
+++ lib/taxt_converter.rb
@@ -9,37 +9,21 @@ class TaxtConverter
   NAME_TAG_TYPE = 3
   EDITABLE_ID_DIGITS = %{abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ}
 
-  # TODO inline?
-  class ReferenceNotFound < StandardError; end
-
-  # TODO inline?
-  # TODO maybe not used remove?
-  class TaxonNotFound < StandardError; end
-
-  # TODO inline?
-  # TODO maybe not used remove?
-  class NameNotFound < StandardError
-    attr_accessor :id
-
-    def initialize message = nil, id = nil
-      super message
-      self.id = id
-    end
-  end
+  ReferenceNotFound = Class.new StandardError
 
   def initialize taxt_from_db_or_js
-    @taxt = taxt_from_db_or_js || ""
+    @taxt = taxt_from_db_or_js.try :dup
   end
   class << self; alias_method :[], :new end
 
   def to_editor_format
-    taxt = @taxt.dup
-    TaxtConverter.to_editable taxt
+    return "" unless @taxt
+    TaxtConverter.to_editable @taxt.dup
   end
 
   def from_editor_format
-    taxt = @taxt.dup
-    TaxtConverter.from_editable taxt
+    return "" unless @taxt
+    TaxtConverter.from_editable @taxt.dup
   end
 
   def self.to_editable_reference reference
@@ -56,36 +40,28 @@ class TaxtConverter
 
   # These can be called from outside this file, but please don't.
   private
+    # TODO this method used to do `if taxt =~ /{tax/` before gsubing. Investigate performance.
     def self.to_editable taxt
-      return '' unless taxt
-
-      if taxt =~ /{ref/
-        taxt.gsub! /{ref (\d+)}/ do |ref|
-          editable_id = id_for_editable $1, REFERENCE_TAG_TYPE
-          to_editable_reference Reference.find($1) rescue "{#{editable_id}}"
-        end
+      taxt.gsub! /{ref (\d+)}/ do |ref|
+        editable_id = id_for_editable $1, REFERENCE_TAG_TYPE
+        to_editable_reference Reference.find($1) rescue "{#{editable_id}}"
       end
 
-      if taxt =~ /{tax/
-        taxt.gsub! /{tax (\d+)}/ do |tax|
-          editable_id = id_for_editable $1, TAXON_TAG_TYPE
-          to_editable_taxon Taxon.find($1) rescue "{#{editable_id}}"
-        end
+      taxt.gsub! /{tax (\d+)}/ do |tax|
+        editable_id = id_for_editable $1, TAXON_TAG_TYPE
+        to_editable_taxon Taxon.find($1) rescue "{#{editable_id}}"
       end
 
-      if taxt =~ /{nam/
-        taxt.gsub! /{nam (\d+)}/ do |nam|
-          editable_id = id_for_editable $1, NAME_TAG_TYPE
-          to_editable_name Name.find($1) rescue "{#{editable_id}}"
-        end
+      taxt.gsub! /{nam (\d+)}/ do |nam|
+        editable_id = id_for_editable $1, NAME_TAG_TYPE
+        to_editable_name Name.find($1) rescue "{#{editable_id}}"
       end
 
       taxt
     end
 
+    # TODO create `SomethingTaxtError` that would encompass all errors.
     def self.from_editable taxt
-      return '' unless taxt
-
       taxt.gsub /{((.*?)? )?([#{Regexp.escape EDITABLE_ID_DIGITS}]+)}/ do |string|
         id, type_number = id_from_editable $3
         case type_number
@@ -93,10 +69,10 @@ class TaxtConverter
           raise ReferenceNotFound.new(string) unless Reference.find_by(id: id)
           "{ref #{id}}"
         when TAXON_TAG_TYPE
-          raise TaxonNotFound.new(string) unless Taxon.find_by(id: id)
+          raise unless Taxon.find_by(id: id)
           "{tax #{id}}"
         when NAME_TAG_TYPE
-          raise NameNotFound.new(string, id) unless Name.find_by(id: id)
+          raise unless Name.find_by(id: id)
           "{nam #{id}}"
         end
       end
@@ -107,11 +83,14 @@ class TaxtConverter
       "{#{text} #{editable_id}}"
     end
 
+    # TODO extract this + related + `AnyBase` into a new class `TaxtIdTranslator`.
+    # New working name: `id_from_jumbled_id`
     def self.id_for_editable id, type_number
       AnyBase.base_10_to_base_x(id.to_i * 10 + type_number, EDITABLE_ID_DIGITS).reverse
     end
 
     # This code is duplicated in `taxt_editor.coffee`.
+    # New working name: `id_from_jumbled_id`
     def self.id_from_editable editable_id
       number = AnyBase.base_x_to_base_10(editable_id.reverse, EDITABLE_ID_DIGITS)
       id = number / 10
