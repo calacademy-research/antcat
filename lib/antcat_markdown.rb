68f347cd550aad1b0c12c3be926e79a59209cd49
jonkerz <jonkerz@gmail.com>
2016-11-26 02:23:20 +0100

    Markdown: extracted AntCat hooks into a class

--- lib/antcat_markdown.rb
+++ lib/antcat_markdown.rb
@@ -1,10 +1,7 @@
-# Probably GitHub markdown (?) with some custom tags.
 require "redcarpet/render_strip"
 
+# This is more "just Markdown", but the namesapce `Markdown` is used by Red Carpet.
 class AntcatMarkdown < Redcarpet::Render::HTML
-  include Rails.application.routes.url_helpers
-  include ActionView::Helpers::UrlHelper
-
   def self.render text
     options = {
       hard_wrap: true,
@@ -12,6 +9,7 @@ class AntcatMarkdown < Redcarpet::Render::HTML
       fenced_code_blocks: true,
       underline: false,
     }
+    renderer = AntcatMarkdown.new options
 
     extensions = {
       autolink: true,
@@ -22,10 +20,7 @@ class AntcatMarkdown < Redcarpet::Render::HTML
       no_intra_emphasis: true,
       strikethrough: true,
     }
-
-    renderer = AntcatMarkdown.new options
     markdowner = Redcarpet::Markdown.new renderer, extensions
-
     markdowner.render(text).html_safe
   end
 
@@ -35,111 +30,8 @@ class AntcatMarkdown < Redcarpet::Render::HTML
     markdowner.render text
   end
 
+  # Without this "AntcatMarkdown" is "Markdown".
   def preprocess full_document
-    parse_taxon_ids! full_document
-    parse_reference_ids! full_document
-    parse_journal_ids! full_document
-    parse_task_ids! full_document
-    parse_feedback_ids! full_document
-    parse_github_ids! full_document
-    parse_user_ids! full_document
-    full_document
+    AntcatMarkdownUtils.parse_antcat_hooks full_document
   end
-
-  private
-    # matches: %taxon429349
-    # renders: link to the taxon (Formica)
-    def parse_taxon_ids! full_document
-      full_document.gsub!(/%taxon(\d+)/) do
-        try_linking_taxon_id $1
-      end
-    end
-
-    # matches: %reference130628
-    # renders: referece as used in the catalog (Abdalla & Cruz-Landim, 2001)
-    def parse_reference_ids! full_document
-      full_document.gsub!(/%reference?(\d+)/) do
-        if Reference.exists? $1
-          reference = Reference.find($1)
-          reference.decorate.inline_citation
-        else
-          broken_markdown_link "reference", $1
-        end
-      end
-    end
-
-    # matches: %journal123
-    # renders: link to the journal, with the journal's name as the title
-    def parse_journal_ids! full_document
-      full_document.gsub!(/%journal(\d+)/) do
-        if Journal.exists? $1
-          journal = Journal.find($1)
-          link_to "<i>#{journal.name}</i>".html_safe, journal_path(journal)
-        else
-          broken_markdown_link "journal", $1
-        end
-      end
-    end
-
-    # matches: %task123
-    # renders: a link to the task
-    def parse_task_ids! full_document
-      full_document.gsub!(/%task(\d+)/) do
-        if Task.exists? $1
-          task = Task.find($1)
-          link_to "task ##{$1} (#{task.title})", task_path($1)
-        else
-          broken_markdown_link "task", $1
-        end
-      end
-    end
-
-    # matches: %feedback123
-    # renders: a link to the feedback (including non-existing)
-    def parse_feedback_ids! full_document
-      full_document.gsub!(/%feedback(\d+)/) do
-        link_to "feedback ##{$1}", feedback_path($1)
-      end
-    end
-
-    # matches: %github123
-    # renders: a link to the GitHub issue (including non-existing and PRs)
-    def parse_github_ids! full_document
-      full_document.gsub!(/%github(\d+)/) do
-        # Also works for PRs becuase GH figures that out.
-        url = "https://github.com/calacademy-research/antcat/issues/#{$1}"
-        link_to "GitHub ##{$1}", url
-      end
-    end
-
-    # matches: %user123
-    # renders: a link to the user's user page.
-    def parse_user_ids! full_document
-      full_document.gsub!(/@user(\d+)/) do
-        if User.exists? $1
-          user = User.find($1)
-          user.decorate.ping_user_link
-        else
-          broken_markdown_link "user", $1
-        end
-      end
-    end
-
-    # internal only
-    def try_linking_taxon_id string
-      if Taxon.exists? string
-        taxon = Taxon.find(string)
-        taxon.decorate.link_to_taxon
-      else
-        broken_markdown_link "taxon", string
-      end
-    end
-
-    def broken_markdown_link type, string
-      <<-HTML.squish
-        <span class="broken-markdown-link">
-          could not find #{type} with id #{string}
-        </span>
-      HTML
-    end
 end
