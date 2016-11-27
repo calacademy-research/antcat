calacademy-research/antcat

commit 384a72726a19d38568ecafc21a9a868ff9ffe5ee

PaperTrail initializer: DRY

config/initializers/paper_trail.rb
@@ -1,5 +1,3 @@
-require 'protected_attributes'
-
 PaperTrail::Rails::Engine.eager_load!
 
 module PaperTrail

app/helpers/comments_helper.rb
@@ -1,7 +1,6 @@
 module CommentsHelper
   def link_comments_section commentable
-    count = commentable.comments_count
-    label = "#{count} #{"comment".pluralize(count)}"
+    label = pluralize commentable.comments_count, "comment"
     link_to label, url_for(commentable) + "#comments"
   end
 

config/application.rb
@@ -2,27 +2,6 @@ require File.expand_path '../boot', __FILE__
 
 require 'rails/all'
 
-#
-# This block is a duplicate of the paper_trail.rb initializer.
-# There is an intermittent (!) loading order issue where we're seeing
-# change_id being flagged as not writable. Putting this block here
-# seems to have fixed it; remove this block if/when we move beyond 4.0 beta 2 and see
-# if deleting and undoing a delete still causes the problem. Do at least three tests.
-#
-# Note: This should probably be require 'config/initializers/paper_trail.rb',
-# but because this is intermittent I'm not changing anything until I know this is the fix.
-#
-require 'protected_attributes'
-
-module PaperTrail
-  class Version < ::ActiveRecord::Base
-    attr_accessible :change_id
-  end
-end
-#
-#  End of stupid
-#
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require *Rails.groups

commit aafa52f163bff590b9325fd298a320f52d64302f

PaperTrail: probably fix issue introduced in 384a72726a19d38568ecafc21a9a868ff9ffe5ee

app/models/paper_trail/version.rb
@@ -1,0 +1,18 @@
+# Per http://stackoverflow.com/questions/35595171/why-do-methods-defined-in-an-initializer-intermittently-raise-a-not-defined-er
+
+# Confirm that autoloading works after editing this file by going to
+# the changes page and look for "<username and link> deleted <taxon>".
+# Edit any autoloaded file and refresh the page. It should not say
+# "Someone deleted <taxon>" this time.
+
+module PaperTrail
+  class Version < ActiveRecord::Base
+    include PaperTrail::VersionConcern
+
+    attr_accessible :change_id
+
+    def user
+      User.find(whodunnit) if whodunnit
+    end
+  end
+end

config/initializers/paper_trail.rb
@@ -1,11 +1,0 @@
-PaperTrail::Rails::Engine.eager_load!
-
-module PaperTrail
-  class Version < ::ActiveRecord::Base
-    attr_accessible :change_id
-
-    def user
-      User.find(whodunnit) if whodunnit
-    end
-  end
-end