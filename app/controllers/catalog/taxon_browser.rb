4fa293b4633900ce1804dcb324ce7a74e73efaad
jonkerz <jonkerz@gmail.com>
2016-11-28 05:19:05 +0100

    Taxon browser: refactor

--- app/controllers/catalog/taxon_browser.rb
+++ app/controllers/catalog/taxon_browser.rb
@@ -5,14 +5,7 @@ module Catalog
     def initialize taxon, show_invalid, display_param
       @taxon = taxon
       @show_invalid = show_invalid
-      @display =  case taxon
-                  when Subfamily
-                    "all_genera_in_subfamily" # TODO only if blank.
-                  when Subgenus
-                    "subgenera_in_parent_genus"
-                  else
-                    display_param
-                  end
+      @display = default_or_display display_param
 
       setup_tabs
     end
@@ -30,17 +23,24 @@ module Catalog
     end
 
     private
+      def default_or_display display
+        case @taxon
+        when Subfamily then :all_genera_in_subfamily if display.blank?
+        when Subgenus  then :subgenera_in_parent_genus
+        end || display.try(:to_sym)
+      end
+
       def setup_tabs
         @tabs = taxa_for_tabs.map do |taxon|
           TaxonTab.new(taxon, self)
         end
 
-        extra_tab = non_standard_tab
+        extra_tab = ExtraTab.create @taxon, @display, self
         @tabs << extra_tab if extra_tab
       end
 
-      # The "main progression", from the lowest rank and up is: Subspecies ->
-      # Species -> Genus -> Tribe -> Subfamily -> Family.
+      # Follows the "main progression", which from the lowest rank and up is:
+      # Subspecies -> Species -> Genus -> Tribe -> Subfamily -> Family.
       # See https://github.com/calacademy-research/antcat/wiki/For-developers
       def taxa_for_tabs
         # We do not want to include all ranks in the tabs.
@@ -48,44 +48,12 @@ module Catalog
           # Never show the [children of] subspecies tab (has no children).
           taxon.is_a?(Subspecies) ||
 
-          # Don't show [children of] species tab unless the species has subspecies.
+          # Don't show [subspecies in] species tab unless the species has subspecies.
           (taxon.is_a?(Species) && !taxon.children.exists?) ||
 
-          # No [children of] subgenus tab (not part of the 'normal' rank hierarchy).
+          # Hide [species in] subgenus tab because there are none as of 2016.
           taxon.is_a?(Subgenus)
-
-          # Tabs of subfamilies, tribes and genera without any children at
-          # all could also be excluded here, to avoid showing empty tabs.
-          # However, that can only happen to invalid taxa (all valid taxa of
-          # these ranks have children unless the database is incomplete), so
-          # it makes more sense to just show "No valid child taxa".
         end
-      end
-
-      # TODO improve.
-      def non_standard_tab
-        return unless @display
-
-        case @display
-        when /^incertae_sedis_in/
-          title = "Genera <i>incertae sedis</i> in #{@taxon.taxon_label}"
-          children = @taxon.genera_incertae_sedis_in
-        when /^all_genera_in/
-          title = "All #{@taxon.taxon_label} genera"
-          children = @taxon.all_displayable_genera
-        when "all_taxa_in_genus"
-          title = "All #{@taxon.taxon_label} taxa"
-          children = @taxon.displayable_child_taxa
-        when "subgenera_in_genus"
-          title = "#{@taxon.taxon_label} subgenera"
-          children = @taxon.displayable_subgenera
-        when "subgenera_in_parent_genus"
-          # Works because [currently] all subgenera have parents.
-          title = "#{@taxon.parent.taxon_label} subgenera"
-          children = @taxon.parent.displayable_subgenera
-        end
-
-        ExtraTab.new title.html_safe, children, self
       end
 
       def selected_in_tabs
@@ -114,7 +82,6 @@ class TaxonBrowserTab
 
   def initialize children, taxon_browser
     @taxon_browser = taxon_browser
-
     @children = if show_invalid?
                   children
                 else
@@ -123,7 +90,7 @@ class TaxonBrowserTab
   end
 
   def each_child
-    sorted_children.each do |child|
+    sorted_children.includes(:name).each do |child|
       yield child, selected_in_tab?(child)
     end
   end
@@ -132,25 +99,9 @@ class TaxonBrowserTab
     tab_open? self
   end
 
-  def notify_about_no_valid_children?
-    @children.empty? && !is_a_subfamily_with_valid_genera_incertae_sedis?
-  end
-
   private
-    # Exception for subfamilies *only* containing genera that are
-    # incertae sedis in that subfamily (that is Martialinae, #430173).
-    def is_a_subfamily_with_valid_genera_incertae_sedis?
-      return unless @tab_taxon.is_a? Subfamily
-      @tab_taxon.genera_incertae_sedis_in.valid.exists?
-    end
-
-    # "All taxa" (in genus) children must be sorted by `names.epithet`.
     def sorted_children
-      if display == "all_taxa_in_genus"
-        @children.order_by_joined_epithet
-      else
-        @children.order_by_name_cache
-      end.includes(:name)
+      @children.order_by_name_cache
     end
 end
 
@@ -167,26 +118,71 @@ class TaxonTab < TaxonBrowserTab
     "#{@tab_taxon.taxon_label} #{@tab_taxon.childrens_rank_in_words}".html_safe
   end
 
+  def notify_about_no_valid_children?
+    @children.empty? && !is_a_subfamily_with_valid_genera_incertae_sedis?
+  end
+
   private
-    # For the "All taxa" and "Subgenera" special cases, because
-    # this would be confusing/false:
-    #   "Formicidae ... Lasius species > Lasius subgenera"
-    #   "Formicidae ... Lasius species > All Lasius taxa"
+    # Exception for subfamilies *only* containing genera that are
+    # incertae sedis in that subfamily (that is Martialinae, #430173).
+    def is_a_subfamily_with_valid_genera_incertae_sedis?
+      return unless @tab_taxon.is_a? Subfamily
+      @tab_taxon.genera_incertae_sedis_in.valid.exists?
+    end
+
+    # Tweak for the [species of] genus tab to change eg
+    # "Lasius species > Lasius subgenera" to "Lasius > Lasius subgenera".
     def show_only_genus_name?
       return unless @tab_taxon.is_a? Genus
-      display.in? %w( all_taxa_in_genus
-                      subgenera_in_genus
-                      subgenera_in_parent_genus )
+      display.in? [ :all_taxa_in_genus,
+                    :subgenera_in_genus,
+                    :subgenera_in_parent_genus ]
     end
 end
 
 class ExtraTab < TaxonBrowserTab
+  def self.create taxon, display, taxon_browser
+    return unless display
+
+    label = taxon.taxon_label
+
+    title, children = case display
+      when :incertae_sedis_in_family, :incertae_sedis_in_subfamily
+        [ "Genera <i>incertae sedis</i> in #{label}",
+          taxon.genera_incertae_sedis_in ]
+      when :all_genera_in_family, :all_genera_in_subfamily
+        [ "All #{label} genera", taxon.all_displayable_genera ]
+      when :all_taxa_in_genus
+        [ "All #{label} taxa", taxon.displayable_child_taxa ]
+      when :subgenera_in_genus
+        [ "#{label} subgenera", taxon.displayable_subgenera ]
+      when :subgenera_in_parent_genus
+        [ "#{taxon.genus.taxon_label} subgenera",
+          taxon.genus.displayable_subgenera ]
+      else
+        raise
+      end
+
+    new title, children, taxon_browser
+  end
+
   def initialize title, children, taxon_browser
     super children, taxon_browser
-    @title = title
+    @title = title.html_safe
   end
 
   def title
     @title
   end
+
+  def notify_about_no_valid_children?
+    false
+  end
+
+  private
+    ## "All taxa" in genus must be sorted by `names.epithet`.
+    def sorted_children
+      return @children.order_by_joined_epithet if display == :all_taxa_in_genus
+      super
+    end
 end
--- app/helpers/taxon_browser_extra_panel_links_helper.rb
+++ app/helpers/taxon_browser_extra_panel_links_helper.rb
@@ -2,15 +2,15 @@
 # only are applicable to some ranks.
 
 module TaxonBrowserExtraPanelLinksHelper
-  def extra_panel_links_for_selected selected
+  def extra_tab_links selected
     links = []
 
     case selected
     when Family, Subfamily
-      links << all_genera_link(selected)
+      links << extra_tab_link(selected, "All genera", "all_genera_in_#{selected.rank}")
       links << incertae_sedis_link(selected)
     when Genus
-      links << all_taxa_link(selected)
+      links << extra_tab_link(selected, "All taxa", "all_taxa_in_#{selected.rank}")
       links << subgenera_link(selected)
     end
 
@@ -18,40 +18,29 @@ module TaxonBrowserExtraPanelLinksHelper
   end
 
  private
-    # Only for Formicidae/subfamilies.
-    def all_genera_link selected
-      extra_panel_link selected, "All genera", "all_genera_in_#{selected.rank}"
-    end
-
-    # Only shown on genus pages. The TB defaults to showing all valid species
-    # in the selected genus. "ALL TAXA" also includes subspecies (and possible more?).
-    def all_taxa_link selected
-      extra_panel_link selected, "All taxa", "all_taxa_in_#{selected.rank}"
-    end
-
-    # Only shown if the taxon is a genus with subgenera
+    # Only shown if the taxon is a genus with displayable subgenera
     # For example Lasius, http://localhost:3000/catalog/429161)
     def subgenera_link selected
       return unless selected.displayable_subgenera.exists?
-      extra_panel_link selected, "Subgenera", "subgenera_in_#{selected.rank}"
+      extra_tab_link selected, "Subgenera", :subgenera_in_genus
     end
 
     # Only for Formicidae/subfamilies.
     def incertae_sedis_link selected
       return unless selected.genera_incertae_sedis_in.exists?
-      extra_panel_link selected, "Incertae sedis", "incertae_sedis_in_#{selected.rank}"
+      extra_tab_link selected, "Incertae sedis", "incertae_sedis_in_#{selected.rank}"
     end
 
-    def extra_panel_link selected, label, param
+    def extra_tab_link selected, label, param
       content_tag :li do
-        content_tag :span, class: extra_panel_link_css(param) do
+        content_tag :span, class: extra_tab_css(param) do
           link_to label, catalog_path(selected, display: param)
         end
       end
     end
 
-    def extra_panel_link_css param
-      return "upcase selected" if @taxon_browser.display == param
+    def extra_tab_css param
+      return "upcase selected" if @taxon_browser.display == param.to_sym
       "upcase white-label"
     end
 end
