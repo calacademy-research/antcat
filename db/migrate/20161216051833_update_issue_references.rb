23276e80d998cd0c2565d82d0fd4e0a4367bdd56
jonkerz <jonkerz@gmail.com>
2016-12-16 08:29:13 +0100

    Finish migration `RenameTasksToIssues`

---
+++ db/migrate/20161216051833_update_issue_references.rb
@@ -0,0 +1,15 @@
+# Was not handled in `RenameTasksToIssues`.
+
+class UpdateIssueReferences < ActiveRecord::Migration
+  def up
+    PaperTrail::Version.where(item_type: "Task").update_all item_type: "Issue"
+    Activity.where(trackable_type: "Task").update_all trackable_type: "Issue"
+    Comment.where(commentable_type: "Task").update_all commentable_type: "Issue"
+  end
+
+  def down
+    PaperTrail::Version.where(item_type: "Issue").update_all item_type: "Task"
+    Activity.where(trackable_type: "Issue").update_all trackable_type: "Task"
+    Comment.where(commentable_type: "Issue").update_all commentable_type: "Task"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204151622) do
+ActiveRecord::Schema.define(version: 20161216051833) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
