dc2351c2970ecc1f8466016a7ea03a151555d6f6
jonkerz <jonkerz@gmail.com>
2017-06-30 00:56:31 +0200

    Database script: 'Types: Type specimen repositories'

---
+++ lib/database_scripts/scripts/type_specimen_repositories.rb
@@ -0,0 +1,22 @@
+class DatabaseScripts::Scripts::TypeSpecimenRepositories
+  include DatabaseScripts::DatabaseScript
+
+  def results
+    Taxon.where.not(type_specimen_repository: ["", nil])
+  end
+
+  def render
+    as_table do
+      header :subspecies, :status, :type_specimen_repository
+      rows do |taxon|
+        [ markdown_taxon_link(taxon), taxon.status, taxon.type_specimen_repository ]
+      end
+    end
+  end
+end
+
+__END__
+description: >
+  This script is just lists of all taxa with a `type_specimen_repository`. See %github207.
+tags: [new!]
+topic_areas: [types]
