38359f01b0f60a66a8cb9181f6c640b31d049ede
jonkerz <jonkerz@gmail.com>
2017-07-03 20:12:05 +0200

    Move stuff around before refactoring

--- app/models/name.rb
+++ app/models/name.rb
@@ -15,57 +15,6 @@ class Name < ApplicationRecord
 
   has_paper_trail meta: { change_id: proc { UndoTracker.get_current_change_id } }
 
-  def change name_string
-    existing_names = Name.where.not(id: id).where(name: name_string)
-    raise Taxon::TaxonExists if existing_names.any? { |name| not name.references.empty? }
-    update! name: name_string, name_html: italicize(name_string)
-  end
-
-  # TODO maybe raise?
-  def change_parent _; end
-
-  # Only used in specs.
-  def quadrinomial?
-    name.split(' ').size == 4
-  end
-
-  def self.picklist_matching letters_in_name, options = {}
-    Names::PicklistMatching.new(letters_in_name, options).call
-  end
-
-  # TODO remove? Too magical.
-  def to_s
-    name
-  end
-
-  def to_html
-    name_html
-  end
-
-  def to_html_with_fossil fossil
-    "#{dagger_html if fossil}#{name_html}".html_safe
-  end
-
-  def rank
-    self.class.name.gsub(/Name$/, "").underscore
-  end
-
-  def self.make_epithet_set epithet
-    EpithetSearchSet.new(epithet).epithets
-  end
-
-  def epithet_with_fossil_html fossil
-    "#{dagger_html if fossil}#{epithet_html}".html_safe
-  end
-
-  def protonym_with_fossil_html fossil
-    "#{dagger_html if fossil}#{name_html}".html_safe
-  end
-
-  def dagger_html
-    '&dagger;'.html_safe
-  end
-
   def self.duplicates
     name_strings = Name.find_by_sql(<<-SQL).map(&:name)
       SELECT * FROM names GROUP BY name HAVING COUNT(*) > 1
@@ -89,14 +38,59 @@ class Name < ApplicationRecord
     results
   end
 
-  def references
-    references_in_fields.concat(references_in_taxt)
-  end
-
   # TODO rename to avoid confusing this with [Rails'] dynamic finder methods.
   def self.find_by_name string
     Name.joins("LEFT JOIN taxa ON (taxa.name_id = names.id)").readonly(false)
       .where(name: string).order('taxa.id DESC').order(:name).first
+  end
+
+  def self.make_epithet_set epithet
+    EpithetSearchSet.new(epithet).epithets
+  end
+
+  def self.picklist_matching letters_in_name, options = {}
+    Names::PicklistMatching.new(letters_in_name, options).call
+  end
+
+  def rank
+    self.class.name.gsub(/Name$/, "").underscore
+  end
+
+  # TODO maybe raise?
+  def change_parent _; end
+
+  # Only used in specs.
+  def quadrinomial?
+    name.split(' ').size == 4
+  end
+
+  # TODO remove? Too magical.
+  def to_s
+    name
+  end
+
+  def to_html
+    name_html
+  end
+
+  def to_html_with_fossil fossil
+    "#{dagger_html if fossil}#{name_html}".html_safe
+  end
+
+  def epithet_with_fossil_html fossil
+    "#{dagger_html if fossil}#{epithet_html}".html_safe
+  end
+
+  def protonym_with_fossil_html fossil
+    "#{dagger_html if fossil}#{name_html}".html_safe
+  end
+
+  def dagger_html
+    '&dagger;'.html_safe
+  end
+
+  def references
+    references_in_fields.concat(references_in_taxt)
   end
 
   private
@@ -151,5 +145,11 @@ class Name < ApplicationRecord
 
     def table_ref table, field, id
       { table: table, field: field, id: id }
+    end
+
+    def change name_string
+      existing_names = Name.where.not(id: id).where(name: name_string)
+      raise Taxon::TaxonExists if existing_names.any? { |name| not name.references.empty? }
+      update! name: name_string, name_html: italicize(name_string)
     end
 end
