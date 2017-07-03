a053377a6f78982a7bcbcf696f4907b443920a67
jonkerz <jonkerz@gmail.com>
2017-07-03 20:18:11 +0200

    Convert Name#references to a service object

--- app/models/name.rb
+++ app/models/name.rb
@@ -90,7 +90,7 @@ class Name < ApplicationRecord
   end
 
   def references
-    references_in_fields.concat(references_in_taxt)
+    Names::WhatLinksHere.new(self).call
   end
 
   private
@@ -101,50 +101,6 @@ class Name < ApplicationRecord
     def set_taxon_caches
       Taxon.where(name: self).update_all(name_cache: name)
       Taxon.where(name: self).update_all(name_html_cache: name_html)
-    end
-
-    def references_in_fields
-      references_to_taxon_name
-        .concat(references_to_taxon_type_name)
-        .concat(references_to_protonym_name)
-    end
-
-    def references_to_taxon_name
-      Taxon.where(name: self).map do |taxon|
-        table_ref 'taxa', :name_id, taxon.id
-      end
-    end
-
-    def references_to_taxon_type_name
-      Taxon.where(type_name: self).map do |taxon|
-        table_ref 'taxa', :type_name_id, taxon.id
-      end
-    end
-
-    def references_to_protonym_name
-      Protonym.where(name: self).map do |protonym|
-        table_ref 'protonyms', :name_id, protonym.id
-      end
-    end
-
-    def references_in_taxt
-      references = []
-      Taxt::TAXT_FIELDS.each do |klass, fields|
-        table = klass.arel_table
-        fields.each do |field|
-          klass.where(table[field].matches("%{nam #{id}}%")).each do |record|
-            next unless record[field]
-            if record[field] =~ /{nam #{id}}/
-              references << table_ref(klass.table_name, field, record.id)
-            end
-          end
-        end
-      end
-      references
-    end
-
-    def table_ref table, field, id
-      { table: table, field: field, id: id }
     end
 
     def change name_string
--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -92,53 +92,11 @@ describe Name do
   end
 
   describe "#references" do
-    it "returns references in fields" do
-      atta = create_genus 'Atta'
-      protonym = create :protonym, name: atta.name
-      atta.update_attribute :protonym, protonym
-      atta.update_attribute :type_name, atta.name
+    subject { Name.create! name: 'Atta' }
 
-      expect(atta.name.references).to match_array [
-        { table: 'taxa', field: :name_id, id: atta.id },
-        { table: 'taxa', field: :type_name_id, id: atta.id },
-        { table: 'protonyms', field: :name_id, id: protonym.id }
-      ]
-    end
-
-    it "returns references in taxt" do
-      atta = create_genus 'Atta'
-      eciton = create_genus 'Eciton'
-      eciton.update_attribute :type_taxt, "{nam #{atta.name.id}}"
-      expect(atta.name.references).to match_array [
-        { table: 'taxa', field: :name_id, id: atta.id },
-        { table: 'taxa', field: :type_taxt, id: eciton.id }
-      ]
-    end
-  end
-
-  describe "#references_in_taxt" do
-    it "returns instances that reference this name" do
-      name = Name.create! name: 'Atta'
-
-      # Create an instance for each type of taxt.
-      Taxt::TAXT_FIELDS.each do |klass, fields|
-        fields.each { |field| create klass, field => "{nam #{name.id}}" }
-      end
-
-      # Count the total referencing items.
-      refs = name.send :references_in_taxt
-      expect(refs.length).to eq(
-        Taxt::TAXT_FIELDS.map { |klass, fields| fields.length }.reduce(&:+)
-      )
-
-      # Count the total referencing items of each type.
-      Taxt::TAXT_FIELDS.each do |klass, fields|
-        fields.each do |field|
-          expect(refs.select { |i| i[:table] == klass.table_name }.length).to eq(
-            Taxt::TAXT_FIELDS.detect { |k, f| k == klass }[1].length
-          )
-        end
-      end
+    it "calls `Names::WhatLinksHere`" do
+      expect(Names::WhatLinksHere).to receive(:new).with(subject).and_call_original
+      subject.references
     end
   end
 
