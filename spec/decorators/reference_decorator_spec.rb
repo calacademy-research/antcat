bc504453900502ac7d42650bf0c8e4d0346ef039
jonkerz <jonkerz@gmail.com>
2017-09-01 01:24:32 +0200

    refactor: Move/remove specs

--- spec/decorators/reference_decorator_spec.rb
+++ spec/decorators/reference_decorator_spec.rb
@@ -19,55 +19,6 @@ describe ReferenceDecorator do
     end
   end
 
-  describe "#keey" do
-    # TODO test this and all other methods not already tested.
-  end
-
-  # TODO moved here from `citation_spec.rb`.
-  xdescribe "#year or Reference#year or wherever this method will be defined" do
-    it "shows the year" do
-      reference = reference_factory author_name: 'Bolton', citation_year: '2001'
-      citation = build_stubbed :citation, reference: reference
-
-      expect(citation.year).to eq '2001'
-    end
-
-    it "handles nil years" do
-      reference = reference_factory author_name: 'Bolton', citation_year: nil
-      citation = build_stubbed :citation, reference: reference
-
-      expect(citation.year).to eq "[no year]"
-    end
-  end
-
-  # TODO moved here from `citation_spec.rb`.
-  # TODO move to wherever `#authorship_string` is tested.
-  xdescribe "#keey_without_letters_in_year" do
-    it "shows the author and year" do
-      reference = reference_factory author_name: 'Bolton', citation_year: '2001'
-      citation = build_stubbed :citation, reference: reference
-
-      expect(citation.authorship_string).to eq 'Bolton, 2001'
-    end
-
-    it "handles multiple authors" do
-      reference = build_stubbed :article_reference,
-        author_names: [ create(:author_name, name: 'Bolton, B.'),
-                        create(:author_name, name: 'Fisher, R.')],
-        citation_year: '2001', year: '2001'
-      citation = build_stubbed :citation, reference: reference
-
-      expect(citation.authorship_string).to eq 'Bolton & Fisher, 2001'
-    end
-
-    it "doesn't include the year ordinal" do
-      reference = reference_factory author_name: 'Bolton', citation_year: '1885g'
-      citation = build_stubbed :citation, reference: reference
-
-      expect(citation.authorship_string).to eq 'Bolton, 1885'
-    end
-  end
-
   describe "#make_html_safe" do
     def make_html_safe string
       nil_decorator.send :make_html_safe, string
--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -386,4 +386,24 @@ describe Reference do
       expect(reference.keey).to eq 'Bolton, Fisher & Ward, 1970a'
     end
   end
+
+  describe "#keey_without_letters_in_year" do
+    it "shows the author and year" do
+      reference = reference_factory author_name: 'Bolton', citation_year: '2001'
+      expect(reference.keey_without_letters_in_year).to eq 'Bolton, 2001'
+    end
+
+    it "handles multiple authors" do
+      reference = build_stubbed :article_reference,
+        author_names: [ create(:author_name, name: 'Bolton, B.'),
+                        create(:author_name, name: 'Fisher, R.')],
+        citation_year: '2001', year: '2001'
+      expect(reference.keey_without_letters_in_year).to eq 'Bolton & Fisher, 2001'
+    end
+
+    it "doesn't include the year ordinal" do
+      reference = reference_factory author_name: 'Bolton', citation_year: '1885g'
+      expect(reference.keey_without_letters_in_year).to eq 'Bolton, 1885'
+    end
+  end
 end
