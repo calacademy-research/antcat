bc9ee5902e5b2008d6e24638c37a4ea35d4a367e
jonkerz <jonkerz@gmail.com>
2017-09-01 01:26:09 +0200

    feat: Remove 'clear_logs.rb'
    We do not need it any longer.

--- config/initializers/clear_logs.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development?
-  MAX_LOG_SIZE = 1000.megabytes
-
-  logs = File.join(Rails.root, 'log', '*.log')
-  if Dir[logs].any? { |log| File.size?(log).to_i > MAX_LOG_SIZE }
-    $stdout.puts "Clearing log files..."
-    `rake log:clear`
-  end
-end
