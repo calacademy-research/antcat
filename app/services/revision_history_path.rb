4ba3843b6e483a6941d11271a580e266cb023a66
jonkerz <jonkerz@gmail.com>
2017-09-09 19:22:31 +0200

    refactor: Extract into RevisionHistoryPath service

---
+++ app/services/revision_history_path.rb
@@ -0,0 +1,25 @@
+class RevisionHistoryPath
+  include Rails.application.routes.url_helpers
+  include ActionView::Helpers::UrlHelper
+
+  def initialize type, id
+    @type = type
+    @id = id
+  end
+
+  def call
+    url
+  end
+
+  private
+    attr_reader :type, :id
+
+    def url
+      case type
+      when "Reference"        then reference_history_index_path id
+      when "TaxonHistoryItem" then taxon_history_item_path id
+      when "ReferenceSection" then reference_section_path id
+      when "Taxon"            then taxon_history_path id
+      end
+    end
+end
