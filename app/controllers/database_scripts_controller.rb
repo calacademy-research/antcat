b7e9e6684a122fed7556888c1b8ee5f0eb7a731f
jonkerz <jonkerz@gmail.com>
2017-09-12 00:06:48 +0200

    refactor: Tweaks

--- app/controllers/database_scripts_controller.rb
+++ app/controllers/database_scripts_controller.rb
@@ -1,7 +1,7 @@
 class DatabaseScriptsController < ApplicationController
-  EXPIRES_IN = Rails.env.development? ? 1.second : 24.hours
+  EXPIRES_IN = Rails.env.development? ? 2.seconds : 24.hours
 
-  before_action :authenticate_editor, except: [:index]
+  before_action :authenticate_editor, except: :index
   before_action :set_script, only: [:show, :source, :regenerate]
 
   def index
@@ -9,13 +9,9 @@ class DatabaseScriptsController < ApplicationController
   end
 
   def show
-    @used_cached = used_cached? @script
-
-    start = Time.now
-    @cached_render = cached_render @script
-    @render_duration = Time.now - start
-
-    @cached_at = cached_at @script
+    @used_cached = used_cached?
+    @cached_render, @render_duration = cached_render
+    @cached_at = cached_at
   end
 
   def source
@@ -33,19 +29,25 @@ class DatabaseScriptsController < ApplicationController
       raise ActionController::RoutingError.new("Not Found")
     end
 
-    def used_cached? script
-      Rails.cache.exist? script.cache_key
+    def used_cached?
+      Rails.cache.exist? @script.cache_key
     end
 
-    def cached_render script
-      Rails.cache.fetch(script, expires_in: EXPIRES_IN) do
-        script.render
+    def cached_render
+      start = Time.now
+
+      results = Rails.cache.fetch(@script, expires_in: EXPIRES_IN) do
+        @script.render
       end
+
+      render_duration = Time.now - start
+
+      [results, render_duration]
     end
 
     # TODO improve.
-    def cached_at script
-      entry = Rails.cache.send :read_entry, script.cache_key, {}
+    def cached_at
+      entry = Rails.cache.send :read_entry, @script.cache_key, {}
       Time.at entry.instance_variable_get(:@created_at)
     end
 end
