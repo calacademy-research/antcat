9210b1ae2bb75b2f94686f118a9295135094352b
jonkerz <jonkerz@gmail.com>
2017-09-16 22:14:19 +0200

    refactor: Tweak spec

--- spec/services/taxa/what_links_here_spec.rb
+++ spec/services/taxa/what_links_here_spec.rb
@@ -3,68 +3,77 @@ require "spec_helper"
 describe Taxa::WhatLinksHere do
   describe "#call" do
     let!(:atta) { create_genus 'Atta' }
-    subject { described_class.new(atta) }
 
-    it "has no references, if alone" do
-      expect(subject.call.size).to eq 0
+    context "when there are no references" do
+      specify { expect(described_class[atta]).to be_empty }
     end
 
-    describe "references in Taxon fields" do
-      it "has a reference if it's a taxon's genus" do
-        species = create_species genus: atta
-        expect(subject.call).to match_array [
-          { table: 'taxa', field: :genus_id, id: species.id }
-        ]
+    describe "references in taxon fields" do
+      context "when there are references in a genus" do
+        let!(:species) { create_species genus: atta }
+
+        specify do
+          expect(described_class[atta]).to match_array [
+            { table: 'taxa', field: :genus_id, id: species.id }
+          ]
+        end
       end
 
-      it "has a reference if it's a taxon's subfamily" do
-        subject = described_class.new(atta.subfamily)
-        expect(subject.call).to match_array [
-          { table: 'taxa', field: :subfamily_id, id: atta.id },
-          { table: 'taxa', field: :subfamily_id, id: atta.tribe.id }
-        ]
+      context "when there are references in a subfamily" do
+        specify do
+          expect(described_class[atta.subfamily]).to match_array [
+            { table: 'taxa', field: :subfamily_id, id: atta.id },
+            { table: 'taxa', field: :subfamily_id, id: atta.tribe.id }
+          ]
+        end
       end
     end
 
     describe "references in taxt" do
-      it "returns references in taxt" do
-        eciton = create_genus 'Eciton'
-        eciton.update_attribute :type_taxt, "{tax #{atta.id}}"
+      context "when there are references in taxts" do
+        let!(:eciton) { create_genus 'Eciton' }
 
-        expect(subject.call).to match_array [
-          { table: 'taxa', field: :type_taxt, id: eciton.id }
-        ]
+        before { eciton.update_attribute :type_taxt, "{tax #{atta.id}}"  }
+
+        specify do
+          expect(described_class[atta]).to match_array [
+            { table: 'taxa', field: :type_taxt, id: eciton.id }
+          ]
+        end
       end
 
-      it "doesn't return references in its own taxt" do
-        atta.update_attribute :type_taxt, "{tax #{atta.id}}"
-        expect(subject.call).to be_empty
+      describe "when references in its own taxt" do
+        before { atta.update_attribute :type_taxt, "{tax #{atta.id}}" }
+
+        it "doesn't consider this an external reference" do
+          expect(described_class[atta]).to be_empty
+        end
       end
     end
 
-    describe "Reference in its authorship taxt" do
+    describe "when reference in its authorship taxt" do
       before { atta.protonym.authorship.update_attribute :notes_taxt, "{tax #{atta.id}}" }
 
       it "doesn't consider this an external reference" do
-        expect(subject.call).to be_empty
+        expect(described_class[atta]).to be_empty
       end
     end
 
     describe "references as synonym" do
       let(:eciton) { create_genus 'Eciton' }
 
-      before { Synonym.create! junior_synonym: eciton, senior_synonym: atta }
+      context "when there are references" do
+        before { Synonym.create! junior_synonym: eciton, senior_synonym: atta }
 
-      specify do
-        subject = described_class.new(atta)
-        expect(subject.call).to match_array [
-          { table: 'synonyms', field: :senior_synonym_id, id: eciton.id }
-        ]
+        specify do
+          expect(described_class[atta]).to match_array [
+            { table: 'synonyms', field: :senior_synonym_id, id: eciton.id }
+          ]
 
-        subject = described_class.new(eciton)
-        expect(subject.call).to match_array [
-          { table: 'synonyms', field: :junior_synonym_id, id: atta.id }
-        ]
+          expect(described_class[eciton]).to match_array [
+            { table: 'synonyms', field: :junior_synonym_id, id: atta.id }
+          ]
+        end
       end
     end
   end
