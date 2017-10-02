55a8d7b755ec82d9f521932295b56a00df944880
jonkerz <jonkerz@gmail.com>
2017-10-02 08:51:30 +0200

    refactor: Extract method

--- app/decorators/taxon_decorator/taxon_status.rb
+++ app/decorators/taxon_decorator/taxon_status.rb
@@ -6,45 +6,45 @@ class TaxonDecorator::TaxonStatus
   end
 
   def call
-    # Note: Cleverness is used here to make these queries (e.g.: obsolete_combination?)
-    # appear as tags. That's how CSS does its coloring.
-    labels = []
-    labels << "<i>incertae sedis</i> in #{taxon.incertae_sedis_in}" if taxon.incertae_sedis_in
-
-    labels << if taxon.homonym? && taxon.homonym_replaced_by
-                "homonym replaced by #{taxon.homonym_replaced_by.decorate.link_to_taxon}"
-              elsif taxon.unidentifiable?
-                "unidentifiable"
-              elsif taxon.unresolved_homonym?
-                if taxon.current_valid_taxon_including_synonyms
-                  "unresolved junior homonym, junior synonym#{format_senior_synonym}"
-                else
-                  "unresolved junior homonym"
-                end
-              elsif taxon.nomen_nudum?
-                "<i>nomen nudum</i>"
-              elsif taxon.synonym?
-                "junior synonym#{format_senior_synonym}"
-              elsif taxon.obsolete_combination?
-                "an obsolete combination of #{format_valid_combination}"
-              elsif taxon.unavailable_misspelling?
-                "a misspelling of #{format_valid_combination}"
-              elsif taxon.unavailable_uncategorized?
-                "see #{format_valid_combination}"
-              elsif taxon.nonconfirming_synonym?
-                "a non standard form of #{format_valid_combination}"
-              elsif taxon.invalid?
-                Status[taxon].to_s.dup
-              else
-                "valid"
-              end
-
-    labels << 'ichnotaxon' if taxon.ichnotaxon?
-    labels.join(', ').html_safe
+    parts = []
+    parts << "<i>incertae sedis</i> in #{taxon.incertae_sedis_in}" if taxon.incertae_sedis_in
+    parts << main_status
+    parts << 'ichnotaxon' if taxon.ichnotaxon?
+    parts.join(', ').html_safe
   end
 
   private
     attr_reader :taxon
+
+    def main_status
+      if taxon.homonym? && taxon.homonym_replaced_by
+        "homonym replaced by #{taxon.homonym_replaced_by.decorate.link_to_taxon}"
+      elsif taxon.unidentifiable?
+        "unidentifiable"
+      elsif taxon.unresolved_homonym?
+        if taxon.current_valid_taxon_including_synonyms
+          "unresolved junior homonym, junior synonym#{format_senior_synonym}"
+        else
+          "unresolved junior homonym"
+        end
+      elsif taxon.nomen_nudum?
+        "<i>nomen nudum</i>"
+      elsif taxon.synonym?
+        "junior synonym#{format_senior_synonym}"
+      elsif taxon.obsolete_combination?
+        "an obsolete combination of #{format_valid_combination}"
+      elsif taxon.unavailable_misspelling?
+        "a misspelling of #{format_valid_combination}"
+      elsif taxon.unavailable_uncategorized?
+        "see #{format_valid_combination}"
+      elsif taxon.nonconfirming_synonym?
+        "a non standard form of #{format_valid_combination}"
+      elsif taxon.invalid?
+        Status[taxon].to_s.dup
+      else
+        "valid"
+      end
+    end
 
     def format_senior_synonym
       current_valid_taxon = taxon.current_valid_taxon_including_synonyms
