9970ccbf143cbff909724ede12752d803388cf03
jonkerz <jonkerz@gmail.com>
2017-10-03 00:01:23 +0200

    feat: Tweaks

--- app/controllers/default_references_controller.rb
+++ app/controllers/default_references_controller.rb
@@ -1,6 +1,8 @@
 # TODO namespace under `References`.
 
 class DefaultReferencesController < ApplicationController
+  before_action :authenticate_editor
+
   def update
     DefaultReference.set session, Reference.find(params[:id])
     if request.xhr?
