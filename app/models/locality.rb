85ca15e432b1e79769ab51850304922f256066b8
jonkerz <jonkerz@gmail.com>
2017-10-05 17:07:31 +0200

    feat: More `ActiveModel::ForbiddenAttributesProtection`

--- app/models/locality.rb
+++ app/models/locality.rb
@@ -1,6 +1,8 @@
 # TODO let's use this before we have a proper model, like `TypeSpecimenRepository`.
 
 class Locality < ActiveRecord::Base
+  include ActiveModel::ForbiddenAttributesProtection
+
   def self.unique_sorted
     Protonym.uniq.pluck(:locality).compact.sort
   end
