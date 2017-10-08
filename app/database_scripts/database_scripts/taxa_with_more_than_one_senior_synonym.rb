f462d2d9160d4a7fb68947d5dfdc4f87f09f2eac
jonkerz <jonkerz@gmail.com>
2017-10-08 23:09:54 +0200

    feat: Db script - 'Synonyms: Taxa with more than one senior synonym'

---
+++ app/database_scripts/database_scripts/taxa_with_more_than_one_senior_synonym.rb
@@ -0,0 +1,31 @@
+module DatabaseScripts
+  class TaxaWithMoreThanOneSeniorSynonym < DatabaseScript
+    def results
+      Taxon.where(id: synonym_records_with_more_than_one_senior)
+    end
+
+    def render
+      as_table do |t|
+        t.header :taxon, :status, :senior_synonyms_count
+        t.rows do |taxon|
+          [ markdown_taxon_link(taxon), taxon.status, taxon.senior_synonyms.count ]
+        end
+      end
+    end
+
+    private
+      def synonym_records_with_more_than_one_senior
+        Synonym.group(:junior_synonym_id)
+          .having('COUNT(synonyms.senior_synonym_id) > 1')
+          .pluck(:junior_synonym_id)
+      end
+  end
+end
+
+__END__
+description: >
+  If the senior synonyms count is different from the number of senior synonyms listed
+  in synonyms section on the edit page, that may have to do with
+  [Synonym records referencing non existent taxa](/database_scripts/synonym_records_referencing_non_existent_taxa).
+tags: [new!]
+topic_areas: [synonyms]
