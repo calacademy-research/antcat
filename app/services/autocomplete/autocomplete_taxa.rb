0efb05d062df5986427635d530908a1d51c40fcc
jonkerz <jonkerz@gmail.com>
2017-11-22 20:40:05 +0100

    feat: Show dagger in catalog autocompletion search box

--- app/services/autocomplete/autocomplete_taxa.rb
+++ app/services/autocomplete/autocomplete_taxa.rb
@@ -12,6 +12,7 @@ module Autocomplete
           id: taxon.id,
           name: taxon.name_cache,
           name_html: taxon.name_html_cache,
+          name_with_fossil: taxon.name_with_fossil,
           author_citation: taxon.author_citation
         }
       end
