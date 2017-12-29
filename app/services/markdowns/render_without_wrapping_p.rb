76d216cf388fbc3ab67b9fba94b23c7853ef87e5
jonkerz <jonkerz@gmail.com>
2017-12-29 21:04:40 +0100

    refactor: Extract into `Markdowns::RenderWithoutWrappingP`

---
+++ app/services/markdowns/render_without_wrapping_p.rb
@@ -0,0 +1,24 @@
+module Markdowns
+  class RenderWithoutWrappingP
+    P_HTML_ELEMENT_REGEX = /\A<p>(.*)<\/p>\Z/m
+
+    include Service
+
+    def initialize content
+      @content = content
+    end
+
+    def call
+      return unless content
+      rendered = Markdowns::Render[content.dup]
+      strip_wrapping_p(rendered).html_safe
+    end
+
+    private
+      attr_reader :content
+
+      def strip_wrapping_p rendered
+        Regexp.new(P_HTML_ELEMENT_REGEX).match(rendered)[1] rescue rendered
+      end
+  end
+end
