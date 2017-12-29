e0dae854d2947eb937b9ace9a06b3220f259338a
jonkerz <jonkerz@gmail.com>
2017-12-29 21:40:52 +0100

    feat: Validate presence of `TaxonHistoryItem#taxon_id`

--- spec/factories/taxon_history_items.rb
+++ spec/factories/taxon_history_items.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :taxon_history_item, class: TaxonHistoryItem do
     taxt 'Taxonomic history'
+    taxon_id 123999
   end
 end
--- spec/models/taxon_history_item_spec.rb
+++ spec/models/taxon_history_item_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe TaxonHistoryItem do
   it { is_expected.to be_versioned }
   it { is_expected.to validate_presence_of :taxt }
+  it { is_expected.to validate_presence_of :taxon_id }
   it { is_expected.to belong_to :taxon }
 
   describe "#update_taxt_from_editable" do
