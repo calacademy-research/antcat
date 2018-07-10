ad66e19edf26b88f1d307f34dc4fa6621fad030a
jonkerz <jonkerz@gmail.com>
2018-07-10 21:48:11 +0200

    feat: New db script: 'Synonyms: Pass through names with synonyms'

---
+++ app/database_scripts/database_scripts/pass_through_names_with_synonyms.rb
@@ -0,0 +1,29 @@
+module DatabaseScripts
+  class PassThroughNamesWithSynonyms < DatabaseScript
+    def results
+      with_senior_synonyms + with_junior_synonyms
+    end
+
+    private
+      def pass_through_names
+        Taxon.where(
+          status: ["obsolete combination", "original combination", "unavailable misspelling"]
+        )
+      end
+
+      def with_senior_synonyms
+        pass_through_names.joins(:senior_synonyms).distinct
+      end
+
+      def with_junior_synonyms
+        pass_through_names.joins(:junior_synonyms).distinct
+      end
+  end
+end
+
+__END__
+description: >
+  Original combinations, obsolete combination and unavailable misspellings with synonyms.
+  See %github375.
+tags: [new!]
+topic_areas: [synonyms]
