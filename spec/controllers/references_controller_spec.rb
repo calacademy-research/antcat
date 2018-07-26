c23dab198e51e566a3d36ef841e77fa7363dcc70
jonkerz <jonkerz@gmail.com>
2018-07-26 22:50:40 +0200

    test: Fix reference search controller spec

--- spec/controllers/references_controller_spec.rb
+++ spec/controllers/references_controller_spec.rb
@@ -14,27 +14,6 @@ describe ReferencesController do
     end
   end
 
-  # TODO fix after merging `tech/various-tweaks-ep2`.
-  xdescribe "GET search" do
-    describe "search terms matching ids" do
-      context "when reference exists" do
-        let!(:reference) { reference_factory author_name: 'E.O. Wilson', id: 99999 }
-
-        it "redirects to #show" do
-          get :search, params: { q: reference.id }
-          expect(response).to redirect_to reference_path(reference)
-        end
-      end
-
-      context "when reference does not exists" do
-        it "does not redirect unless the reference exists" do
-          get :search, params: { q: "11111" }
-          expect(response).to render_template :search
-        end
-      end
-    end
-  end
-
   describe "GET autocomplete", search: true do
     context "when there are matches" do
       before do
