1b8655da185799f4890f0a294fab294c09f49dd2
jonkerz <jonkerz@gmail.com>
2018-08-02 19:06:03 +0200

    refactor: Extract into service object `Taxa::ElevateToSpecies`

--- app/models/taxa/species_group/subspecies.rb
+++ app/models/taxa/species_group/subspecies.rb
@@ -32,34 +32,8 @@ class Subspecies < SpeciesGroupTaxon
     Subspecies.none
   end
 
-  # This possibly should go through taxon_mother. It's a taxon change, after all,
-  # and the others are handled there.
   def elevate_to_species
-    raise NoSpeciesForSubspeciesError unless species
-    # Removed commented out code + comments that looked very WIP
-    # See 37064da56f47a530a388b268289a73cb24b93d75
-
-    new_name_string = "#{species.genus.name.name} #{name.epithet}"
-    new_name = SpeciesName.find_by_name new_name_string
-    unless new_name
-      new_name = SpeciesName.new
-      new_name.update name: new_name_string,
-                      name_html: italicize(new_name_string),
-                      epithet: name.epithet,
-                      epithet_html: name.epithet_html,
-                      epithets: nil
-      new_name.save
-    end
-
-    create_elevate_to_species_activity new_name
-
-    # writes directly to db, bypasses save. "update" operates in memory and
-    # lets you use the "save" path
-    update_columns name_id: new_name.id,
-                   species_id: nil,
-                   name_cache: new_name.name,
-                   name_html_cache: new_name.name_html,
-                   type: 'Species'
+    Taxa::ElevateToSpecies[self]
   end
 
   private
@@ -67,10 +41,5 @@ class Subspecies < SpeciesGroupTaxon
     def set_genus
       return if genus
       self.genus = species.genus if species
-    end
-
-    def create_elevate_to_species_activity new_name
-      create_activity :elevate_subspecies_to_species,
-        parameters: { name_was: name_html_cache, name: new_name.name_html }
     end
 end
---
+++ spec/services/taxa/elevate_to_species_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe Taxa::ElevateToSpecies do
+  describe "#call" do
+    let(:genus) { create_genus 'Atta' }
+
+    it "turns the record into a Species" do
+      taxon = create_subspecies 'Atta major colobopsis'
+      expect(taxon).to be_kind_of Subspecies
+
+      described_class[taxon]
+
+      taxon = Species.find taxon.id
+      expect(taxon).to be_kind_of Species
+    end
+
+    it "forms the new species name from the epithet" do
+      species = create_species 'Atta major', genus: genus
+      subspecies_name = SubspeciesName.create! name: 'Atta major colobopsis',
+        epithet: 'colobopsis', epithets: 'major colobopsis'
+      taxon = create_subspecies name: subspecies_name, genus: genus, species: species
+
+      described_class[taxon]
+
+      taxon = Species.find taxon.id
+      expect(taxon.name.name).to eq 'Atta colobopsis'
+      expect(taxon.name.epithet).to eq 'colobopsis'
+      expect(taxon.name.epithets).to be_nil
+    end
+
+    it "creates a new species name, if necessary" do
+      species = create_species 'Atta major', genus: genus
+      subspecies_name = SubspeciesName.create! name: 'Atta major colobopsis',
+        epithet: 'colobopsis', epithets: 'major colobopsis'
+      taxon = create_subspecies name: subspecies_name, genus: genus, species: species
+      name_count = Name.count
+
+      described_class[taxon]
+
+      expect(Name.count).to eq(name_count + 1)
+    end
+
+    it "reuses existing species name, if possible" do
+      species = create_species 'Atta colobopsis', genus: genus
+      subspecies_name = SubspeciesName.create! name: 'Atta major colobopsis',
+        epithet: 'colobopsis', epithets: 'major colobopsis'
+      taxon = create_subspecies name: subspecies_name, genus: genus, species: species
+
+      described_class[taxon]
+
+      taxon = Species.find taxon.id
+      expect(taxon.name).to eq species.name
+    end
+
+    it "doesn't crash and burn if the species already exists" do
+      species = create_species 'Atta major', genus: genus
+      subspecies_name = SubspeciesName.create! name: 'Atta batta major',
+        epithet: 'major', epithets: 'batta major'
+      taxon = create_subspecies name: subspecies_name, species: species
+
+      expect { described_class[taxon] }.not_to raise_error
+    end
+  end
+end
