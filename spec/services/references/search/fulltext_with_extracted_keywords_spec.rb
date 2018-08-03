dd3ea126ef322d09ebd6da71e44d202b0e039724
jonkerz <jonkerz@gmail.com>
2018-08-04 01:38:48 +0200

    fix: Add Solr character mappings for two missing Hungarian letters

--- spec/services/references/search/fulltext_with_extracted_keywords_spec.rb
+++ spec/services/references/search/fulltext_with_extracted_keywords_spec.rb
@@ -64,16 +64,20 @@ describe References::Search::FulltextWithExtractedKeywords do
         end
 
         describe 'Author names' do
-          let!(:reference) { reference_factory author_name: 'Hölldobler' }
-
           before { Sunspot.commit }
 
-          it 'handles diacritics in the search term' do
-            expect(described_class[q: 'Hölldobler']).to eq [reference]
+          context "when author contains German diacritics" do
+            let!(:reference) { reference_factory author_name: 'Hölldobler' }
+
+            specify { expect(described_class[q: 'Hölldobler']).to eq [reference] }
+            specify { expect(described_class[q: 'holldobler']).to eq [reference] }
           end
 
-          it 'substitutes diacritics with English letters' do
-            expect(described_class[q: 'holldobler']).to eq [reference]
+          context "when author contains Hungarian diacritics" do
+            let!(:reference) { reference_factory author_name: 'Csősz' }
+
+            specify { expect(described_class[q: 'Csősz']).to eq [reference] }
+            specify { expect(described_class[q: 'csosz']).to eq [reference] }
           end
         end
 
