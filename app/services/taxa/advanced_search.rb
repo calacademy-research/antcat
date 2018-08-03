1e464b238fb4f3f8d0636b01b043966726533f55
jonkerz <jonkerz@gmail.com>
2018-08-03 20:18:30 +0200

    feat: Add more taxon search options
    These: `:nomen_nudum, :unresolved_junior_homonym, :ichnotaxon, :hong`.

--- app/services/taxa/advanced_search.rb
+++ app/services/taxa/advanced_search.rb
@@ -30,7 +30,11 @@ module Taxa
       def search_results
         query = Taxon.joins(protonym: [{ authorship: :reference }]).order_by_name_cache
 
-        query = filter_by_fossil!(query) if params[:fossil]
+        query = filter_by_fossil(query, params[:fossil])
+        query = filter_by_nomen_nudum(query, params[:nomen_nudum])
+        query = filter_by_unresolved_junior_homonym(query, params[:unresolved_junior_homonym])
+        query = filter_by_ichnotaxon(query, params[:ichnotaxon])
+        query = filter_by_hong(query, params[:hong])
 
         query = query.where(status: params[:status]) if params[:status]
         query = query.where(type: params[:rank]) if params[:rank]
@@ -91,10 +95,47 @@ module Taxa
         query.includes(:name, protonym: { authorship: :reference })
       end
 
-      def filter_by_fossil!(query)
-        case params[:fossil]
+      # TODO quick non-DRY code.
+      # TODO sync/normalize db column names and true/false/nil, consider creating scopes, etc.
+      # Maybe refactor, or maybe it will refactor itself when  we start using Solr.
+      def filter_by_fossil(query, fossil)
+        return query unless fossil
+        case fossil
         when "true"  then query.fossil
         when "false" then query.extant
+        else raise "not a valid option"
+        end
+      end
+
+      def filter_by_nomen_nudum(query, nomen_nudum)
+        return query unless nomen_nudum
+        case nomen_nudum
+        when "true"  then query.where(nomen_nudum: true)
+        when "false" then query.where(nomen_nudum: [false, nil])
+        end
+      end
+
+      def filter_by_unresolved_junior_homonym(query, unresolved_junior_homonym)
+        return query unless unresolved_junior_homonym
+        case unresolved_junior_homonym
+        when "true"  then query.where(unresolved_homonym: true)
+        when "false" then query.where(unresolved_homonym: [false, nil])
+        end
+      end
+
+      def filter_by_ichnotaxon(query, ichnotaxon)
+        return query unless ichnotaxon
+        case ichnotaxon
+        when "true"  then query.where(ichnotaxon: true)
+        when "false" then query.where(ichnotaxon: [false, nil])
+        end
+      end
+
+      def filter_by_hong(query, hong)
+        return query unless hong
+        case hong
+        when "true"  then query.where(hong: true)
+        when "false" then query.where(hong: [false, nil])
         end
       end
   end
