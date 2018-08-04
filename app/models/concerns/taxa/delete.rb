de64da4c2c89c964372c1fc72f8e1b10f7e6c579
jonkerz <jonkerz@gmail.com>
2018-08-04 05:51:18 +0200

    refactor: Extract into service `Taxa::DeleteTaxonAndChildren`

--- app/models/concerns/taxa/delete.rb
+++ app/models/concerns/taxa/delete.rb
@@ -1,44 +1,11 @@
-# TODO add `before_destroy :check_not_referenced`, but allow suppressing it.
-
 module Taxa::Delete
   extend ActiveSupport::Concern
-
-  def delete_with_state!
-    Taxon.transaction do
-      # Bit of a hack; this is a new table which may lack the depth of other tables.
-      # Creation doesn't add a record, so you can't "step back to" a valid version.
-      # doing touch_with_version (creeate a fallback point) in the migration makes an
-      # enourmous and unnecessary pile of these.
-      taxon_state.paper_trail.touch_with_version if taxon_state.versions.empty?
-
-      taxon_state.deleted = true
-      taxon_state.review_state = TaxonState::WAITING
-      taxon_state.save
-      destroy!
-    end
-  end
 
   def delete_impact_list
     Taxa::DeleteImpactList[self]
   end
 
   def delete_taxon_and_children
-    Feed.without_tracking do
-      Taxon.transaction do
-        UndoTracker.setup_change self, :delete
-        delete_taxon_children self
-        delete_with_state!
-      end
-    end
-    create_activity :destroy
+    Taxa::DeleteTaxonAndChildren[self]
   end
-
-  private
-
-    def delete_taxon_children taxon
-      taxon.children.each do |child|
-        child.delete_with_state!
-        delete_taxon_children child
-      end
-    end
 end
