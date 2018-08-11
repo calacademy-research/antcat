80de2cc0b73444a3fc013c769c5ba100b042cfb9
jonkerz <jonkerz@gmail.com>
2018-08-11 05:24:04 +0200

    feat: Create bespoke admin controller for managing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: :index
-  before_action :set_user, only: :show
+  before_action :authenticate_superadmin, except: [:index, :show, :mentionables]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
     @users = User.order_by_name
@@ -9,6 +9,31 @@ class UsersController < ApplicationController
   def show
     @recent_user_activities = @user.activities.most_recent 5
     @recent_user_comments = @user.comments.most_recent 5
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to @user, notice: "Successfully added user."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update_without_password user_params.except(:current_password)
+      redirect_to @user, notice: "Successfully updated user."
+    else
+      render :edit
+    end
   end
 
   def mentionables
@@ -26,5 +51,9 @@ class UsersController < ApplicationController
 
     def set_user
       @user = User.find params[:id]
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :can_edit, :is_superadmin)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -140,7 +140,7 @@ Rails.application.routes.draw do
   get 'name_fields/:type/:id' => 'name_fields#show'
 
   devise_for :users, path_prefix: 'my', controllers: { registrations: "registrations" }
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show, :new, :create, :edit, :update] do
     collection do
       get :mentionables
     end
