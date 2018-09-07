cdff6f24c131b1fabf86cf3703a1fb92a3a9666f
jonkerz <jonkerz@gmail.com>
2018-09-08 01:44:42 +0200

    test: Remove long-time pending specs

--- spec/controllers/api/v1/citations_controller_spec.rb
+++ spec/controllers/api/v1/citations_controller_spec.rb
@@ -4,15 +4,6 @@ describe Api::V1::CitationsController do
   let!(:species) { create :species }
 
   describe "GET index" do
-    # TODO depends on being run before any other specs.
-    xit "gets all citations greater than a given number" do
-      get :index, params: { starts_at: species.id } # Get index starting at four.
-
-      # Since we want no ids less than 4, we should get a starting id at 4.
-      expect(json_response[0]['citation']['id']).to eq species.id
-      expect(json_response.count).to eq 1
-    end
-
     it "gets all citations" do
       get :index
 
