ed6c0190e1b7700e4fd8dfb928b5d3680d7e7042
jonkerz <jonkerz@gmail.com>
2018-09-08 01:32:57 +0200

    tweak: Was more useful when `Taxon#hol_id` was a method

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -9,9 +9,8 @@ module LinkHelper
   end
 
   def link_to_hol taxon
-    hol_id = taxon.hol_id
-    return unless hol_id
-    link_to_external_site 'HOL', "http://hol.osu.edu/index.html?id=#{hol_id}"
+    return unless taxon.hol_id
+    link_to_external_site 'HOL', "http://hol.osu.edu/index.html?id=#{taxon.hol_id}"
   end
 
   def link_to_antweb taxon
