288f80ec1a7bdcc194974a0e4e0b2629d27ddbb0
jonkerz <jonkerz@gmail.com>
2018-09-15 03:16:40 +0200

    remove: We don't have to test this

--- spec/controllers/taxa_controller_spec.rb
+++ spec/controllers/taxa_controller_spec.rb
@@ -11,16 +11,4 @@ describe TaxaController do
       specify { expect(post(:update, params: { id: 1 })).to have_http_status :forbidden }
     end
   end
-
-  describe "#build_relationships" do
-    it "builds" do
-      taxon = controller.send :build_new_taxon, :species
-
-      expect(taxon.name).not_to be_blank
-      expect(taxon.type_name).not_to be_blank
-      expect(taxon.protonym).not_to be_blank
-      expect(taxon.protonym.name).not_to be_blank
-      expect(taxon.protonym.authorship).not_to be_blank
-    end
-  end
 end
