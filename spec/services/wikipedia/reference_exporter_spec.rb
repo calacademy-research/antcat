f276e5383ce34a6fa8a1c083acbb021845f1c59c
jonkerz <jonkerz@gmail.com>
2018-09-18 21:00:20 +0200

    tweak: `Wikipedia::ReferenceExporter` spec

--- spec/services/wikipedia/reference_exporter_spec.rb
+++ spec/services/wikipedia/reference_exporter_spec.rb
@@ -1,18 +1,17 @@
 require "spec_helper"
 
 describe Wikipedia::ReferenceExporter do
-  let(:batiatus) do
-    create :author_name, name: "Batiatus, Q. L.", author: build_stubbed(:author)
-  end
+  let(:batiatus) { create :author_name, name: "Batiatus, Q. L." }
+  let(:glaber) { create :author_name, name: "Glaber, G. C." }
 
-  describe "ArticleReference" do
+  describe "when reference is an `ArticleReference`" do
     let(:reference) do
       build_stubbed :article_reference,
         author_names: [batiatus], title: "*Formica* and Apples",
         pagination: "7-14", year: "2000", doi: "10.10.1038/nphys1170"
     end
 
-    it "formats" do
+    specify do
       expect(described_class[reference]).to eq <<-TEMPLATE.squish
         <ref name="Batiatus_2000">{{cite journal
         |first1=Q. L. |last1=Batiatus |year=2000 |title=''Formica'' and Apples
@@ -22,15 +21,14 @@ describe Wikipedia::ReferenceExporter do
     end
   end
 
-  describe "BookReference" do
+  describe "when reference is a `BookReference`" do
     let(:reference) do
-      glaber = create :author_name, name: "Glaber, G. C."
       create :book_reference,
         author_names: [batiatus, glaber], title: "*Formica* and Apples",
         pagination: "7-14", citation_year: "2000"
     end
 
-    it "formats" do
+    specify do
       expect(described_class[reference]).to eq <<-TEMPLATE.squish
         <ref name="Batiatus_&_Glaber_2000">{{cite book
         |first1=Q. L. |last1=Batiatus |first2=G. C. |last2=Glaber
@@ -41,28 +39,26 @@ describe Wikipedia::ReferenceExporter do
     end
   end
 
-  describe "#reference_name" do
-    it "handles single authors" do
-      set_exporter_with_stubbed_reference "Batiatus"
-      expect(@exporter.send(:reference_name)).to eq "Batiatus_2016"
+  describe "name tag" do
+    let(:reference) { build_stubbed :article_reference, author_names: author_names, year: "2016" }
+
+    context "when reference has one author" do
+      let(:author_names) { [batiatus] }
+
+      specify { expect(described_class[reference]).to match 'name="Batiatus_2016"' }
     end
 
-    it "handles two authors" do
-      set_exporter_with_stubbed_reference "Batiatus", "Glaber"
-      expect(@exporter.send(:reference_name)).to eq "Batiatus_&_Glaber_2016"
+    context "when reference has two authors" do
+      let(:author_names) { [batiatus, glaber] }
+
+      specify { expect(described_class[reference]).to match 'name="Batiatus_&_Glaber_2016"' }
     end
 
-    it "handles three authors" do
-      set_exporter_with_stubbed_reference "Batiatus", "Glaber", "Varro"
-      expect(@exporter.send(:reference_name)).to eq "Batiatus_et_al_2016"
+    context "when reference has more than two authors" do
+      let(:varro) { create :author_name, name: "Varro" }
+      let(:author_names) { [batiatus, glaber, varro] }
+
+      specify { expect(described_class[reference]).to match 'name="Batiatus_et_al_2016"' }
     end
   end
-end
-
-def set_exporter_with_stubbed_reference *last_names
-  allow_message_expectations_on_nil # TODO remove.
-  expect(@reference).to receive(:author_names).
-    and_return(last_names.map { |last_name| OpenStruct.new last_name: last_name })
-  expect(@reference).to receive(:year).and_return "2016"
-  @exporter = described_class.new @reference
 end
