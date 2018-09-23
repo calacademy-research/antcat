6641d495655ab84d4799bb5724b6734e4903ff0a
jonkerz <jonkerz@gmail.com>
2018-09-23 06:18:00 +0200

    feat: Only rescue once

--- app/services/exporters/antweb/exporter.rb
+++ app/services/exporters/antweb/exporter.rb
@@ -36,36 +36,31 @@ class Exporters::Antweb::Exporter
             joins(protonym: [{ authorship: :reference }]).
             includes(protonym: [{ authorship: :reference }]).
             each do |taxon|
+
+            next if taxon.name.nonconforming_name && taxon.name_cache.index('?')
+
+            puts "Processing: #{taxon.id}" if ENV['DEBUG']
+            progress.increment unless Rails.env.test?
+
             begin
-              if !taxon.name.nonconforming_name && !taxon.name_cache.index('?')
-                puts "Processing: #{taxon.id}" if ENV['DEBUG']
-                progress.increment unless Rails.env.test?
-
-                begin
-                  row = Exporters::Antweb::ExportTaxon.new.call(taxon)
-                rescue StandardError => e
-                  STDERR.puts "========================#{taxon.id}===================="
-                  STDERR.puts "An error of type #{e} happened, message is #{e.message}"
-                  STDERR.puts e.backtrace
-                  STDERR.puts "======================================================="
-                end
-
-                if row
-                  row[20].delete!('\"') if row[20]
-                  row.each do |col|
-                    if col.is_a? String
-                      col.delete!("\n")
-                      col.delete!("\r")
-                    end
-                  end
-                end
-                file.puts row.join("\t") if row
-              end
+              row = Exporters::Antweb::ExportTaxon.new.call(taxon)
             rescue StandardError => e
-              puts "Fatal error exporting taxon id: #{taxon.id}"
-              puts e.message
-              puts e.backtrace.inspect
+              STDERR.puts "========================#{taxon.id}===================="
+              STDERR.puts "An error of type #{e} happened, message is #{e.message}"
+              STDERR.puts e.backtrace
+              STDERR.puts "======================================================="
             end
+
+            if row
+              row[20].delete!('\"') if row[20]
+              row.each do |col|
+                if col.is_a? String
+                  col.delete!("\n")
+                  col.delete!("\r")
+                end
+              end
+            end
+            file.puts row.join("\t") if row
           end
         end
       end
