ded13d6524fc6fd8d78dc43757e7cb68709b1f5f
jonkerz <jonkerz@gmail.com>
2018-09-28 21:02:17 +0200

    feat: Add `TaxonDecorator#id_and_name_and_author_citation`

--- spec/decorators/taxon_decorator_spec.rb
+++ spec/decorators/taxon_decorator_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
 describe TaxonDecorator do
-  describe "#link_to_taxon" do
-    let(:taxon) { create :family }
+  let(:taxon) { create :family }
+  let(:decorated) { taxon.decorate }
 
-    it "creates the link" do
-      expect(taxon.decorate.link_to_taxon).
-        to eq %(<a href="/catalog/#{taxon.id}">#{taxon.name.name}</a>)
+  describe "#link_to_taxon" do
+    specify do
+      expect(decorated.link_to_taxon).to eq <<~HTML.squish
+        <a href="/catalog/#{taxon.id}">#{taxon.name_with_fossil}</a>
+      HTML
+    end
+  end
+
+  describe "#id_and_name_and_author_citation" do
+    specify do
+      expect(decorated.id_and_name_and_author_citation).to eq <<~HTML.squish
+        <span><small class="gray">##{taxon.id}</small>
+        <a href="/catalog/#{taxon.id}">#{taxon.name_with_fossil}</a>
+        <small class="gray">#{taxon.author_citation}</small></span>
+      HTML
     end
   end
 end
