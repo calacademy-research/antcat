0553a5e261947beaea233ab5dc4c2e71c6e1d588
jonkerz <jonkerz@gmail.com>
2018-09-29 05:30:34 +0200

    tweak: After refactoring

--- app/services/authors/find_or_create_names_from_string.rb
+++ app/services/authors/find_or_create_names_from_string.rb
@@ -7,20 +7,15 @@ module Authors
     end
 
     def call
-      import
+      parsed_author_names.map do |name|
+        AuthorName.where(name: name).find { |case_sensitive_name| case_sensitive_name.name == name } ||
+          AuthorName.create!(name: name, author: Author.create!)
+      end
     end
 
     private
 
       attr_reader :string
-
-      # TODO rename.
-      def import
-        parsed_author_names.map do |name|
-          AuthorName.all.where(name: name).find { |possible_name| possible_name.name == name } ||
-            AuthorName.create!(name: name, author: Author.create!)
-        end
-      end
 
       def parsed_author_names
         Parsers::AuthorParser.parse!(string)
--- spec/services/authors/find_or_create_names_from_string_spec.rb
+++ spec/services/authors/find_or_create_names_from_string_spec.rb
@@ -4,33 +4,33 @@ describe Authors::FindOrCreateNamesFromString do
   describe "#call" do
     it "finds or creates authors with names in the string" do
       AuthorName.create! name: 'Bolton, B.', author: Author.create!
-      author_data = described_class['Ward, P.S.; Bolton, B.']
-      expect(author_data.first.name).to eq 'Ward, P.S.'
-      expect(author_data.second.name).to eq 'Bolton, B.'
+      author_names = described_class['Ward, P.S.; Bolton, B.']
+      expect(author_names.first.name).to eq 'Ward, P.S.'
+      expect(author_names.second.name).to eq 'Bolton, B.'
       expect(AuthorName.count).to eq 2
     end
 
     it "returns the authors suffix" do
-      author_data = described_class['Ward, P.S.; Bolton, B. (eds.)']
-      expect(author_data.first.name).to eq 'Ward, P.S.'
-      expect(author_data.second.name).to eq 'Bolton, B.'
+      author_names = described_class['Ward, P.S.; Bolton, B. (eds.)']
+      expect(author_names.first.name).to eq 'Ward, P.S.'
+      expect(author_names.second.name).to eq 'Bolton, B.'
     end
 
     it "handles 'the Andres'" do
-      author_data = described_class['Andre, Edm.; Andre, Ern.']
-      expect(author_data.first.name).to eq 'Andre, Edm.'
-      expect(author_data.second.name).to eq 'Andre, Ern.'
+      author_names = described_class['Andre, Edm.; Andre, Ern.']
+      expect(author_names.first.name).to eq 'Andre, Edm.'
+      expect(author_names.second.name).to eq 'Andre, Ern.'
     end
 
     it "handles invalid input" do
-      author_data = described_class[' ; ']
-      expect(author_data).to eq []
+      author_names = described_class[' ; ']
+      expect(author_names).to eq []
     end
 
     it "handles a semicolon followed by a space at the end" do
-      author_data = described_class['Ward, P. S.; ']
-      expect(author_data.size).to eq 1
-      expect(author_data.first.name).to eq 'Ward, P. S.'
+      author_names = described_class['Ward, P. S.; ']
+      expect(author_names.size).to eq 1
+      expect(author_names.first.name).to eq 'Ward, P. S.'
     end
   end
 end
