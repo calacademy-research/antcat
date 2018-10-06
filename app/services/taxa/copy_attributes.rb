7a95d978d4d12e737ccaebd185f920813659780f
jonkerz <jonkerz@gmail.com>
2018-10-06 22:30:50 +0200

    feat: Add service for copying taxon attributes

---
+++ app/services/taxa/copy_attributes.rb
@@ -0,0 +1,58 @@
+# See also Taxa::AttributesForNewUsage`.
+
+module Taxa
+  class CopyAttributes
+    include Service
+
+    def initialize taxon
+      @taxon = taxon
+    end
+
+    def call
+      taxon.slice(attributes_to_copy)
+    end
+
+    private
+
+      attr_reader :taxon
+
+      # Not copied:
+      #  :name (and cached :name_cache, :name_html_cache)
+      #  :hol_id
+      #  :origin
+      #
+      #  Taxonomic relationships:
+      #    :subfamily,
+      #    :tribe,
+      #    :genus,
+      #    :species,
+      #    :subgenus,
+      #    :name,
+      #    :family
+      #
+      # In the order the appear in `schema.rb`.
+      def attributes_to_copy
+        [
+          :fossil,
+          :status,
+          :homonym_replaced_by_id,
+          :incertae_sedis_in,
+          :protonym_id,
+          :type_taxt,
+          :headline_notes_taxt,
+          :hong,
+          :type_name_id,
+          :genus_species_header_notes_taxt,
+          :type_fossil,
+          :unresolved_homonym,
+          :current_valid_taxon_id,
+          :ichnotaxon,
+          :nomen_nudum,
+          :biogeographic_region,
+          :primary_type_information,
+          :secondary_type_information,
+          :type_notes
+        ]
+      end
+  end
+end
