b70f7a63ea8b326793fa28bbd46e8b2448ca60b7
jonkerz <jonkerz@gmail.com>
2018-10-07 06:55:14 +0200

    tweak: All species and subspecies have genera

--- app/decorators/taxon_decorator/link_each_epithet.rb
+++ app/decorators/taxon_decorator/link_each_epithet.rb
@@ -11,7 +11,7 @@ class TaxonDecorator::LinkEachEpithet
   def call
     return @taxon.decorate.link_to_taxon unless @taxon.is_a? SpeciesGroupTaxon
 
-    string = genus_link_or_blank_string @taxon
+    string = genus_link @taxon
 
     if @taxon.is_a? Species
       return string << header_link(@taxon, @taxon.name.epithet_html.html_safe)
@@ -32,9 +32,7 @@ class TaxonDecorator::LinkEachEpithet
 
   private
 
-    def genus_link_or_blank_string taxon
-      return "".html_safe unless taxon.genus
-
+    def genus_link taxon
       # Link name of the genus, but add dagger per to taxon's fossil status.
       label = taxon.genus.name.to_html_with_fossil @taxon.fossil?
       taxon.genus.decorate.link_to_taxon_with_label(label.html_safe) << " "
