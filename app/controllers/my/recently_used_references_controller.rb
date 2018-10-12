58d827403564b67bb3796e7677dc06766d93ae7c
jonkerz <jonkerz@gmail.com>
2018-10-13 01:15:12 +0200

    feat: Remember 15 most recently used references (taxt fields only)

---
+++ app/controllers/my/recently_used_references_controller.rb
@@ -0,0 +1,26 @@
+module My
+  class RecentlyUsedReferencesController < ApplicationController
+    NUMBER_OF_RECENT_REFERENCES = 15
+
+    def index
+      render json: Autocomplete::FormatLinkableReferences[recently_used_references]
+    end
+
+    def create
+      update_most_recently_used_references!
+      head :ok
+    end
+
+    private
+
+      def recently_used_references
+        Reference.find(session[:recently_used_reference_ids] || [])
+      end
+
+      def update_most_recently_used_references!
+        reference_ids = session[:recently_used_reference_ids] || []
+        reference_ids.prepend params[:id]
+        session[:recently_used_reference_ids] = reference_ids.uniq[0...NUMBER_OF_RECENT_REFERENCES]
+      end
+  end
+end
