8ba9161ba2f45bfd3611a419e6ffa86020dcec37
jonkerz <jonkerz@gmail.com>
2018-11-12 22:29:00 +0100

    performance: Show less items

--- app/controllers/reference_sections_controller.rb
+++ app/controllers/reference_sections_controller.rb
@@ -6,7 +6,7 @@ class ReferenceSectionsController < ApplicationController
   def index
     @reference_sections = ReferenceSection.all
     @reference_sections = @reference_sections.search_objects(search_params) if params[:q].present?
-    @reference_sections = @reference_sections.includes(taxon: [:name]).paginate(page: params[:page], per_page: 100)
+    @reference_sections = @reference_sections.includes(taxon: [:name]).paginate(page: params[:page], per_page: 30)
   end
 
   def show
