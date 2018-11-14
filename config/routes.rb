e6f63cb9e7b679641d0b120363e5a067644e4fc0
jonkerz <jonkerz@gmail.com>
2018-11-14 21:33:31 +0100

    refactor: Namespace `ConvertToSubspeciesController`

--- config/routes.rb
+++ config/routes.rb
@@ -118,9 +118,9 @@ Rails.application.routes.draw do
         put :reverse_synonymy
       end
     end
-    resource :convert_to_subspecies, only: [:new, :create]
     scope module: :taxa do
       resource :create_combination, only: [:new, :show]
+      resource :convert_to_subspecies, only: [:new, :create]
       resource :force_parent_change, only: [:show, :create]
       resource :elevate_to_species, only: [:create]
       resource :create_obsolete_combination, only: [:show, :create]
