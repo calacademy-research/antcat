988b279b3a7e1a2abbbd7bfcdea42befd9a67ab5
jonkerz <jonkerz@gmail.com>
2019-03-05 18:12:03 +0100

    feat: Add db script 'non_standard_reference_sections'

---
+++ app/database_scripts/database_scripts/non_standard_reference_sections.rb
@@ -0,0 +1,47 @@
+module DatabaseScripts
+  class NonStandardReferenceSections < DatabaseScript
+    def results
+      non_standard = []
+
+      ReferenceSection.find_each do |reference_section|
+        next unless reference_section.references_taxt
+
+        reference_section.references_taxt.split(';').each do |content|
+          case content.scan("{ref ").size
+          when 0
+            non_standard << [reference_section, content, 'no reference']
+          when 2..99
+            non_standard << [reference_section, content, 'multiple references']
+          end
+        end
+      end
+
+      non_standard
+    end
+
+    def render
+      as_table do |t|
+        t.header :reference_section, :taxon, :content, :issue
+        t.rows do |reference_section, content, issue|
+          [
+            reference_section_link(reference_section.id),
+            markdown_taxon_link(reference_section.taxon),
+            content,
+            issue
+          ]
+        end
+      end
+    end
+
+    private
+
+      def reference_section_link id
+        "<a href='/reference_sections/#{id}>/edit'>#{id}</a>"
+      end
+  end
+end
+
+__END__
+description: See %github452 for why we want this.
+tags: [new!]
+topic_areas: [catalog]
