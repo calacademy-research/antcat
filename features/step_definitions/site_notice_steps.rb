dfea53ba0cf4a2ef5de8ef02d6fc8caca8d24a54
jonkerz <jonkerz@gmail.com>
2019-03-05 21:30:16 +0100

    tweak: Rename to `current_cucumber_user`

--- features/step_definitions/site_notice_steps.rb
+++ features/step_definitions/site_notice_steps.rb
@@ -1,7 +1,7 @@
 Given("there is a( new) site notice I haven't read yet") do
   sleep 1 # To please the `unread` gem which uses timestamps.
   create :site_notice, title: "A Site Notice", message: "Cash is King, that's the message."
-  expect(SiteNotice.unread_by(@user)).to be_present
+  expect(SiteNotice.unread_by(@current_cucumber_user)).to be_present
 end
 
 Then("I should see an unread site notice") do
