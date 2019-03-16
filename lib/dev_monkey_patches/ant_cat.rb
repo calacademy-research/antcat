8790973ddd476c4f43574526639850da03e773f2
jonkerz <jonkerz@gmail.com>
2019-03-16 02:16:49 +0100

    monkey-patch: Dev links for reference sections

--- lib/dev_monkey_patches/ant_cat.rb
+++ lib/dev_monkey_patches/ant_cat.rb
@@ -1,12 +1,30 @@
 module DevMonkeyPatches::AntCat
   def self.patch!
     ::Taxon.include Taxon
+    ::ReferenceSection.include ReferenceSection
   end
 
   module Taxon
     def dev_dev_link localhost: false
       host = localhost ? "localhost:3000" : "antcat.org"
       link = "http://#{host}/catalog/#{id}?#{name_cache.tr(' ', '_')}"
+      def link.open
+        `xdg-open "#{self}"`
+      end
+      link
+    end
+    alias_method :l, :dev_dev_link
+
+    def dev_dev_link_localhost
+      dev_dev_link localhost: true
+    end
+    alias_method :ll, :dev_dev_link_localhost
+  end
+
+  module ReferenceSection
+    def dev_dev_link localhost: false
+      host = localhost ? "localhost:3000" : "antcat.org"
+      link = "http://#{host}/reference_sections/#{id}/edit"
       def link.open
         `xdg-open "#{self}"`
       end
