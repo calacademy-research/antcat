017013851d3bd5e33c7c7e23eed7a1ba0be1d5e7
jonkerz <jonkerz@gmail.com>
2019-03-20 18:16:27 +0100

    tweak: Link protonyms in db script

--- app/database_scripts/database_scripts/orphaned_protonyms.rb
+++ app/database_scripts/database_scripts/orphaned_protonyms.rb
@@ -1,17 +1,21 @@
 module DatabaseScripts
   class OrphanedProtonyms < DatabaseScript
+    include Rails.application.routes.url_helpers
+    include ActionView::Helpers::UrlHelper
+
     def results
       Protonym.where("id NOT IN (SELECT protonym_id FROM taxa)")
     end
 
     def render
       as_table do |t|
-        t.header :protonym, :id, :created_at, :updated_at
+        t.header :id, :protonym, :search_link, :created_at, :updated_at
 
         t.rows do |protonym|
           [
-            protonym_name_with_search_link(protonym),
             protonym.id,
+            link_to(protonym.decorate.format_name, protonym_path(protonym)),
+            protonym_name_with_search_link(protonym),
             protonym.created_at,
             protonym.updated_at
           ]
@@ -24,7 +28,7 @@ module DatabaseScripts
       def protonym_name_with_search_link protonym
         # rubocop:disable Lint/UriEscapeUnescape
         search_path = "/catalog/search/quick_search?&search_type=containing&qq="
-        label = protonym.name.protonym_with_fossil_html protonym.fossil
+        label = 'Search'
         "<a href='#{search_path}#{URI.encode(protonym.name.name, /\W/)}'>#{label}</a>"
         # rubocop:enable Lint/UriEscapeUnescape
       end
