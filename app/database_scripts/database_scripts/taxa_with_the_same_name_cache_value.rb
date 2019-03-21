fe21e600fe29df1f75a5c585401e13608fd37dd6
jonkerz <jonkerz@gmail.com>
2019-03-21 18:38:09 +0100

    tweak: Tag db scripts as lists

--- app/database_scripts/database_scripts/taxa_with_the_same_name_cache_value.rb
+++ app/database_scripts/database_scripts/taxa_with_the_same_name_cache_value.rb
@@ -37,4 +37,5 @@ description: >
   [Taxa with the same name object](/database_scripts/taxa_with_the_same_name_object);
   open both and sort by count to see the difference.
 
+tags: [list]
 topic_areas: [catalog]
