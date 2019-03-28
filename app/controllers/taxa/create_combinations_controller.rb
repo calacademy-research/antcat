b9392165ece1455de59ddea895797fdb8b06b53b
jonkerz <jonkerz@gmail.com>
2019-03-28 20:29:04 +0100

    feat: Replace "Create combination" feature with a help page
    Minimal commit to make tests pass. Unused code will be cleaned up in the next commit.

--- app/controllers/taxa/create_combinations_controller.rb
+++ app/controllers/taxa/create_combinations_controller.rb
@@ -1,6 +1,5 @@
 module Taxa
   class CreateCombinationsController < ApplicationController
-    before_action :ensure_can_edit_catalog
     before_action :set_taxon
     before_action :set_new_parent, only: :show
 
@@ -14,11 +13,7 @@ module Taxa
         return
       end
 
-      @options = if @taxon.is_a? Subspecies
-                   @new_parent.parent.find_subspecies_in_genus(@taxon.name.epithet)
-                 else
-                   @new_parent.find_epithet_in_genus(@taxon.name.epithet)
-                 end
+      @possibly_existing_combinations = possibly_existing_combinations
     end
 
     private
@@ -29,6 +24,14 @@ module Taxa
 
       def set_new_parent
         @new_parent = Taxon.find_by(id: params[:new_parent_id])
+      end
+
+      def possibly_existing_combinations
+        if @taxon.is_a? Subspecies
+          @new_parent.parent.find_subspecies_in_genus(@taxon.name.epithet)
+        else
+          @new_parent.find_epithet_in_genus(@taxon.name.epithet)
+        end
       end
 
       def target_rank
