b8c8ec8305d6d717dcb268fbc692f185412a56ac
jonkerz <jonkerz@gmail.com>
2019-03-29 03:01:32 +0100

    remove: `Taxon#junior_synonyms_recursive`
    As it can lead to infinite loops.

--- spec/models/concerns/taxa/synonyms_spec.rb
+++ spec/models/concerns/taxa/synonyms_spec.rb
@@ -1,51 +1,6 @@
 require "spec_helper"
 
 describe Taxa::Synonyms do
-  describe "#junior_synonyms_recursive" do
-    let(:taxon) { create :family }
-
-    context "when there are no `junior_synonyms`" do
-      specify { expect(taxon.junior_synonyms_recursive).to be_empty }
-    end
-
-    context "when there are direct junior_synonyms" do
-      let(:junior_synonym) { create :family }
-      let(:another_junior_synonym) { create :family }
-
-      before do
-        create :synonym, senior_synonym: taxon, junior_synonym: junior_synonym
-        create :synonym, senior_synonym: taxon, junior_synonym: another_junior_synonym
-      end
-
-      specify do
-        expect(taxon.junior_synonyms_recursive).to eq [junior_synonym, another_junior_synonym]
-      end
-    end
-
-    context "when there are nested `junior_synonyms`" do
-      let(:junior_synonym) { create :family }
-      let(:nested_junior_synonym) { create :family }
-      let(:deeply_nested_junior_synonym) { create :family }
-      let(:another_deeply_nested_junior_synonym) { create :family }
-
-      before do
-        create :synonym, senior_synonym: taxon, junior_synonym: junior_synonym
-        create :synonym, senior_synonym: junior_synonym, junior_synonym: nested_junior_synonym
-        create :synonym, senior_synonym: nested_junior_synonym, junior_synonym: deeply_nested_junior_synonym
-        create :synonym, senior_synonym: nested_junior_synonym, junior_synonym: another_deeply_nested_junior_synonym
-      end
-
-      specify do
-        expect(taxon.junior_synonyms_recursive).to eq [
-          junior_synonym,
-          nested_junior_synonym,
-          deeply_nested_junior_synonym,
-          another_deeply_nested_junior_synonym
-        ]
-      end
-    end
-  end
-
   describe "deleting synonyms when status changed" do
     let(:senior) { create :family }
     let(:junior) { create :family, :synonym }
