f50bcb3c70734faaacbcd54ff733685cc1e8fdd2
jonkerz <jonkerz@gmail.com>
2019-04-01 01:25:55 +0200

    tweak: Don't sanitize db script descriptions

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,7 +1,7 @@
 module MarkdownHelper
-  def markdown content, no_wrapping_p: false
+  def markdown content, no_wrapping_p: false, sanitize_content: true
     return '' unless content
     return Markdowns::RenderWithoutWrappingP[content.dup] if no_wrapping_p
-    Markdowns::Render[content.dup]
+    Markdowns::Render[content.dup, sanitize_content: sanitize_content]
   end
 end
