5b3b52cae03f2e29017eefb4fc770e09691b5881
jonkerz <jonkerz@gmail.com>
2019-04-06 00:11:19 +0200

    refactor: Shorter with delegation (reference_has_document)

--- app/models/references/reference_has_document.rb
+++ app/models/references/reference_has_document.rb
@@ -4,8 +4,4 @@ class Reference < ApplicationRecord
   accepts_nested_attributes_for :document, reject_if: :all_blank
 
   delegate :url, :downloadable?, to: :document, allow_nil: true
-
-  def document_host= host
-    document.host = host if document
-  end
 end
--- spec/models/references/reference_has_document_spec.rb
+++ spec/models/references/reference_has_document_spec.rb
@@ -3,44 +3,6 @@ require 'spec_helper'
 describe Reference do
   it { is_expected.to have_one :document }
 
-  describe "#downloadable?" do
-    context "with a document" do
-      it "delegates to its document" do
-        reference = create :reference, document: create(:reference_document)
-
-        expect(reference.document).to receive :downloadable?
-        reference.downloadable?
-      end
-    end
-
-    context "without a document" do
-      it "returns false" do
-        expect(build_stubbed(:reference)).not_to be_downloadable
-      end
-    end
-  end
-
-  describe "#url" do
-    it "is be nil if there is no document" do
-      expect(build_stubbed(:reference).url).to be_nil
-    end
-
-    it "delegates to its document" do
-      reference = create :reference, document: create(:reference_document)
-      expect(reference.document).to receive :url
-      reference.url
-    end
-  end
-
-  describe "#document_host=" do
-    it "doesn't crash if there is no document" do
-      build_stubbed(:reference).document_host = 'localhost'
-    end
-
-    it "delegates to its document" do
-      reference = create :reference, document: create(:reference_document)
-      expect(reference.document).to receive :host=
-      reference.document_host = 'localhost'
-    end
-  end
+  it { is_expected.to delegate_method(:url).to(:document).allow_nil }
+  it { is_expected.to delegate_method(:downloadable?).to(:document).allow_nil }
 end
