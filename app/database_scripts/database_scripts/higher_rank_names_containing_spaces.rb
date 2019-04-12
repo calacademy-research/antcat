518c9120bfd6b515e8baf6a7a8f68f7bf37375a2
jonkerz <jonkerz@gmail.com>
2019-04-13 01:58:07 +0200

    db-script: Higher rank names containing spaces

---
+++ app/database_scripts/database_scripts/higher_rank_names_containing_spaces.rb
@@ -0,0 +1,35 @@
+module DatabaseScripts
+  class HigherRankNamesContainingSpaces < DatabaseScript
+    include Rails.application.routes.url_helpers
+    include ActionView::Helpers::UrlHelper
+
+    def results
+      Name.where.not(type: ["SubgenusName", "SpeciesName", "SubspeciesName"]).where("name LIKE ?", "% %")
+    end
+
+    def render
+      as_table do |t|
+        t.header :id, :name, :name_type, :orphaned?
+        t.rows do |name|
+          [
+            name.id,
+            link_to(name.name_html, name_path(name)),
+            name.type,
+            ('Yes' if name.orphaned?)
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+description: >
+  Names excluding subgenus-, species- and subspecies names that contain spaces.
+
+
+  It is not possible to fix these names without using scripts. As of writing, all names here
+  are subspecies names miscategorized as genus names; these will be fixed by script.
+
+tags: [new!]
+topic_areas: [names]
