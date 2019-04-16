ddf71cebd08e49853b8687de00bedf7ff6507b58
jonkerz <jonkerz@gmail.com>
2019-04-16 18:03:55 +0200

    tweak: link_each_epithet

--- app/decorators/taxon_decorator/link_each_epithet.rb
+++ app/decorators/taxon_decorator/link_each_epithet.rb
@@ -11,31 +11,29 @@ class TaxonDecorator::LinkEachEpithet
   def call
     return @taxon.decorate.link_to_taxon unless @taxon.is_a? SpeciesGroupTaxon
 
-    string = genus_link @taxon
-
     if @taxon.is_a? Species
-      return string << header_link(@taxon, @taxon.name.epithet_html.html_safe)
+      return genus_link << header_link(@taxon, @taxon.name.epithet_html.html_safe)
     end
 
     species = @taxon.species
 
+    string = genus_link
     string << if species
-             header_link(species, species.name.epithet_html.html_safe)
-           else
-             no_species_warning
-           end
+                header_link(species, species.name.epithet_html.html_safe)
+              else
+                no_species_warning
+              end
     string << ' '.html_safe
     string << header_link(@taxon, italicize(@taxon.name.subspecies_epithets))
-
     string
   end
 
   private
 
-    def genus_link taxon
+    def genus_link
       # Link name of the genus, but add dagger per to taxon's fossil status.
-      label = taxon.genus.name.name_with_fossil_html @taxon.fossil?
-      taxon.genus.decorate.link_to_taxon_with_label(label.html_safe) << " "
+      label = @taxon.genus.name.name_with_fossil_html @taxon.fossil?
+      @taxon.genus.decorate.link_to_taxon_with_label(label.html_safe) << " "
     end
 
     def header_link taxon, label
