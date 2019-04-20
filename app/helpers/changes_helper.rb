c473c22f66acf7074bd02f94b87f16beb287d2ff
jonkerz <jonkerz@gmail.com>
2019-04-20 22:43:19 +0200

    refactor: Inline one-off links

--- app/helpers/changes_helper.rb
+++ app/helpers/changes_helper.rb
@@ -1,16 +1,4 @@
 module ChangesHelper
-  def confirm_before_undo_button change
-    link_to 'Undo...', change_undos_path(change), class: "btn-warning btn-tiny"
-  end
-
-  def approve_all_changes_button
-    return unless user_is_superadmin?
-
-    link_to append_superadmin_icon("Approve all"), approve_all_changes_path,
-      method: :put, class: "btn-warning",
-      data: { confirm: "Are you sure you want to approve all changes?" }
-  end
-
   # TODO copy-pasted from `ChangesDecorator`.
   def format_change_type_verb change_type
     case change_type
