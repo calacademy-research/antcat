4f4d8923fb61c4880db3236312c972bb92ff8a53
jonkerz <jonkerz@gmail.com>
2019-05-11 01:04:54 +0200

    remove: Unused code for implicitly creating activities

--- app/models/concerns/trackable_actions/create.rb
+++
@@ -1,7 +0,0 @@
-module TrackableActions::Create
-  extend ActiveSupport::Concern
-
-  included do
-    after_create { create_activity :create }
-  end
-end
--- app/models/concerns/trackable_actions/update.rb
+++
@@ -1,7 +0,0 @@
-module TrackableActions::Update
-  extend ActiveSupport::Concern
-
-  included do
-    after_update { create_activity :update }
-  end
-end
