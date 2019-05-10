16cf4e5833d1aad720194148419ea8243f6f74cf
jonkerz <jonkerz@gmail.com>
2019-05-11 00:49:45 +0200

    refactor: Move registrations_controller

---
+++ spec/controllers/my/registrations_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe My::RegistrationsController do
+  # To fix:
+  #   Could not find devise mapping for path "/my/users".
+  #   ...
+  #   2) You are testing a Devise controller bypassing the router.
+  #      If so, you can explicitly tell Devise which mapping to use ...
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  describe "POST create" do
+    let!(:user_params) do
+      {
+        email: 'pizza@example.com"',
+        name: 'Quintus Batiatus',
+        password: 'secret123',
+        password_confirmation: 'secret123'
+      }
+    end
+
+    it 'creates a user' do
+      expect { post(:create, params: { user: user_params }) }.to change { User.count }.by(1)
+
+      user = User.last
+      expect(user.email).to eq user_params[:email]
+      expect(user.name).to eq user_params[:name]
+      expect(user.helper).to eq false
+      expect(user.editor).to eq false
+      expect(user.superadmin).to eq false
+    end
+
+    context 'when submitting unpermitted attributes' do
+      let!(:with_unpermitted_params) do
+        user_params.merge(helper: true, editor: true, superadmin: true)
+      end
+
+      it 'ignores them' do
+        expect { post(:create, params: { user: with_unpermitted_params }) }.to change { User.count }.by(1)
+
+        user = User.last
+        expect(user.helper).to eq false
+        expect(user.editor).to eq false
+        expect(user.superadmin).to eq false
+      end
+    end
+  end
+end
