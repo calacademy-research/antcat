7d91732ad0a2a70dd0923c584c37ab8c8c607ba6
jonkerz <jonkerz@gmail.com>
2019-05-11 08:58:00 +0200

    tweak: More functional

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,7 +1,7 @@
 module LayoutsHelper
   # For namespacing stylesheet assets.
-  def current_controller_css_id
-    "#{params[:controller].tr('/', '_')}-controller"
+  def current_controller_css_id controller_name
+    "#{controller_name.tr('/', '_')}-controller"
   end
 
   # The <title> tag which is shown on all pages.
