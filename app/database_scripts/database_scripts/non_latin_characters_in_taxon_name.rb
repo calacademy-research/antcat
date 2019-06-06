dd41d249e9a10c641ff891414dae0e2d27a502dc
jonkerz <jonkerz@gmail.com>
2019-06-06 23:52:42 +0200

    feat: Update db script non_latin_characters_in_taxon_name

--- app/database_scripts/database_scripts/non_latin_characters_in_taxon_name.rb
+++ app/database_scripts/database_scripts/non_latin_characters_in_taxon_name.rb
@@ -1,14 +1,13 @@
-# https://github.com/calacademy-research/antcat/issues/168
-
 module DatabaseScripts
   class NonLatinCharactersInTaxonName < DatabaseScript
     def results
-      Taxon.where("name_cache REGEXP ?", "[^A-Za-z -\\(\)]")
+      Taxon.where("name_cache REGEXP ?", "[^-A-Za-z \(\)]")
     end
   end
 end
 
 __END__
-description: See also %github168.
-tags: [list]
+description: >
+  Valid characters: A-Z, a-z, `(`, `)`
+tags: [list, updated!]
 topic_areas: [names]
