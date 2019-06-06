b2ce02e782d38c8dbdf3f3c232534367241d96ae
jonkerz <jonkerz@gmail.com>
2019-06-07 00:03:37 +0200

    db-script: Merge 2 scripts into taxa_with_same_name

---
+++ app/database_scripts/database_scripts/taxa_with_same_name.rb
@@ -0,0 +1,27 @@
+module DatabaseScripts
+  class TaxaWithSameName < DatabaseScript
+    def results
+      same_name = Taxon.joins(:name).group('names.name').having('COUNT(*) > 1')
+
+      Taxon.joins(:name).where(names: { name: same_name.select(:name) }).order('names.name')
+    end
+
+    def render
+      as_table do |t|
+        t.header :taxon, :authorship, :status
+        t.rows do |taxon|
+          [
+            markdown_taxon_link(taxon),
+            taxon.authorship_reference.decorate.expandable_reference,
+            taxon.status
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+topic_areas: [catalog]
+tags: [list, updated!]
