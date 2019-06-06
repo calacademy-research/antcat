8bb14e5f239211b6899eab270b42df5cdf765a70
jonkerz <jonkerz@gmail.com>
2019-06-07 01:00:13 +0200

    feat: Link related activities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -40,6 +40,18 @@ module ApplicationHelper
     link_to label, url, class: 'pdf-link'
   end
 
+  def activities_link_for_trackable trackable
+    activities_link trackable.class.name, trackable.id
+  end
+
+  def activities_link trackable_type, trackable_id
+    return unless trackable_type
+
+    type = trackable_type.constantize.base_class
+    url = activities_path(trackable_type: type, trackable_id: trackable_id)
+    link_to (antcat_icon("filter") + 'Activities'), url, class: "btn-normal filter-activities-link"
+  end
+
   def beta_label
     content_tag :span, "beta", class: "rounded-badge"
   end
