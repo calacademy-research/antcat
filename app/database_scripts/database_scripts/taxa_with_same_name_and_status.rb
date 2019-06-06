8f495dc8c5e5401c64bad06a9bc4101dcdd88702
jonkerz <jonkerz@gmail.com>
2019-06-07 00:06:02 +0200

    tweak: db script taxa_with_same_name_and_status

--- app/database_scripts/database_scripts/taxa_with_same_name_and_status.rb
+++ app/database_scripts/database_scripts/taxa_with_same_name_and_status.rb
@@ -1,42 +1,28 @@
 module DatabaseScripts
   class TaxaWithSameNameAndStatus < DatabaseScript
     def results
-      Taxon.find_by_sql(<<-SQL)
-        SELECT name_cache,
-          GROUP_CONCAT(id SEPARATOR ',') AS grouped_ids,
-          GROUP_CONCAT(status SEPARATOR ',') AS grouped_statuses
-        FROM taxa
-        GROUP BY name_cache, status
-        HAVING COUNT(*) > 1
-      SQL
+      name_and_status = Taxon.joins(:name).group('names.name, status').having('COUNT(*) > 1')
+
+      Taxon.joins(:name).
+        where(names: { name: name_and_status.select(:name) }, status: name_and_status.select(:status)).
+        order('names.name')
     end
 
     def render
       as_table do |t|
-        t.header :name_cache, :taxa
-
+        t.header :taxon, :authorship, :status
         t.rows do |taxon|
-          ids = taxon.grouped_ids.split(",")
-          statuses = taxon.grouped_statuses.split(",")
-          taxa = ids.zip statuses
-
-          [taxon.name_cache, taxa_links_with_status(taxa)]
+          [
+            markdown_taxon_link(taxon),
+            taxon.authorship_reference.decorate.expandable_reference,
+            taxon.status
+          ]
         end
       end
     end
-
-    private
-
-      def taxa_links_with_status taxa
-        list = "<ul class='no-bullet'>"
-        list << taxa.map do |id, status|
-          "<li>%taxon#{id} (##{id}, #{status})</li>"
-        end.join
-        list << "</ul>"
-      end
   end
 end
 
 __END__
 topic_areas: [catalog]
-tags: [list]
+tags: [list, updated!]
