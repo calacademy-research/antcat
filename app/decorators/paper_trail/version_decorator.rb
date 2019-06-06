c9ad27371da0f1566dc6ef5117a97766175251a6
jonkerz <jonkerz@gmail.com>
2019-06-07 00:07:55 +0200

    refactor: Extract into `PaperTrail::VersionDecorator`

---
+++ app/decorators/paper_trail/version_decorator.rb
@@ -0,0 +1,10 @@
+class PaperTrail::VersionDecorator < Draper::Decorator
+  delegate :item_type, :item_id
+
+  def revision_history_link
+    url = RevisionHistoryPath[item_type, item_id]
+    return unless url
+
+    h.link_to "History", url, class: "btn-normal btn-tiny"
+  end
+end
