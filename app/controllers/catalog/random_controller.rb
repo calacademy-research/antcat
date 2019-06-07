af872d8e96cb95e832d4413610679f941624876b
jonkerz <jonkerz@gmail.com>
2019-06-07 02:57:51 +0200

    fix: Brakeman SQL
    ```
    Confidence: High
    Category: SQL Injection
    Check: SQL
    Message: Possible SQL injection
    Code: ActiveRecord::Base.connection.execute("          SELECT taxa.id\n          FROM taxa\n          LIMIT 1\n          OFFSET #{rand(Taxon.count)}\n")
    File: app/controllers/catalog/random_controller.rb
    Line: 15
    ```

--- app/controllers/catalog/random_controller.rb
+++ app/controllers/catalog/random_controller.rb
@@ -8,13 +8,7 @@ module Catalog
 
       # TODO: Performance.
       def random_taxon_id
-        offset = rand Taxon.count
-        ActiveRecord::Base.connection.execute(<<~SQL).first.first
-          SELECT taxa.id
-          FROM taxa
-          LIMIT 1
-          OFFSET #{offset}
-        SQL
+        Taxon.order(Arel.sql('RAND()')).select(:id).first.id
       end
   end
 end
