4b6c39a266508c6e4a5f477894570daf19771fcd
jonkerz <jonkerz@gmail.com>
2019-06-11 05:51:29 +0200

    tweak: Memoize `Taxon#current_valid_taxon_including_synonyms`

--- app/models/concerns/taxa/synonyms.rb
+++ app/models/concerns/taxa/synonyms.rb
@@ -2,7 +2,7 @@ module Taxa::Synonyms
   extend ActiveSupport::Concern
 
   def current_valid_taxon_including_synonyms
-    Taxa::CurrentValidTaxonIncludingSynonyms[self]
+    @current_valid_taxon_including_synonyms ||= Taxa::CurrentValidTaxonIncludingSynonyms[self]
   end
 
   def junior_synonyms_with_own_id
