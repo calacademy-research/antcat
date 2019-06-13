984cbfbb721fd113da7486b6ec473ddc09991b0b
jonkerz <jonkerz@gmail.com>
2019-06-13 09:18:41 +0200

    db-script: Show ranks in protonyms_with_more_than_one_valid_taxon

--- app/database_scripts/database_scripts/protonyms_with_more_than_one_valid_taxon.rb
+++ app/database_scripts/database_scripts/protonyms_with_more_than_one_valid_taxon.rb
@@ -1,7 +1,23 @@
 module DatabaseScripts
   class ProtonymsWithMoreThanOneValidTaxon < DatabaseScript
+    include Rails.application.routes.url_helpers
+    include ActionView::Helpers::UrlHelper
+
     def results
       Protonym.joins(:taxa).where(taxa: { status: Status::VALID }).group(:protonym_id).having('COUNT(protonym_id) > 1')
+    end
+
+    def render
+      as_table do |t|
+        t.header :protonym, :authorship, :ranks_of_taxa
+        t.rows do |protonym|
+          [
+            link_to(protonym.decorate.format_name, protonym_path(protonym)),
+            protonym.authorship.reference.decorate.expandable_reference,
+            protonym.taxa.pluck(:type).join(', ')
+          ]
+        end
+      end
     end
   end
 end
