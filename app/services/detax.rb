7c45536eed41466067135414b83f9611c8954aba
jonkerz <jonkerz@gmail.com>
2019-06-15 05:48:47 +0200

    refactor: Rename to `Detax`

---
+++ app/services/detax.rb
@@ -0,0 +1,18 @@
+class Detax
+  include Service
+
+  def initialize taxt
+    @taxt = taxt.try :dup
+  end
+
+  # Parses "example {tax 429361}"
+  # into   "example <a href=\"/catalog/429361\">Melophorini</a>"
+  def call
+    return '' unless taxt
+    Markdowns::ParseAntcatHooks[taxt].html_safe
+  end
+
+  private
+
+    attr_reader :taxt
+end
