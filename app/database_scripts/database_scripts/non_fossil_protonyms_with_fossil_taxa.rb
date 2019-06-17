894acf7215a636393206c6dd485ed982a774723f
jonkerz <jonkerz@gmail.com>
2019-06-17 22:14:08 +0200

    db-scripts: Add scripts x 5

---
+++ app/database_scripts/database_scripts/non_fossil_protonyms_with_fossil_taxa.rb
@@ -0,0 +1,27 @@
+module DatabaseScripts
+  class NonFossilProtonymsWithFossilTaxa < DatabaseScript
+    def results
+      Protonym.where(fossil: false).joins(:taxa).where(taxa: { fossil: true }).includes(:taxa)
+    end
+
+    def render
+      as_table do |t|
+        t.header :protonym, :taxa
+        t.rows do |protonym|
+          [
+            link_to(protonym.decorate.format_name, protonym_path(protonym)),
+            protonym.taxa.map { |taxon| taxon.decorate.link_to_taxon }.join('<br>')
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+description: >
+  This is not necessarily incorrect.
+
+tags: [new!]
+topic_areas: [protonyms]
