06abd1f7d6d4b837adb7f69fa8e218ea34e7888c
jonkerz <jonkerz@gmail.com>
2019-06-20 07:34:35 +0200

    feat: Allow editors to delete unused author names

--- spec/controllers/author_names_controller_spec.rb
+++ spec/controllers/author_names_controller_spec.rb
@@ -2,19 +2,13 @@ require 'spec_helper'
 
 describe AuthorNamesController do
   describe "forbidden actions" do
-    context "when signed in as a user" do
-      before { sign_in create(:user) }
+    context "when signed in as a helper" do
+      before { sign_in create(:user, :helper) }
 
       specify { expect(get(:new, params: { author_id: 1 })).to have_http_status :forbidden }
       specify { expect(get(:edit, params: { id: 1 })).to have_http_status :forbidden }
       specify { expect(post(:create, params: { author_id: 1 })).to have_http_status :forbidden }
       specify { expect(post(:update, params: { id: 1 })).to have_http_status :forbidden }
-      specify { expect(delete(:destroy, params: { id: 1 })).to have_http_status :forbidden }
-    end
-
-    context "when signed in as an editor" do
-      before { sign_in create(:user, :editor) }
-
       specify { expect(delete(:destroy, params: { id: 1 })).to have_http_status :forbidden }
     end
   end
