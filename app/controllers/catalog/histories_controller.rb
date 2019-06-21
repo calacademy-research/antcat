24bc4db251c9da68f56186817abdfa7abcd792b4
jonkerz <jonkerz@gmail.com>
2019-06-21 08:14:58 +0200

    refactor: Use singular resource (taxon_history)

--- app/controllers/catalog/histories_controller.rb
+++ app/controllers/catalog/histories_controller.rb
@@ -1,5 +1,5 @@
 module Catalog
-  class HistoryController < ApplicationController
+  class HistoriesController < ApplicationController
     def show
       @comparer = Taxon.revision_comparer_for params[:id],
         params[:selected_id], params[:diff_with_id]
