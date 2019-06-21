69d4d61b7c9683b2d9c08e6570645fc41b98ef20
jonkerz <jonkerz@gmail.com>
2019-06-21 06:32:18 +0200

    tweaks: Micro

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,11 +1,9 @@
 module LayoutsHelper
-  # For namespacing stylesheet assets.
   def current_controller_css_id controller_name
     "#{controller_name.tr('/', '_')}-controller"
   end
 
-  # The <title> tag which is shown on all pages.
-  def make_title title
+  def title_tag title
     string = ''.html_safe
     string << "#{title} - " if title
     string << "AntCat"
