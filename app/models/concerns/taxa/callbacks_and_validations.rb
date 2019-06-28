210eef7c1c3a0232c462999a55c3e47079b23d27
jonkerz <jonkerz@gmail.com>
2019-06-28 20:02:34 +0200

    feat: Validate homonyms have a `homonym_replaced_by`

--- app/models/concerns/taxa/callbacks_and_validations.rb
+++ app/models/concerns/taxa/callbacks_and_validations.rb
@@ -10,6 +10,7 @@ module Taxa
       validates :status, inclusion: { in: Status::STATUSES }
       validates :incertae_sedis_in, inclusion: { in: INCERTAE_SEDIS_IN_RANKS, allow_nil: true }
       validates :homonym_replaced_by, absence: { message: "can't be set for non-homonyms" }, unless: -> { homonym? }
+      validates :homonym_replaced_by, presence: { message: "must be set for homonyms" }, if: -> { homonym? }
       validates :unresolved_homonym, absence: { message: "can't be set for homonyms" }, if: -> { homonym? }
       validates :nomen_nudum, absence: { message: "can only be set for unavailable taxa" }, unless: -> { unavailable? }
 
