acbbab927998bf61608e2fa08334e23ba5c2c771
jonkerz <jonkerz@gmail.com>
2019-07-02 17:44:26 +0200

    refactor: DRY references factories

--- spec/factories/references.rb
+++ spec/factories/references.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     transient do
       author_name {}
     end
+
     sequence(:title) { |n| "Ants are my life#{n}" }
     sequence(:citation_year) { |n| "201#{n}d" }
 
@@ -13,43 +14,35 @@ FactoryBot.define do
         reference.author_names << author_name
       end
     end
-  end
 
-  factory :article_reference do
-    author_names { [create(:author_name)] }
-    sequence(:title) { |n| "Ants are my life#{n}" }
-    sequence(:citation_year) { |n| "201#{n}d" }
-    journal
-    sequence(:series_volume_issue) { |n| n }
-    sequence(:pagination) { |n| n }
-  end
+    factory :article_reference, class: ArticleReference do
+      author_names { [create(:author_name)] }
+      journal
+      sequence(:series_volume_issue) { |n| n }
+      sequence(:pagination) { |n| n }
+    end
 
-  factory :book_reference do
-    author_names { [create(:author_name)] }
-    sequence(:title) { |n| "Ants are my life#{n}" }
-    sequence(:citation_year) { |n| "201#{n}d" }
-    publisher
-    sequence(:pagination) { |n| "#{n} pp." }
-  end
+    factory :book_reference, class: BookReference do
+      author_names { [create(:author_name)] }
+      publisher
+      sequence(:pagination) { |n| "#{n} pp." }
+    end
 
-  factory :unknown_reference do
-    author_names { [create(:author_name)] }
-    sequence(:title) { |n| "Ants are my life#{n}" }
-    sequence(:citation_year) { |n| "201#{n}d" }
-    citation { 'New York' }
-  end
+    factory :unknown_reference, class: UnknownReference do
+      author_names { [create(:author_name)] }
+      citation { 'New York' }
+    end
 
-  factory :missing_reference do
-    title { '(missing)' }
-    citation_year { '2009' }
-    citation { 'Latreille, 2009' }
-  end
+    factory :missing_reference, class: MissingReference do
+      title { '(missing)' }
+      citation_year { '2009' }
+      citation { 'Latreille, 2009' }
+    end
 
-  factory :nested_reference do
-    author_names { [create(:author_name)] }
-    sequence(:title) { |n| "Nested ants #{n}" }
-    sequence(:citation_year) { |n| "201#{n}d" }
-    pages_in { 'In: ' }
-    nesting_reference { create :book_reference }
+    factory :nested_reference, class: NestedReference do
+      author_names { [create(:author_name)] }
+      pages_in { 'In: ' }
+      nesting_reference { create :book_reference }
+    end
   end
 end
