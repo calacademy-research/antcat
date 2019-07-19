dbc8a84d9fb6fc04faef24481b04c43a0466c4be
jonkerz <jonkerz@gmail.com>
2019-07-19 21:16:40 +0200

    db-script: Add x 2 for references without PDFs

---
+++ app/database_scripts/database_scripts/references_without_pdfs.rb
@@ -0,0 +1,26 @@
+module DatabaseScripts
+  class ReferencesWithoutPdfs < DatabaseScript
+    def results
+      Reference.left_outer_joins(:document).where(reference_documents: { id: nil }).
+        no_missing.order_by_author_names_and_year
+    end
+
+    def render
+      as_table do |t|
+        t.header :reference
+        t.rows do |reference|
+          [
+            link_to(reference.keey, reference_path(reference))
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+description: >
+
+tags: [new!, slow]
+topic_areas: [references]
