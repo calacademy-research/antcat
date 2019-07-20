24795d80f585203243e13512f061ec2be5e992fc
jonkerz <jonkerz@gmail.com>
2019-07-20 08:24:32 +0200

    refactor: Convert `AuthorNameObserver` to a callback
    Slightly better than an observer.

--- app/models/observers/author_name_observer.rb
+++
@@ -1,7 +0,0 @@
-class AuthorNameObserver < ActiveRecord::Observer
-  def after_update author_name
-    references = author_name.references.reload
-    references.each &:refresh_author_names_caches
-    references.each &:invalidate_caches
-  end
-end
