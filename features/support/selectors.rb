ecffaa043c558f65f14c067c9c10cc7ece8961e1
jonkerz <jonkerz@gmail.com>
2019-08-24 02:10:21 +0200

    tweak: Simplify `features/support/selectors.rb`

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -2,19 +2,11 @@ module HtmlSelectorsHelpers
   def selector_for locator
     case locator
 
-    # TODO: Change this or wherever the "default element" is defined to
-    # "html > body #content" and use "I should see in the header" where required.
-    # It would make error messages easier to read and steps easier to read/write
-    # (less "I follow the first").
-    when /the page/
-      "html > body"
-
     # Catalog.
     when /^the taxon browser$/
       "#taxon_browser"
     when /^the (\w*) taxon browser tab$/
-      tab_title_target = find(:link, $1)[:href]
-      tab_title_target
+      find(:link, $1)[:href]
     when /the protonym/
       "#taxon_description .headline > span.name"
     when /the header/
