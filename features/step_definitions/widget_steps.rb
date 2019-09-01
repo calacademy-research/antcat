e805f3712d8bbe283b570406556d37e1551c6cae
jonkerz <jonkerz@gmail.com>
2019-09-01 20:00:21 +0200

    style: Fix RuboCop RSpec/AnyInstance

--- features/step_definitions/widget_steps.rb
+++
@@ -1,7 +0,0 @@
-# Reference selector.
-# HACK.
-Given("the reference selector returns {int} results per page") do |items_per_page|
-  allow_any_instance_of(Autocomplete::AutocompleteReferences).
-    to receive(:default_search_options).
-    and_return(reference_type: :nomissing, items_per_page: items_per_page)
-end
