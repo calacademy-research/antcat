c6042831b82c67900689e183fe6037c6a3d4b68d
jonkerz <jonkerz@gmail.com>
2019-10-04 22:11:02 +0200

    refactor: Create activity in controller (reorder_history_items_controller)

--- app/controllers/taxa/reorder_history_items_controller.rb
+++ app/controllers/taxa/reorder_history_items_controller.rb
@@ -4,7 +4,10 @@ module Taxa
     before_action :set_taxon
 
     def create
+      previous_ids = @taxon.history_items.pluck(:id)
+
       if Taxa::Operations::ReorderHistoryItems[@taxon, params[:taxon_history_item]]
+        create_activity previous_ids
         render json: { success: true }
       else
         render json: @taxon.errors, status: :unprocessable_entity
@@ -15,6 +18,11 @@ module Taxa
 
       def set_taxon
         @taxon = Taxon.find(params[:taxa_id])
+      end
+
+      def create_activity previous_ids
+        @taxon.create_activity :reorder_taxon_history_items,
+          parameters: { previous_ids: previous_ids, reordered_ids: @taxon.history_items.pluck(:id) }
       end
   end
 end
