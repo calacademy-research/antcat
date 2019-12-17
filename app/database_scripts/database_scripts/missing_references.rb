01fa920c06af25597194ba376b6a80e427504ec6
jonkerz <jonkerz@gmail.com>
2019-12-17 18:23:39 +0100

    vinegar: add notice about missing references

--- app/database_scripts/database_scripts/missing_references.rb
+++ app/database_scripts/database_scripts/missing_references.rb
@@ -6,7 +6,7 @@ module DatabaseScripts
 
     def render
       as_table do |t|
-        t.header :id, :reference, :what_link_here, :replace
+        t.header :id, :reference, :what_links_here, :replace
         t.rows do |reference|
           [
             link_to(reference.id, reference_path(reference)),
@@ -23,6 +23,25 @@ end
 __END__
 
 category: References
+
+description: >
+  "Missing references" is a type of incomplete reference where only the author's last name and/or year is known.
+  We want to replace them with proper references.
+
+
+  **How to fix**
+
+  Use the 'What Links Here' button too see where a missing reference is being used.
+
+
+  Once you know which reference it should be replaced with, use the 'Replace' button to replace all occurrences with a different reference.
+
+
+  If no replacement reference exits, you can [add a new reference to AntCat](/references/new).
+
+
+  To replace a single occurence, use the 'What Links Here' button, navigate to the item, and edit it.
+
 related_scripts:
   - MissingReferences
   - UnknownReferences
