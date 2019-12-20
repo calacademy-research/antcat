4fbe761730e500c15af6d474d7a65308cde2c310
Joe Russack <jrussack@calacademy.org>
2019-12-19 18:52:10 -0800

    Removing gemfile lock... nasty stuff.

--- docker/export_database.sh
+++ docker/export_database.sh
@@ -4,6 +4,7 @@ git fetch origin
 git fetch --all
 git reset --hard origin/master
 git pull
+rm ../Gemfile.lock
 perl -p -i -e "s/ruby \'2.3.3\'/ruby \'2.4.0\'/g" ../Gemfile
 docker-compose run antcat ./docker/download-database.sh
 docker-compose down
