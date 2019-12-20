f651dac0b2e894f6666b7a1c9a96b03f3fd15b71
Joe Russack <jrussack@calacademy.org>
2019-12-19 17:26:22 -0800

    That's one way to get an updated ruby

--- docker/export_database.sh
+++ docker/export_database.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 rm -rf ../db/schema.rb Gemfile Gemfile.lock
 git fetch --all
+perl -p -i -e "s/ruby \'2.3.3\'/ruby \'2.4.0\'/g" ../Gemfile
 #git pull
 docker-compose run antcat ./docker/download-database.sh
 docker-compose down
