f37fcb278795f83e9543aa67ea4169b18d0bbd5f
jonkerz <jonkerz@gmail.com>
2019-12-23 02:54:39 +0100

    tweak: simplify What Links Here check
    Less performant, but less complicated.

--- app/controllers/taxa_controller.rb
+++ app/controllers/taxa_controller.rb
@@ -46,7 +46,7 @@ class TaxaController < ApplicationController
   end
 
   def destroy
-    if @taxon.what_links_here predicate: true
+    if @taxon.what_links_here.present?
       redirect_to taxon_what_links_here_path(@taxon), alert: <<~MSG
         Other taxa refer to this taxon, so it can't be deleted.
         Please see the table on this page for items referring to it.
