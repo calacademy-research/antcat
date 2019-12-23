e278d81905a40ee50ec75626d6c16f6f4f3f837a
jonkerz <jonkerz@gmail.com>
2019-12-23 02:37:51 +0100

    tweaks: x 2

--- app/models/default_reference.rb
+++ app/models/default_reference.rb
@@ -1,9 +1,11 @@
 class DefaultReference
+  DEFAULT_REFERENCE_SESSION_KEY = :default_reference_id
+
   def self.get session
-    Reference.find_by(id: session[:default_reference_id])
+    Reference.find_by(id: session[DEFAULT_REFERENCE_SESSION_KEY])
   end
 
   def self.set session, reference
-    session[:default_reference_id] = reference.id
+    session[DEFAULT_REFERENCE_SESSION_KEY] = reference.id
   end
 end
