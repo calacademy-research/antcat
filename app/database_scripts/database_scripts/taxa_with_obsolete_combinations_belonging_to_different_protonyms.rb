bfb17369b8318fe27c0795fc0b7967d192cd2959
jonkerz <jonkerz@gmail.com>
2020-01-03 23:08:22 +0100

    db-scripts: split script and show warnings in catalog (obsolete combination protonyms)

---
+++ app/database_scripts/database_scripts/taxa_with_obsolete_combinations_belonging_to_different_protonyms.rb
@@ -0,0 +1,32 @@
+module DatabaseScripts
+  class TaxaWithObsoleteCombinationsBelongingToDifferentProtonyms < DatabaseScript
+    def results
+      current_valid_taxa_of_obsolete_combinations.joins(:obsolete_combinations).
+        where("taxa.protonym_id <> obsolete_combinations_taxa.protonym_id").distinct
+    end
+
+    private
+
+      def current_valid_taxa_of_obsolete_combinations
+        current_valid_taxon_ids = Taxon.obsolete_combinations.select(:current_valid_taxon_id)
+        Taxon.where(id: current_valid_taxon_ids)
+      end
+  end
+end
+
+__END__
+
+category: Catalog
+tags: []
+
+issue_description: This taxon has an obsolete combination that belongs to a different protonym.
+
+description: >
+  Click on a taxon to see the obsolete combinations in the catalog.
+
+
+  This script is the reverse of %dbscript:ObsoleteCombinationsWithProtonymsNotMatchingItsCurrentValidTaxonsProtonym
+
+related_scripts:
+  - ObsoleteCombinationsWithProtonymsNotMatchingItsCurrentValidTaxonsProtonym
+  - TaxaWithObsoleteCombinationsBelongingToDifferentProtonyms
