47910c54346bb6a438ffd8ef5024b2542ec490c4
jonkerz <jonkerz@gmail.com>
2020-01-07 09:24:07 +0100

    db-script: add ObsoleteCombinationsInRanksAboveSpecies

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -7,6 +7,7 @@ class Taxon < ApplicationRecord
 
   TYPES = %w[Family Subfamily Tribe Subtribe Genus Subgenus Species Subspecies Infrasubspecies]
   TYPES_ABOVE_GENUS = %w[Family Subfamily Subtribe Tribe]
+  TYPES_ABOVE_SPECIES = %w[Family Subfamily Tribe Subtribe Genus Subgenus]
   TAXA_FIELDS_REFERENCING_TAXA = [:subfamily_id, :tribe_id, :genus_id, :subgenus_id,
     :species_id, :homonym_replaced_by_id, :current_valid_taxon_id, :type_taxon_id]
   INCERTAE_SEDIS_IN_RANKS = [
