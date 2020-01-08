60e3de82339cae6700382fcb114a4206a324df0c
jonkerz <jonkerz@gmail.com>
2020-01-08 06:05:11 +0100

    feat(catalog): improve chilren page and link in More menu

--- app/controllers/taxa/children_controller.rb
+++ app/controllers/taxa/children_controller.rb
@@ -1,12 +1,10 @@
-# Show children on another page for performance reasons.
-# Example of a very slow page: http://localhost:3000/taxa/429244/edit
-
 module Taxa
   class ChildrenController < ApplicationController
     before_action :set_taxon
 
     def show
-      @children = @taxon.children.includes(:name).order(status: :desc, name_cache: :asc).paginate(per_page: 1000, page: params[:page])
+      @children = @taxon.children.includes(:name).order(status: :desc, name_cache: :asc).paginate(per_page: 100, page: params[:page])
+      @check_what_links_heres = params[:check_what_links_heres]
     end
 
     private
--- app/database_scripts/database_scripts/unavailable_misspellings.rb
+++ app/database_scripts/database_scripts/unavailable_misspellings.rb
@@ -6,7 +6,7 @@ module DatabaseScripts
 
     def render
       as_table do |t|
-        t.header :taxon, :status, :any_what_links_here_columns?, :any_what_links_here_taxts?, :any?, :suggested_action, :what_links_here
+        t.header :taxon, :status, :any_what_links_here_columns?, :any_what_links_here_taxts?, :any?, :suggested_action, :what_links_here, :children
         t.rows do |taxon|
           wlh_columns = Taxa::WhatLinksHereColumns[taxon, predicate: true]
           wlh_taxts = Taxa::WhatLinksHereTaxts[taxon, predicate: true]
@@ -19,7 +19,8 @@ module DatabaseScripts
             ('Yes' if wlh_taxts),
             ('Yes' if any_wlhs),
             ('Delete since no WLHs' unless any_wlhs),
-            link_to('What Links Here', taxon_what_links_here_path(taxon), class: 'btn-normal btn-tiny')
+            link_to('What Links Here', taxon_what_links_here_path(taxon), class: 'btn-normal btn-tiny'),
+            link_to('Children + check WHLs (slow)', taxa_children_path(taxon, check_what_links_heres: 'yes'), class: 'btn-normal btn-tiny')
           ]
         end
       end
