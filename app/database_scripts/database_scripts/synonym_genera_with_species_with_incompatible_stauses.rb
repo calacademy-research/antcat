9fedc99919a8ac61c39f615d05da9fc232a51372
jonkerz <jonkerz@gmail.com>
2020-01-08 10:54:12 +0100

    db-scripts: add x 3

---
+++ app/database_scripts/database_scripts/synonym_genera_with_species_with_incompatible_stauses.rb
@@ -0,0 +1,63 @@
+module DatabaseScripts
+  class SynonymGeneraWithSpeciesWithIncompatibleStauses < DatabaseScript
+    COMPATIBLE_STATUSES = [
+      Status::SYNONYM,
+      Status::HOMONYM,
+      Status::UNIDENTIFIABLE,
+      Status::UNAVAILABLE,
+      Status::EXCLUDED_FROM_FORMICIDAE,
+      Status::UNAVAILABLE_MISSPELLING,
+      Status::UNAVAILABLE_UNCATEGORIZED
+    ]
+
+    def results
+      Genus.where(status: Status::SYNONYM).joins(:species).where.not(species_taxa: { status: COMPATIBLE_STATUSES }).distinct
+    end
+
+    def render
+      as_table do |t|
+        t.header :taxon, :status, :unique_species_statuses, :incompatible_species
+        t.rows do |taxon|
+          taxa = taxon.species
+          incompatible_taxa = taxa.where.not(status: COMPATIBLE_STATUSES)
+
+          [
+            markdown_taxon_link(taxon),
+            taxon.status,
+            taxa.pluck(:status).uniq.join(', '),
+            taxa_list(incompatible_taxa)
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+category: Catalog
+tags: [new!]
+
+issue_description: This genus is a synonym with species that have incompatible statuses.
+
+description: >
+  Compatible statuses:
+
+
+  * synonym
+
+  * homonym
+
+  * unidentifiable
+
+  * unavailable
+
+  * excluded from Formicidae
+
+  * unavailable misspelling
+
+  * unavailable uncategorized
+
+related_scripts:
+  - SynonymGeneraWithSpeciesWithIncompatibleStauses
+  - SynonymSpeciesWithSubspeciesWithIncompatibleStauses
---
+++ app/database_scripts/database_scripts/synonym_species_with_subspecies_with_incompatible_stauses.rb
@@ -0,0 +1,63 @@
+module DatabaseScripts
+  class SynonymSpeciesWithSubspeciesWithIncompatibleStauses < DatabaseScript
+    COMPATIBLE_STATUSES = [
+      Status::SYNONYM,
+      Status::HOMONYM,
+      Status::UNIDENTIFIABLE,
+      Status::UNAVAILABLE,
+      Status::EXCLUDED_FROM_FORMICIDAE,
+      Status::UNAVAILABLE_MISSPELLING,
+      Status::UNAVAILABLE_UNCATEGORIZED
+    ]
+
+    def results
+      Species.where(status: Status::SYNONYM).joins(:subspecies).where.not(subspecies_taxa: { status: COMPATIBLE_STATUSES }).distinct
+    end
+
+    def render
+      as_table do |t|
+        t.header :taxon, :status, :unique_subspecies_statuses, :incompatible_subspecies
+        t.rows do |taxon|
+          taxa = taxon.subspecies
+          incompatible_taxa = taxa.where.not(status: COMPATIBLE_STATUSES)
+
+          [
+            markdown_taxon_link(taxon),
+            taxon.status,
+            taxa.pluck(:status).uniq.join(', '),
+            taxa_list(incompatible_taxa)
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+category: Catalog
+tags: [new!]
+
+issue_description: This species is a synonym with subspecies that have incompatible statuses.
+
+description: >
+  Compatible statuses:
+
+
+  * synonym
+
+  * homonym
+
+  * unidentifiable
+
+  * unavailable
+
+  * excluded from Formicidae
+
+  * unavailable misspelling
+
+  * unavailable uncategorized
+
+related_scripts:
+  - SynonymGeneraWithSpeciesWithIncompatibleStauses
+  - SynonymSpeciesWithSubspeciesWithIncompatibleStauses
