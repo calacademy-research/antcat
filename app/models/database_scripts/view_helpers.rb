330ceaeee5ed3250ac55a5f487c978e78f9a6ce5
jonkerz <jonkerz@gmail.com>
2020-01-08 11:45:58 +0100

    refactor: move to `DatabaseScripts::ViewHelpers`

--- app/models/database_scripts/view_helpers.rb
+++ app/models/database_scripts/view_helpers.rb
@@ -3,5 +3,18 @@ module DatabaseScripts
     def taxa_list taxa
       taxa.map(&:link_to_taxon).join('<br>')
     end
+
+    def markdown content
+      Markdowns::Render[content, sanitize_content: false]
+    end
+
+    def markdown_taxon_link taxon_or_id
+      return "" unless taxon_or_id
+      "%taxon#{taxon_or_id.try(:id) || taxon_or_id}"
+    end
+
+    def markdown_reference_link reference
+      "%reference#{reference.id}"
+    end
   end
 end
