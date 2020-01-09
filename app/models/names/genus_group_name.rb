ea7a86ca5f81cf780e76a6c828be77344d138a92
jonkerz <jonkerz@gmail.com>
2020-01-09 13:43:46 +0100

    refactor(name): italicization
    In cope of #774. Still "hmm" but a step in the right direction.

--- app/models/names/genus_group_name.rb
+++ app/models/names/genus_group_name.rb
@@ -1,15 +1,2 @@
 class GenusGroupName < Name
-  include Formatters::ItalicsHelper
-
-  def name_html
-    italicize name
-  end
-
-  def epithet_html
-    italicize epithet
-  end
-
-  def dagger_html
-    italicize super
-  end
 end
--- app/models/names/species_group_name.rb
+++ app/models/names/species_group_name.rb
@@ -1,24 +1,10 @@
 class SpeciesGroupName < Name
-  include Formatters::ItalicsHelper
-
-  def name_html
-    italicize name
-  end
-
-  def epithet_html
-    italicize epithet
-  end
-
   def genus_epithet
     name_parts[0]
   end
 
   def species_epithet
     name_parts[1]
-  end
-
-  def dagger_html
-    italicize super
   end
 
   private
