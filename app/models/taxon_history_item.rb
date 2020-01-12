eca11685c869d4a9d6a75c996ceec9783986a95f
jonkerz <jonkerz@gmail.com>
2020-01-12 08:17:12 +0100

    wip: start categorizing history items

--- app/models/taxon_history_item.rb
+++ app/models/taxon_history_item.rb
@@ -2,6 +2,54 @@ class TaxonHistoryItem < ApplicationRecord
   include Trackable
   include RevisionsCanBeCompared
 
+  # TODO: WIP, see https://github.com/calacademy-research/antcat/issues/779
+  VIRTUAL_HISTORY_ITEM_CANDIDATES = [
+    # Being worked on.
+    VHIC_SUBSPECIES_LIST = ['taxt LIKE ?', "%Current subspecies%"], # See https://github.com/calacademy-research/antcat/issues/780
+
+    # Good next candidates to work on.
+    VHIC_FORM_DESCRIPTION = ["taxt REGEXP ?", "^{ref [0-9]+}: [0-9]+ \\("], # Good candidate because it often appears first in the list, and no tax tags.
+    VHIC_SEE_ALSO =         ["taxt LIKE ?", "See also: {ref %"], # Good candiate because it ususally appears last in the list, and no tax tags.
+
+    # Synonym of.
+    VHIC_SYNONYM_OF =                    ["taxt LIKE ?", "Synonym of%"], # TODO: These should probably be "Junior synonym of ...".
+    VHIC_SENIOR_SYNONYM_OF =             ["taxt LIKE ?", "Senior synonym of%"],
+    VHIC_JUNIOR_SYNONYM_OF =             ["taxt LIKE ?", "Junior synonym of%"],
+    VHIC_PROVISIONAL_JUNIOR_SYNONYM_OF = ["taxt LIKE ?", "Provisional junior synonym o%"],
+
+    # Homonym of.
+    VHIC_JUNIOR_PRIMARY_HOMONYM_OF =              ["taxt LIKE ?", "[Junior primary homonym of%"],
+    VHIC_JUNIOR_SECONDARY_HOMONYM =               ["taxt LIKE ?", "[Junior secondary homonym%"],
+    VHIC_UNRESOLVED_JUNIOR_PRIMARY_HOMONYM_OF =   ["taxt LIKE ?", "[Unresolved junior primary homonym of%"],
+    VHIC_UNRESOLVED_JUNIOR_SECONDARY_HOMONYM_OF = ["taxt LIKE ?", "[Unresolved junior secondary homonym of%"],
+
+    # Placement/raised/revived/etc.
+    VHIC_SUBSPECIES_OF =             ["taxt LIKE ?", "Subspecies of%"],
+    VHIC_RAISED_TO_SPECIES =         ["taxt LIKE ?", "Raised to species%"],
+    VHIC_REVIVED_STATUS_AS_SPECIES = ["taxt LIKE ?", "Revived status as species%"],
+    VHIC_REVIVED_FROM_SYNONYMY =     ["taxt LIKE ?", "Revived from synonymy%"],
+    VHIC_CURRENTLY_SUBSPECIES_OF =   ["taxt LIKE ?", "Currently subspecies of%"],
+    VHIC_STATUS_AS_SPECIES =         ["taxt LIKE ?", "Status as species%"],
+
+    # Status-ish.
+    VHIC_NOMEN_NUDUM =      ["taxt = ?", '<i>Nomen nudum</i>'],
+    VHIC_UNAVAILABLE_NAME = ["taxt LIKE ?", "Unavailable name%"],
+
+    # Unidentifiable etc.
+    VHIC_UNIDENTIFIABLE_TAXON =    ["taxt LIKE ?", "Unidentifiable taxon%"],
+    VHIC_UNRECOGNISABLE_TAXON =    ["taxt LIKE ?", "Unrecognisable taxon%"],
+    VHIC_UNIDENTIFIABLE_TO_GENUS = ["taxt LIKE ?", "Unidentifiable to genus%"],
+    VHIC_SUBGENUS_INDETERMINATE =  ["taxt LIKE ?", "Subgenus indeterminate%"],
+
+    # Misc.
+    VHIC_ALSO_DESCRIBED_AS_NEW_BY =     ["taxt LIKE ?", "[Also described as new by%"],
+    VHIC_REPLACEMENT_NAME =             ["taxt LIKE ?", "Replacement name%"],
+    VHIC_UNNECESSARY_REPLACEMENT_NAME = ["taxt LIKE ?", "Unnecessary replacement name%"],
+    VHIC_FIRST_AVAILABLE_USE_OF =       ["taxt LIKE ?", "[First available use of%"],
+    VHIC_MISSPELLED_AS =                ["taxt LIKE ?", "[Misspelled as%"],
+    VHIC_COMBINATION_IN =               ["taxt LIKE ?", "Combination in%"]
+  ]
+
   belongs_to :taxon
 
   validates :taxt, :taxon, presence: true
@@ -9,6 +57,14 @@ class TaxonHistoryItem < ApplicationRecord
   before_validation :cleanup_taxts
 
   scope :persisted, -> { where.not(id: nil) }
+
+  scope :vhic_unknown, -> do
+    scope = self
+    VIRTUAL_HISTORY_ITEM_CANDIDATES.each do |where_filter|
+      scope = scope.where.not(where_filter)
+    end
+    scope
+  end
 
   acts_as_list scope: :taxon
   has_paper_trail meta: { change_id: proc { UndoTracker.current_change_id } }
