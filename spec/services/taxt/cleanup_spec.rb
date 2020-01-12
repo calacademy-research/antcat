afe8c5e11838c34e6232dc74f1eec4c1da4a23f5
jonkerz <jonkerz@gmail.com>
2020-01-12 08:05:41 +0100

    feat(taxts): automatically cleanup more punctuation in taxts

--- spec/services/taxt/cleanup_spec.rb
+++ spec/services/taxt/cleanup_spec.rb
@@ -2,9 +2,32 @@ require 'rails_helper'
 
 describe Taxt::Cleanup do
   describe '#call' do
-    it 'strips double spaces and colons' do
-      expect(described_class['Pi   zz : : a']).to eq 'Pi zz : a'
-      expect(described_class[nil]).to eq nil
+    context 'when string is nil' do
+      specify { expect(described_class[nil]).to eq nil }
+    end
+
+    it 'removes multiple consecutive spaces' do
+      expect(described_class['p    izza']).to eq 'p izza'
+    end
+
+    it 'removes leading and trailing whitespace' do
+      expect(described_class['   pizza ']).to eq 'pizza'
+    end
+
+    it 'removes colons separated by spaces' do
+      expect(described_class[': : pizza']).to eq ': pizza'
+    end
+
+    it 'moves spaces before colons to after the colon' do
+      expect(described_class['p :izza']).to eq 'p: izza'
+    end
+
+    it 'adds spaces after colons' do
+      expect(described_class[':pizza']).to eq ': pizza'
+    end
+
+    it 'can beat the final boss and his minions' do
+      expect(described_class['  Pi   zz : : a pescatore :1:']).to eq 'Pi zz: a pescatore: 1:'
     end
   end
 end
