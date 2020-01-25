e317f3e4bff8b13908280c37df5c08c321e90970
jonkerz <jonkerz@gmail.com>
2020-01-25 19:41:27 +0100

    fix: capitalization of non-ASCII characters

--- spec/decorators/protonym_decorator_spec.rb
+++ spec/decorators/protonym_decorator_spec.rb
@@ -9,11 +9,10 @@ describe ProtonymDecorator do
         expect(protonym.decorate.format_locality).to eq "INDONESIA (Timor)."
       end
 
-      # TODO: Use `mb_chars`, https://api.rubyonrails.org/classes/String.html#method-i-mb_chars
       context 'when locality contains non-ASCII characters' do
         let(:protonym) { build_stubbed :protonym, locality: 'São Tomé & Príncipe (São Tomé I.)' }
 
-        xit 'capitalizes properly' do
+        it 'capitalizes properly' do
           expect(protonym.decorate.format_locality).to eq "SÃO TOMÉ & PRÍNCIPE (São Tomé I.)."
         end
       end
