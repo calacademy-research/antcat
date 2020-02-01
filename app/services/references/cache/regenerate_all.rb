eb34d177bd2a09f44050f8457277713b469c3b86
jonkerz <jonkerz@gmail.com>
2020-02-01 01:21:56 +0100

    refactor: inline `lib/progress.rb`

--- app/services/references/cache/regenerate_all.rb
+++ app/services/references/cache/regenerate_all.rb
@@ -7,13 +7,19 @@ module References
       include Service
 
       def call
-        progress = Progress.create total: Reference.count
+        progress = progress_bar Reference.count
 
         Reference.find_each do |reference|
           progress.increment
           References::Cache::Regenerate[reference]
         end
       end
+
+      private
+
+        def progress_bar total
+          ProgressBar.create total: total, format: "%a %e %P% Processed: %c from %C", throttle_rate: 0.5
+        end
     end
   end
 end
