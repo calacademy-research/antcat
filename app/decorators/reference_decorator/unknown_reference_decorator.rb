3e2ddda79d00cc43d2cb0eb086a6e9c9fce5263e
jonkerz <jonkerz@gmail.com>
2020-02-22 12:36:52 +0100

    refactor: make private (`UnknownReferenceDecorator#format_citation`)

--- app/decorators/reference_decorator/unknown_reference_decorator.rb
+++ app/decorators/reference_decorator/unknown_reference_decorator.rb
@@ -1,8 +1,9 @@
 class UnknownReferenceDecorator < ReferenceDecorator
   delegate :citation
 
-  # NOTE: Not private like its siblings because we need it in a view.
-  def format_citation
-    sanitize citation
-  end
+  private
+
+    def format_citation
+      sanitize citation
+    end
 end
