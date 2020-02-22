ab7bd2433a3ba1bfbbc1cc3ca341e30de16384d4
jonkerz <jonkerz@gmail.com>
2020-02-22 13:23:20 +0100

    tests(reference): sort specs

--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -32,6 +32,28 @@ describe Reference do
   it { is_expected.to delegate_method(:url).to(:document).allow_nil }
   it { is_expected.to delegate_method(:downloadable?).to(:document).allow_nil }
 
+  describe 'callbacks' do
+    describe "changing `citation_year`" do
+      context 'when `citation_year` contains a letter' do
+        let(:reference) { create :reference, citation_year: '1910a' }
+
+        it "sets `year` to the stated year, if present" do
+          expect { reference.update!(citation_year: '2010b') }.
+            to change { reference.reload.year }.from(1910).to(2010)
+        end
+      end
+
+      context 'when `citation_year` contains a bracketed year' do
+        let(:reference) { create :reference, citation_year: '1910a ["1958"]' }
+
+        it "sets `year` to the stated year, if present" do
+          expect { reference.update!(citation_year: '2010b ["2009"]') }.
+            to change { reference.reload.year }.from(1958).to(2009)
+        end
+      end
+    end
+  end
+
   describe "scopes" do
     describe ".unreviewed_references" do
       let!(:unreviewed) { create :article_reference, review_state: "reviewing" }
@@ -93,25 +115,6 @@ describe Reference do
     end
   end
 
-  describe "#author_names_string_with_suffix" do
-    describe "formatting" do
-      it "consists of one author_name if that's all there is" do
-        reference = build_stubbed :reference, author_names: [fisher_bl]
-        expect(reference.author_names_string_with_suffix).to eq 'Fisher, B.L.'
-      end
-
-      it "separates multiple author_names with semicolons" do
-        reference = build_stubbed :reference, author_names: [fisher_bl, ward_ps]
-        expect(reference.author_names_string_with_suffix).to eq 'Fisher, B.L.; Ward, P.S.'
-      end
-
-      it "includes the author_names' suffix" do
-        reference = build_stubbed :reference, author_names: [fisher_bl], author_names_suffix: '(ed.)'
-        expect(reference.author_names_string_with_suffix).to eq 'Fisher, B.L. (ed.)'
-      end
-    end
-  end
-
   describe "#author_names_string" do
     describe "formatting" do
       it "consists of one author_name if that's all there is" do
@@ -168,49 +171,22 @@ describe Reference do
     end
   end
 
-  describe "#principal_author_last_name" do
-    context "when there are no authors" do
-      let!(:reference) { build_stubbed :reference, author_names: [] }
-
-      specify { expect(reference.principal_author_last_name).to eq nil }
-    end
-
-    context 'when there are authors' do
-      let!(:reference) { build_stubbed :reference, author_names: [ward_ps, fisher_bl] }
-
-      it "is the last name of the principal author" do
-        expect(reference.principal_author_last_name).to eq 'Ward'
+  describe "#author_names_string_with_suffix" do
+    describe "formatting" do
+      it "consists of one author_name if that's all there is" do
+        reference = build_stubbed :reference, author_names: [fisher_bl]
+        expect(reference.author_names_string_with_suffix).to eq 'Fisher, B.L.'
       end
-    end
-  end
 
-  describe "changing `citation_year`" do
-    context 'when `citation_year` contains a letter' do
-      let(:reference) { create :reference, citation_year: '1910a' }
-
-      it "sets `year` to the stated year, if present" do
-        expect { reference.update!(citation_year: '2010b') }.
-          to change { reference.reload.year }.from(1910).to(2010)
+      it "separates multiple author_names with semicolons" do
+        reference = build_stubbed :reference, author_names: [fisher_bl, ward_ps]
+        expect(reference.author_names_string_with_suffix).to eq 'Fisher, B.L.; Ward, P.S.'
       end
-    end
 
-    context 'when `citation_year` contains a bracketed year' do
-      let(:reference) { create :reference, citation_year: '1910a ["1958"]' }
-
-      it "sets `year` to the stated year, if present" do
-        expect { reference.update!(citation_year: '2010b ["2009"]') }.
-          to change { reference.reload.year }.from(1958).to(2009)
+      it "includes the author_names' suffix" do
+        reference = build_stubbed :reference, author_names: [fisher_bl], author_names_suffix: '(ed.)'
+        expect(reference.author_names_string_with_suffix).to eq 'Fisher, B.L. (ed.)'
       end
-    end
-  end
-
-  describe "#what_links_here" do
-    let(:reference) { build_stubbed :article_reference }
-
-    it "calls `References::WhatLinksHere`" do
-      expect(References::WhatLinksHere).to receive(:new).
-        with(reference, predicate: false).and_call_original
-      reference.what_links_here
     end
   end
 
@@ -266,6 +242,32 @@ describe Reference do
 
     it "doesn't include the year ordinal" do
       expect(reference.keey_without_letters_in_year).to eq 'Bolton, 1885'
+    end
+  end
+
+  describe "#principal_author_last_name" do
+    context "when there are no authors" do
+      let!(:reference) { build_stubbed :reference, author_names: [] }
+
+      specify { expect(reference.principal_author_last_name).to eq nil }
+    end
+
+    context 'when there are authors' do
+      let!(:reference) { build_stubbed :reference, author_names: [ward_ps, fisher_bl] }
+
+      it "is the last name of the principal author" do
+        expect(reference.principal_author_last_name).to eq 'Ward'
+      end
+    end
+  end
+
+  describe "#what_links_here" do
+    let(:reference) { build_stubbed :article_reference }
+
+    it "calls `References::WhatLinksHere`" do
+      expect(References::WhatLinksHere).to receive(:new).
+        with(reference, predicate: false).and_call_original
+      reference.what_links_here
     end
   end
 end
