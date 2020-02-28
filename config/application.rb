8a22049cd84216d4cff0673a1a9c381db4acb09a
jonkerz <jonkerz@gmail.com>
2020-02-28 17:40:03 +0100

    feat: embrace new Rails config defaults

--- config/application.rb
+++ config/application.rb
@@ -14,12 +14,15 @@ module AntCat
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.active_record.belongs_to_required_by_default = false # TODO: See if we can change this.
+    config.action_view.form_with_generates_remote_forms = false
 
     config.action_mailer.delivery_method = :sendmail
     config.action_mailer.sendmail_settings = { arguments: '-i' }
