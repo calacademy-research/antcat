9863edb7134d2e10b1f8f511a130266ad9d0f4d9
jonkerz <jonkerz@gmail.com>
2020-02-29 02:12:20 +0100

    style: fix RuboCop Layout/FirstHashElementLineBreak

--- app/models/tooltip.rb
+++ app/models/tooltip.rb
@@ -4,8 +4,7 @@ class Tooltip < ApplicationRecord
 
   validates :scope, :text, presence: true
   validates :key, presence: true, uniqueness: true,
-    format: { with: /\A[a-zA-Z0-9._:\-]+\z/,
-      message: "can only contain alphanumeric characters and '.-_:'" }
+    format: { with: /\A[a-zA-Z0-9._:\-]+\z/, message: "can only contain alphanumeric characters and '.-_:'" }
 
   has_paper_trail
   trackable parameters: proc { { scope_and_key: "#{scope}.#{key}" } }
