c3cbb1a3c0bdf9e442c53115539824ab5b7431bb
jonkerz <jonkerz@gmail.com>
2020-03-01 21:36:49 +0100

    tweak: less comments

--- app/models/taxa/genus_group_taxon.rb
+++ app/models/taxa/genus_group_taxon.rb
@@ -1,6 +1,3 @@
-# NOTE: This is the superclass of `Genus` and `Subgenus`, not to
-# be confused with "genus group" or "genus-group name" as used in taxonomy.
-
 class GenusGroupTaxon < Taxon
   belongs_to :subfamily
   belongs_to :tribe
