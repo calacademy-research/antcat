914e88ad3cd0c80207df2b26c7331d165170a6a0
jonkerz <jonkerz@gmail.com>
2020-03-07 11:13:51 +0100

    experimental: `CatalogPresenter`
    Not sure how useful, let's try it out.

---
+++ app/presenters/catalog_presenter.rb
@@ -0,0 +1,36 @@
+class CatalogPresenter
+  def initialize taxon, params:, session:, formicidae_landing_page: false
+    @taxon = taxon
+    @params = params
+    @session = session
+    @formicidae_landing_page = formicidae_landing_page
+  end
+
+  def taxon_browser
+    @taxon_browser ||= TaxonBrowser::Browser.new(taxon, session[:show_invalid], params[:display]&.to_sym)
+  end
+
+  def statistics
+    @statistics ||= begin
+      if show_full_statistics?
+        taxon.decorate.statistics
+      else
+        # If there's no valid-only stats, try including invalid too before giving up.
+        taxon.decorate.statistics(valid_only: true) || taxon.decorate.statistics
+      end
+    end
+  end
+
+  def show_full_statistics?
+    taxon.invalid? || params[:include_full_statistics].present?
+  end
+
+  # NOTE: Special case to avoid showing ~6 A4 pages of Formicidae references and use a different title.
+  def formicidae_landing_page?
+    formicidae_landing_page
+  end
+
+  private
+
+    attr_reader :taxon, :params, :session, :formicidae_landing_page
+end
