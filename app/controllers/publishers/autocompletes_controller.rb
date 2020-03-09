a360741cf261c04160d987413cca99805fd8ad09
jonkerz <jonkerz@gmail.com>
2020-03-09 20:05:12 +0100

    refactor(publisher-autocompletion): separate fetch/format

--- app/controllers/publishers/autocompletes_controller.rb
+++ app/controllers/publishers/autocompletes_controller.rb
@@ -1,7 +1,7 @@
 module Publishers
   class AutocompletesController < ApplicationController
     def show
-      render json: Autocomplete::AutocompletePublishers[params[:term]]
+      render json: Autocomplete::AutocompletePublishers[params[:term]].map(&:display_name)
     end
   end
 end
--- spec/controllers/publishers/autocompletes_controller_spec.rb
+++ spec/controllers/publishers/autocompletes_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe Publishers::AutocompletesController do
   describe "GET show" do
-    let(:term) { "wiley" }
-
     it "calls `Autocomplete::AutocompletePublishers`" do
-      expect(Autocomplete::AutocompletePublishers).to receive(:new).with(term).and_call_original
-      get :show, params: { term: term }
+      expect(Autocomplete::AutocompletePublishers).to receive(:new).with("wiley").and_call_original
+      get :show, params: { term: "wiley" }
+    end
+
+    context 'with publishers' do
+      let!(:publisher) { create :publisher, name: 'Wiley', place_name: 'California' }
+
+      it "returns publishers in an array" do
+        get :show, params: { term: "wiley" }
+        expect(json_response).to eq [publisher.display_name]
+      end
     end
   end
 end
