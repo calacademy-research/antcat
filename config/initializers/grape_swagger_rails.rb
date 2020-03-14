2b28c82fe62c85ebdb7f417b01b3fdd8b7d2496b
jonkerz <jonkerz@gmail.com>
2020-03-15 00:41:28 +0100

    api: update to HTTPS

--- config/initializers/grape_swagger_rails.rb
+++ config/initializers/grape_swagger_rails.rb
@@ -1,2 +1,6 @@
+# NOTE: If updates in `swagger.yaml` seems to not be picked up in the development env,
+# try opening http://localhost:3000/api/v1/swagger.yaml in the browser and then refresh
+# http://localhost:3000/api_docs
+
 GrapeSwaggerRails.options.url = '/api/v1/swagger.yaml'
 GrapeSwaggerRails.options.app_url = ''
