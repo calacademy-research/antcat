8f8fff7adbf4bcde41b1817f4d27dec30c2b8ed6
jonkerz <jonkerz@gmail.com>
2020-03-20 23:28:46 +0100

    tests(api): add missing specs

---
+++ spec/controllers/api/v1/reference_author_names_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe Api::V1::ReferenceAuthorNamesController do
+  describe "GET index" do
+    specify do
+      create :reference_author_name
+      get :index
+      expect(json_response).to eq(ReferenceAuthorName.all.as_json)
+    end
+
+    specify { expect(get(:index)).to have_http_status :ok }
+  end
+
+  describe "GET show" do
+    let!(:reference_author_name) { create :reference_author_name }
+
+    specify do
+      get :show, params: { id: reference_author_name.id }
+      expect(json_response).to eq(
+        {
+          "reference_author_name" => {
+            "id" => reference_author_name.id,
+            "author_name_id" => reference_author_name.author_name_id,
+            "reference_id" => reference_author_name.reference_id,
+            "position" => reference_author_name.position,
+            "created_at" => reference_author_name.created_at.as_json,
+            "updated_at" => reference_author_name.updated_at.as_json
+          }
+        }
+      )
+    end
+
+    specify { expect(get(:show, params: { id: reference_author_name.id })).to have_http_status :ok }
+  end
+end
---
+++ spec/controllers/api/v1/reference_documents_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe Api::V1::ReferenceDocumentsController do
+  describe "GET index" do
+    specify do
+      reference_document = create :reference_document
+      get :index
+      expect(json_response).to eq([reference_document.as_json])
+    end
+
+    specify { expect(get(:index)).to have_http_status :ok }
+  end
+
+  describe "GET show" do
+    let!(:reference_document) { create :reference_document }
+
+    specify do
+      get :show, params: { id: reference_document.id }
+      expect(json_response).to eq(
+        {
+          "reference_document" => {
+            "id" => reference_document.id,
+            "reference_id" => reference_document.reference_id,
+            "file_file_name" => reference_document.file_file_name,
+            "url" => reference_document.url,
+            "public" => reference_document.public,
+            "created_at" => reference_document.created_at.as_json,
+            "updated_at" => reference_document.updated_at.as_json
+          }
+        }
+      )
+    end
+
+    specify { expect(get(:show, params: { id: reference_document.id })).to have_http_status :ok }
+  end
+end
