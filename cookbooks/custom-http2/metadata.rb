5d515fdcb0a28211cf7cea54ef158e8c49bd23a9
jonkerz <jonkerz@gmail.com>
2020-03-21 13:17:41 +0100

    feat: custom-http2 cookbook

---
+++ cookbooks/custom-http2/metadata.rb
@@ -0,0 +1,10 @@
+name 'custom-http2'
+description 'Configure nginx to use http2 on Engine Yard'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '1.0'
+issues_url 'https://github.com/engineyard/ey-cookbooks-stable-v6/issues'
+source_url 'https://github.com/engineyard/ey-cookbooks-stable-v6'
+
+depends 'nginx'
