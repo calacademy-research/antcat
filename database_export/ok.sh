3dd4e9d2de7d6bc6baaa03302ae58ddd7fcd537e
Joe Russack <jrussack@calacademy.org>
2020-03-25 18:10:29 -0700

    Using PWD

--- database_export/ok.sh
+++ database_export/ok.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 filename=$PWD/antcat.antweb.txt
 echo "Checking on $filename"
 if [ ! -f "$filename" ]; then
@@ -9,7 +10,7 @@ fi
 #if [[ $(find "$filename" -mmin +999 -print) ]]; then
 if [[ $(find "$filename" -mtime +1 -print) ]]; then
   echo "File $filename exists and is older than 1 day"
-  exit 1
+  exit 2
 else
   echo "File $filename exists and is young."
   exit 0
