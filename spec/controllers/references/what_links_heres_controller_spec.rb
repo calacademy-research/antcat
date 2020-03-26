64365ea19fd479755c407f4dfba9f631096d5979
jonkerz <jonkerz@gmail.com>
2020-03-26 14:27:59 +0100

    feat: require logged in for UnconfirmedsController

---
+++ spec/controllers/references/what_links_heres_controller_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe References::WhatLinksHeresController do
+  describe 'GET show', as: :visitor do
+    let!(:reference) { create :article_reference }
+
+    specify { expect(get(:show, params: { reference_id: reference.id })).to render_template :show }
+  end
+end
