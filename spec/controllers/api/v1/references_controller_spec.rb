813f05cb80377849552e0c15c4462bee8c615f7b
jonkerz <jonkerz@gmail.com>
2020-04-01 23:45:05 +0200

    migrations: remove `references.reason_missing`

--- spec/controllers/api/v1/references_controller_spec.rb
+++ spec/controllers/api/v1/references_controller_spec.rb
@@ -38,8 +38,6 @@ describe Api::V1::ReferencesController, as: :visitor do
             "publisher_id" => nil,
             "series_volume_issue" => reference.series_volume_issue,
             "nesting_reference_id" => nil,
-            # TODO: Remove and `remove_column :reason_missing` now after removing `MissingReference`.
-            "reason_missing" => nil,
             "citation" => nil,
 
             # Notes.
