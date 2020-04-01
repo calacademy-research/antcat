4043f39205bdb1f7c33e4aea2569a92ab61a95b4
jonkerz <jonkerz@gmail.com>
2020-04-01 22:35:47 +0200

    tweak: link_specimen_identifiers.rb

--- app/services/types/link_specimen_identifiers.rb
+++ app/services/types/link_specimen_identifiers.rb
@@ -21,14 +21,8 @@ module Types
       "MCZTYPE",
       "MCZ-ENT"
     ]
-
-    STOP_REGEX = %r{
-      (?!               # Negative look-ahead.
-        (?:             # Non-capturing.
-          [^$\]\), .;/] # Only match abbreviations followed by any of these characters.
-        )
-      )
-    }x
+    STOP_REGEX = /[A-Za-z0-9-]+/
+    ANTWEB_SPECIMEN_BASE_URL = 'https://www.antweb.org/specimen/'
 
     attr_private_initialize :content
 
@@ -40,12 +34,16 @@ module Types
 
       def link_specimen_identifiers!
         SPECIMEN_IDENTIFIER_PREFIXES.each do |prefix|
-          content.gsub!(/\b(#{prefix}[A-Za-z0-9-]+)\b/) do |id|
-            "<a href='https://www.antweb.org/specimen/#{id}'>#{id}</a>"
+          content.gsub!(/\b(#{prefix})#{STOP_REGEX}\b/) do |id|
+            antweb_specimen_link(id)
           end
         end
 
         content
+      end
+
+      def antweb_specimen_link id
+        "<a href='#{ANTWEB_SPECIMEN_BASE_URL}#{id}'>#{id}</a>"
       end
   end
 end
