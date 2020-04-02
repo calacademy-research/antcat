f52f8fa5fe0806e993415ca5a8a984d06ef5325f
jonkerz <jonkerz@gmail.com>
2020-04-02 19:45:31 +0200

    tweak: retun less empty string, to catch nils

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -2,12 +2,12 @@
 
 module MarkdownHelper
   def markdown content, sanitize_content: true
-    return '' unless content
+    return unless content
     Markdowns::Render[content.dup, sanitize_content: sanitize_content]
   end
 
   def markdown_without_wrapping content
-    return '' unless content
+    return unless content
     Markdowns::RenderWithoutWrappingP[content.dup]
   end
 end
--- app/services/detax.rb
+++ app/services/detax.rb
@@ -3,18 +3,12 @@
 class Detax
   include Service
 
-  def initialize taxt
-    @taxt = taxt.try(:dup)
-  end
+  attr_private_initialize :taxt
 
   # Parses "example {tax 429361}"
   # into   "example <a href=\"/catalog/429361\">Melophorini</a>"
   def call
-    return '' unless taxt
-    Markdowns::ParseAntcatHooks[taxt, catalog_tags_only: true].html_safe
+    return unless taxt
+    Markdowns::ParseAntcatHooks[taxt.dup, catalog_tags_only: true].html_safe
   end
-
-  private
-
-    attr_reader :taxt
 end
