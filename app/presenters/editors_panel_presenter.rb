e533da23b1d58eb99a3b8d6d881365c7238556cf
jonkerz <jonkerz@gmail.com>
2020-04-02 17:55:25 +0200

    refactor: extract into `EditorsPanelPresenter`

---
+++ app/presenters/editors_panel_presenter.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class EditorsPanelPresenter
+  attr_private_initialize :current_user
+
+  def recent_activities
+    Activity.non_automated_edits.most_recent(10).includes(:user)
+  end
+
+  def recent_comments
+    Comment.most_recent(5)
+  end
+
+  def recent_unconfirmed_activities
+    Activity.unconfirmed.most_recent(5).includes(:user)
+  end
+
+  def open_issues_count
+    Issue.open.count
+  end
+
+  def unreviewed_references_count
+    Reference.unreviewed.count
+  end
+
+  def pending_user_feedbacks_count
+    Feedback.pending.count
+  end
+
+  def unread_site_notices_count
+    current_user.unread_site_notices.count
+  end
+end
