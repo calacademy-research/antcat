5f6e16ab14c3c5e24b7706362068be3cbd85571b
Joe Russack <jrussack@calacademy.org>
2020-04-07 10:35:12 -0700

    ok.sh

--- database_export/ok.sh
+++ database_export/ok.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+database_export_dir='../database_archive'
+db_file=`ls -Art $database_export_dir | tail -n 1`
+echo "Found: $database_export_dir/$db_file"
+if [[ $(find "$database_export_dir/$db_file" -mtime +1 -print) ]]; then
+  echo "Database export file $db_file exists and is older than 1 day"
+  exit 2
+else
+  echo "Database export $db_file exists and is young."
+fi
+
+
+
 filename=$PWD/antcat.antweb.txt
 echo "Checking on $filename"
 if [ ! -f "$filename" ]; then
