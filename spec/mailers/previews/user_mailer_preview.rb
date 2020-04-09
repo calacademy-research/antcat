7f1aa9ded8ec5b05192e8948d14dd863dc64d5c9
jonkerz <jonkerz@gmail.com>
2020-04-09 15:31:31 +0200

    tweaks: medium

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class UserMailerPreview < ActionMailer::Preview
-  include Rails.application.routes.url_helpers
-
   def new_notification
     user = User.find(60)
     UserMailer.new_notification(user, user.notifications.last)
