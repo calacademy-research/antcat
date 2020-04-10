2b5675f4dc1ca3fed8f6a9ad9aaf5e31442f4686
jonkerz <jonkerz@gmail.com>
2020-04-10 16:58:22 +0200

    refactor(autocomplete): separate query/serializing (wiki pages)

--- spec/controllers/wiki_pages/autocompletes_controller_spec.rb
+++ spec/controllers/wiki_pages/autocompletes_controller_spec.rb
@@ -4,11 +4,24 @@ require 'rails_helper'
 
 describe WikiPages::AutocompletesController do
   describe "GET show", as: :visitor do
-    let(:q) { "help" }
+    let!(:help_wiki_page) { create :wiki_page, title: 'Help' }
+    let!(:news_wiki_page) { create :wiki_page, title: 'News' }
 
     it "calls `Autocomplete::AutocompleteWikiPages`" do
-      expect(Autocomplete::AutocompleteWikiPages).to receive(:new).with(q).and_call_original
-      get :show, params: { q: q, format: :json }
+      expect(Autocomplete::AutocompleteWikiPages).to receive(:new).with('q').and_call_original
+      get :show, params: { q: 'q', format: :json }
+    end
+
+    specify do
+      get :show, params: { q: 'hel', format: :json }
+      expect(json_response).to eq [{ 'id' => help_wiki_page.id, 'title' => help_wiki_page.title }]
+    end
+
+    context 'when a wiki page ID is given' do
+      specify do
+        get :show, params: { q: news_wiki_page.id.to_s, format: :json }
+        expect(json_response).to eq [{ 'id' => news_wiki_page.id, 'title' => news_wiki_page.title }]
+      end
     end
   end
 end
--- spec/services/autocomplete/autocomplete_wiki_pages_spec.rb
+++ spec/services/autocomplete/autocomplete_wiki_pages_spec.rb
@@ -8,12 +8,12 @@ describe Autocomplete::AutocompleteWikiPages do
     let!(:news_wiki_page) { create :wiki_page, title: 'News' }
 
     specify do
-      expect(described_class['Hel']).to eq [{ id: help_wiki_page.id, title: help_wiki_page.title }]
+      expect(described_class['Hel']).to eq [help_wiki_page]
     end
 
     context 'when a wiki page ID is given' do
       specify do
-        expect(described_class[news_wiki_page.id.to_s]).to eq [{ id: news_wiki_page.id, title: news_wiki_page.title }]
+        expect(described_class[news_wiki_page.id.to_s]).to eq [news_wiki_page]
       end
     end
   end
