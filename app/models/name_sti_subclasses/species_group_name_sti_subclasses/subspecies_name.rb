173abfa4d6aa8d6a5befc47da995660f0ea043c0
jonkerz <jonkerz@gmail.com>
2020-04-12 22:21:53 +0200

    feat: add `SubspeciesName#short_name`

--- app/models/name_sti_subclasses/species_group_name_sti_subclasses/subspecies_name.rb
+++ app/models/name_sti_subclasses/species_group_name_sti_subclasses/subspecies_name.rb
@@ -2,6 +2,10 @@
 
 class SubspeciesName < SpeciesGroupName
   def subspecies_epithets
-    name_parts[2..-1].join ' '
+    name_parts[2..-1].join(' ')
+  end
+
+  def short_name
+    [genus_epithet[0] + '.', species_epithet[0] + '.', subspecies_epithets].join(' ')
   end
 end
