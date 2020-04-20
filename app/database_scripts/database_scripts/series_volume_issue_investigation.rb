4877f734a02ff9f575804633a3a2d68f14885e44
jonkerz <jonkerz@gmail.com>
2020-04-20 19:12:16 +0200

    feat(db-scripts): add SeriesVolumeIssueInvestigation

---
+++ app/database_scripts/database_scripts/series_volume_issue_investigation.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module DatabaseScripts
+  class SeriesVolumeIssueInvestigation < DatabaseScript
+    def results
+      ArticleReference.
+        limit(10000).
+        where("series_volume_issue NOT REGEXP ?", "^[0-9]+$").                  # 12
+        where("series_volume_issue NOT REGEXP ?", "^[0-9]+-[0-9]+$").           # 12-34
+        where("series_volume_issue NOT REGEXP ?", "^[0-9]+\\([0-9]+\\)$").      # 12(34)
+        where("series_volume_issue NOT REGEXP ?", "^[0-9]+ \\([0-9]+\\)$").     # 12 (34)
+        where("series_volume_issue NOT REGEXP ?", "^\\([0-9]+\\)[0-9]+$").      # (12)34
+        where("series_volume_issue NOT REGEXP ?", "^[0-9]+\\([0-9]+\\)$[0-9]+") # 12(34)56
+    end
+
+    def render
+      as_table do |t|
+        t.header 'Reference', 'series_volume_issue'
+        t.rows do |reference|
+          [
+            link_to(reference.keey, reference_path(reference)),
+            reference.series_volume_issue
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+title: <code>series_volume_issue</code> investigation
+
+section: research
+category: References
+tags: [new!]
+
+issue_description:
+
+description: >
+  Excluded formats:
+
+
+  * 12
+
+  * 12-34
+
+  * 12(34)
+
+  * 12 (34)
+
+  * (12)34
+
+  * 12(34)56
+
+related_scripts:
+  - SeriesVolumeIssueInvestigation
