d6b4245b48ed116683b4965ecef04a82756f6c21
jonkerz <jonkerz@gmail.com>
2020-04-23 19:03:10 +0200

    refactor(aw-export): separate fetch/format steps

--- app/exporters/exporters/antweb/export_taxon.rb
+++ app/exporters/exporters/antweb/export_taxon.rb
@@ -4,9 +4,7 @@
 
 module Exporters
   module Antweb
-    class ExportTaxon # rubocop:disable Metrics/ClassLength
-      include ActionView::Context # For `#content_tag`.
-      include ActionView::Helpers::TagHelper # For `#content_tag`.
+    class ExportTaxon
       include Service
 
       HEADER = [
@@ -45,32 +43,7 @@ module Exporters
       private
 
         def attributes
-          {
-            antcat_id:                taxon.id,
-            subfamily:                taxonomic_attributes[:subfamily],
-            tribe:                    taxonomic_attributes[:tribe],
-            genus:                    taxonomic_attributes[:genus],
-            subgenus:                 taxonomic_attributes[:subgenus],
-            species:                  taxonomic_attributes[:species],
-            subspecies:               taxonomic_attributes[:subspecies],
-            author_date:              taxon.author_citation,
-            author_date_html:         authorship_html_string,
-            authors:                  taxon.authorship_reference.key.authors_for_keey,
-            year:                     taxon.authorship_reference.year,
-            status:                   taxon.status,
-            available:                taxon.valid_status?,
-            current_valid_name:       current_valid_name,
-            original_combination:     taxon.original_combination?,
-            was_original_combination: original_combination&.name&.name,
-            fossil:                   taxon.fossil?,
-            taxonomic_history_html:   export_history,
-            reference_id:             taxon.authorship_reference.id,
-            bioregion:                taxon.protonym.biogeographic_region,
-            country:                  taxon.protonym.locality,
-            current_valid_rank:       taxon.class.to_s,
-            hol_id:                   taxon.hol_id,
-            current_valid_parent:     current_valid_parent&.name&.name || 'Formicidae'
-          }
+          Exporters::Antweb::TaxonAttributes[taxon]
         end
 
         def convert_to_antweb_array values
@@ -109,41 +82,6 @@ module Exporters
           when nil   then nil
           else            raise
           end
-        end
-
-        def taxonomic_attributes
-          @_taxonomic_attributes = Exporters::Antweb::TaxonomicAttributes[taxon]
-        end
-
-        def current_valid_name
-          return unless (current_valid_name = taxon.current_valid_taxon&.name&.name)
-          "#{taxonomic_attributes[:subfamily]} #{current_valid_name}"
-        end
-
-        def authorship_html_string
-          reference = taxon.authorship_reference
-          content_tag :span, reference.keey, title: reference.decorate.plain_text
-        end
-
-        def original_combination
-          taxon.class.where(original_combination: true, current_valid_taxon: taxon).first
-        end
-
-        def export_history
-          content_tag :div, class: 'antcat_taxon' do # NOTE: `.antcat_taxon` is used on AntWeb.
-            content = ''.html_safe
-            content << taxon.decorate.statistics(valid_only: true)
-            content << Exporters::Antweb::History::Headline[taxon]
-            content << Exporters::Antweb::History::HistoryItems[taxon]
-            content << Exporters::Antweb::History::ChildList[taxon]
-            content << Exporters::Antweb::History::ReferenceSections[taxon]
-          end
-        end
-
-        def current_valid_parent
-          return unless taxon.parent
-          parent = taxon.parent.is_a?(Subgenus) ? taxon.parent.parent : taxon.parent
-          parent.current_valid_taxon || parent
         end
     end
   end
