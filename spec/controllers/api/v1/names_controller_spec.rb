6194c379cbd9dc712195ac6b94245c72bed6edf1
jonkerz <jonkerz@gmail.com>
2020-04-24 20:50:07 +0200

    tests: add/tweak specs

--- spec/controllers/api/v1/names_controller_spec.rb
+++ spec/controllers/api/v1/names_controller_spec.rb
@@ -14,11 +14,25 @@ describe Api::V1::NamesController, as: :visitor do
   end
 
   describe "GET show" do
-    let!(:name) { create :family_name }
+    let!(:name) { create :family_name, name: 'Formicidae' }
 
     specify do
       get :show, params: { id: name.id }
-      expect(json_response).to eq(name.as_json) # TOOD: Lazy.
+      expect(json_response).to eq(
+        {
+          "family_name" => {
+            "id" => name.id,
+            "auto_generated" => name.auto_generated,
+            "epithet" => name.epithet,
+            "gender" => name.gender,
+            "name" => 'Formicidae',
+            "nonconforming_name" => name.nonconforming_name,
+            "origin" => name.origin,
+            "created_at" => name.created_at.as_json,
+            "updated_at" => name.updated_at.as_json
+          }
+        }
+      )
     end
 
     specify { expect(get(:show, params: { id: name.id })).to have_http_status :ok }
