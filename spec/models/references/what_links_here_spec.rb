92bd41c343a33ea47a5e20b2a176ae3b7ccee91a
jonkerz <jonkerz@gmail.com>
2020-05-02 13:06:06 +0200

    tests(whl): tweak specs

--- spec/models/references/what_links_here_spec.rb
+++ spec/models/references/what_links_here_spec.rb
@@ -3,17 +3,35 @@
 require 'rails_helper'
 
 describe References::WhatLinksHere do
-  describe "#call" do
-    subject(:what_links_here) { described_class.new(reference.reload) }
+  subject(:what_links_here) { described_class.new(reference.reload) }
 
-    let(:reference) { create :any_reference }
+  let(:reference) { create :any_reference }
 
-    context 'when there are no references' do
-      specify { expect(what_links_here.all).to eq [] }
-      specify { expect(what_links_here.any?).to eq false }
+  context 'when there are no references' do
+    specify { expect(what_links_here.all).to eq [] }
+    specify { expect(what_links_here.any?).to eq false }
+  end
+
+  context 'when there are column references' do
+    let!(:taxon) { create :family }
+    let!(:nested_reference) { create :nested_reference, nesting_reference: reference }
+
+    before do
+      taxon.protonym.authorship.update!(reference: reference)
     end
 
-    context 'when there are taxt references' do
+    specify do
+      expect(what_links_here.all).to match_array [
+        WhatLinksHereItem.new('citations',  :reference_id,         taxon.protonym.authorship.id),
+        WhatLinksHereItem.new('references', :nesting_reference_id, nested_reference.id)
+      ]
+    end
+
+    specify { expect(what_links_here.any?).to eq true }
+  end
+
+  context 'when there are taxt references' do
+    describe "tag: `ref`" do
       let(:ref_tag) { "{ref #{reference.id}}" }
 
       let!(:citation) { create :citation, reference: reference, notes_taxt: ref_tag }
@@ -31,24 +49,6 @@ describe References::WhatLinksHere do
           WhatLinksHereItem.new('reference_sections',  :subtitle_taxt,       reference_section.id),
           WhatLinksHereItem.new('reference_sections',  :references_taxt,     reference_section.id),
           WhatLinksHereItem.new('taxon_history_items', :taxt,                history_item.id)
-        ]
-      end
-
-      specify { expect(what_links_here.any?).to eq true }
-    end
-
-    context 'when there are references in relations' do
-      let!(:taxon) { create :family }
-      let!(:nested_reference) { create :nested_reference, nesting_reference: reference }
-
-      before do
-        taxon.protonym.authorship.update!(reference: reference)
-      end
-
-      specify do
-        expect(what_links_here.all).to match_array [
-          WhatLinksHereItem.new('citations',  :reference_id,         taxon.protonym.authorship.id),
-          WhatLinksHereItem.new('references', :nesting_reference_id, nested_reference.id)
         ]
       end
 
