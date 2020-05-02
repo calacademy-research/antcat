38d590642f81f1cfd37a4727bd12bc61c2764442
jonkerz <jonkerz@gmail.com>
2020-05-02 12:40:42 +0200

    refactor(wlh): streamline

--- app/services/what_links_here_columns.rb
+++ app/services/what_links_here_columns.rb
@@ -7,38 +7,33 @@ class WhatLinksHereColumns
   end
 
   def all
-    what_links_here_items
+    columns
   end
 
   def any?
-    return @_any if defined? @_any
-    @_any ||= any_what_links_here_items?
+    any_columns?
   end
 
   private
 
     attr_reader :record, :columns_referencing_record
 
-    def any_what_links_here_items?
+    def any_columns?
       columns_referencing_record.each do |(model, column)|
         return true if model.where(column => record.id).exists?
       end
       false
     end
 
-    def what_links_here_items
+    def columns
       wlh_items = []
 
       columns_referencing_record.each do |(model, column)|
         model.where(column => record.id).pluck(:id).each do |matched_id|
-          wlh_items << wlh_item(model.table_name, column, matched_id)
+          wlh_items << WhatLinksHereItem.new(model.table_name, column, matched_id)
         end
       end
 
       wlh_items
-    end
-
-    def wlh_item table, field, id
-      WhatLinksHereItem.new(table, field, id)
     end
 end
--- app/services/what_links_here_taxts.rb
+++ app/services/what_links_here_taxts.rb
@@ -7,38 +7,33 @@ class WhatLinksHereTaxts
   end
 
   def all
-    what_links_here_items
+    taxts
   end
 
   def any?
-    return @_any if defined? @_any
-    @_any ||= any_what_links_here_items?
+    any_taxts?
   end
 
   private
 
     attr_reader :record, :taxt_tag
 
-    def any_what_links_here_items?
-      Taxt::TAXTABLES.each do |(model, _table, field)|
-        return true if model.where("#{field} REGEXP ?", taxt_tag).exists?
+    def any_taxts?
+      Taxt::TAXTABLES.each do |(model, _table, column)|
+        return true if model.where("#{column} REGEXP ?", taxt_tag).exists?
       end
       false
     end
 
-    def what_links_here_items
+    def taxts
       wlh_items = []
 
-      Taxt::TAXTABLES.each do |(model, _table, field)|
-        model.where("#{field} REGEXP ?", taxt_tag).pluck(:id).each do |matched_id|
-          wlh_items << wlh_item(model.table_name, field.to_sym, matched_id)
+      Taxt::TAXTABLES.each do |(model, _table, column)|
+        model.where("#{column} REGEXP ?", taxt_tag).pluck(:id).each do |matched_id|
+          wlh_items << WhatLinksHereItem.new(model.table_name, column.to_sym, matched_id)
         end
       end
 
       wlh_items
-    end
-
-    def wlh_item table, field, id
-      WhatLinksHereItem.new(table, field, id)
     end
 end
