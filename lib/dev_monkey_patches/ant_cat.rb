5a4fc82f87bc92485f4541a6a718b7cb424d97d9
jonkerz <jonkerz@gmail.com>
2020-05-02 12:05:35 +0200

    feat: more monkey patches! (protonym links)

--- lib/dev_monkey_patches/ant_cat.rb
+++ lib/dev_monkey_patches/ant_cat.rb
@@ -7,6 +7,7 @@ module DevMonkeyPatches
 
     def self.patch!
       ::Taxon.include Taxon
+      ::Protonym.include Protonym
       ::Reference.include Reference
       ::ReferenceSection.include ReferenceSection
     end
@@ -15,6 +16,25 @@ module DevMonkeyPatches
       def dev_dev_link localhost: false
         base_url = localhost ? LOCALHOST_BASE_URL : PRODUCTION_BASE_URL
         link = +"#{base_url}/catalog/#{id}?#{name_cache.tr(' ', '_')}"
+
+        def link.open
+          `xdg-open "#{self}"`
+        end
+
+        link
+      end
+      alias_method :l, :dev_dev_link
+
+      def dev_dev_link_localhost
+        dev_dev_link localhost: true
+      end
+      alias_method :ll, :dev_dev_link_localhost
+    end
+
+    module Protonym
+      def dev_dev_link localhost: false
+        base_url = localhost ? LOCALHOST_BASE_URL : PRODUCTION_BASE_URL
+        link = +"#{base_url}/protonyms/#{id}"
 
         def link.open
           `xdg-open "#{self}"`
