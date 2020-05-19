72b722d6b8741f98b6efa3ac100218a15ccde9e6
jonkerz <jonkerz@gmail.com>
2020-05-20 01:29:30 +0200

    feat(type-names): add support code (`ProtonymForm`)

---
+++ app/forms/protonym_form.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+class ProtonymForm
+  include ActiveModel::Model
+
+  ATTRIBUTES = %w[
+    fossil
+    sic
+    locality
+    primary_type_information_taxt
+    secondary_type_information_taxt
+    type_notes_taxt
+    biogeographic_region
+    uncertain_locality
+  ]
+
+  attr_accessor(*ATTRIBUTES)
+  attr_reader :protonym
+
+  delegate :id, :persisted?, :authorship, :type_name, to: :protonym
+
+  validate :validate_children
+
+  def self.model_name
+    Protonym.model_name
+  end
+
+  def initialize protonym, params = {}, form_params = {}
+    @protonym = protonym
+    @params = params
+    @form_params = form_params
+
+    if (protonym_name_string = form_params[:protonym_name_string])
+      @protonym.name = Names::BuildNameFromString[protonym_name_string]
+    end
+
+    super params
+  end
+
+  def authorship_attributes= attributes
+    protonym.authorship.attributes = attributes
+  end
+
+  def type_name_attributes= attributes
+    return if protonym.type_name.nil? && type_name_attributes_blank?(attributes)
+
+    protonym.build_type_name unless protonym.type_name
+    protonym.type_name.attributes = attributes
+  end
+
+  def save
+    if valid?
+      persist!
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+    attr_reader :params, :form_params
+
+    def persist!
+      ActiveRecord::Base.transaction do
+        if destroy_type_name?
+          protonym.type_name.destroy!
+        elsif protonym.type_name
+          protonym.type_name.save!
+        end
+
+        protonym.authorship.save!
+        protonym.attributes = params.slice(*ATTRIBUTES)
+        protonym.save!
+      end
+    end
+
+    def promote_errors child_errors, error_prefix = ''
+      child_errors.full_messages.each do |full_message|
+        errors.add(:base, "#{error_prefix}#{full_message}")
+      end
+    end
+
+    def validate_children
+      if protonym.name.invalid?
+        promote_errors(protonym.name.errors)
+      end
+
+      if protonym.authorship.invalid?
+        promote_errors(protonym.authorship.errors, "Authorship: ")
+      end
+
+      unless destroy_type_name?
+        if protonym.type_name&.invalid?
+          promote_errors(protonym.type_name.errors, "Type name: ")
+        end
+      end
+    end
+
+    def destroy_type_name?
+      ActiveModel::Type::Boolean.new.cast(form_params[:destroy_type_name])
+    end
+
+    def type_name_attributes_blank? attributes
+      attributes.with_indifferent_access.slice(:taxon_id, :fixation_method, :reference_id, :pages).values.reject(&:blank?).blank?
+    end
+end
