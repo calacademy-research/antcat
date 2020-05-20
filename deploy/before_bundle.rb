3975b371089974739c7f36cc1572a7401cd2b6cb
jonkerz <jonkerz@gmail.com>
2020-05-20 13:38:41 +0200

    feat: staging env

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-# To copy local file to production:
+# To copy local file to server:
+# mkdir /data/antcat/shared/config/settings
 # scp config/settings/production.secret.yml deploy@antcat.org:/data/antcat/shared/config/settings/production.secret.yml
-run "ln -nfs #{config.shared_path}/config/settings/production.secret.yml #{config.release_path}/config/settings/production.secret.yml"
+# scp config/settings/staging.secret.yml deploy@$ANTCAT_STAGING_HOST:/data/antcat/shared/config/settings/staging.secret.yml
+
+settings_file = "config/settings/#{config.environment}.secret.yml"
+run "ln -nfs #{config.shared_path}/#{settings_file} #{config.release_path}/#{settings_file}"
