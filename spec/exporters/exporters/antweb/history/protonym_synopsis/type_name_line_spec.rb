8545ea65802421ba8a3fbdf74c372f4af7708e33
jonkerz <jonkerz@gmail.com>
2020-05-22 00:44:08 +0200

    refactor: pass in protonym instead (`TypeNameLine`)

--- spec/exporters/exporters/antweb/history/protonym_synopsis/type_name_line_spec.rb
+++ spec/exporters/exporters/antweb/history/protonym_synopsis/type_name_line_spec.rb
@@ -6,27 +6,23 @@ describe Exporters::Antweb::History::ProtonymSynopsis::TypeNameLine do
   include TestLinksHelpers
 
   describe "#call" do
-    context "when taxon has a type name" do
+    context "when protonym has a type name" do
       let(:type_species) { create :species, name_string: 'Atta major' }
-      let(:taxon) { create :genus }
       let(:type_name) { create :type_name, :by_subsequent_designation_of, taxon: type_species, pages: '1' }
-
-      before do
-        taxon.protonym.update!(type_name: type_name)
-      end
+      let(:protonym) { create :protonym, type_name: type_name }
 
       it "links the type name" do
         reference_link = antweb_reference_link(type_name.reference)
 
-        expect(described_class[taxon]).
+        expect(described_class[protonym]).
           to eq %(Type-species: #{antweb_taxon_link(type_species)}, by subsequent designation of #{reference_link}: 1.)
       end
     end
 
-    context "when taxon does not have a type name" do
-      let(:taxon) { create :genus }
+    context "when protonym does not have a type name" do
+      let(:protonym) { create :protonym }
 
-      specify { expect(described_class[taxon]).to eq "" }
+      specify { expect(described_class[protonym]).to eq "" }
     end
   end
 end
