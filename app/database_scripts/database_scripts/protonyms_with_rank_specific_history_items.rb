d7087b50bbeb3822dbb04feaa968bc0dd142b434
jonkerz <jonkerz@gmail.com>
2020-05-26 20:40:45 +0200

    feat(db-scripts): add ProtonymsWithRankSpecificHistoryItems and ProtonymsWithRankSpecificAndUnrankedHistoryItems

---
+++ app/database_scripts/database_scripts/protonyms_with_rank_specific_history_items.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module DatabaseScripts
+  class ProtonymsWithRankSpecificHistoryItems < DatabaseScript
+    def results
+      Protonym.joins(:history_items).where.not(taxon_history_items: { rank: nil }).distinct
+    end
+
+    def render
+      as_table do |t|
+        t.header 'Protonym', 'Authorship', 'Taxa', 'Ranks of taxa', 'Statuses of taxa'
+        t.rows do |protonym|
+          [
+            protonym.decorate.link_to_protonym,
+            protonym.authorship.reference.keey,
+            protonym.taxa.pluck(:name_cache).join('<br>'),
+            protonym.taxa.pluck(:type).join('<br>'),
+            protonym.taxa.pluck(:status).join('<br>')
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+title: Protonyms with rank-specific history items
+
+section: research
+category: Protonyms
+tags: [new!, slow]
+
+issue_description:
+
+description: >
+  This is for the "Protonym items simulation", and ultimately for moving `TaxonHistoryItem`s to the protonym.
+
+
+  See also wiki page: %wiki7
+
+related_scripts:
+  - ProtonymsWithRankSpecificAndUnrankedHistoryItems
+  - ProtonymsWithRankSpecificHistoryItems
