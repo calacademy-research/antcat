d4f0b591ffdbc5e2b2cbcbd6c7a0cdb8c89265cc
jonkerz <jonkerz@gmail.com>
2020-05-30 10:44:49 +0200

    migration: add unique index on `users.name`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_082307) do
+ActiveRecord::Schema.define(version: 2020_05_30_083150) do
 
   create_table "activities", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "trackable_id"
@@ -360,6 +360,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_082307) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_users_on_author_id", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "ux_users__name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
