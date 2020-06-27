2ae9d1d95c18bf5a772bb4efe5bdd5f293f58e6f
jonkerz <jonkerz@gmail.com>
2020-06-27 09:57:07 +0200

    refactor: move to `Protonyms::semi_normalized_notes_taxt?`

--- app/cleanup/protonyms.rb
+++ app/cleanup/protonyms.rb
@@ -2,6 +2,13 @@
 
 # :nocov:
 module Protonyms
+  SEMI_NORMALIZED_NOTES_TAXTS = [
+    /^ ?\[as subgenus of \{tax|taxac \d+\}\]$/,
+    /^ ?\[as "section" of \{tax|taxac \d+\}\]$/,
+    /^ ?\[as "group" of \{tax|taxac \d+\}\]$/,
+    /^ ?\[as "branch" of \{tax|taxac \d+\}\]$/
+  ]
+
   module_function
 
   # TODO: This does not belong anywhere, but it's a step towards moving data to the protonym.
@@ -17,6 +24,10 @@ module Protonyms
   # TODO: This does not belong anywhere, but it's a step towards moving data to the protonym.
   def taxa_genus_and_subgenus_pair? taxa
     taxa.pluck(:type).sort == %w[Genus Subgenus]
+  end
+
+  def semi_normalized_notes_taxt? notes_taxt
+    SEMI_NORMALIZED_NOTES_TAXTS.any? { |re| re =~ notes_taxt }
   end
 end
 # :nocov:
