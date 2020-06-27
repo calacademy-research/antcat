e6e60ffc20642cf6b46d2f400f1c1b7b7d5250e1
jonkerz <jonkerz@gmail.com>
2020-06-27 09:28:44 +0200

    refactor: always autocomplete authors with `with_ids`

--- app/assets/javascripts/controllers/authors/merges.coffee
+++ app/assets/javascripts/controllers/authors/merges.coffee
@@ -5,5 +5,5 @@ setupAuthorAutocomplete = ->
   $("#author_to_merge_name").autocomplete
     autoFocus: true
     minLength: 1
-    source: '/authors/autocomplete?with_ids=y'
+    source: '/authors/autocomplete'
     select: (event, ui) -> $("#author_to_merge_id").val ui.item.author_id
--- spec/controllers/authors/autocompletes_controller_spec.rb
+++ spec/controllers/authors/autocompletes_controller_spec.rb
@@ -14,26 +14,17 @@ describe Authors::AutocompletesController do
     context 'with results' do
       let!(:author_name) { create :author_name, name: 'Bolton' }
 
-      context 'when `with_ids` is given' do
-        specify do
-          get :show, params: { term: 'bol', with_ids: true, format: :json }
-          expect(json_response).to eq(
-            [
-              {
-                'label' => author_name.name,
-                'author_id' => author_name.author_id,
-                'url' => "/authors/#{author_name.author_id}"
-              }
-            ]
-          )
-        end
-      end
-
-      context 'when `with_ids` is not given' do
-        specify do
-          get :show, params: { term: 'bol', format: :json }
-          expect(json_response).to eq ['Bolton']
-        end
+      specify do
+        get :show, params: { term: 'bol', format: :json }
+        expect(json_response).to eq(
+          [
+            {
+              'label' => author_name.name,
+              'author_id' => author_name.author_id,
+              'url' => "/authors/#{author_name.author_id}"
+            }
+          ]
+        )
       end
     end
   end
