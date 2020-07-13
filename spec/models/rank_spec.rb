beb6b6f42b7cccb395c1ad6d1e5c712e0fae0b41
jonkerz <jonkerz@gmail.com>
2020-07-13 18:33:31 +0200

    tests(rank): meta spec

--- spec/models/rank_spec.rb
+++ spec/models/rank_spec.rb
@@ -4,22 +4,26 @@ require 'rails_helper'
 
 describe Rank do
   describe 'constants' do
-    it "covers all types in '*_GROUP_TYPES'" do
-      types = described_class::FAMILY_GROUP_NAMES + described_class::GENUS_GROUP_NAMES + described_class::SPECIES_GROUP_NAMES
-      expect(types).to eq described_class::SORTED_TYPES
+    specify "'*_GROUP_TYPES'" do
+      it_covers_all_types_without_overlaps :FAMILY_GROUP_NAMES, :GENUS_GROUP_NAMES, :SPECIES_GROUP_NAMES
     end
 
-    it "covers all types in 'ABOVE_SPECIES' and 'SPECIES_GROUP_NAMES'" do
-      expect(described_class::ABOVE_SPECIES + described_class::SPECIES_GROUP_NAMES).to eq described_class::TYPES
+    describe "'ABOVE_*' and 'BELOW_*'" do
+      specify 'ABOVE_GENUS' do
+        it_covers_all_types_without_overlaps :ABOVE_GENUS, :GENUS_AND_BELOW
+      end
+
+      specify 'GENUS_AND_BELOW' do
+        it_covers_all_types_without_overlaps :FAMILY_GROUP_NAMES, :GENUS_AND_BELOW
+      end
+
+      specify 'ABOVE_SPECIES' do
+        it_covers_all_types_without_overlaps :ABOVE_SPECIES, :SPECIES_GROUP_NAMES
+      end
     end
 
-    it "covers all types in 'UNI-BI-TRI-QUADRI-NOMIAL'" do
-      expect(
-        described_class::UNINOMIAL +
-        described_class::BINOMIAL +
-        described_class::TRINOMIAL +
-        described_class::QUADRINOMIAL
-      ).to eq described_class::TYPES
+    specify "'UNI-BI-TRI-QUADRI-NOMIAL'" do
+      it_covers_all_types_without_overlaps :UNINOMIAL, :BINOMIAL, :TRINOMIAL, :QUADRINOMIAL
     end
 
     described_class.constants.select { |const_sym| const_sym.to_s['TYPE'] }.each do |const_sym|
@@ -29,6 +33,11 @@ describe Rank do
 
         expect(types).to eq sorted_types
       end
+    end
+
+    def it_covers_all_types_without_overlaps *rank_collections
+      type_constants = rank_collections.map { |const_sym| described_class.const_get(const_sym) }
+      expect(type_constants.flatten).to eq described_class::TYPES
     end
   end
 
