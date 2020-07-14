ac54d88f5719cc5922a5a41d79d4388e02809539
jonkerz <jonkerz@gmail.com>
2020-07-14 20:43:24 +0200

    tweak: include query params in lazy links

--- app/helpers/dev_helper.rb
+++ app/helpers/dev_helper.rb
@@ -3,10 +3,13 @@
 module DevHelper
   # Show link to localhost on live site and vice versa because I am lazy.
   def link_to_current_page_on_live_site_or_localhost
+    query_params = "?#{request.query_parameters.to_param}" if request.query_parameters
+    path = "#{request.path}#{query_params}"
+
     if Rails.env.development?
-      link_to "on antcat.org", "#{Settings.production_url}#{request.path}", class: "show-on-hover"
+      link_to "on antcat.org", "#{Settings.production_url}#{path}", class: "show-on-hover"
     else
-      link_to "on localhost", "http://localhost:3000#{request.path}", class: "show-on-hover"
+      link_to "on localhost", "http://localhost:3000#{path}", class: "show-on-hover"
     end
   end
 end
