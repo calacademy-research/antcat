366ffcaf324f2250b55d8e85e42cf1f3161c3f69
jonkerz <jonkerz@gmail.com>
2020-07-22 20:16:24 +0200

    refactor(citations): extract into `OrderByPages`

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 class Citation < ApplicationRecord
+  include OrderByPages
+
   belongs_to :reference
 
   # TODO: [grep:notes_taxt].
   has_one :protonym, inverse_of: :authorship, foreign_key: :authorship_id, dependent: :destroy
 
   validates :pages, presence: true
-
-  # NOTE: Good enough for now (MySQL 5). It does not handle Roman numerals or other complicated formats.
-  scope :order_by_pages, -> { order(Arel.sql("CAST(pages AS UNSIGNED), pages")) }
 
   strip_attributes only: [:pages], replace_newlines: true
   has_paper_trail
