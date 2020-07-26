6672f790e2688a4b144b8e5878bc50ea6a5079d4
jonkerz <jonkerz@gmail.com>
2020-07-26 14:25:41 +0200

    feat(catalog): show infrasubspecies in the catalog

--- app/models/name_sti_subclasses/species_group_name_sti_subclasses/infrasubspecies_name.rb
+++ app/models/name_sti_subclasses/species_group_name_sti_subclasses/infrasubspecies_name.rb
@@ -4,4 +4,13 @@ class InfrasubspeciesName < SpeciesGroupName
   def subspecies_epithet
     name_parts[2]
   end
+
+  def short_name
+    [
+      genus_epithet[0] + '.',
+      species_epithet[0] + '.',
+      subspecies_epithet[0] + '.',
+      epithet
+    ].join(' ')
+  end
 end
--- spec/models/name_sti_subclasses/species_group_name_sti_subclasses/infrasubspecies_name_spec.rb
+++ spec/models/name_sti_subclasses/species_group_name_sti_subclasses/infrasubspecies_name_spec.rb
@@ -8,4 +8,10 @@ describe InfrasubspeciesName do
 
     specify { expect(name.subspecies_epithet).to eq 'capa' }
   end
+
+  describe '#short_name' do
+    it 'uses first letter only for genus, species and subspecies epithets' do
+      expect(described_class.new(name: 'Atta baba capa dapa').short_name).to eq 'A. b. c. dapa'
+    end
+  end
 end
