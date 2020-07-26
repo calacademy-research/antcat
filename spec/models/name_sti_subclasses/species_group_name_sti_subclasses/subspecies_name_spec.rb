6729ff6412aaf9d02d2d166550604a7aca27d5d7
jonkerz <jonkerz@gmail.com>
2020-07-26 14:11:53 +0200

    tests: tweaks

--- spec/models/name_sti_subclasses/species_group_name_sti_subclasses/subspecies_name_spec.rb
+++ spec/models/name_sti_subclasses/species_group_name_sti_subclasses/subspecies_name_spec.rb
@@ -3,24 +3,24 @@
 require 'rails_helper'
 
 describe SubspeciesName do
-  describe '#short_name' do
-    it 'uses first letter only for genus and species epithets' do
-      expect(described_class.new(name: 'Atta major minor').short_name).to eq 'A. m. minor'
-      expect(described_class.new(name: 'Atta major var. minor').short_name).to eq 'A. m. var. minor'
+  describe "#subspecies_epithets" do
+    context 'when three name parts' do
+      let(:name) { described_class.new(name: 'Atta baba capa') }
+
+      specify { expect(name.subspecies_epithets).to eq 'capa' }
+    end
+
+    # TODO: Remove now that there are no broken quadrinomials.
+    context 'when four name parts' do
+      let(:name) { described_class.new(name: 'Atta baba capa dapa') }
+
+      specify { expect(name.subspecies_epithets).to eq 'capa dapa' }
     end
   end
 
-  describe "#subspecies_epithets" do
-    context 'when three name parts' do
-      let(:name) { described_class.new(name: 'Atta major minor') }
-
-      specify { expect(name.subspecies_epithets).to eq 'minor' }
-    end
-
-    context 'when four name parts' do
-      let(:name) { described_class.new(name: 'Acus major minor medium') }
-
-      specify { expect(name.subspecies_epithets).to eq 'minor medium' }
+  describe '#short_name' do
+    it 'uses first letter only for genus and species epithets' do
+      expect(described_class.new(name: 'Atta baba capa').short_name).to eq 'A. b. capa'
     end
   end
 end
