540fb9548f8c2e4d17c019efed6207e40e22a6ee
jonkerz <jonkerz@gmail.com>
2020-07-28 18:36:52 +0200

    feat(references): remove confirmation popups for reviews

--- app/presenters/editors/reference_presenter.rb
+++ app/presenters/editors/reference_presenter.rb
@@ -10,16 +10,13 @@ module Editors
     def review_reference_button
       if reference.can_finish_reviewing?
         link_to 'Finish reviewing', finish_reviewing_reference_path(reference),
-          method: :post, class: "btn-saves btn-tiny",
-          data: { confirm: "Have you finished reviewing this reference?" }
+          method: :post, class: "btn-saves btn-tiny"
       elsif reference.can_start_reviewing?
         link_to 'Start reviewing', start_reviewing_reference_path(reference),
-          method: :post, class: "btn-saves btn-tiny",
-          data: { confirm: 'Are you ready to start reviewing this reference?' }
+          method: :post, class: "btn-saves btn-tiny"
       elsif reference.can_restart_reviewing?
         link_to 'Restart reviewing', restart_reviewing_reference_path(reference),
-          method: :post, class: "btn-warning btn-tiny",
-          data: { confirm: 'Do you want to start reviewing this reference again?' }
+          method: :post, class: "btn-warning btn-tiny"
       end
     end
 
