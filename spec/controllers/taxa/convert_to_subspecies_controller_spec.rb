e4d60e01293632cdf5f311a51477bf9ef9b829df
jonkerz <jonkerz@gmail.com>
2020-08-29 10:47:13 +0200

    tests: add missing spec (convert_to_subspecies_controller_spec.rb)

--- spec/controllers/taxa/convert_to_subspecies_controller_spec.rb
+++ spec/controllers/taxa/convert_to_subspecies_controller_spec.rb
@@ -9,4 +9,28 @@ describe Taxa::ConvertToSubspeciesController do
       specify { expect(post(:create, params: { taxa_id: 1 })).to have_http_status :forbidden }
     end
   end
+
+  describe 'POST create', as: :editor do
+    let!(:species) { create :species }
+    let!(:new_species) { create :species, genus: species.genus }
+
+    it 'calls `Taxa::Operations::ConvertToSubspecies`' do
+      expect(Taxa::Operations::ConvertToSubspecies).to receive(:new).with(species, new_species).and_call_original
+      post :create, params: { taxa_id: species.id, new_species_id: new_species.id }
+    end
+
+    it 'creates an activity' do
+      expect { post :create, params: { taxa_id: species.id, new_species_id: new_species.id } }.
+        to change { Activity.count }.by(1)
+
+      activity = Activity.last
+      expect(activity.action).to eq 'convert_species_to_subspecies'
+      expect(activity.trackable).to be_a Subspecies
+      expect(activity.parameters).to eq(
+        name: "<i>#{activity.trackable.name.name}</i>",
+        name_was: "<i>#{species.name.name}</i>",
+        original_species_id: species.id
+      )
+    end
+  end
 end
