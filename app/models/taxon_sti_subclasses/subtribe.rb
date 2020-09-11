e04d199e0aa47e2f28a984e6b1a9ca137c327b05
jonkerz <jonkerz@gmail.com>
2020-09-11 18:09:50 +0200

    feat(names): add support code for `SubtribeName.valid_subtribe_name?`

--- app/models/taxon_sti_subclasses/subtribe.rb
+++ app/models/taxon_sti_subclasses/subtribe.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class Subtribe < Taxon
+  VALID_ENDINGS_REGEX = /(ina|iti)\z/
+
   belongs_to :subfamily
   belongs_to :tribe
+
+  # TODO: Added here for now while refactoring and cleaning up data. Not sure where it really belongs.
+  # NOTE: This method is more like "not_invalid_subtribe_name?", since validations already
+  # present in `Name` (like valid characters) are not repeated here.
+  def self.valid_subtribe_name? name_string
+    return false unless name_string.split.size == 1
+    name_string.match?(VALID_ENDINGS_REGEX)
+  end
 
   def parent
     tribe
