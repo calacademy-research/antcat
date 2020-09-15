4ad33b9551a0319e31ba23e9afe5407f59a07934
Joe Russack <jrussack@calacademy.org>
2020-09-15 14:40:21 -0700

    revved docker web server version

--- docker/web_server_daemon.sh
+++ docker/web_server_daemon.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ../database_export
-docker run -d --restart unless-stopped --name web -p 9090:80 -v $PWD:/usr/local/apache2/htdocs/ httpd:2.4
+docker run -d --restart unless-stopped --name web -p 9090:80 -v $PWD:/usr/local/apache2/htdocs/ httpd:latest
--- docker/web_server_interactive.sh
+++ docker/web_server_interactive.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ../database_export
 
-docker run -it --rm --name web -p 9090:80 -v $PWD:/usr/local/apache2/htdocs/ httpd:2.4
+docker run -it --rm --name web -p 9090:80 -v $PWD:/usr/local/apache2/htdocs/ httpd:latest
