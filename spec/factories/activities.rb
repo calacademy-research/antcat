7498b8e4b08f541e729cf6e67adca5966e8ab113
jonkerz <jonkerz@gmail.com>
2020-09-17 19:52:27 +0200

    feat(activities): validate `#user`

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -2,9 +2,14 @@
 
 FactoryBot.define do
   factory :activity do
-    user { nil }
     trackable factory: :journal
     action { "create" }
+
+    with_user
+
+    trait :with_user do
+      user
+    end
 
     trait :execute_script do
       trackable { nil }
