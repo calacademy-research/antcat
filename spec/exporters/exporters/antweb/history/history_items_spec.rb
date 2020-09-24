74cf917a9ecaf7cfe4c283083e9d3b44c74e7b31
jonkerz <jonkerz@gmail.com>
2020-09-24 20:56:03 +0200

    feat: remove concept for virtual history items
    Since it is only used for subspecies lists, which are now shown in the
    catalog.
    
    The concept would not work as well once we move history items to protonyms, and
    it can be restored later if we need it again.

--- spec/exporters/exporters/antweb/history/history_items_spec.rb
+++ spec/exporters/exporters/antweb/history/history_items_spec.rb
@@ -28,24 +28,5 @@ describe Exporters::Antweb::History::HistoryItems do
 
       specify { expect(described_class[taxon].html_safe?).to eq true }
     end
-
-    context 'when taxon has history items and virtual history items' do
-      let(:taxon) { create :species }
-      let!(:subspecies) { create :subspecies, name_string: 'Lasius niger aa', species: taxon }
-      let(:subspecies_label) { '<i>L. n. aa</i>' }
-
-      before do
-        create :taxon_history_item, taxon: taxon, taxt: "Taxon: {tax #{taxon.id}}"
-      end
-
-      specify do
-        item = "<div>Taxon: #{antweb_taxon_link(taxon)}.</div>"
-        virtual_item = "<div>Current subspecies: nominal plus #{antweb_taxon_link(subspecies, subspecies_label)}.</div>"
-
-        expect(described_class[taxon]).to eq(header + '<div>' + item + virtual_item + '</div>')
-      end
-
-      specify { expect(described_class[taxon].html_safe?).to eq true }
-    end
   end
 end
