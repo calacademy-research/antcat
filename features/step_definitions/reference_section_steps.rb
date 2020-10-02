e0e6b5cead39488f1846cba406e11d795db44bfd
jonkerz <jonkerz@gmail.com>
2020-10-02 23:26:30 +0200

    tests: merge step files

--- features/step_definitions/reference_section_steps.rb
+++ features/step_definitions/reference_section_steps.rb
@@ -15,3 +15,13 @@ Given("there is a reference section for {string} that includes a tag for {string
 
   create :reference_section, references_taxt: "{tax #{tag_taxon.id}}", taxon: taxon
 end
+
+# Editing.
+Then("the reference section should be empty") do
+  expect(page).to_not have_css '#reference-sections .reference_section'
+end
+
+Then("the reference section should be {string}") do |content|
+  element = first('#references-section').find('.taxt-presenter')
+  expect(element.text).to match /#{content}/
+end
--- features/step_definitions/taxon_history_item_steps.rb
+++ features/step_definitions/taxon_history_item_steps.rb
@@ -14,3 +14,40 @@ Given("there is a subfamily {string} with a history item {string} and a markdown
   taxt = "#{content} {ref #{reference.id}}"
   step %(there is a subfamily "#{name}" with a history item "#{taxt}")
 end
+
+# Editing.
+Then("the history should be {string}") do |content|
+  element = first('#history-items').find('.taxt-presenter')
+  expect(element.text).to match /#{content}/
+end
+
+Then("the history item field should be {string}") do |content|
+  element = first('#history-items').find('textarea')
+  expect(element.text).to match /#{content}/
+end
+
+Then("the history item field should not be visible") do
+  expect(page).to_not have_css '#history-items textarea'
+end
+
+Then("the history item field should be visible") do
+  expect(page).to have_css '#history-items textarea'
+end
+
+Then("the history should be empty") do
+  expect(page).to_not have_css '#history-items .history-item'
+end
+
+When("I add a history item {string}") do |content|
+  step %(I click on the add taxon history item button)
+  step %(I fill in "taxt" with "#{content}")
+  step %(I press "Save")
+end
+
+When("I update the most recent history item to say {string}") do |content|
+  TaxonHistoryItem.last.update!(taxt: content)
+end
+
+When("I delete the most recent history item") do
+  TaxonHistoryItem.last.destroy
+end
