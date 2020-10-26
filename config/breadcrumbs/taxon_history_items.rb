e624a499d69857b1c5fe1d111e0361a94457834d
jonkerz <jonkerz@gmail.com>
2020-10-26 19:08:14 +0100

    feat: rename to "history items" (breadcrumbs)

--- config/breadcrumbs/taxon_history_items.rb
+++ config/breadcrumbs/taxon_history_items.rb
@@ -1,35 +1,35 @@
 # frozen_string_literal: true
 
-crumb :taxon_history_items do |protonym, protonym_fallback_link|
+crumb :history_items do |protonym, protonym_fallback_link|
   link "History Items"
   parent :protonym, protonym, protonym_fallback_link
 end
 
-crumb :taxon_history_item do |history_item, protonym_fallback_link|
+crumb :history_item do |history_item, protonym_fallback_link|
   if history_item.persisted?
     link "##{history_item.id}", taxon_history_item_path(history_item)
   else
     link "##{history_item.id} [deleted]"
   end
-  parent :taxon_history_items, history_item.protonym, protonym_fallback_link
+  parent :history_items, history_item.protonym, protonym_fallback_link
 end
 
-crumb :edit_taxon_history_item do |history_item|
+crumb :edit_history_item do |history_item|
   link "Edit"
-  parent :taxon_history_item, history_item
+  parent :history_item, history_item
 end
 
-crumb :taxon_history_item_history do |history_item|
+crumb :history_item_history do |history_item|
   protonym_fallback_link =
     if history_item.protonym.nil?
       "[no protonym ID] (previous taxon ID: #{history_item.taxon_id || '[no ID]'})"
     end
 
   link "History"
-  parent :taxon_history_item, history_item, protonym_fallback_link
+  parent :history_item, history_item, protonym_fallback_link
 end
 
-crumb :new_taxon_history_item do |history_item|
+crumb :new_history_item do |history_item|
   link "New"
-  parent :taxon_history_items, history_item.protonym
+  parent :history_items, history_item.protonym
 end
