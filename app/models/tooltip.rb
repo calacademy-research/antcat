05a162194025d454115948c5cca3234660f9d846
jonkerz <jonkerz@gmail.com>
2020-11-08 19:52:58 +0100

    feat(tooltips): validate format of scope

--- app/models/tooltip.rb
+++ app/models/tooltip.rb
@@ -6,6 +6,7 @@ class Tooltip < ApplicationRecord
   validates :scope, :text, presence: true
   validates :key, presence: true, uniqueness: { case_sensitive: true },
     format: { with: /\A[a-zA-Z0-9._:\-]+\z/, message: "can only contain alphanumeric characters and '.-_:'" }
+  validates :scope, format: { with: /\A[a-z_]+\z/, message: "can only contain lowercase letters and '_'" }
 
   has_paper_trail
   trackable parameters: proc { { scope_and_key: "#{scope}.#{key}" } }
