1c427b22cd7a895cd10ce48ae3dec199442f5e50
jonkerz <jonkerz@gmail.com>
2020-11-20 22:33:50 +0100

    feat(wiki/tooltips): add null object `MissingWikiPage`

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -9,6 +9,16 @@ class WikiPage < ApplicationRecord
     NEW_CONTRIBUTORS_HELP_PAGE = 'new_contributors_help_page'
   ]
 
+  MissingWikiPage = Struct.new(:permanent_identifier) do
+    def title
+      "Error: Could not find wiki page '#{permanent_identifier}'"
+    end
+
+    def to_model
+      WikiPage.new
+    end
+  end
+
   validates :title, presence: true, length: { maximum: TITLE_MAX_LENGTH }, uniqueness: { case_sensitive: true }
   validates :content, presence: true
   validates :permanent_identifier, uniqueness: { case_sensitive: true, allow_nil: true }
@@ -17,4 +27,8 @@ class WikiPage < ApplicationRecord
   trackable parameters: proc { { title: title } }
 
   scope :featured, -> { where(featured: true) }
+
+  def self.from_permanent_identifier_or_missing permanent_identifier
+    find_by(permanent_identifier: permanent_identifier) || MissingWikiPage.new(permanent_identifier)
+  end
 end
