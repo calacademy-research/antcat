60ab3d67b9822bb3cfc6d78596af855e3d5501d3
jonkerz <jonkerz@gmail.com>
2020-11-21 02:12:34 +0100

    tweak(tooltips): validate and standardize key format

--- app/models/tooltip.rb
+++ app/models/tooltip.rb
@@ -5,7 +5,7 @@ class Tooltip < ApplicationRecord
 
   validates :scope, :text, presence: true
   validates :key, presence: true, uniqueness: { case_sensitive: true },
-    format: { with: /\A[a-zA-Z0-9._:\-]+\z/, message: "can only contain alphanumeric characters and '.-_:'" }
+    format: { with: /\A[a-zA-Z0-9_]+\z/, message: "can only contain alphanumeric characters and '_'" }
   validates :scope, format: { with: /\A[a-z_]+\z/, message: "can only contain lowercase letters and '_'" }
 
   has_paper_trail
