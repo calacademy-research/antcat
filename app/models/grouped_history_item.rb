9e1d8e6ed28fe1d81f6cddd27da93164f8ae5295
jonkerz <jonkerz@gmail.com>
2020-12-21 22:06:45 +0100

    feat(history): show type of grouped items

--- app/models/grouped_history_item.rb
+++ app/models/grouped_history_item.rb
@@ -4,7 +4,7 @@ class GroupedHistoryItem
   attr_reader :items
 
   # TODO: This should be delegated to the definiton once they have been extracted into new classes.
-  delegate :groupable?, :group_key, :type, to: :any_item_in_group
+  delegate :underscored_type, :groupable?, :group_key, :type, to: :any_item_in_group
 
   def initialize items
     @items = items
@@ -20,6 +20,10 @@ class GroupedHistoryItem
 
   def grouped?
     !items.one?
+  end
+
+  def number_of_items
+    items.size
   end
 
   private
