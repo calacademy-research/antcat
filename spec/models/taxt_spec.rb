a0c16f509e7d596d14bfd3e8b4d4d11c5c7de9a3
jonkerz <jonkerz@gmail.com>
2020-12-24 22:10:42 +0100

    feat(taxt): add support code for protonym tags

--- spec/models/taxt_spec.rb
+++ spec/models/taxt_spec.rb
@@ -11,4 +11,58 @@ describe Taxt do
       expect(described_class.to_ref_tag(reference.id)).to eq "{ref #{reference.id}}"
     end
   end
+
+  describe '.extract_ids_from_taxon_tags' do
+    context 'without matching tags' do
+      specify do
+        expect(described_class.extract_ids_from_taxon_tags("{taxa 123}")).to eq []
+        expect(described_class.extract_ids_from_taxon_tags("{tax 123d}")).to eq []
+
+        expect(described_class.extract_ids_from_taxon_tags("{pro 123}")).to eq []
+      end
+    end
+
+    context 'with matching tags' do
+      specify do
+        expect(described_class.extract_ids_from_taxon_tags("{tax 123}")).to eq [123]
+
+        expect(described_class.extract_ids_from_taxon_tags("pizza {tax 123} pizza")).to eq [123]
+        expect(described_class.extract_ids_from_taxon_tags("pizza {taxac 123} pizza")).to eq [123]
+      end
+    end
+
+    context 'with mixed tags' do
+      specify do
+        expect(described_class.extract_ids_from_taxon_tags("pizza {taxac 1} {tax 2} {ref 3}")).to eq [1, 2]
+      end
+    end
+  end
+
+  describe '.extract_ids_from_protonym_tags' do
+    context 'without matching tags' do
+      specify do
+        expect(described_class.extract_ids_from_protonym_tags("{prot 123}")).to eq []
+        expect(described_class.extract_ids_from_protonym_tags("{pro 123d}")).to eq []
+
+        expect(described_class.extract_ids_from_protonym_tags("{tax 123}")).to eq []
+      end
+    end
+
+    context 'with matching tags' do
+      specify do
+        expect(described_class.extract_ids_from_protonym_tags("{pro 123}")).to eq [123]
+
+        expect(described_class.extract_ids_from_protonym_tags("pizza {pro 123} pizza")).to eq [123]
+        expect(described_class.extract_ids_from_protonym_tags("pizza {prott 123} pizza")).to eq [123]
+        expect(described_class.extract_ids_from_protonym_tags("pizza {proac 123} pizza")).to eq [123]
+        expect(described_class.extract_ids_from_protonym_tags("pizza {prottac 123} pizza")).to eq [123]
+      end
+    end
+
+    context 'with mixed tags' do
+      specify do
+        expect(described_class.extract_ids_from_protonym_tags("pizza {pro 1} {prott 2} {ref 3}")).to eq [1, 2]
+      end
+    end
+  end
 end
