3e7cc7ff58e9b56c2ea0a95043d3c841c17b7b88
jonkerz <jonkerz@gmail.com>
2021-01-16 18:59:12 +0100

    tweaks: more various tweaks

--- db/migrate/20200617215601_rename_taxa_current_valid_taxon_to_current_taxon.rb
+++ db/migrate/20200617215601_rename_taxa_current_valid_taxon_to_current_taxon.rb
@@ -3,7 +3,6 @@
 class RenameTaxaCurrentValidTaxonToCurrentTaxon < ActiveRecord::Migration[6.0]
   def change
     safety_assured do
-      # TODO: Rename indexes and constraints.
       rename_column :taxa, :current_valid_taxon_id, :current_taxon_id
     end
   end
--- spec/support/test_links_helpers.rb
+++ spec/support/test_links_helpers.rb
@@ -9,30 +9,49 @@ module TestLinksHelpers
 
   def taxon_link taxon, label = nil
     css_classes = CatalogFormatter.taxon_disco_mode_css(taxon)
-    %(<a v-hover-taxon="#{taxon.id}" class="#{css_classes}" href="/catalog/#{taxon.id}">#{label || taxon.name_with_fossil}</a>)
+
+    <<~HTML.squish
+      <a v-hover-taxon="#{taxon.id}" class="#{css_classes}"
+      href="/catalog/#{taxon.id}">#{label || taxon.name_with_fossil}</a>
+    HTML
   end
 
   def taxon_link_with_author_citation taxon
     css_classes = CatalogFormatter.taxon_disco_mode_css(taxon)
-    %(<a v-hover-taxon="#{taxon.id}" class="#{css_classes}" href="/catalog/#{taxon.id}">#{taxon.name_with_fossil}</a> #{taxon.author_citation})
+
+    <<~HTML.squish
+      <a v-hover-taxon="#{taxon.id}" class="#{css_classes}"
+      href="/catalog/#{taxon.id}">#{taxon.name_with_fossil}</a> #{taxon.author_citation}
+    HTML
   end
 
   def protonym_link protonym
-    %(<a v-hover-protonym="#{protonym.id}" class="protonym" href="/protonyms/#{protonym.id}">#{protonym.name.name_html}</a>)
+    <<~HTML.squish
+      <a v-hover-protonym="#{protonym.id}" class="protonym"
+      href="/protonyms/#{protonym.id}">#{protonym.name.name_html}</a>
+    HTML
   end
 
   def reference_link reference
-    %(<a v-hover-reference="#{reference.id}" href="/references/#{reference.id}">#{reference.key_with_suffixed_year}</a>)
+    <<~HTML.squish
+      <a v-hover-reference="#{reference.id}"
+      href="/references/#{reference.id}">#{reference.key_with_suffixed_year}</a>
+    HTML
   end
 
-  # rubocop:disable Layout/LineLength
   def reference_taxt_link reference
-    %(<a v-hover-reference="#{reference.id}" class="taxt-hover-reference" href="/references/#{reference.id}">#{reference.key_with_suffixed_year}</a>)
+    <<~HTML.squish
+      <a v-hover-reference="#{reference.id}" class="taxt-hover-reference"
+      href="/references/#{reference.id}">#{reference.key_with_suffixed_year}</a>
+    HTML
   end
-  # rubocop:enable Layout/LineLength
 
   def taxon_authorship_link taxon
     reference = taxon.authorship_reference
-    %(<a v-hover-reference="#{reference.id}" href="/references/#{reference.id}">#{taxon.author_citation}</a>)
+
+    <<~HTML.squish
+      <a v-hover-reference="#{reference.id}"
+      href="/references/#{reference.id}">#{taxon.author_citation}</a>
+    HTML
   end
 end
