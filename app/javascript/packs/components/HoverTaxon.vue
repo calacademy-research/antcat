6902bd1bdbfb41f5d3202b213dac18dbadf26ee9
jonkerz <jonkerz@gmail.com>
2021-01-16 21:15:17 +0100

    feat(pickers): add hover record components

---
+++ app/javascript/packs/components/HoverTaxon.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>
+    <a :href="itemUrl" @mouseover="onMouseover" @mouseleave="onMouseleave">
+      <span :class="item.cssClasses" v-html="item.nameWithFossil" />
+
+      <div v-show="isHover" class="vue-hover-preview-component" @mouseleave="onMouseleave">
+        <span ref="preview_content" @mouseleave="onMouseleave" v-html="previewContent" />
+      </div>
+    </a>
+
+    <span class="discret-author-citation" v-html="item.authorCitation" />
+  </span>
+</template>
+
+<script>
+import HoverRecord from '@mixins/HoverRecord'
+import { previewTaxonUrl } from '@config/ApiUrls'
+import CachedFetcher from '@services/CachedFetcher'
+
+const fetcher = new CachedFetcher(previewTaxonUrl)
+
+export default {
+  name: 'HoverTaxon',
+  mixins: [HoverRecord],
+  methods: {
+    fetcher() {
+      return fetcher
+    },
+  },
+}
+</script>
