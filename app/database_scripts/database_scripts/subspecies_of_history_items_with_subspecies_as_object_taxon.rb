d552ac0f1d83903f32e97828a0e5d989afea2f24
jonkerz <jonkerz@gmail.com>
2021-02-05 19:33:42 +0100

    feat(db-scripts): add SubspeciesOfHistoryItemsWithSubspeciesAsObjectTaxon

---
+++ app/database_scripts/database_scripts/subspecies_of_history_items_with_subspecies_as_object_taxon.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module DatabaseScripts
+  class SubspeciesOfHistoryItemsWithSubspeciesAsObjectTaxon < DatabaseScript
+    LIMIT = 500
+
+    def statistics
+      <<~STR.html_safe
+        Results: #{results.limit(nil).count} (showing first #{LIMIT})<br>
+      STR
+    end
+
+    def results
+      HistoryItem.where(type: History::Definitions::SUBSPECIES_OF).joins(:object_taxon).
+        where(taxa: { type: Rank::SUBSPECIES }).limit(LIMIT)
+    end
+
+    def render
+      as_table do |t|
+        t.header 'History item', 'Protonym', 'to_taxt'
+        t.rows do |history_item|
+          taxt = history_item.to_taxt
+          protonym = history_item.protonym
+
+          [
+            link_to(history_item.id, history_item_path(history_item)),
+            protonym.decorate.link_to_protonym,
+            taxt
+          ]
+        end
+      end
+    end
+  end
+end
+
+__END__
+
+title: >
+  'Subspecies of' history items with subspecies as object taxon
+
+section: research
+category: History
+tags: [new!]
+
+description: >
+
+related_scripts:
+  - SubspeciesOfHistoryItemsWithSubspeciesAsObjectTaxon
