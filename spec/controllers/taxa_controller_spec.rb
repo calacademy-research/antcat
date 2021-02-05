e168534942ee2c17615055a90db837c9ad7d4479
jonkerz <jonkerz@gmail.com>
2021-02-05 18:03:25 +0100

    test: split taxon and protonym specs per rank

--- spec/controllers/taxa_controller_spec.rb
+++ spec/controllers/taxa_controller_spec.rb
@@ -49,141 +49,216 @@ describe TaxaController do
       )
     end
 
-    context 'with valid params' do
-      # TODO: Add spec for species rank for things like `biogeographic_region`, `forms`, and `locality`.
-      context "when rank is genus" do
-        let(:parent) { create :subfamily }
-        let(:genus_params) do
-          {
-            parent_id: parent.id,
-            rank_to_create: Rank::GENUS,
-            taxon_name_string: "Atta",
-            protonym_name_string: "Atta"
-          }
+    describe "general attributes not specific for a single rank (using a genus as a stand-in)" do
+      let(:parent) { create :subfamily }
+      let(:params_without_taxon) do
+        {
+          parent_id: parent.id,
+          rank_to_create: Rank::GENUS,
+          taxon_name_string: "Atta",
+          protonym_name_string: "Atta"
+        }
+      end
+
+      describe "most minimal amount of params" do
+        it "creates a record" do
+          params = params_without_taxon.merge(taxon: base_params)
+          expect { post :create, params: params }.to change { Taxon.count }.by(1)
+        end
+      end
+
+      describe "general taxon attributes" do
+        let(:taxon_params) do
+          base_params.deep_merge(
+            fossil: true,
+            unresolved_homonym: true
+          )
         end
 
-        describe "most minimal amount of params" do
-          it "creates a record" do
-            params = genus_params.merge(taxon: base_params)
-            expect { post :create, params: params }.to change { Taxon.count }.by(1)
+        specify do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
 
-            taxon = Taxon.last
-            expect(taxon.subfamily).to eq parent
-            expect(taxon).to be_a Genus
-          end
+          taxon = Taxon.last
+
+          expect(taxon.status).to eq taxon_params[:status]
+          expect(taxon.fossil).to eq taxon_params[:fossil]
+          expect(taxon.unresolved_homonym).to eq taxon_params[:unresolved_homonym]
         end
+      end
 
-        describe "basic taxon attributes" do
-          let(:taxon_params) do
-            base_params.deep_merge(
-              incertae_sedis_in: Rank::FAMILY,
+      describe "general protonym attributes" do
+        let(:taxon_params) do
+          base_params.deep_merge(
+            protonym_attributes: {
               fossil: true,
-              original_combination: true,
-              collective_group_name: true,
-              unresolved_homonym: true,
-              hong: true
-            )
-          end
-
-          specify do
-            post :create, params: genus_params.merge(taxon: taxon_params)
-
-            taxon = Taxon.last
-
-            expect(taxon.status).to eq taxon_params[:status]
-            expect(taxon.incertae_sedis_in).to eq taxon_params[:incertae_sedis_in]
-            expect(taxon.fossil).to eq taxon_params[:fossil]
-            expect(taxon.original_combination).to eq taxon_params[:original_combination]
-            expect(taxon.collective_group_name).to eq taxon_params[:collective_group_name]
-            expect(taxon.unresolved_homonym).to eq taxon_params[:unresolved_homonym]
-            expect(taxon.hong).to eq taxon_params[:hong]
-          end
-        end
-
-        describe "protonym attributes" do
-          let(:taxon_params) do
-            base_params.deep_merge(
-              protonym_attributes: {
-                fossil: true,
-                sic: true,
-                primary_type_information_taxt: "primary type information",
-                secondary_type_information_taxt: "secondary type information",
-                type_notes_taxt: "type notes",
-                notes_taxt: "notes taxt",
-                authorship_attributes: {
-                  pages: "21 pp."
-                }
+              sic: true,
+              notes_taxt: "notes taxt",
+              authorship_attributes: {
+                pages: "21 pp."
               }
-            )
-          end
-
-          it 'sets protonym_attributes' do
-            post :create, params: genus_params.merge(taxon: taxon_params)
-
-            protonym = Taxon.last.protonym
-            protonym_attributes = taxon_params[:protonym_attributes]
-
-            expect(protonym.fossil).to eq protonym_attributes[:fossil]
-            expect(protonym.sic).to eq protonym_attributes[:sic]
-            expect(protonym.primary_type_information_taxt).to eq protonym_attributes[:primary_type_information_taxt]
-            expect(protonym.secondary_type_information_taxt).to eq protonym_attributes[:secondary_type_information_taxt]
-            expect(protonym.type_notes_taxt).to eq protonym_attributes[:type_notes_taxt]
-            expect(protonym.notes_taxt).to eq protonym_attributes[:notes_taxt]
-          end
-
-          it 'sets authorship_attributes' do
-            post :create, params: genus_params.merge(taxon: taxon_params)
-
-            authorship = Taxon.last.protonym.authorship
-            authorship_attributes = taxon_params[:protonym_attributes][:authorship_attributes]
-
-            expect(authorship.pages).to eq authorship_attributes[:pages]
-          end
-
-          context 'when `protonym_id` is supplied' do
-            let(:protonym) { create :protonym }
-
-            it 'uses the ID and ignores the protonym attributes' do
-              post :create, params: genus_params.merge(taxon: taxon_params.merge(protonym_id: protonym.id))
-              expect(Taxon.last.protonym.id).to eq protonym.id
-            end
-          end
+            }
+          )
         end
 
-        describe "setting `current_taxon`" do
-          let(:current_taxon) { create :genus }
-          let(:taxon_params) do
-            base_params.deep_merge(
-              status: Status::SYNONYM,
-              current_taxon_id: current_taxon.id
-            )
-          end
+        it 'sets protonym_attributes' do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
 
-          specify do
-            post :create, params: genus_params.merge(taxon: taxon_params)
-            expect(Taxon.last.current_taxon).to eq current_taxon
+          protonym = Taxon.last.protonym
+          protonym_attributes = taxon_params[:protonym_attributes]
+
+          expect(protonym.fossil).to eq protonym_attributes[:fossil]
+          expect(protonym.sic).to eq protonym_attributes[:sic]
+
+          expect(protonym.notes_taxt).to eq protonym_attributes[:notes_taxt]
+        end
+
+        it 'sets authorship_attributes' do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
+
+          authorship = Taxon.last.protonym.authorship
+          authorship_attributes = taxon_params[:protonym_attributes][:authorship_attributes]
+
+          expect(authorship.pages).to eq authorship_attributes[:pages]
+        end
+
+        context 'when `protonym_id` is supplied' do
+          let(:protonym) { create :protonym }
+
+          it 'uses the ID and ignores the protonym attributes' do
+            post :create, params: params_without_taxon.merge(taxon: taxon_params.merge(protonym_id: protonym.id))
+            expect(Taxon.last.protonym.id).to eq protonym.id
           end
         end
       end
 
-      context 'with non-matching name type' do
-        let(:parent) { create :subfamily }
-        let(:genus_params) do
-          {
-            parent_id: parent.id,
-            rank_to_create: Rank::GENUS,
-            taxon_name_string: "Attini",
-            protonym_name_string: "Atta"
-          }
+      describe "setting `current_taxon`" do
+        let(:current_taxon) { create :genus }
+        let(:taxon_params) do
+          base_params.deep_merge(
+            status: Status::SYNONYM,
+            current_taxon_id: current_taxon.id
+          )
         end
 
-        it "does not create a record" do
-          params = genus_params.merge(taxon: base_params)
-          expect { post :create, params: params }.to_not change { Taxon.count }
+        specify do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
+          expect(Taxon.last.current_taxon).to eq current_taxon
+        end
+      end
+    end
 
-          taxon_assign = assigns(:taxon) # TODO: Hmm.
-          expect(taxon_assign.errors.empty?).to eq false
-          expect(taxon_assign.errors[:base]).to eq ["Rank (`Genus`) and name type (`TribeName`) must match."]
+    context 'with non-matching name type' do
+      let(:parent) { create :subfamily }
+      let(:params_without_taxon) do
+        {
+          parent_id: parent.id,
+          rank_to_create: Rank::GENUS,
+          taxon_name_string: "Attini",
+          protonym_name_string: "Atta"
+        }
+      end
+
+      it "does not create a record" do
+        params = params_without_taxon.merge(taxon: base_params)
+        expect { post :create, params: params }.to_not change { Taxon.count }
+
+        taxon_assign = assigns(:taxon) # TODO: Hmm.
+        expect(taxon_assign.errors.empty?).to eq false
+        expect(taxon_assign.errors[:base]).to eq ["Rank (`Genus`) and name type (`TribeName`) must match."]
+      end
+    end
+
+    context "when rank is genus" do
+      let(:parent) { create :subfamily }
+      let(:params_without_taxon) do
+        {
+          parent_id: parent.id,
+          rank_to_create: Rank::GENUS,
+          taxon_name_string: "Atta",
+          protonym_name_string: "Atta"
+        }
+      end
+
+      describe "genus-group taxon attributes" do
+        let(:taxon_params) do
+          base_params.deep_merge(
+            incertae_sedis_in: Rank::FAMILY,
+            fossil: true,
+            collective_group_name: true,
+            hong: true
+          )
+        end
+
+        it 'creates a genus record' do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
+
+          taxon = Taxon.last
+
+          expect(taxon).to be_a Genus
+          expect(taxon.subfamily).to eq parent
+
+          expect(taxon.incertae_sedis_in).to eq taxon_params[:incertae_sedis_in]
+          expect(taxon.collective_group_name).to eq taxon_params[:collective_group_name]
+          expect(taxon.hong).to eq taxon_params[:hong]
+        end
+      end
+    end
+
+    context "when rank is species" do
+      let(:parent) { create :genus }
+      let(:params_without_taxon) do
+        {
+          parent_id: parent.id,
+          rank_to_create: Rank::SPECIES,
+          taxon_name_string: "Atta bapa",
+          protonym_name_string: "Atta bapa"
+        }
+      end
+
+      describe "species-group taxon attributes" do
+        let(:taxon_params) do
+          base_params.deep_merge(
+            original_combination: true
+          )
+        end
+
+        it 'creates a species record' do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
+
+          taxon = Taxon.last
+
+          expect(taxon).to be_a Species
+          expect(taxon.genus).to eq parent
+
+          expect(taxon.original_combination).to eq taxon_params[:original_combination]
+        end
+      end
+
+      describe "species-group protonym attributes" do
+        let(:taxon_params) do
+          base_params.deep_merge(
+            protonym_attributes: {
+              locality: "San Francisco",
+              forms: "w.",
+              primary_type_information_taxt: "primary type information",
+              secondary_type_information_taxt: "secondary type information",
+              type_notes_taxt: "type notes"
+            }
+          )
+        end
+
+        it 'sets protonym_attributes' do
+          post :create, params: params_without_taxon.merge(taxon: taxon_params)
+
+          protonym = Taxon.last.protonym
+          protonym_attributes = taxon_params[:protonym_attributes]
+
+          expect(protonym.locality).to eq protonym_attributes[:locality]
+          expect(protonym.forms).to eq protonym_attributes[:forms]
+
+          expect(protonym.primary_type_information_taxt).to eq protonym_attributes[:primary_type_information_taxt]
+          expect(protonym.secondary_type_information_taxt).to eq protonym_attributes[:secondary_type_information_taxt]
+          expect(protonym.type_notes_taxt).to eq protonym_attributes[:type_notes_taxt]
         end
       end
     end
