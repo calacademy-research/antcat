035ee658b0c2484f3fe3fb69512710617fbe23e2
Fredrik Palmkron <jonkerz@gmail.com>
2021-04-03 21:33:21 +0200

    refactor: remove config now handled in 'app/assets/config/manifest.js'

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -24,42 +24,5 @@ Rails.application.config.assets.precompile += %w[
   jquery-ui/widgets/draggable.js
   jquery-ui/widgets/sortable.js
 
-  application.js
-  logged_in_only.js
-  markdown_and_friends.js
-
-  check_name_conflicts.js
-  highlight_text.js
   jquery3.js
-  locality_autocompletion.js
-  reorder_items.js
-  revision_comparer.js
-  sortable_tables.js
-  taxt_editor.js
-
-  controllers/activities/scroll_to_highlighted_activity.js
-  controllers/authors/merges.js
-  controllers/catalog/search/authors_autocompletion.js
-  controllers/feedbacks/show.js
-  controllers/references/form.js
-  controllers/taxa/copy_name_to_protonym.js
-  controllers/taxa/move_items/select_checkboxes.js
-
-  globals/create_copy_buttons.js
-
-  logged_in_only/history_item_form
-]
-
-Rails.application.config.assets.precompile += %w[
-  diffy.css
-  font_awesome_and_custom_icons.css
-  foundation_and_overrides.css
-  logged_in_only.css
-  vendor.css
-  vue.css
-
-  controllers/*.css
-  env_specific/*.css
-  site/*.css
-  widgets/*.css
 ]
