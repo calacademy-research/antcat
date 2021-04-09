27b9ec0858a5741009a36dc8a6b3c0abd5b180df
Fredrik Palmkron <jonkerz@gmail.com>
2021-04-09 21:17:08 +0200

    fix(api): fix assets loading and remove duplicated swagger key

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_tree ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_directory ../stylesheets/env_specific .css
+
+//= link grape_swagger_rails/application.js
+//= link grape_swagger_rails/application.css
