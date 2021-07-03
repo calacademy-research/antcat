c0fd09ba2b63d9075b88030ce953668f3628a1c3
Fredrik Palmkron <jonkerz@gmail.com>
2021-07-03 19:51:40 +0200

    feat: just show raw changes of protonyms/institutions/references

--- app/controllers/institutions/histories_controller.rb
+++ app/controllers/institutions/histories_controller.rb
@@ -4,10 +4,7 @@ module Institutions
   class HistoriesController < ApplicationController
     def show
       @comparer = RevisionComparer.new(Institution, params[:institution_id], params[:selected_id], params[:diff_with_id])
-      @revision_presenter = RevisionPresenter.new(
-        comparer: @comparer,
-        template: "institutions/histories/compare_revision_template"
-      )
+      @revision_presenter = RevisionPresenter.new(comparer: @comparer)
     end
   end
 end
--- app/controllers/protonyms/histories_controller.rb
+++ app/controllers/protonyms/histories_controller.rb
@@ -4,10 +4,7 @@ module Protonyms
   class HistoriesController < ApplicationController
     def show
       @comparer = RevisionComparer.new(Protonym, params[:protonym_id], params[:selected_id], params[:diff_with_id])
-      @revision_presenter = RevisionPresenter.new(
-        comparer: @comparer,
-        template: "protonyms/histories/compare_revision_template"
-      )
+      @revision_presenter = RevisionPresenter.new(comparer: @comparer)
     end
   end
 end
