de5dd6d3000cacf51e556ccf82a350d27101cd6b
Fredrik Palmkron <jonkerz@gmail.com>
2022-02-13 00:01:57 +0100

    feat(config): embrace new framework defaults for rails 7.0

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module AntCat
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     config.middleware.insert_before(0, Rack::Cors) do
       allow do
