9f63c7bbc04847940e980000dc357503740f3e45
Fredrik Palmkron <jonkerz@gmail.com>
2022-02-25 21:32:27 +0100

    refactor(versions): rename variables

--- app/models/revision_comparer.rb
+++ app/models/revision_comparer.rb
@@ -3,11 +3,13 @@
 class RevisionComparer
   attr_reader :most_recent, :revisions, :selected, :diff_with
 
-  def initialize klass, id, selected_id = nil, diff_with_id = nil
+  def initialize item_class, item_id, selected_id = nil, diff_with_id = nil
+    @item_class = item_class
+    @item_id = item_id
     @selected_id = selected_id
     @diff_with_id = diff_with_id
 
-    set_most_recent_and_revisions klass, id
+    set_most_recent_and_revisions
 
     @selected = find_revision selected_id
     @diff_with = find_revision diff_with_id
@@ -33,18 +35,18 @@ class RevisionComparer
 
   private
 
-    attr_reader :selected_id, :diff_with_id
+    attr_reader :item_class, :item_id, :selected_id, :diff_with_id
 
-    def set_most_recent_and_revisions klass, id
-      @most_recent = klass.find(id)
+    def set_most_recent_and_revisions
+      @most_recent = item_class.find(item_id)
       @revisions = @most_recent.versions.not_creates
     rescue ActiveRecord::RecordNotFound
-      reify_and_set_most_recent_and_revisions klass, id
+      reify_and_set_most_recent_and_revisions
     end
 
     # To make deleted object comparable.
-    def reify_and_set_most_recent_and_revisions klass, id
-      versions = PaperTrail::Version.with_item_keys(klass.name, id)
+    def reify_and_set_most_recent_and_revisions
+      versions = PaperTrail::Version.with_item_keys(item_class.name, item_id)
 
       # Since we can only get here by following links leading to now deleted
       # taxa, the last version will be the most recent destroy event.
