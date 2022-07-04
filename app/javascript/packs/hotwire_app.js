7c0bc0bf0f20fcc1a50ad104b0bfd5576beaeed6
Fredrik Palmkron <jonkerz@gmail.com>
2022-07-04 18:32:50 +0200

    refactor: use hotwired/stimulus-webpack-helpers

--- app/javascript/packs/hotwire_app.js
+++ app/javascript/packs/hotwire_app.js
@@ -1,14 +1,10 @@
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 const application = Application.start()
 
 application.debug = false
 window.Stimulus = application
 
-/* eslint-disable */
-import ClipboardController from "../controllers/clipboard_controller"
-application.register("clipboard", ClipboardController)
-
-import HoverPreviewController from "../controllers/hover_preview_controller"
-application.register("hover-preview", HoverPreviewController)
-/* eslint-enable */
+const context = require.context("../controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context)) // eslint-disable-line no-undef
