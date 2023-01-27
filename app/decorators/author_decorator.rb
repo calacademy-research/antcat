948792fc4fd8ba48aaddfa21074c4b85fa3c7931
Fredrik Palmkron <jonkerz@gmail.com>
2023-01-27 19:46:35 +0100

    tweaks: prettier "between"s

--- app/decorators/author_decorator.rb
+++ app/decorators/author_decorator.rb
@@ -4,17 +4,14 @@ class AuthorDecorator < Draper::Decorator
   delegate :references, :described_taxa
 
   def published_between
-    first_year, most_recent_year = references.pluck(Arel.sql('MIN(references.year), MAX(references.year)')).flatten
-    return first_year if first_year == most_recent_year
+    min_year, max_year = references.pluck(Arel.sql('MIN(references.year), MAX(references.year)')).flatten
 
-    [first_year, h.ndash, most_recent_year].join
+    [min_year, max_year].uniq.join(h.ndash).presence
   end
 
   def taxon_descriptions_between
-    first_year, most_recent_year = described_taxa.pluck(Arel.sql('MIN(references.year), MAX(references.year)')).flatten
-    return unless first_year || most_recent_year
-    return first_year if first_year == most_recent_year
+    min_year, max_year = described_taxa.pluck(Arel.sql('MIN(references.year), MAX(references.year)')).flatten
 
-    [first_year, h.ndash, most_recent_year].join
+    [min_year, max_year].uniq.join(h.ndash).presence
   end
 end
--- app/decorators/journal_decorator.rb
+++ app/decorators/journal_decorator.rb
@@ -4,9 +4,8 @@ class JournalDecorator < Draper::Decorator
   delegate :references
 
   def publications_between
-    first_year, most_recent_year = references.pluck(Arel.sql('MIN(references.year), MAX(references.year)')).flatten
-    return first_year if first_year == most_recent_year
+    min_year, max_year = references.pluck(Arel.sql('MIN(references.year), MAX(references.year)')).flatten
 
-    [first_year, h.ndash, most_recent_year].join
+    [min_year, max_year].uniq.join(h.ndash).presence
   end
 end
