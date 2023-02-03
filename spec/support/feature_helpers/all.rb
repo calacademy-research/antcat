463a6e18e650e83127ee23b0eb71b7753352b172
Fredrik Palmkron <jonkerz@gmail.com>
2023-02-03 18:18:23 +0100

    tests: rename to `FeatureHelpers::All`

--- spec/support/feature_helpers/all.rb
+++ spec/support/feature_helpers/all.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-Dir[Rails.root.join("spec/support/feature_helpers/steps/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/feature_helpers/**/*.rb")].each { |f| require f }
 
 module FeatureHelpers
-  module StepHelpers
+  module All
     include FeatureHelpers::Paths
     include FeatureHelpers::Selectors
     include FeatureHelpers::Steps
