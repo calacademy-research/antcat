c8080de502270fc4dc3e535712a11e51ea587e4a
Fredrik Palmkron <jonkerz@gmail.com>
2023-02-25 20:27:29 +0100

    style(test): fix RuboCops

--- spec/features/taxon_operations/elevate_to_species_spec.rb
+++ spec/features/taxon_operations/elevate_to_species_spec.rb
@@ -3,14 +3,11 @@
 require 'rails_helper'
 
 feature "Elevating subspecies to species", as: :editor do
-  background do
-    genus = create(:genus, name_string: "Solenopsis")
-    species = create :species, name_string: "Solenopsis speccus", genus: genus
-    @taxon = create :subspecies, name_string: "Solenopsis speccus subbus", species: species, genus: species.genus
-  end
-
   scenario "Elevating subspecies to species (with feed)" do
-    visit catalog_path(@taxon)
+    species = create :species, name_string: "Solenopsis speccus", genus: create(:genus, name_string: "Solenopsis")
+    taxon = create :subspecies, name_string: "Solenopsis speccus subbus", species: species, genus: species.genus
+
+    visit catalog_path(taxon)
     i_will_confirm_on_the_next_step
     i_follow "Elevate to species"
     i_should_see "Subspecies was successfully elevated to a species."
@@ -22,9 +19,12 @@ feature "Elevating subspecies to species", as: :editor do
   end
 
   scenario "Elevating to species when the species name exists" do
+    species = create :species, name_string: "Solenopsis speccus", genus: create(:genus, name_string: "Solenopsis")
+    taxon = create :subspecies, name_string: "Solenopsis speccus subbus", species: species, genus: species.genus
+
     create :species, name_string: "Solenopsis subbus"
 
-    visit catalog_path(@taxon)
+    visit catalog_path(taxon)
     i_will_confirm_on_the_next_step
     i_follow "Elevate to species"
     i_should_see "This name is in use by another taxon"
