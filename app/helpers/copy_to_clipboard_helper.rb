965e8c0864a2788f3473cb58c97ba9083a9c104e
Fredrik Palmkron <jonkerz@gmail.com>
2023-03-21 17:43:27 +0100

    tweak(stimulus): use explicit actions

--- app/helpers/copy_to_clipboard_helper.rb
+++ app/helpers/copy_to_clipboard_helper.rb
@@ -3,7 +3,7 @@
 module CopyToClipboardHelper
   def copy_to_clipboard_button string_to_copy, label = nil
     tag.button label || string_to_copy,
-      'data-action': 'clipboard#copy',
+      'data-action': 'click->clipboard#copy',
       'data-clipboard-string-to-copy-value': string_to_copy,
       'data-controller': 'clipboard',
       class: "btn-neutral font-courier"
