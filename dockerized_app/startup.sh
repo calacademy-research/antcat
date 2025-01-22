4c2ed17fc1fdf2f77090f6a26774519cdef9183b
Joe Russack <jrussack@calacademy.org>
2025-01-22 11:27:22 -0800

    Added network to fix db issue, but css issue remains

--- dockerized_app/startup.sh
+++ dockerized_app/startup.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
+# Remove any pre-existing PID files
+rm -rf /app/app/tmp/pids/server.pid
 
-
-# Remove any pre-exisitng PID files
-rm -rf /app/tmp/pids/server.pid
-
+cd /app
 bundle install --path=vendor/bundle
 yarn install
 
 # Configure bundle to use a local path for gems
 bundle config path vendor/bundle
 
+cd /app/app
 bundle exec rake assets:precompile
 
 # Start the Solr service for Sunspot
@@ -17,3 +16,4 @@ bundle exec rails sunspot:solr:start
 
 # Start the Rails server, binding it to all network interfaces
 bundle exec rails s -p 8080 -b '0.0.0.0'
+
